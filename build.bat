a0b3e5b3196ba30520d209fd14a97ed722fea182
dsmall0 <dsmall0>
2004-08-25 00:40:26 +0000

    Set default target to orbeon-dist-packages

--- build.bat
+++ build.bat
@@ -22,10 +22,17 @@ if "%ERROR%" == "true" goto end
 rem ------------------------------------------------------
 rem Run Ant
 
+set target=%1
+set default_target=orbeon-dist-packages
+if not defined target (
+    set target=%default_target%
+    echo Defaulting to target %default_target%
+)
+
 for %%I in (.) do set BUILD_ROOT=%%~fI
 set CLASSPATH=.\lib\junit.jar
 set PATH=%JAVA_HOME%\bin;%PATH%
 set ANT_HOME=tools\ant
-"%ANT_HOME%\bin\ant.bat" "-Djava.home=%JAVA_HOME%" "-Dant.home=%ANT_HOME%" "-Dbuild.root=%BUILD_ROOT%" "-Ddebug.enabled=%DEBUG_ENABLED%" "-Dweblogic.home=%WEBLOGIC_HOME%" %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%ANT_HOME%\bin\ant.bat" "-Djava.home=%JAVA_HOME%" "-Dant.home=%ANT_HOME%" "-Dbuild.root=%BUILD_ROOT%" "-Ddebug.enabled=%DEBUG_ENABLED%" "-Dweblogic.home=%WEBLOGIC_HOME%" %target% %2 %3 %4 %5 %6 %7 %8 %9
 
 :end

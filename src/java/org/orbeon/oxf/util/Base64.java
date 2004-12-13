3f61a4e4f8364f5f754d180195d56b9be545358a
Erik Bruchez <ebruchez@orbeon.com>
2004-12-13 18:20:00 +0000

    Re-added missing Apache license.

--- src/java/org/orbeon/oxf/util/Base64.java
+++ src/java/org/orbeon/oxf/util/Base64.java
@@ -1,15 +1,20 @@
-/**
- *  Copyright (C) 2004 Orbeon, Inc.
+/*
+ * Copyright 1999,2004 The Apache Software Foundation.
  *
- *  This program is free software; you can redistribute it and/or modify it under the terms of the
- *  GNU Lesser General Public License as published by the Free Software Foundation; either version
- *  2.1 of the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU Lesser General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * With some modifications made for the Orbeon project.
  */
 package org.orbeon.oxf.util;
 
@@ -27,7 +32,6 @@ package org.orbeon.oxf.util;
  *
  * @author Jeffrey Rodriguez
  * @author Sandy Gao
- * @version $Id: Base64.java,v 1.2 2004/12/10 00:19:10 avernet Exp $
  */
 public class Base64 {
 

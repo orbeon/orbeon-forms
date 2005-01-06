38edb6c3fa6c1c4b5fd0256b76534ba65224e967
dsmall0 <dsmall0>
2005-01-06 01:55:49 +0000

    Fix xinclude cache support so that we can work on jboss.  Basically fix re-rooting and throw away ;) Erik's changes to xerces.

--- src/java/org/orbeon/oxf/xml/XIncludeHandler.java
+++ src/java/org/orbeon/oxf/xml/XIncludeHandler.java
@@ -1,5 +1,7 @@
 package org.orbeon.oxf.xml;
 
+import orbeon.apache.xerces.impl.Constants;
+import orbeon.apache.xerces.xni.parser.XMLParserConfiguration;
 import orbeon.apache.xerces.xni.XMLAttributes;
 import orbeon.apache.xerces.xni.XNIException;
 
@@ -19,6 +21,27 @@ public class XIncludeHandler extends orbeon.apache.xerces.xinclude.XIncludeHandl
     }
 
     protected boolean handleIncludeElement(XMLAttributes xmlAttributes) throws XNIException {
+        if ( fChildConfig == null ) {
+            fChildConfig = new XIncludeParserConfiguration();
+            // use the same error reporter, entity resolver, and security manager.
+            if (fErrorReporter != null) fChildConfig.setProperty(ERROR_REPORTER, fErrorReporter);
+            if (fEntityResolver != null) fChildConfig.setProperty(ENTITY_RESOLVER, fEntityResolver);
+            if (fSecurityManager != null) fChildConfig.setProperty(SECURITY_MANAGER, fSecurityManager);
+
+            // use the same namespace context
+            fChildConfig.setProperty(
+                Constants.XERCES_PROPERTY_PREFIX
+                    + Constants.NAMESPACE_CONTEXT_PROPERTY,
+                fNamespaceContext);
+
+            XIncludeHandler newHandler =
+                (XIncludeHandler)fChildConfig.getProperty(
+                    Constants.XERCES_PROPERTY_PREFIX
+                        + Constants.XINCLUDE_HANDLER_PROPERTY);
+            newHandler.setParent(this);
+            newHandler.setDocumentHandler(this.getDocumentHandler());
+
+        }
         XIncludeListener xIncludeListener = (XIncludeListener) threadLocal.get();
         if (xIncludeListener != null)
             xIncludeListener.inclusion(getBaseURI(0), xmlAttributes.getValue("href"));

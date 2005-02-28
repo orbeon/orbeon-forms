3b0e5056c6055aadde11ec40c0f9e92fe2c01bb0
Dan Small <dsmall@orbeon.com>
2005-02-28 00:14:03 +0000

    Move del of bogus cert from build.xml to update-unstable.sh

--- tools/update-unstable.sh
+++ tools/update-unstable.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-echo  y | sitecopy -u ops-unstable
+SITE=ops-unstable
+
+# 2/27/2005 d : If we don't delete this then sitecopy complains that because the
+# the certificate is for forge.objectweb.org but has been received when 
+# accessing ops.forge.objectweb.org.  i.e. It thinks someone's spoofing the 
+# site.
+BOGUSCERT=$HOME/.sitecopy/$SITE.crt
+if [ -e $BOGUSCERT ]
+then
+  rm $BOGUSCERT
+fi  
+
+echo  y | sitecopy -u studio-unstable

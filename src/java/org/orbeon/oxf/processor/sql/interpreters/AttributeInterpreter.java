4de2f5d45d6f25d99602213129244074213402ea
Erik Bruchez <ebruchez@orbeon.com>
2005-08-05 20:27:42 +0000

    Attribute name can be QName.

--- src/java/org/orbeon/oxf/processor/sql/interpreters/AttributeInterpreter.java
+++ src/java/org/orbeon/oxf/processor/sql/interpreters/AttributeInterpreter.java
@@ -18,8 +18,15 @@ import org.orbeon.oxf.processor.sql.SQLProcessorInterpreterContext;
 import org.orbeon.oxf.xml.ContentHandlerAdapter;
 import org.orbeon.oxf.xml.DeferredContentHandler;
 import org.orbeon.oxf.xml.DeferredContentHandlerImpl;
+import org.orbeon.oxf.xml.dom4j.LocationData;
+import org.orbeon.oxf.common.ValidationException;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.Locator;
+import org.dom4j.QName;
+import org.dom4j.Namespace;
+
+import java.util.Map;
 
 /**
  * Add an attribute to the last element output.
@@ -66,12 +73,31 @@ public class AttributeInterpreter extends SQLProcessor.InterpreterContentHandler
             contentString = content.toString().trim();
 
         // Output attribute
-        // TODO: handle namespaces
-        getInterpreterContext().getOutput().addAttribute("", attributeName, attributeName, contentString);
+        final QName attributeQName = getQName(getDocumentLocator(), attributeName, getInterpreterContext().getPrefixesMap());
+        getInterpreterContext().getOutput().startPrefixMapping(attributeQName.getName(), attributeQName.getNamespaceURI());// NOTE: we may have to check that the mapping does not exist yet
+        getInterpreterContext().getOutput().addAttribute(attributeQName.getNamespaceURI(), attributeQName.getName(), attributeName, contentString);
 
         // Clear state
         savedOutput = null;
         attributeName = null;
         content = null;
+    }
+
+    private static QName getQName(Locator locator, String qNameString, Map prefixesMap) {
+
+        final int colonIndex = qNameString.indexOf(':');
+        if (colonIndex == -1)
+            return new QName(qNameString);
+        if (colonIndex == 0)
+            throw new ValidationException("Invalid QName:" + qNameString, new LocationData(locator));
+
+        final String prefix = qNameString.substring(0, colonIndex);
+        final String localName = qNameString.substring(colonIndex + 1);
+
+        if (prefixesMap.get(prefix) == null) {
+            throw new ValidationException("Undeclared prefix for QName:" + qNameString, new LocationData(locator));
+        } else {
+            return new QName(localName, new Namespace(prefix, (String) prefixesMap.get(prefix)));
+        }
     }
 }

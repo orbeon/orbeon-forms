7239271ce09fbf7d1e197871eb72d62cca25ffd0
Erik Bruchez <ebruchez@orbeon.com>
2005-09-23 01:09:33 +0000

    Added Pipeline processor doc. Minor changes.

--- src/java/org/orbeon/oxf/xforms/XFormsControls.java
+++ src/java/org/orbeon/oxf/xforms/XFormsControls.java
@@ -24,6 +24,7 @@ import org.orbeon.oxf.xforms.event.*;
 import org.orbeon.oxf.xforms.event.events.XFormsLinkErrorEvent;
 import org.orbeon.oxf.xforms.event.events.XFormsSelectEvent;
 import org.orbeon.oxf.xforms.event.events.XFormsSubmitEvent;
+import org.orbeon.oxf.xforms.event.events.XFormsDeselectEvent;
 import org.orbeon.oxf.xml.dom4j.Dom4jUtils;
 import org.orbeon.oxf.xml.dom4j.LocationData;
 import org.orbeon.oxf.xml.dom4j.ExtendedLocationData;
@@ -516,7 +517,7 @@ public class XFormsControls {
             currentControlsState.getSwitchIdToSelectedCaseIdMap().put(switchControlInfo.getId(), selectedCaseId);
 
             // "1. Dispatching an xforms-deselect event to the currently selected case."
-            containingDocument.dispatchEvent(pipelineContext, new org.orbeon.oxf.xforms.event.events.XFormsDeselectEvent((XFormsEventTarget) currentControlsState.getIdsToControlInfo().get(currentSelectedCaseId)));
+            containingDocument.dispatchEvent(pipelineContext, new XFormsDeselectEvent((XFormsEventTarget) currentControlsState.getIdsToControlInfo().get(currentSelectedCaseId)));
 
             // "2. Dispatching an xform-select event to the case to be selected."
             containingDocument.dispatchEvent(pipelineContext, new XFormsSelectEvent((XFormsEventTarget) currentControlsState.getIdsToControlInfo().get(selectedCaseId)));

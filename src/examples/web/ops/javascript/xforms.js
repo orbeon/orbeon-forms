9e1b202a141207ea4f6d01f1beffd41f70f8464c
Alessandro Vernet <avernet@orbeon.com>
2006-06-19 18:30:55 +0000

    Fix [ #305531 ] Client crashes when JSON tree info is too large

--- src/examples/web/ops/javascript/xforms.js
+++ src/examples/web/ops/javascript/xforms.js
@@ -865,7 +865,7 @@ function xformsInitializeControlsUnder(root) {
                 // Save in the control if it allows multiple selection
                 control.xformsAllowMultipleSelection = YAHOO.util.Dom.hasClass(control, "xforms-select-tree");
                 // Parse data put by the server in the div
-                var treeArray = eval(control.firstChild.nodeValue);
+                var treeArray = xformsStringValue(control);
                 control.firstChild.nodeValue = "";
                 // Create, populate, and show the tree
                 var yahooTree = new YAHOO.widget.TreeView(control.id);

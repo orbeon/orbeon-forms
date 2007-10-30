99b93220937eec11bd24619bf5ab0f7ac8279234
Alessandro Vernet <avernet@orbeon.com>
2007-10-30 00:48:44 +0000

    Now always include animation.js

--- src/java/org/orbeon/oxf/xforms/processor/XFormsFeatures.java
+++ src/java/org/orbeon/oxf/xforms/processor/XFormsFeatures.java
@@ -142,12 +142,7 @@ public class XFormsFeatures {
             new ResourceConfig("/ops/javascript/yui/connection.js", "/ops/javascript/yui/connection-min.js"),
             new ResourceConfig("/ops/javascript/yui/container.js", "/ops/javascript/yui/container-min.js"),
             new ResourceConfig("/ops/javascript/yui/dragdrop.js", "/ops/javascript/yui/dragdrop-min.js"),
-            new ResourceConfig("/ops/javascript/yui/animation.js", "/ops/javascript/yui/animation-min.js") {
-                public boolean isInUse(Map appearancesMap) {
-                    return isRangeInUse(appearancesMap);
-                }
-                public String getFeatureName() { return "range"; }
-            },
+            new ResourceConfig("/ops/javascript/yui/animation.js", "/ops/javascript/yui/animation-min.js"),
             new ResourceConfig("/ops/javascript/yui/dragdrop.js", "/ops/javascript/yui/dragdrop-min.js") {
                 public boolean isInUse(Map appearancesMap) {
                     return isRangeInUse(appearancesMap);

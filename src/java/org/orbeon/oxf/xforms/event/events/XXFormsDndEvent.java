3b90b106db8896f629100eed4332dbc796d50ad1
Erik Bruchez <ebruchez@orbeon.com>
2008-04-17 01:46:30 +0000

    Missed checkin.

---
+++ src/java/org/orbeon/oxf/xforms/event/events/XXFormsDndEvent.java
@@ -0,0 +1,41 @@
+/**
+ *  Copyright (C) 2008 Orbeon, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms of the
+ *  GNU Lesser General Public License as published by the Free Software Foundation; either version
+ *  2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+ *
+ *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+package org.orbeon.oxf.xforms.event.events;
+
+import org.orbeon.oxf.xforms.event.XFormsEvent;
+import org.orbeon.oxf.xforms.event.XFormsEvents;
+import org.orbeon.oxf.xforms.event.XFormsEventTarget;
+
+/**
+ * Internal xxforms-dnd event.
+ */
+public class XXFormsDndEvent extends XFormsEvent {
+
+    private String dndStart;
+    private String dndEnd;
+
+    public XXFormsDndEvent(XFormsEventTarget targetObject, String dndStart, String dndEnd) {
+        super(XFormsEvents.XXFORMS_DND, targetObject, false, false);
+        this.dndStart = dndStart;
+        this.dndEnd = dndEnd;
+    }
+
+    public String getDndStart() {
+        return dndStart;
+    }
+
+    public String getDndEnd() {
+        return dndEnd;
+    }
+}

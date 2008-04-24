2f98663a421e2c3a5a5d5fb457b385b6c815c000
Erik Bruchez <ebruchez@orbeon.com>
2008-04-24 23:29:51 +0000

    Fixed "[ #309265 ] Bug with preceding-sibling after insertion".

--- src/java/org/orbeon/oxf/xforms/Variable.java
+++ src/java/org/orbeon/oxf/xforms/Variable.java
@@ -1,12 +1,20 @@
 package org.orbeon.oxf.xforms;
 
 import org.dom4j.Element;
-import org.orbeon.saxon.om.ValueRepresentation;
-import org.orbeon.saxon.value.EmptySequence;
+import org.orbeon.oxf.common.OXFException;
+import org.orbeon.oxf.common.ValidationException;
 import org.orbeon.oxf.pipeline.api.PipelineContext;
 import org.orbeon.oxf.util.XPathCache;
 import org.orbeon.oxf.xml.dom4j.LocationData;
-import org.orbeon.oxf.common.ValidationException;
+import org.orbeon.saxon.dom4j.DocumentWrapper;
+import org.orbeon.saxon.dom4j.NodeWrapper;
+import org.orbeon.saxon.expr.LastPositionFinder;
+import org.orbeon.saxon.om.Item;
+import org.orbeon.saxon.om.SequenceIterator;
+import org.orbeon.saxon.om.ValueRepresentation;
+import org.orbeon.saxon.trans.XPathException;
+import org.orbeon.saxon.value.EmptySequence;
+import org.orbeon.saxon.value.SequenceExtent;
 
 import java.util.List;
 
@@ -60,14 +68,83 @@ public class Variable {
     }
 
     public ValueRepresentation getVariableValue(PipelineContext pipelineContext, boolean useCache) {
+        // Make sure the variable is evaluated
         if (!evaluated) {
             evaluated = true;
             evaluate(pipelineContext, useCache);
         }
-        return variableValue;
+
+        // Return value and rewrap if necessary
+        if (variableValue instanceof SequenceExtent) {
+            // Rewrap NodeWrapper contained in the variable value. Not the most efficient, but at this point we have to
+            // to ensure that things work properly. See RewrappingSequenceIterator for more details.
+            try {
+                return new SequenceExtent(new RewrappingSequenceIterator(((SequenceExtent) variableValue).iterate(null)));
+            } catch (XPathException e) {
+                // Should not happen with SequenceExtent
+                throw new OXFException(e);
+            }
+        } else {
+            // Return value as is
+            return variableValue;
+        }
     }
 
     public LocationData getLocationData() {
         return (element != null) ? (LocationData) element.getData() : null;
+    }
+
+    /**
+     * This iterator rewrapps NodeWrapper elements so that the original NodeWrapper is discarded and a new one created.
+     * The reason we do this is that when we keep variables around, we don't want NodeWrapper.index to be set to
+     * anything but -1. If we did that, then upon insertions of nodes in the DOM, the index would be out of date.
+     */
+    private static class RewrappingSequenceIterator implements SequenceIterator, LastPositionFinder {
+
+        private SequenceIterator iter;
+        private Item current;
+
+        public RewrappingSequenceIterator(SequenceIterator iter) {
+            this.iter = iter;
+        }
+
+        public Item next() throws XPathException {
+            final Item item = iter.next();
+
+            if (item instanceof NodeWrapper) {
+                // Rewrap
+                final NodeWrapper nodeWrapper = (NodeWrapper) item;
+                final DocumentWrapper documentWrapper = (DocumentWrapper) nodeWrapper.getDocumentRoot();
+
+                current = documentWrapper.wrap(nodeWrapper.getUnderlyingNode());
+            } else {
+                // Pass through
+                current = item;
+            }
+
+            return current;
+        }
+
+        public Item current() {
+            return current;
+        }
+
+        public int position() {
+            return iter.position();
+        }
+
+        public SequenceIterator getAnother() throws XPathException {
+            return new RewrappingSequenceIterator(iter.getAnother());
+        }
+
+        public int getProperties() {
+            return iter.getProperties();
+        }
+
+        public int getLastPosition() throws XPathException {
+            if (iter instanceof LastPositionFinder)
+                return ((LastPositionFinder) iter).getLastPosition();
+            throw new OXFException("Call to getLastPosition() and nested iterator is not a LastPositionFinder.");
+        }
     }
 }

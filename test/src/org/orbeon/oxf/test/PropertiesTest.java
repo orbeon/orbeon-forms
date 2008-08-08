66dfc84f29ebf7e010ed8f942c31e32c14095027
Erik Bruchez <ebruchez@orbeon.com>
2008-08-08 00:46:48 +0000

    Added unit test for wildcard properties.

---
+++ test/src/org/orbeon/oxf/test/PropertiesTest.java
@@ -0,0 +1,54 @@
+/**
+ *  Copyright (C) 2008 Orbeon, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms of the
+ *  GNU Lesser General Public License as published by the Free Software Foundation; either version
+ *  2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+ *
+ *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+package org.orbeon.oxf.test;
+
+import junit.framework.TestCase;
+import org.orbeon.oxf.properties.PropertyStore;
+import org.orbeon.oxf.properties.PropertySet;
+import org.orbeon.oxf.xml.dom4j.Dom4jUtils;
+
+
+public class PropertiesTest extends TestCase {
+
+    private PropertyStore propertyStore;
+
+    private static final String PROPERTIES
+            = "<properties xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">\n" +
+            "    <property as=\"xs:string\"  name=\"test.orbeon.builder.form\"  value=\"value0\"/>\n" +
+            "    <property as=\"xs:string\"  name=\"test.*.builder.form\"  value=\"value1\"/>\n" +
+            "    <property as=\"xs:string\"  name=\"test.orbeon.*.form\"  value=\"value2\"/>\n" +
+            "    <property as=\"xs:string\"  name=\"test.*.*.form\"  value=\"value3\"/>\n" +
+            "    <property as=\"xs:string\"  name=\"test.orbeon.builder.*\"  value=\"value4\"/>\n" +
+            "    <property as=\"xs:string\"  name=\"test.*.builder.*\"  value=\"value5\"/>\n" +
+            "    <property as=\"xs:string\"  name=\"test.orbeon.*.*\"  value=\"value6\"/>\n" +
+            "    <property as=\"xs:string\"  name=\"test.*.*.*\"  value=\"value7\"/>\n" +
+            "</properties>";
+
+    protected void setUp() throws Exception {
+        propertyStore = new PropertyStore(Dom4jUtils.readDom4j(PROPERTIES, false, false));
+    }
+
+    public void testWildcardMatches() {
+        final PropertySet propertySet = propertyStore.getGlobalPropertySet();
+
+        assertEquals(propertySet.getString("test.orbeon.builder.form"), "value0");
+        assertEquals(propertySet.getString("test.foo.builder.form"), "value1");
+        assertEquals(propertySet.getString("test.orbeon.foo.form"), "value2");
+        assertEquals(propertySet.getString("test.foo.bar.form"), "value3");
+        assertEquals(propertySet.getString("test.orbeon.builder.foo"), "value4");
+        assertEquals(propertySet.getString("test.foo.builder.bar"), "value5");
+        assertEquals(propertySet.getString("test.orbeon.foo.bar"), "value6");
+        assertEquals(propertySet.getString("test.foo.bar.bat"), "value7");
+    }
+}

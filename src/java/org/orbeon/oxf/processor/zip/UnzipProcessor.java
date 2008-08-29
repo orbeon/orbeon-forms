d549731c8174e7c0d6d372918b86b9ec468f5b15
Alessandro Vernet <avernet@orbeon.com>
2008-08-29 21:57:01 +0000

    Implemented [ #310974 ] RFE: Provide new zip and unzip processors

---
+++ src/java/org/orbeon/oxf/processor/zip/UnzipProcessor.java
@@ -0,0 +1,84 @@
+package org.orbeon.oxf.processor.zip;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.orbeon.oxf.common.OXFException;
+import org.orbeon.oxf.pipeline.api.PipelineContext;
+import org.orbeon.oxf.processor.ProcessorImpl;
+import org.orbeon.oxf.processor.ProcessorInputOutputInfo;
+import org.orbeon.oxf.processor.ProcessorOutput;
+import org.orbeon.oxf.processor.serializer.BinaryTextContentHandler;
+import org.orbeon.oxf.util.NetUtils;
+import org.orbeon.oxf.xml.XMLUtils;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class UnzipProcessor extends ProcessorImpl {
+
+    public UnzipProcessor() {
+        addInputInfo(new ProcessorInputOutputInfo(INPUT_DATA));
+        addOutputInfo(new ProcessorInputOutputInfo(OUTPUT_DATA));
+    }
+
+    public ProcessorOutput createOutput(String name) {
+        ProcessorOutput output = new ProcessorImpl.ProcessorOutputImpl(getClass(), name) {
+
+            public void readImpl(PipelineContext context, ContentHandler contentHandler) {
+                try {
+                    // Read input in a temporary file
+                    final File temporaryZipFile;
+                    {
+                        final FileItem fileItem = NetUtils.prepareFileItem(context, NetUtils.REQUEST_SCOPE);
+                        final OutputStream fileOutputStream = fileItem.getOutputStream();
+                        readInputAsSAX(context, getInputByName(INPUT_DATA), new BinaryTextContentHandler(null, fileOutputStream, true, false, null, false, false, null, false));
+                        temporaryZipFile = ((DiskFileItem) fileItem).getStoreLocation();
+                    }
+
+                    contentHandler.startDocument();
+                    // <files>
+                    contentHandler.startElement("", "files", "files", XMLUtils.EMPTY_ATTRIBUTES);
+                    ZipFile zipFile = new ZipFile(temporaryZipFile);
+                    for (Enumeration entries = zipFile.entries(); entries.hasMoreElements();) {
+                        // Go through each entry in the zip file
+                        ZipEntry zipEntry = (ZipEntry) entries.nextElement();
+                        // Get file name
+                        String fileName = zipEntry.getName();
+
+                        InputStream entryInputStream = zipFile.getInputStream(zipEntry);
+                        String uri = NetUtils.inputStreamToAnyURI(context, entryInputStream, NetUtils.REQUEST_SCOPE);
+                        // <file name="filename.ext">uri</file>
+                        AttributesImpl fileAttributes = new AttributesImpl();
+                        fileAttributes.addAttribute("", "name", "name", "CDATA", fileName);
+                        contentHandler.startElement("", "file", "file", fileAttributes);
+                        contentHandler.characters(uri.toCharArray(), 0, uri.length());
+                        // </file>
+                        contentHandler.endElement("", "file", "file");
+                    }
+                    // </files>
+                    contentHandler.endElement("", "files", "files");
+                    contentHandler.endDocument();
+
+                } catch (IOException e) {
+                    throw new OXFException(e);
+                } catch (SAXException e) {
+                    throw new OXFException(e);
+                }
+            }
+
+            // We don't do any caching here since the file we produce are temporary. So we don't want a processor
+            // downstream to keep a reference to a document that contains temporary URI that have since been deleted.
+
+        };
+        addOutput(name, output);
+        return output;
+    }
+}
---
+++ src/java/org/orbeon/oxf/processor/zip/ZipProcessor.java
@@ -0,0 +1,111 @@
+package org.orbeon.oxf.processor.zip;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.orbeon.oxf.common.OXFException;
+import org.orbeon.oxf.pipeline.api.PipelineContext;
+import org.orbeon.oxf.processor.ProcessorImpl;
+import org.orbeon.oxf.processor.ProcessorInputOutputInfo;
+import org.orbeon.oxf.processor.ProcessorOutput;
+import org.orbeon.oxf.processor.ProcessorUtils;
+import org.orbeon.oxf.util.NetUtils;
+import org.orbeon.oxf.xml.ContentHandlerAdapter;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipProcessor extends ProcessorImpl {
+
+    public ZipProcessor() {
+        addInputInfo(new ProcessorInputOutputInfo(INPUT_DATA));
+        addOutputInfo(new ProcessorInputOutputInfo(OUTPUT_DATA));
+    }
+
+    public ProcessorOutput createOutput(String name) {
+        ProcessorOutput output = new ProcessorImpl.ProcessorOutputImpl(getClass(), name) {
+
+            public void readImpl(PipelineContext context, ContentHandler contentHandler) {
+                try {
+                    // Create temporary zip file
+                    final FileItem fileItem = NetUtils.prepareFileItem(context, NetUtils.REQUEST_SCOPE);
+                    fileItem.getOutputStream().close();
+                    final File temporaryZipFile = ((DiskFileItem) fileItem).getStoreLocation();
+                    temporaryZipFile.createNewFile();
+                    final ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(temporaryZipFile));
+
+                    try {
+                        // Read list of files and write to zip output stream as we go
+                        readInputAsSAX(context, INPUT_DATA, new ContentHandlerAdapter() {
+
+                            String name;
+                            StringBuffer uri;
+
+                            // Get the file name, store it
+                            public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException {
+                                if ("file".equals(localName)) {
+                                    name = atts.getValue("name");
+                                    uri = new StringBuffer();
+                                }
+                            }
+
+                            // Get the URI to the file, store it
+                            public void characters(char ch[], int start, int length) throws SAXException {
+                                uri.append(ch, start, length);
+                            }
+
+                            // Process file
+                            public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
+                                try {
+                                    if ("file".equals(localName)) {
+                                        zipOutputStream.putNextEntry(new ZipEntry(name));
+                                        InputStream fileInputStream = new FileInputStream(new File(new URI(uri.toString())));
+                                        try {
+                                            NetUtils.copyStream(fileInputStream, zipOutputStream);
+                                        } finally {
+                                            fileInputStream.close();
+                                        }
+                                    }
+                                } catch (IOException e) {
+                                    throw new OXFException(e);
+                                } catch (URISyntaxException e) {
+                                    throw new OXFException(e);
+                                }
+                            }
+                        });
+                    } finally {
+                        zipOutputStream.close();
+                    }
+
+                    // Generate an Orbeon binary document with the content of the zip file
+                    FileInputStream zipInputStream = new FileInputStream(temporaryZipFile);
+                    try {
+                        ProcessorUtils.readBinary(zipInputStream, contentHandler, "multipart/x-gzip", null);
+                    } finally {
+                        zipInputStream.close();
+                    }
+                } catch (FileNotFoundException e) {
+                    throw new OXFException(e);
+                } catch (IOException e) {
+                    throw new OXFException(e);
+                }
+            }
+
+            // We could assume that we cache if the input document hasn't changed. But this would be unsafe as some of
+            // the files referenced from the input document could have changed. So to be on the safe side, and also
+            // because the cases where caching could happen are rather rare, we just don't cache.
+        };
+        addOutput(name, output);
+        return output;
+    }
+}

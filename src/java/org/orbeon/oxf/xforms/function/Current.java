31c976b5d4a2b2a15989714719406b1af38306ee
Erik Bruchez <ebruchez@orbeon.com>
2008-09-22 23:39:45 +0000

    Placeholder.

---
+++ src/java/org/orbeon/oxf/xforms/function/Current.java
@@ -0,0 +1,17 @@
+package org.orbeon.oxf.xforms.function;
+
+/**
+ * Copyright (C) 2007 Orbeon, Inc.
+ * <p/>
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ * <p/>
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+public class Current {
+}

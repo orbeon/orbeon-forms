e0d68452b91af607998becc80e4c10ca97fe387a
Erik Bruchez <ebruchez@orbeon.com>
2009-05-09 01:31:33 +0000

    XFormsResourceServer proxy tries to produce last modification date for oxf: and file: protocols.

--- src/java/org/orbeon/oxf/processor/serializer/FileSerializer.java
+++ src/java/org/orbeon/oxf/processor/serializer/FileSerializer.java
@@ -316,7 +316,7 @@ public class FileSerializer extends ProcessorImpl {
                     {
                         final String localURL = ((DiskFileItem) fileItem).getStoreLocation().toURI().toString();
                         if ("session".equals(config.getScope()) && config.isProxyResult())
-                            resultURL = NetUtils.proxyURI(pipelineContext, localURL, null, config.getRequestedContentType(), 0);
+                            resultURL = NetUtils.proxyURI(pipelineContext, localURL, null, config.getRequestedContentType(), -1);
                         else
                             resultURL = localURL;
                     }

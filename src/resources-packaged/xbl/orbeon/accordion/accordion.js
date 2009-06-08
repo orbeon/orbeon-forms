49ace28b05090117ae093499654bec3854446e89
Alessandro Vernet <avernet@orbeon.com>
2009-06-08 06:25:59 +0000

    Fix issue of JS error when datatable is used in accordion menu

--- src/resources-packaged/xbl/orbeon/accordion/accordion.js
+++ src/resources-packaged/xbl/orbeon/accordion/accordion.js
@@ -7,11 +7,9 @@ YAHOO.xbl.fr.Accordion = {
         if (! YAHOO.xbl.fr.Accordion.instances[container.id]) {
             var dlElement = YAHOO.util.Dom.getElementsByClassName("xbl-fr-accordion-dl", null, container)[0];
             var firstDtElement = YAHOO.util.Dom.getFirstChild(dlElement);
-            YAHOO.util.Dom.generateId(firstDtElement);
-            console.log("firstDtElement.id", firstDtElement.id);
             new AccordionMenu.setting(dlElement, {
                 dependent: false,
-                openedIds: [ firstDtElement ],
+                openedIds: [ ],
                 seconds: 0.1,
                 easeOut: false,
                 animation:true

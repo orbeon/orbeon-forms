16932e54cf33ba127e346624bed1bcd49db54fbb
Alessandro Vernet <avernet@orbeon.com>
2009-06-17 07:47:36 +0000

    Implement [ #313907 ] RFE: Support for inline date picker

---
+++ src/resources-packaged/oxf/xslt/utils/xbl.xsl
@@ -0,0 +1,64 @@
+<!--
+    Copyright (C) 2009 Orbeon, Inc.
+
+    This program is free software; you can redistribute it and/or modify it under the terms of the
+    GNU Lesser General Public License as published by the Free Software Foundation; either version
+    2.1 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Lesser General Public License for more details.
+
+    The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+-->
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+        xmlns:xs="http://www.w3.org/2001/XMLSchema"
+        xmlns:xforms="http://www.w3.org/2002/xforms"
+        xmlns:xxforms="http://orbeon.org/oxf/xml/xforms"
+        xmlns:ev="http://www.w3.org/2001/xml-events"
+        xmlns:xbl="http://www.w3.org/ns/xbl"
+        xmlns:xxbl="http://orbeon.org/oxf/xml/xbl">
+
+    <xsl:function name="xxbl:parameter">
+        <xsl:param name="context" as="element()"/>
+        <xsl:param name="prefix" as="xs:string"/>
+        <xsl:param name="namespace" as="xs:string"/>
+        <xsl:param name="component" as="xs:string"/>
+        <xsl:param name="property" as="xs:string"/>
+
+        <xsl:choose>
+            <xsl:when test="exists($context/*[local-name() = $property and namespace-uri() = $namespace])">
+                <xforms:input class="xbl-{$prefix}-{$component}-{$property}" xxbl:attr="{$prefix}:{$property}/@*" style="display: none">
+                    <xxforms:script ev:event="xforms-value-changed">
+                        <xsl:text>YAHOO.xbl.</xsl:text>
+                        <xsl:value-of select="$prefix"/>
+                        <xsl:text>.</xsl:text>
+                        <xsl:value-of select="xxbl:to-camel-case($component)"/>
+                        <xsl:text>.property</xsl:text>
+                        <xsl:value-of select="xxbl:to-camel-case($property)"/>
+                        <xsl:text>Changed(this);</xsl:text>
+                    </xxforms:script>
+                </xforms:input>
+            </xsl:when>
+            <xsl:otherwise>
+                <xxforms:variable name="{$property}" xbl:attr="xbl:text={$property}"/>
+                <xforms:output class="xbl-{$prefix}-{$component}-{$property}" style="display: none"
+                    value="if (${$property}) then ${$property} else xxforms:property('oxf.xforms.xbl.{$prefix}.{$component}.{$property}')"/>
+            </xsl:otherwise>
+        </xsl:choose>
+
+    </xsl:function>
+
+    <!-- Converts a name such as my-grand-name into MyGrandName -->
+    <xsl:function name="xxbl:to-camel-case" as="xs:string">
+        <xsl:param name="dash-separated-name" as="xs:string"/>
+
+        <xsl:variable name="result">
+            <xsl:for-each select="tokenize($dash-separated-name, '-')">
+                <xsl:value-of select="concat(upper-case(substring(., 1, 1)), substring(., 2))"/>
+            </xsl:for-each>
+        </xsl:variable>
+        <xsl:value-of select="string-join($result, '')"/>
+    </xsl:function>
+
+</xsl:stylesheet>
---
+++ src/resources-packaged/xbl/orbeon/date/date.js
@@ -0,0 +1,114 @@
+YAHOO.namespace("xbl.fr");
+YAHOO.xbl.fr.Date = {
+    instances: {},
+
+    init: function(target) {
+        var container = YAHOO.util.Dom.getAncestorByClassName(target, "xbl-fr-date");
+        if (! YAHOO.xbl.fr.Date.instances[container.id]) {
+
+            // Get information from the DOM
+            var calendarDivElement = YAHOO.util.Dom.getElementsByClassName("xbl-fr-date-calendar-div", null, container)[0];
+            var inputElement = YAHOO.util.Dom.getElementsByClassName("xbl-fr-date-input", null, container)[0];
+            var mindateElement = YAHOO.util.Dom.getElementsByClassName("xbl-fr-date-mindate", null, container)[0];
+            var maxdateElement = YAHOO.util.Dom.getElementsByClassName("xbl-fr-date-maxdate", null, container)[0];
+
+            // Create instance
+            var instance = {
+                valueChanged: function() {
+                    var date = ORBEON.util.DateTime.magicDateToJSDate(ORBEON.xforms.Document.getValue(inputElement.id));
+                    if (date == null) {
+                        yuiCalendar.cfg.setProperty("selected", "", false);
+                        yuiCalendar.cfg.setProperty("pagedate", new Date(), false);
+                    } else {
+                        // Date must be the internal format expected by YUI
+                        var dateStringForYUI = (date.getMonth() + 1)
+                           + "/" + date.getDate()
+                           + "/" + date.getFullYear();
+                        yuiCalendar.cfg.setProperty("selected", dateStringForYUI, false);
+                        yuiCalendar.cfg.setProperty("pagedate", date, false);
+                    }
+                    yuiCalendar.render();
+                },
+                propertyMindateChanged: function() {
+                    yuiCalendar.cfg.setProperty("mindate", mindateElement == null ? null :
+                        ORBEON.util.DateTime.magicDateToJSDate(ORBEON.xforms.Document.getValue(mindateElement.id)));
+                    yuiCalendar.render();
+                },
+                propertyMaxdateChanged: function() {
+                    yuiCalendar.cfg.setProperty("maxdate", maxdateElement == null ? null :
+                        ORBEON.util.DateTime.magicDateToJSDate(ORBEON.xforms.Document.getValue(maxdateElement.id)));
+                    yuiCalendar.render();
+                },
+                setLanguage: function() {
+                    var RESOURCES = {
+                        "en": {
+                            "MONTHS_LONG": [ "January", "February", "March", "April", "May", "June", "July",
+                                             "August",  "September",  "October",  "November",  "December" ],
+                            "WEEKDAYS_SHORT": ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+                            "START_WEEKDAY": 0
+                        },
+                        "fr": {
+                            "MONTHS_LONG": [ "Janvier", "F\xe9vrier", "Mars", "Avril", "Mai", "Juin", "Juillet",
+                                             "Ao\xfbt",  "Septembre",  "Octobre",  "Novembre",  "D\xe9cembre" ],
+                            "WEEKDAYS_SHORT": ["Di", "Lu", "Ma", "Me", "Je", "Ve", "Sa"],
+                            "START_WEEKDAY": 1
+                        },
+                        "es": {
+                            "MONTHS_LONG": [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+                                             "Agosto",  "Septiembre",  "Octubre",  "Noviembre",  "Diciembre" ],
+                            "WEEKDAYS_SHORT": ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "S\xe1"],
+                            "START_WEEKDAY": 1
+                        }
+                    };
+
+                    // Get language from html/@lang
+                    var lang = ORBEON.util.Dom.getAttribute(document.documentElement, "lang");
+                    // If not language is set there, use English
+                    if (lang == null)
+                        lang = "en";
+                    // Just keep first 2 letters (fr_FR becomes fr)
+                    lang = lang.substring(0, 2);
+                    // Find resource for selected language
+                    var resources = RESOURCES[lang];
+                    for (var key in resources)
+                        yuiCalendar.cfg.setProperty(key, resources[key]);
+                },
+                dateSelected: function() {
+                    var jsDate = yuiCalendar.getSelectedDates()[0];
+                    var formattedDate = ORBEON.util.DateTime.jsDateToISODate(jsDate);
+                    ORBEON.xforms.Document.setValue(inputElement.id, formattedDate);
+                }
+            };
+
+            // Create YUI calendar
+            var yuiCalendar = ORBEON.util.Utils.getProperty(DATE_PICKER_NAVIGATOR_PROPERTY)
+                ? new YAHOO.widget.CalendarGroup(calendarDivElement)
+                : new YAHOO.widget.Calendar(calendarDivElement);
+            yuiCalendar.selectEvent.subscribe(instance.dateSelected, instance, true);
+            instance.setLanguage();
+            instance.valueChanged();
+            instance.propertyMindateChanged();
+            instance.propertyMaxdateChanged();
+            yuiCalendar.render();
+
+            YAHOO.xbl.fr.Date.instances[container.id] = instance;
+        }
+    },
+    valueChanged: function(target) {
+        var container = YAHOO.util.Dom.getAncestorByClassName(target, "xbl-fr-date");
+        var instance = YAHOO.xbl.fr.Date.instances[container.id];
+        instance.valueChanged();
+    },
+    propertyMindateChanged: function(target) {
+        var container = YAHOO.util.Dom.getAncestorByClassName(target, "xbl-fr-date");
+        var instance = YAHOO.xbl.fr.Date.instances[container.id];
+        if (! YAHOO.lang.isUndefined(instance))
+            instance.propertyMindateChanged();
+    },
+    propertyMaxdateChanged: function(target) {
+        var container = YAHOO.util.Dom.getAncestorByClassName(target, "xbl-fr-date");
+        var instance = YAHOO.xbl.fr.Date.instances[container.id];
+        if (! YAHOO.lang.isUndefined(instance))
+            instance.propertyMaxdateChanged();
+    }
+};

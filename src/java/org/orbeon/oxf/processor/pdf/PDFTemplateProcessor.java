259d07562341d59d7c1ea35a26bbc60ed3a1be70
ebruchez <ebruchez@orbeon.com>
2009-07-28 16:19:52 -0700

    Refactoring.

--- src/java/org/orbeon/oxf/processor/pdf/PDFTemplateProcessor.java
+++ src/java/org/orbeon/oxf/processor/pdf/PDFTemplateProcessor.java
@@ -1,15 +1,15 @@
 /**
- *  Copyright (C) 2004 Orbeon, Inc.
+ * Copyright (C) 2009 Orbeon, Inc.
  *
- *  This program is free software; you can redistribute it and/or modify it under the terms of the
- *  GNU Lesser General Public License as published by the Free Software Foundation; either version
- *  2.1 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
  */
 package org.orbeon.oxf.processor.pdf;
 
@@ -20,11 +20,11 @@ import com.lowagie.text.pdf.*;
 import org.dom4j.Element;
 import org.orbeon.oxf.common.OXFException;
 import org.orbeon.oxf.pipeline.api.PipelineContext;
+import org.orbeon.oxf.processor.ProcessorImpl;
 import org.orbeon.oxf.processor.ProcessorInput;
 import org.orbeon.oxf.processor.ProcessorInputOutputInfo;
-import org.orbeon.oxf.processor.ProcessorImpl;
-import org.orbeon.oxf.processor.serializer.legacy.HttpBinarySerializer;
 import org.orbeon.oxf.processor.serializer.BinaryTextContentHandler;
+import org.orbeon.oxf.processor.serializer.legacy.HttpBinarySerializer;
 import org.orbeon.oxf.resources.URLFactory;
 import org.orbeon.oxf.util.XPathCache;
 import org.orbeon.oxf.xml.dom4j.Dom4jUtils;
@@ -33,13 +33,20 @@ import org.orbeon.saxon.Configuration;
 import org.orbeon.saxon.dom4j.DocumentWrapper;
 import org.orbeon.saxon.functions.FunctionLibrary;
 import org.orbeon.saxon.om.DocumentInfo;
+import org.orbeon.saxon.om.Item;
 import org.orbeon.saxon.om.NodeInfo;
+import org.orbeon.saxon.om.ValueRepresentation;
+import org.orbeon.saxon.value.FloatValue;
+import org.orbeon.saxon.value.IntegerValue;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.ByteArrayOutputStream;
-import java.util.*;
 import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * The PDF Template processor reads a PDF template and performs textual annotations on it.
@@ -96,22 +103,22 @@ public class PDFTemplateProcessor extends HttpBinarySerializer {// TODO: HttpBin
             final int pageCount = reader.getNumberOfPages();
 
             // Get size of first page
-            final Rectangle psize = reader.getPageSize(1);
-            final float width = psize.width();
-            final float height = psize.height();
+            final Rectangle pageSize = reader.getPageSize(1);
+            final float width = pageSize.getWidth();
+            final float height = pageSize.getHeight();
 
             final String showGrid = XPathCache.evaluateAsString(pipelineContext, configDocumentInfo, "/*/template/@show-grid", null, null, functionLibrary, null, null, null);//TODO: LocationData
 
-            final PdfStamper stamper = new PdfStamper(reader,outputStream);
+            final PdfStamper stamper = new PdfStamper(reader, outputStream);
             stamper.setFormFlattening(true);
 
             for (int currentPage = 1; currentPage <= pageCount; currentPage++) {
             	final PdfContentByte contentByte = stamper.getOverContent(currentPage);
                 // Handle root group
                 final GroupContext initialGroupContext = new GroupContext(contentByte,stamper.getAcroFields(), height, currentPage,
-                		Collections.singletonList(instanceDocumentInfo), 1,
+                		Collections.singletonList((Item) instanceDocumentInfo), 1,
                         0, 0, "Courier", 14, 15.9f);
-                handleGroup(pipelineContext, initialGroupContext, configDocument.getRootElement().elements(), functionLibrary, reader);
+                handleGroup(pipelineContext, initialGroupContext, Dom4jUtils.elements(configDocument.getRootElement()), functionLibrary, reader);
 
                 // Handle preview grid (NOTE: This can be heavy in memory.)
                 if ("true".equalsIgnoreCase(showGrid)) {
@@ -173,15 +180,15 @@ public class PDFTemplateProcessor extends HttpBinarySerializer {// TODO: HttpBin
      * @param pipelineContext    current pipeline context
      * @param contextNode        context node for evaluation
      * @param variableToValueMap variables
-     * @param functionLibrary    XPath function libary to use
+     * @param functionLibrary    XPath function library to use
      * @param functionContext    context object to pass to the XForms function
      * @param element            element on which the AVT attribute is present
      * @param attributeValue     attribute value
      * @return                   resolved attribute value
      */
-    private static String resolveAttributeValueTemplates(PipelineContext pipelineContext, NodeInfo contextNode, Map variableToValueMap,
-                                                        FunctionLibrary functionLibrary, XPathCache.FunctionContext functionContext,
-                                                        Element element, String attributeValue) {
+    private static String resolveAttributeValueTemplates(PipelineContext pipelineContext, NodeInfo contextNode, Map<String, ValueRepresentation> variableToValueMap,
+                                                         FunctionLibrary functionLibrary, XPathCache.FunctionContext functionContext,
+                                                         Element element, String attributeValue) {
 
         if (attributeValue == null)
             return null;
@@ -197,7 +204,7 @@ public class PDFTemplateProcessor extends HttpBinarySerializer {// TODO: HttpBin
         public float pageHeight;
         public int pageNumber;
 
-        public List contextNodeSet;
+        public List<Item> contextNodeSet;
         public int contextPosition;
 
         public float offsetX;
@@ -207,8 +214,8 @@ public class PDFTemplateProcessor extends HttpBinarySerializer {// TODO: HttpBin
         public float fontSize;
         public float fontPitch;
 
-        public GroupContext(PdfContentByte contentByte,AcroFields acroFields, float pageHeight, int pageNumber, List contextNodeSet, int contextPosition,
-                            float offsetX, float offsetY, String fontFamily, float fontSize, float fontPitch) {
+        public GroupContext(PdfContentByte contentByte,AcroFields acroFields, float pageHeight, int pageNumber, List<Item> contextNodeSet,
+                            int contextPosition, float offsetX, float offsetY, String fontFamily, float fontSize, float fontPitch) {
             this.contentByte = contentByte;
             this.pageHeight = pageHeight;
             this.pageNumber = pageNumber;
@@ -237,26 +244,25 @@ public class PDFTemplateProcessor extends HttpBinarySerializer {// TODO: HttpBin
         }
     }
 
-    private void handleGroup(PipelineContext pipelineContext, GroupContext groupContext, List statements, FunctionLibrary functionLibrary, PdfReader reader) throws DocumentException, IOException {
+    private void handleGroup(PipelineContext pipelineContext, GroupContext groupContext, List<Element> statements, FunctionLibrary functionLibrary, PdfReader reader) throws DocumentException, IOException {
 
         final NodeInfo contextNode = (NodeInfo) groupContext.contextNodeSet.get(groupContext.contextPosition - 1);
-        final Map variableToValueMap = new HashMap();
+        final Map<String, ValueRepresentation> variableToValueMap = new HashMap<String, ValueRepresentation>();
 
 
-        variableToValueMap.put("page-count", new Integer(reader.getNumberOfPages()));
-        variableToValueMap.put("page-number", new Integer(groupContext.pageNumber));
-        variableToValueMap.put("page-height", new Float(groupContext.pageHeight));
+        variableToValueMap.put("page-count", new IntegerValue(reader.getNumberOfPages()));
+        variableToValueMap.put("page-number", new IntegerValue(groupContext.pageNumber));
+        variableToValueMap.put("page-height", new FloatValue(groupContext.pageHeight));
 
         // Iterate through statements
-        for (Iterator i = statements.iterator(); i.hasNext();) {
-            final Element currentElement = (Element) i.next();
+        for (final Element currentElement: statements) {
 
             // Check whether this statement applies to the current page
             final String elementPage = currentElement.attributeValue("page");
-            if ((elementPage != null ) && !Integer.toString(groupContext.pageNumber).equals(elementPage))
+            if ((elementPage != null) && !Integer.toString(groupContext.pageNumber).equals(elementPage))
                 continue;
 
-            final Map namespaceMap = Dom4jUtils.getNamespaceContextNoDefault(currentElement);
+            final Map<String, String> namespaceMap = Dom4jUtils.getNamespaceContextNoDefault(currentElement);
             final String elementName = currentElement.getName();
             if (elementName.equals("group")) {
                 // Handle group
@@ -270,7 +276,7 @@ public class PDFTemplateProcessor extends HttpBinarySerializer {// TODO: HttpBin
                     if (newContextNode == null)
                         continue;
 
-                    newGroupContext.contextNodeSet = Collections.singletonList(newContextNode);
+                    newGroupContext.contextNodeSet = Collections.singletonList((Item) newContextNode);
                     newGroupContext.contextPosition = 1;
                 }
 
@@ -296,7 +302,7 @@ public class PDFTemplateProcessor extends HttpBinarySerializer {// TODO: HttpBin
                 if (fontSize != null)
                     newGroupContext.fontSize = Float.parseFloat(fontSize);
 
-                handleGroup(pipelineContext, newGroupContext, currentElement.elements(), functionLibrary, reader);
+                handleGroup(pipelineContext, newGroupContext, Dom4jUtils.elements(currentElement), functionLibrary, reader);
 
             } else if (elementName.equals("repeat")) {
                 // Handle repeat
@@ -320,58 +326,58 @@ public class PDFTemplateProcessor extends HttpBinarySerializer {// TODO: HttpBin
                     newGroupContext.offsetX = groupContext.offsetX + (iterationIndex - 1) * offsetIncrementX;
                     newGroupContext.offsetY = groupContext.offsetY + (iterationIndex - 1) * offsetIncrementY;
 
-                    handleGroup(pipelineContext, newGroupContext, currentElement.elements(), functionLibrary, reader);
+                    handleGroup(pipelineContext, newGroupContext, Dom4jUtils.elements(currentElement), functionLibrary, reader);
                 }
             } else if (elementName.equals("field")) {
 
-            	final String fieldNameStr = currentElement.attributeValue("acro-field-name");
+                final String fieldNameStr = currentElement.attributeValue("acro-field-name");
 
-            	if (fieldNameStr != null) {
-            		final String value = currentElement.attributeValue("value") == null ? currentElement.attributeValue("ref") : currentElement.attributeValue("value");
-            		// Get value from instance
+                if (fieldNameStr != null) {
+                    final String value = currentElement.attributeValue("value") == null ? currentElement.attributeValue("ref") : currentElement.attributeValue("value");
+                    // Get value from instance
 
                     final String text = XPathCache.evaluateAsString(pipelineContext, groupContext.contextNodeSet, groupContext.contextPosition, value, namespaceMap, variableToValueMap, functionLibrary, null, null, (LocationData) currentElement.getData());
                     final String fieldName = XPathCache.evaluateAsString(pipelineContext, groupContext.contextNodeSet, groupContext.contextPosition, fieldNameStr, namespaceMap, variableToValueMap, functionLibrary, null, null, (LocationData) currentElement.getData());
                     groupContext.acroFields.setField(fieldName, text);
 
-            	} else {
-	                // Handle field
+                } else {
+                    // Handle field
 
-	                final String leftAttribute = currentElement.attributeValue("left") == null ? currentElement.attributeValue("left-position") : currentElement.attributeValue("left");
-	                final String topAttribute = currentElement.attributeValue("top") == null ? currentElement.attributeValue("top-position") : currentElement.attributeValue("top");
+                    final String leftAttribute = currentElement.attributeValue("left") == null ? currentElement.attributeValue("left-position") : currentElement.attributeValue("left");
+                    final String topAttribute = currentElement.attributeValue("top") == null ? currentElement.attributeValue("top-position") : currentElement.attributeValue("top");
 
-	                final String leftPosition = resolveAttributeValueTemplates(pipelineContext, contextNode, variableToValueMap, null, null, currentElement, leftAttribute);
-	                final String topPosition = resolveAttributeValueTemplates(pipelineContext, contextNode, variableToValueMap, null, null, currentElement, topAttribute);
+                    final String leftPosition = resolveAttributeValueTemplates(pipelineContext, contextNode, variableToValueMap, null, null, currentElement, leftAttribute);
+                    final String topPosition = resolveAttributeValueTemplates(pipelineContext, contextNode, variableToValueMap, null, null, currentElement, topAttribute);
 
-	                final String size = resolveAttributeValueTemplates(pipelineContext, contextNode, variableToValueMap, null, null, currentElement, currentElement.attributeValue("size"));
-	                final String value = currentElement.attributeValue("value") == null ? currentElement.attributeValue("ref") : currentElement.attributeValue("value");
+                    final String size = resolveAttributeValueTemplates(pipelineContext, contextNode, variableToValueMap, null, null, currentElement, currentElement.attributeValue("size"));
+                    final String value = currentElement.attributeValue("value") == null ? currentElement.attributeValue("ref") : currentElement.attributeValue("value");
 
-	                final FontAttributes fontAttributes = getFontAttributes(currentElement, pipelineContext, groupContext, variableToValueMap, contextNode);
+                    final FontAttributes fontAttributes = getFontAttributes(currentElement, pipelineContext, groupContext, variableToValueMap, contextNode);
 
-	                // Output value
-	                final BaseFont baseFont = BaseFont.createFont(fontAttributes.fontFamily, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
-	                groupContext.contentByte.beginText();
-	                {
-	                    groupContext.contentByte.setFontAndSize(baseFont, fontAttributes.fontSize);
+                    // Output value
+                    final BaseFont baseFont = BaseFont.createFont(fontAttributes.fontFamily, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+                    groupContext.contentByte.beginText();
+                    {
+                        groupContext.contentByte.setFontAndSize(baseFont, fontAttributes.fontSize);
 
-	                    final float xPosition = Float.parseFloat(leftPosition) + groupContext.offsetX;
-	                    final float yPosition = groupContext.pageHeight - (Float.parseFloat(topPosition) + groupContext.offsetY);
+                        final float xPosition = Float.parseFloat(leftPosition) + groupContext.offsetX;
+                        final float yPosition = groupContext.pageHeight - (Float.parseFloat(topPosition) + groupContext.offsetY);
 
-	                    // Get value from instance
-	                    final String text = XPathCache.evaluateAsString(pipelineContext, groupContext.contextNodeSet, groupContext.contextPosition, value, namespaceMap, variableToValueMap, functionLibrary, null, null, (LocationData) currentElement.getData());
+                        // Get value from instance
+                        final String text = XPathCache.evaluateAsString(pipelineContext, groupContext.contextNodeSet, groupContext.contextPosition, value, namespaceMap, variableToValueMap, functionLibrary, null, null, (LocationData) currentElement.getData());
 
-	                    // Iterate over characters and print them
-	                    if (text != null) {
-	                        int len = Math.min(text.length(), (size != null) ? Integer.parseInt(size) : Integer.MAX_VALUE);
-	                        for (int j = 0; j < len; j++)
-	                            groupContext.contentByte.showTextAligned(PdfContentByte.ALIGN_CENTER, text.substring(j, j + 1), xPosition + ((float) j) * fontAttributes.fontPitch, yPosition, 0);
-	                    }
-	                }
-	                groupContext.contentByte.endText();
-            	}
+                        // Iterate over characters and print them
+                        if (text != null) {
+                            int len = Math.min(text.length(), (size != null) ? Integer.parseInt(size) : Integer.MAX_VALUE);
+                            for (int j = 0; j < len; j++)
+                                groupContext.contentByte.showTextAligned(PdfContentByte.ALIGN_CENTER, text.substring(j, j + 1), xPosition + ((float) j) * fontAttributes.fontPitch, yPosition, 0);
+                        }
+                    }
+                    groupContext.contentByte.endText();
+                }
             } else if (elementName.equals("barcode")) {
                 // Handle barcode
-                
+
                 final String leftAttribute = currentElement.attributeValue("left");
                 final String topAttribute = currentElement.attributeValue("top");
 
@@ -421,7 +427,7 @@ public class PDFTemplateProcessor extends HttpBinarySerializer {// TODO: HttpBin
                 if (inputName != null) {
                     // Read the input
                     final ByteArrayOutputStream os = new ByteArrayOutputStream();
-                    readInputAsSAX(pipelineContext, inputName,  new BinaryTextContentHandler(null, os, true, false, null, false, false, null, false));
+                    readInputAsSAX(pipelineContext, inputName, new BinaryTextContentHandler(null, os, true, false, null, false, false, null, false));
 
                     // Create the image
                     image = Image.getInstance(os.toByteArray());
@@ -461,7 +467,7 @@ public class PDFTemplateProcessor extends HttpBinarySerializer {// TODO: HttpBin
     private FontAttributes getFontAttributes(Element currentElement,
     		PipelineContext pipelineContext,
     		GroupContext groupContext,
-    		Map variableToValueMap,
+    		Map<String, ValueRepresentation> variableToValueMap,
     		NodeInfo contextNode){
 
         final float fontPitch;

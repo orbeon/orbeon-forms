7cd9d46ffd7ca64fdd5c3662975503c02060250a
ebruchez <ebruchez@orbeon.com>
2009-08-24 11:21:29 -0700

    Fixed "[ #314167 ] new-xhtml-layout: <xforms:output appearance="xxforms:download"> generates a span instead of an anchor".

--- src/java/org/orbeon/oxf/xforms/processor/handlers/XFormsOutputHandler.java
+++ src/java/org/orbeon/oxf/xforms/processor/handlers/XFormsOutputHandler.java
@@ -1,15 +1,15 @@
 /**
- *  Copyright (C) 2005 Orbeon, Inc.
+ * Copyright (C) 2009 Orbeon, Inc.
  *
- *  This program is free software; you can redistribute it and/or modify it under the terms of the
- *  GNU Lesser General Public License as published by the Free Software Foundation; either version
- *  2.1 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
  */
 package org.orbeon.oxf.xforms.processor.handlers;
 
@@ -93,7 +93,8 @@ public class XFormsOutputHandler extends XFormsControlLifecyleHandler {
             // Handle accessibility attributes (de facto, tabindex is supported on all elements)
             handleAccessibilityAttributes(attributes, newAttributes);
 
-            contentHandler.startElement(XMLConstants.XHTML_NAMESPACE_URI, enclosingElementLocalname, enclosingElementQName, newAttributes);
+            if (!handlerContext.isNewXHTMLLayout())
+                contentHandler.startElement(XMLConstants.XHTML_NAMESPACE_URI, enclosingElementLocalname, enclosingElementQName, newAttributes);
             {
                 if (XFormsConstants.XXFORMS_DOWNLOAD_APPEARANCE_QNAME.equals(getAppearance(attributes))) {
                     // Download appearance
@@ -155,7 +156,8 @@ public class XFormsOutputHandler extends XFormsControlLifecyleHandler {
                     }
                 }
             }
-            contentHandler.endElement(XMLConstants.XHTML_NAMESPACE_URI, enclosingElementLocalname, enclosingElementQName);
+            if (!handlerContext.isNewXHTMLLayout())
+                contentHandler.endElement(XMLConstants.XHTML_NAMESPACE_URI, enclosingElementLocalname, enclosingElementQName);
         }
     }
 }

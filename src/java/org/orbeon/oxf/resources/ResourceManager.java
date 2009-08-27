3646e3133b590ce676f53a6e29e4f97910c51469
ebruchez <ebruchez@orbeon.com>
2009-08-27 15:28:29 -0700

    Implemented "[ #314184 ] RFE: Make error-dialog.xml configurable by application". Added exists() function to Resource Managers. Minor refactoring of Resource Managers.

--- src/java/org/orbeon/oxf/resources/ResourceManager.java
+++ src/java/org/orbeon/oxf/resources/ResourceManager.java
@@ -1,15 +1,15 @@
 /**
- *  Copyright (C) 2004 Orbeon, Inc.
+ * Copyright (C) 2009 Orbeon, Inc.
  *
- *  This program is free software; you can redistribute it and/or modify it under the terms of the
- *  GNU Lesser General Public License as published by the Free Software Foundation; either version
- *  2.1 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
  */
 package org.orbeon.oxf.resources;
 
@@ -160,4 +160,12 @@ public interface ResourceManager {
      * returned.
      */
     public String getRealPath(String key);
+
+    /**
+     * Check if a resource exists given its key.
+     *
+     * @param key   A Resource Manager key
+     * @return      true iif the resource exists
+     */
+    public boolean exists(String key);
 }

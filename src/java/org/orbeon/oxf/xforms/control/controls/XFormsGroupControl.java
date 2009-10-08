b98c3c20781bc54204c34cc6b09dce13f59bac97
ebruchez <ebruchez@orbeon.com>
2009-10-08 16:55:11 -0700

    Prevent xfroms:group from having the xforms-static class.

--- src/java/org/orbeon/oxf/xforms/control/controls/XFormsGroupControl.java
+++ src/java/org/orbeon/oxf/xforms/control/controls/XFormsGroupControl.java
@@ -1,23 +1,23 @@
 /**
- *  Copyright (C) 2006 Orbeon, Inc.
+ * Copyright (C) 2009 Orbeon, Inc.
  *
- *  This program is free software; you can redistribute it and/or modify it under the terms of the
- *  GNU Lesser General Public License as published by the Free Software Foundation; either version
- *  2.1 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
  */
 package org.orbeon.oxf.xforms.control.controls;
 
 import org.dom4j.Element;
 import org.orbeon.oxf.xforms.XFormsConstants;
-import org.orbeon.oxf.xforms.xbl.XBLContainer;
 import org.orbeon.oxf.xforms.control.XFormsControl;
 import org.orbeon.oxf.xforms.control.XFormsSingleNodeContainerControl;
+import org.orbeon.oxf.xforms.xbl.XBLContainer;
 import org.orbeon.oxf.xml.dom4j.Dom4jUtils;
 
 /**
@@ -34,5 +34,12 @@ public class XFormsGroupControl extends XFormsSingleNodeContainerControl {
     @Override
     public String getType() {
         return null;
+    }
+
+    @Override
+    public boolean isStaticReadonly() {
+        // Static readonly-ness doesn't seem to make much sense for xforms:group, and we don't want to see the
+        // xforms-static class in the resulting HTML
+        return false;
     }
 }

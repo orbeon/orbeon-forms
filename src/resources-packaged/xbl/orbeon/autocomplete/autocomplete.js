e48d8c2e12e6afe7b6aec1fc77938dd77909f7ae
avernet <avernet@orbeon.com>
2009-10-14 15:49:36 -0700

    Implement [ #314367 ] RFE: Autocomplete: enable autocomplete to know what the current value is for the selected label

--- src/resources-packaged/xbl/orbeon/autocomplete/autocomplete.js
+++ src/resources-packaged/xbl/orbeon/autocomplete/autocomplete.js
@@ -37,6 +37,8 @@
         searchControl: null,        // xforms:input control in which users type
         searchField: null,          // Form field in which users type
         externalValueInput : null,  // xforms:input containing the external value
+        lastSuggestionList: [],     // List is saved so we don't show the same when the Ajax response comes back
+        justMadeSelection: false,   // Reset to false when an Ajax response arrives
 
         /**
          * Constructor
@@ -89,6 +91,7 @@
         itemSelected: function(type, args) {
             var itemLabel = args[2][0];
             var itemValue = args[2][1];
+            this.justMadeSelection = true;
             Document.setValue(this.searchControl.id, itemLabel);
             Document.setValue(this.externalValueInput.id, itemValue);
         },
@@ -102,12 +105,17 @@
             var searchFieldValue = this.searchField.value;
             var select1Element = this.element.getElementsByTagName("select")[0];
             var options = select1Element.options;
+            var matchingOptionIndex = -1;
             for (var optionIndex = 0; optionIndex < options.length; optionIndex++) {
                 var option = options[optionIndex];
                 if (option.text == searchFieldValue) {
-                    Document.setValue(this.externalValueInput.id, option.value);
+                    // Give up if this is not the first match
+                    if (matchingOptionIndex != -1) return;
+                    matchingOptionIndex = optionIndex;
                 }
             }
+            if (matchingOptionIndex != -1)
+                Document.setValue(this.externalValueInput.id, options[matchingOptionIndex].value);
         },
 
         /**
@@ -116,12 +124,32 @@
         ajaxResponseProcessed: function() {
             // Get new list of values
             var query = this.searchField.value;
+            var oldList = this.lastSuggestionList;
             var newList = this.getCurrentValues(query);
-            // Update the list only of the control has the focus, as updating the list will show the suggestion list
-            // and we only want to show the suggestion list if the user happens to be in that field
-            if (this.yuiAutoComplete.isFocused()) {
+
+            var sameSuggestionAsBeforeAjax = function() {
+                var result = newList.length == oldList.length;
+                if (result) {
+                    for (var listIndex = 0; listIndex < newList.length; listIndex++) {
+                        if (newList[listIndex][0] != oldList[listIndex][0] || newList[listIndex][1] != oldList[listIndex][1]) {
+                            result = false;
+                            break;
+                        }
+                    };
+                }
+                return result;
+            }.call(this);
+
+            var doUdateSuggestionList =
+                    // If the user just selected something and the response when the Ajax query comes back has the same
+                    // itemset, don't update the suggestion list
+                    ! (this.justMadeSelection && sameSuggestionAsBeforeAjax)
+                    // Update the list only of the control has the focus, as updating the list will show the suggestion list
+                    // and we only want to show the suggestion list if the user happens to be in that field
+                    && this.yuiAutoComplete.isFocused();
+            if (doUdateSuggestionList)
                 this.yuiAutoComplete._populateList(query, { results: newList }, { query: query });
-            }
+            this.justMadeSelection = false;
         },
 
         /**
@@ -168,9 +196,12 @@
             var autoComplete = this;
             var result = [];
 
+            // YUI autocomplete give us an escaped string
+            query = unescape(query);
             // Look again for the element, as on IE the <select> is recreated when the itemset changes, and so can't be cached
             var select1Element = this.element.getElementsByTagName("select")[0];
             var options = select1Element.options;
+            var foundExactMatch = false;
             if (query != "") {
                 var queryLowerCase = query.toLowerCase();
                 for (var optionIndex = 0; optionIndex < options.length; optionIndex++) {
@@ -178,20 +209,24 @@
                     // We only do filtering for the static itemset mode
                     if (this.isDynamicItemset() || option.text.toLowerCase().indexOf(queryLowerCase) == 0)
                         result[result.length] = [ option.text, option.value ];
+                    if (option.text == query) foundExactMatch = true;
+
                 }
             }
 
+            // If the value in the search field is not in the itemset, set the external value to empty string
+            if (! foundExactMatch)
+                Document.setValue(this.externalValueInput.id, "");
+
+            // If the result only contains one item, and its label is equal to the current value
+            // Set the external value to the value of this option
             if (result.length == 1 && result[0][0] == query) {
-                // If the result only contains one item, and its label is equal to the current value
-                // Set the external value to the value of this option
                 Document.setValue(this.externalValueInput.id, result[0][1]);
                 // Don't return any suggestion
                 result = [];
-            } else {
-                // If the value in the search field is not in the itemset, set the external value to empty string
-                Document.setValue(this.externalValueInput.id, "");
             }
 
+            this.lastSuggestionList = result;
             return result;
         }
     };

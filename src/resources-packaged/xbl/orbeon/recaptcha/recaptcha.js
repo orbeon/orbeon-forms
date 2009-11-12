408f8df5480794687695764501af2031d67b4041
avernet <avernet@orbeon.com>
2009-11-11 23:24:13 -0800

    First checkin of recaptcha component

---
+++ src/resources-packaged/xbl/orbeon/recaptcha/recaptcha.js
@@ -0,0 +1,58 @@
+/**
+ *  Copyright (C) 2009 Orbeon, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify it under the terms of the
+ *  GNU Lesser General Public License as published by the Free Software Foundation; either version
+ *  2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+ *
+ *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+YAHOO.namespace("xbl.fr");
+YAHOO.xbl.fr.Recaptcha = function() {};
+ORBEON.xforms.XBL.declareClass(YAHOO.xbl.fr.Recaptcha, "xbl-fr-recaptcha");
+YAHOO.xbl.fr.Recaptcha.prototype = {
+
+    challengeId: null,
+    responseId: null,
+    publicKeyPropertyId: null,
+    publicKeyLocalId: null,
+
+    /**
+     * Constructor
+     */
+    init: function() {
+        var recaptchaDiv = YAHOO.util.Dom.getElementsByClassName("xbl-fr-recaptcha-div", null, this.container)[0];
+        this.challengeId = YAHOO.util.Dom.getElementsByClassName("xbl-fr-recaptcha-challenge", null, this.container)[0].id;
+        this.responseId = YAHOO.util.Dom.getElementsByClassName("xbl-fr-recaptcha-response", null, this.container)[0].id;
+        
+        // Public key comes from property
+        var publicKeyElement = YAHOO.util.Dom.getElementsByClassName("xbl-fr-recaptcha-public-key", null, this.container)[0];
+        var publicKey = ORBEON.xforms.Document.getValue(publicKeyElement.id);
+        // Other configurations
+        var themeElement = YAHOO.util.Dom.getElementsByClassName("xbl-fr-recaptcha-theme", null, this.container)[0];
+        var theme = ORBEON.xforms.Document.getValue(themeElement.id);
+        var langElement = YAHOO.util.Dom.getElementsByClassName("xbl-fr-recaptcha-lang", null, this.container)[0];
+        var lang = ORBEON.xforms.Document.getValue(langElement.id);
+        
+        console.log("Theme", theme);
+        
+        Recaptcha.create(publicKey, recaptchaDiv.id, {
+           theme: theme,
+           lang: lang,
+           callback: Recaptcha.focus_response_field
+        });
+    },
+    
+    getChallengeResponse: function() {
+        ORBEON.xforms.Document.setValue(this.challengeId, Recaptcha.get_challenge());
+        ORBEON.xforms.Document.setValue(this.responseId, Recaptcha.get_response());
+    },
+    
+    reload: function() {
+        Recaptcha.reload();
+    }
+};

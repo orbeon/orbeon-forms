f462e6cea236384cf6cc162e5805edd401e0e17b
nvdbleek <nick@bleeken.eu>
2010-01-10 17:09:59 +0100

    Made boolean-from-string() in sync with XF 11 spec (now passes 7.6.1.a.xhtml)

--- src/java/org/orbeon/oxf/xforms/function/BooleanFromString.java
+++ src/java/org/orbeon/oxf/xforms/function/BooleanFromString.java
@@ -13,7 +13,6 @@
  */
 package org.orbeon.oxf.xforms.function;
 
-import org.orbeon.oxf.common.OXFException;
 import org.orbeon.saxon.expr.XPathContext;
 import org.orbeon.saxon.om.Item;
 import org.orbeon.saxon.trans.XPathException;
@@ -23,11 +22,11 @@ public class BooleanFromString extends XFormsFunction {
 
     public Item evaluateItem(XPathContext c) throws XPathException {
         String value = argument[0].evaluateAsString(c);
-        if("1".equals(value) || "true".equals(value))
+        if("1".equals(value) || "true".equalsIgnoreCase(value))
             return BooleanValue.TRUE;
-        else if("0".equals(value) || "false".equals(value))
+        else if("0".equals(value) || "false".equalsIgnoreCase(value))
             return BooleanValue.FALSE;
         else
-            throw new OXFException("boolean-to-string() argument must be in (0,1,true,false) and was " + value );
+        	return BooleanValue.FALSE;
     }
 }

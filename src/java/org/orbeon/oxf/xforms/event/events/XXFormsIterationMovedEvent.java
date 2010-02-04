0e1c92538f4ae1859a0afa49e10d10d1ae00080c
ebruchez <ebruchez@orbeon.com>
2010-02-03 22:50:04 -0800

    Missing commit for "[ #314693 ] RFE: add xxforms-iteration-moved event on controls".

---
+++ src/java/org/orbeon/oxf/xforms/event/events/XXFormsIterationMovedEvent.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2010 Orbeon, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+package org.orbeon.oxf.xforms.event.events;
+
+import org.orbeon.oxf.xforms.XFormsContainingDocument;
+import org.orbeon.oxf.xforms.event.XFormsEvent;
+import org.orbeon.oxf.xforms.event.XFormsEventTarget;
+import org.orbeon.oxf.xforms.event.XFormsEvents;
+
+public class XXFormsIterationMovedEvent extends XFormsEvent {
+    public XXFormsIterationMovedEvent(XFormsContainingDocument containingDocument, XFormsEventTarget targetObject) {
+        super(containingDocument, XFormsEvents.XXFORMS_ITERATION_MOVED, targetObject, false, true);
+    }
+}

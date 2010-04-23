6986b84a5fb8773b9128311a258f457d88e317ad
ebruchez <ebruchez@orbeon.com>
2010-04-23 15:23:56 -0700

    Migrated doc to wiki.

--- src/java/org/orbeon/oxf/processor/transformer/TransformerURIResolver.java
+++ src/java/org/orbeon/oxf/processor/transformer/TransformerURIResolver.java
@@ -1,15 +1,15 @@
 /**
- *  Copyright (C) 2004 Orbeon, Inc.
+ * Copyright (C) 2010 Orbeon, Inc.
  *
- *  This program is free software; you can redistribute it and/or modify it under the terms of the
- *  GNU Lesser General Public License as published by the Free Software Foundation; either version
- *  2.1 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
  */
 package org.orbeon.oxf.processor.transformer;
 
@@ -124,12 +124,14 @@ public class TransformerURIResolver implements URIResolver {
 
                     private ContentHandler originalHandler;
 
+                    @Override
                     public void setContentHandler(ContentHandler handler) {
                         originalHandler = handler;
-                        List contentHandlers = Arrays.asList(new Object[]{uriResolverListener.getContentHandler(), handler});
+                        List contentHandlers = Arrays.asList(uriResolverListener.getContentHandler(), handler);
                         super.setContentHandler(new TeeContentHandler(contentHandlers));
                     }
 
+                    @Override
                     public ContentHandler getContentHandler() {
                         return originalHandler;
                     }

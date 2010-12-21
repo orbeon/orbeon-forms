409ca81a109ea8240c3413a00a755545afd4d8c6
ebruchez <ebruchez@orbeon.com>
2010-12-21 09:14:11 -0800

    Fixed "[ #315704 ] xf:select/select1 control with internal appearance must reject xf:setfocus action".

--- src/java/org/orbeon/oxf/xforms/control/controls/XFormsSelect1Control.java
+++ src/java/org/orbeon/oxf/xforms/control/controls/XFormsSelect1Control.java
@@ -19,14 +19,20 @@ import org.dom4j.QName;
 import org.orbeon.oxf.common.ValidationException;
 import org.orbeon.oxf.pipeline.api.PipelineContext;
 import org.orbeon.oxf.util.PropertyContext;
-import org.orbeon.oxf.xforms.*;
+import org.orbeon.oxf.xforms.XFormsConstants;
+import org.orbeon.oxf.xforms.XFormsContainingDocument;
+import org.orbeon.oxf.xforms.XFormsUtils;
 import org.orbeon.oxf.xforms.analysis.XPathDependencies;
 import org.orbeon.oxf.xforms.analysis.controls.SelectionControl;
-import org.orbeon.oxf.xforms.control.*;
+import org.orbeon.oxf.xforms.control.XFormsControl;
+import org.orbeon.oxf.xforms.control.XFormsSingleNodeControl;
+import org.orbeon.oxf.xforms.control.XFormsValueControl;
 import org.orbeon.oxf.xforms.event.XFormsEvent;
 import org.orbeon.oxf.xforms.event.events.XFormsDeselectEvent;
 import org.orbeon.oxf.xforms.event.events.XFormsSelectEvent;
-import org.orbeon.oxf.xforms.itemset.*;
+import org.orbeon.oxf.xforms.itemset.Item;
+import org.orbeon.oxf.xforms.itemset.Itemset;
+import org.orbeon.oxf.xforms.itemset.XFormsItemUtils;
 import org.orbeon.oxf.xforms.xbl.XBLContainer;
 import org.orbeon.oxf.xml.ContentHandlerHelper;
 import org.orbeon.oxf.xml.dom4j.Dom4jUtils;
@@ -378,6 +384,12 @@ public class XFormsSelect1Control extends XFormsValueControl {
             }
             ch.endElement();
         }
+    }
+
+    @Override
+    public boolean setFocus() {
+        // Don't accept focus if we have the internal appearance
+        return !XFormsGroupControl.INTERNAL_APPEARANCE.equals(getAppearance()) && super.setFocus();
     }
 
     @Override

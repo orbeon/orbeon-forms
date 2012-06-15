d582a2fc3c1d4fc9129f58b615612f16f29e7f90
avernet <avernet@orbeon.com>
2012-06-14 17:32:02 -0700

    Fix: now that code right way, not on DOM ready, we can't just get the controls upfront

--- src/resources-packaged/xbl/orbeon/currency/currency-unittest.js
+++ src/resources-packaged/xbl/orbeon/currency/currency-unittest.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2010 Orbeon, Inc.
+ * Copyright (C) 2012 Orbeon, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the
  * GNU Lesser General Public License as published by the Free Software Foundation; either version
@@ -13,17 +13,9 @@
  */
 (function() {
 
-    var emptyInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, "empty")[0];
-    var valueInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, "value")[0];
-    var valueOutput = YAHOO.util.Dom.get("value-output");
-    var doubleInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, "double")[0];
-    var prefixStaticInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, "prefix-static")[0];
-    var prefixDynamicInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, "prefix-dynamic")[0];
-    var floatStaticInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, "float-static")[0];
-    var floatDynamicInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, "float-dynamic")[0];
-    var floatNoDigitsInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, "float-no-digits")[0];
-    var readonlyInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, "readonly")[0];
-    var largeInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, "large")[0];
+    function input(id) {
+        return $(document.getElementById(id)).find('.xbl-fr-number-visible-input')[0];
+    }
 
     YAHOO.tool.TestRunner.add(new YAHOO.tool.TestCase({
 
@@ -31,122 +23,122 @@
 
         // Field should be empty when the page loaded if the instance is empty
         testEmptyInitial: function() {
-            YAHOO.util.Assert.areEqual("", emptyInput.value);
+            YAHOO.util.Assert.areEqual("", input('empty').value);
         },
 
         // If we just enter and leave the field, the value must stay empty
         testEmptyNoChange: function() {
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
-                emptyInput.focus();
-                emptyInput.blur();
-                doubleInput.focus();
+                input('empty').focus();
+                input('empty').blur();
+                input('double').focus();
             }, function() {
-                YAHOO.util.Assert.areEqual("", emptyInput.value);
+                YAHOO.util.Assert.areEqual("", input('empty').value);
             });
         },
 
         testBeforeFocus: function() {
-            YAHOO.util.Assert.areEqual("$ 1,234.00", valueInput.value);
+            YAHOO.util.Assert.areEqual("$ 1,234.00", input('value').value);
         },
 
         testAfterFocus: function() {
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
-                valueInput.focus();
-                YAHOO.util.Assert.areEqual("1234.00", valueInput.value);
+                input('value').focus();
+                YAHOO.util.Assert.areEqual("1234.00", input('value').value);
             }, function() {
             });
         },
 
         testChangeSimple: function() {
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
-                valueInput.value = 42;
-                valueInput.blur();
+                input('value').value = 42;
+                input('value').blur();
             }, function() {
-                YAHOO.util.Assert.areEqual("$ 42.00", valueInput.value);
-                YAHOO.util.Assert.areEqual("$ 84.00", doubleInput.value);
+                YAHOO.util.Assert.areEqual("$ 42.00", input('value').value);
+                YAHOO.util.Assert.areEqual("$ 84.00", input('double').value);
             });
         },
 
         testChangeWhileFocus: function() {
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
-                valueInput.focus();
-                valueInput.value = 43;
-                valueInput.blur();
-                doubleInput.focus();
+                input('value').focus();
+                input('value').value = 43;
+                input('value').blur();
+                input('double').focus();
             }, function() {
-                YAHOO.util.Assert.areEqual("$ 43.00", valueInput.value);
-                YAHOO.util.Assert.areEqual("86.00", doubleInput.value);
+                YAHOO.util.Assert.areEqual("$ 43.00", input('value').value);
+                YAHOO.util.Assert.areEqual("86.00", input('double').value);
             });
         },
 
         testStaticCurrency: function() {
-            YAHOO.util.Assert.areEqual("£ 4,567.00", prefixStaticInput.value);
+            YAHOO.util.Assert.areEqual("£ 4,567.00", input('prefix-static').value);
         },
 
         testDynamicCurrency: function() {
-            YAHOO.util.Assert.areEqual("£ 4,567.00", prefixDynamicInput.value);
+            YAHOO.util.Assert.areEqual("£ 4,567.00", input('prefix-dynamic').value);
         },
 
         testChangeCurrency: function() {
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
                 YAHOO.util.UserAction.click(YAHOO.util.Dom.get("change-prefix"));
             }, function() {
-                YAHOO.util.Assert.areEqual("CHF 4,567.00", prefixDynamicInput.value);
+                YAHOO.util.Assert.areEqual("CHF 4,567.00", input('prefix-dynamic').value);
             });
         },
         testStaticDigits: function() {
-            YAHOO.util.Assert.areEqual("$ 123.456", floatStaticInput.value);
+            YAHOO.util.Assert.areEqual("$ 123.456", input('float-static').value);
         },
         testDynamicDigits: function() {
-            YAHOO.util.Assert.areEqual("$ 123.456", floatDynamicInput.value);
+            YAHOO.util.Assert.areEqual("$ 123.456", input('float-dynamic').value);
         },
         testChangeDigits: function() {
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
                 YAHOO.util.UserAction.click(YAHOO.util.Dom.get("change-digits"));
             }, function() {
-                YAHOO.util.Assert.areEqual("$ 123.46", floatDynamicInput.value);
+                YAHOO.util.Assert.areEqual("$ 123.46", input('float-dynamic').value);
             });
         },
         testNoDigits: function() {
-            YAHOO.util.Assert.areEqual("$ 123", floatNoDigitsInput.value);
+            YAHOO.util.Assert.areEqual("$ 123", input('float-no-digits').value);
         },
         testReadonly: function() {
-            YAHOO.util.Assert.isTrue(readonlyInput.disabled);
+            YAHOO.util.Assert.isTrue(input('readonly').disabled);
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
                 YAHOO.util.UserAction.click(YAHOO.util.Dom.get("set-readwrite"));
             }, function() {
-                YAHOO.util.Assert.isFalse(readonlyInput.disabled);
+                YAHOO.util.Assert.isFalse(input('readonly').disabled);
                 ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
                     YAHOO.util.UserAction.click(YAHOO.util.Dom.get("set-readonly"));
                 }, function() {
-                    YAHOO.util.Assert.isTrue(readonlyInput.disabled);
+                    YAHOO.util.Assert.isTrue(input('readonly').disabled);
                 });
             });
         },
         testCleanupComa: function() {
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
-                valueInput.focus();
-                valueInput.value = "1,1";
-                doubleInput.focus();
+                input('value').focus();
+                input('value').value = "1,1";
+                input('double').focus();
             }, function() {
-                YAHOO.util.Assert.areEqual("11", ORBEON.xforms.Controls.getCurrentValue(valueOutput));
+                YAHOO.util.Assert.areEqual("11", ORBEON.xforms.Controls.getCurrentValue(YAHOO.util.Dom.get("value-output")));
             });
         },
         testCleanupPrefix: function() {
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
-                valueInput.focus();
-                valueInput.value = "$4242";
-                doubleInput.focus();
+                input('value').focus();
+                input('value').value = "$4242";
+                input('double').focus();
             }, function() {
-                YAHOO.util.Assert.areEqual("$ 4,242.00", valueInput.value, "1st attempt, value in field");
-                YAHOO.util.Assert.areEqual("4242", ORBEON.xforms.Controls.getCurrentValue(valueOutput), "1st attempt, value in instance");
+                YAHOO.util.Assert.areEqual("$ 4,242.00", input('value').value, "1st attempt, value in field");
+                YAHOO.util.Assert.areEqual("4242", ORBEON.xforms.Controls.getCurrentValue(YAHOO.util.Dom.get("value-output")), "1st attempt, value in instance");
                 ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
-                    valueInput.focus();
-                    valueInput.value = "$4242";
-                    doubleInput.focus();
+                    input('value').focus();
+                    input('value').value = "$4242";
+                    input('double').focus();
                 }, function() {
-                    YAHOO.util.Assert.areEqual("$ 4,242.00", valueInput.value, "2nd attempt, value in field");
-                    YAHOO.util.Assert.areEqual("4242", ORBEON.xforms.Controls.getCurrentValue(valueOutput), "2nd attempt, value in instance");
+                    YAHOO.util.Assert.areEqual("$ 4,242.00", input('value').value, "2nd attempt, value in field");
+                    YAHOO.util.Assert.areEqual("4242", ORBEON.xforms.Controls.getCurrentValue(YAHOO.util.Dom.get("value-output")), "2nd attempt, value in instance");
                 });
             });
         },
@@ -168,9 +160,7 @@
         },
         testClasses: function() {
             var requiredEmptyControl = YAHOO.util.Dom.get("required-empty");
-            var requiredEmptyInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, requiredEmptyControl)[0];
             var emptyControl = YAHOO.util.Dom.get("empty");
-            var emptyInput = YAHOO.util.Dom.getElementsByClassName("xbl-fr-number-visible-input", null, emptyControl)[0];
 
             var requiredFilledControl = YAHOO.util.Dom.get("required-filled");
 
@@ -185,17 +175,17 @@
             checkClasses(requiredEmptyControl, true, true, false, false, "initially empty");
             checkClasses(requiredFilledControl, false, true, false, true, "initially filled");
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
-                requiredEmptyInput.focus();
-                requiredEmptyInput.blur();
-                emptyInput.focus();
+                input('required-empty').focus();
+                input('required-empty').blur();
+                input('empty').focus();
             }, function() {
                 //  Check we now have the class xforms-invalid-visited
                 checkClasses(requiredEmptyControl, true, true, true, false, "after visited empty");
                 ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
-                    requiredEmptyInput.focus();
-                    requiredEmptyInput.value = "42";
-                    requiredEmptyInput.blur();
-                    emptyInput.focus();
+                    input('required-empty').focus();
+                    input('required-empty').value = "42";
+                    input('required-empty').blur();
+                    input('empty').focus();
                 }, function() {
                     // Check it is now marked valid and filled
                     checkClasses(requiredEmptyControl, false, true, false, true, "after filled empty");
@@ -216,14 +206,14 @@
         // Currency field should not be susceptible to limitations of number precision imposed by the platform
         testLarge: function() {
             ORBEON.util.Test.executeCausingAjaxRequest(this, function() {
-                YAHOO.util.Assert.areEqual("$ 12,345,678,901,234,567,890,123.00", largeInput.value);
-                largeInput.focus();
-                YAHOO.util.Assert.areEqual("12345678901234567890123.00", largeInput.value);
-                largeInput.value = "12345678901234567890124";
-                largeInput.blur();
-                emptyInput.focus();
+                YAHOO.util.Assert.areEqual("$ 12,345,678,901,234,567,890,123.00", input('large').value);
+                input('large').focus();
+                YAHOO.util.Assert.areEqual("12345678901234567890123.00", input('large').value);
+                input('large').value = "12345678901234567890124";
+                input('large').blur();
+                input('empty').focus();
             }, function() {
-                YAHOO.util.Assert.areEqual("$ 12,345,678,901,234,567,890,124.00", largeInput.value);
+                YAHOO.util.Assert.areEqual("$ 12,345,678,901,234,567,890,124.00", input('large').value);
             });
         }
     }));

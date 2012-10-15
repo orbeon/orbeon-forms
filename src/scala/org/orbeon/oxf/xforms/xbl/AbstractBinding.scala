7ae4d23f3e49eafc4346d195ae85cab162f93a78
ebruchez <ebruchez@orbeon.com>
2012-10-15 10:05:01 -0700

    Minor: imports and indent

--- src/scala/org/orbeon/oxf/xforms/xbl/AbstractBinding.scala
+++ src/scala/org/orbeon/oxf/xforms/xbl/AbstractBinding.scala
@@ -36,14 +36,14 @@ case class AbstractBinding(
     def templateElement = Option(bindingElement.element(XBL_TEMPLATE_QNAME))
 
     val containerElementName =          // "div" by default
-        Option(bindingElement.attributeValue(XFormsConstants.XXBL_CONTAINER_QNAME)) getOrElse
+        Option(bindingElement.attributeValue(XXBL_CONTAINER_QNAME)) getOrElse
             "div"
 
     // Return a CSS-friendly name for this binding (no `|` or `:`)
     def cssName = qNameMatch.getQualifiedName.replace(':', '-')
 
     val allowedExternalEvents =
-        attSet(bindingElement, XFormsConstants.XXFORMS_EXTERNAL_EVENTS_ATTRIBUTE_NAME)
+        attSet(bindingElement, XXFORMS_EXTERNAL_EVENTS_ATTRIBUTE_NAME)
 
     private def transformQNameOption = templateElement flatMap
         (e ⇒ Option(Dom4jUtils.extractAttributeValueQName(e, XXBL_TRANSFORM_QNAME)))
@@ -69,16 +69,16 @@ case class AbstractBinding(
 
             // Repackage the result
             val generatedRootElement = generatedDocument.getRootElement.detach.asInstanceOf[Element]
-            generatedDocument.addElement(new QName("template", XFormsConstants.XBL_NAMESPACE, "xbl:template"))
+            generatedDocument.addElement(new QName("template", XBL_NAMESPACE, "xbl:template"))
             val newRoot = generatedDocument.getRootElement
-            newRoot.add(XFormsConstants.XBL_NAMESPACE)
+            newRoot.add(XBL_NAMESPACE)
             newRoot.add(generatedRootElement)
 
             generatedDocument
     }
 
     // XBL mode
-    val xblMode = attSet(bindingElement, XFormsConstants.XXBL_MODE_QNAME)
+    val xblMode = attSet(bindingElement, XXBL_MODE_QNAME)
     def modeAllXForms  = xblMode("xforms")
     def modeBinding    = modeAllXForms || xblMode("binding")
     def modeValue      = modeAllXForms || xblMode("value")
@@ -132,7 +132,12 @@ object AbstractBinding {
         Dom4jUtils.extractTextValueQName(namespaceMapping.mapping, elementQualifiedName(bindingElement), true)
 
     // Find a cached abstract binding or create and cache a new one
-    def findOrCreate(path: Option[String], bindingElement: Element, lastModified: Long, scripts: Seq[Element], namespaceMapping: NamespaceMapping) = {
+    def findOrCreate(
+            path: Option[String],
+            bindingElement: Element,
+            lastModified: Long,
+            scripts: Seq[Element],
+            namespaceMapping: NamespaceMapping): AbstractBinding = {
 
         val qName = qNameMatch(bindingElement, namespaceMapping)
 

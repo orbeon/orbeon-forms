323b642647a968026f2bff674aa00be309681034
ebruchez <ebruchez@orbeon.com>
2012-11-02 12:01:16 -0700

    Minor: missing package

--- test/src/org/orbeon/oxf/util/ScalaUtilsTest.scala
+++ test/src/org/orbeon/oxf/util/ScalaUtilsTest.scala
@@ -12,6 +12,7 @@
  *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
  *  package org.orbeon.oxf.util
  */
+package org.orbeon.oxf.util
 
 import org.junit.Test
 import org.scalatest.junit.AssertionsForJUnit

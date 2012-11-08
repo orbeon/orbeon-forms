orbeon/orbeon-forms

commit 38bf40ca809796c0cd7b784be32f9a904dfc87e0

Fix #151 "JavaScript error when an iteration is added to an empty repeat"
- generate top-level <span> elements around all character data, blank or non-blank
- *except* when repeating around table or list elements
- XFormsAnnotatorContentHandler places separator appearance on repeat in addition to switch/group
- static repeat and switch controls implement AppearanceTrait
- updated and added unit tests
- removed unneeded wrap-xforms-init-allcontrols.xpl

src/scala/org/orbeon/oxf/xforms/processor/handlers/xhtml/XFormsRepeatHandler.scala
@@ -23,6 +23,7 @@ import org.orbeon.oxf.xml.dom4j.ExtendedLocationData
 import org.xml.sax.Attributes
 import java.lang.{StringBuilder ⇒ JStringBuilder}
 import org.orbeon.oxf.xforms.processor.handlers.OutputInterceptor
+import org.orbeon.oxf.xforms.analysis.controls.RepeatControl
 
 /**
  * Handle xforms:repeat.
@@ -38,6 +39,7 @@ class XFormsRepeatHandler extends XFormsControlLifecyleHandler(true, true) { // 
         val isMustGenerateTemplate = (handlerContext.isTemplate || isTopLevelRepeat) && !handlerContext.isNoScript // don't generate templates in noscript mode as they won't be used
         val isMustGenerateDelimiters = !handlerContext.isNoScript
         val isMustGenerateBeginEndDelimiters = isMustGenerateDelimiters && !handlerContext.isFullUpdateTopLevelControl(effectiveId)
+        val namespacedId = XFormsUtils.namespaceId(containingDocument, effectiveId)
 
         val repeatControl = if (handlerContext.isTemplate) null else containingDocument.getObjectByEffectiveId(effectiveId).asInstanceOf[XFormsRepeatControl]
         val isConcreteControl = repeatControl != null
@@ -55,26 +57,23 @@ class XFormsRepeatHandler extends XFormsControlLifecyleHandler(true, true) { // 
         var outputDelimiter: (String, String) ⇒ Unit = null // initialized further below
 
         val outputInterceptor = if (!isMustGenerateDelimiters) null else new OutputInterceptor(savedOutput, spanQName, new OutputInterceptor.Listener {
-                def generateFirstDelimiter(outputInterceptor: OutputInterceptor) {
-
+                def generateFirstDelimiter(outputInterceptor: OutputInterceptor): Unit = {
                     if (isMustGenerateBeginEndDelimiters) {
 
                         def firstDelimiterClasses = "xforms-repeat-begin-end" + (if (userClasses.nonEmpty) (" " + userClasses) else "")
 
                         // Delimiter: begin repeat
-                        outputDelimiter(firstDelimiterClasses, "repeat-begin-" + XFormsUtils.namespaceId(containingDocument, effectiveId))
+                        outputDelimiter(firstDelimiterClasses, "repeat-begin-" + namespacedId)
 
                         // Delimiter: before repeat entries, unless disabled (disabled in case the repeat is completely empty)
                         if (mustOutputFirstDelimiter)
                             outputDelimiter("xforms-repeat-delimiter", null)
                     }
                 }
-            })
+            }, staticControl.asInstanceOf[RepeatControl].isAroundTableOrListElement)
 
         // Shortcut function to output the delimiter
-        outputDelimiter =
-            outputInterceptor.outputDelimiter(savedOutput, outputInterceptor.getDelimiterNamespaceURI,
-                outputInterceptor.getDelimiterPrefix, outputInterceptor.getDelimiterLocalName, _, _)
+        outputDelimiter = outputInterceptor.outputDelimiter(savedOutput, _, _)
 
         def appendClasses(sb: StringBuilder, classes: String) {
             if (classes.nonEmpty) {
@@ -94,6 +93,8 @@ class XFormsRepeatHandler extends XFormsControlLifecyleHandler(true, true) { // 
                     appendClasses(sb, "xforms-dnd-over")
             }
         }
+
+        var bodyRepeated = false
         
         def repeatBody(generateTemplate: Boolean, iteration: Int, repeatSelected: Boolean, classes: StringBuilder) {
 
@@ -115,6 +116,8 @@ class XFormsRepeatHandler extends XFormsControlLifecyleHandler(true, true) { // 
                     throw ValidationException.wrapException(e, new ExtendedLocationData(repeatControl.getLocationData, "unrolling xforms:repeat control", repeatControl.element))
             }
             handlerContext.popRepeatContext()
+
+            bodyRepeated = true
         }
 
         if (isMustGenerateDelimiters)
@@ -166,15 +169,7 @@ class XFormsRepeatHandler extends XFormsControlLifecyleHandler(true, true) { // 
         }
 
         // 3. Handle case where no delimiter was output by repeat iterations or template
-        if (isMustGenerateDelimiters && outputInterceptor.getDelimiterNamespaceURI == null) {
-
-            // This happens if:
-            // o there was no repeat iteration
-            // o AND no template was generated
-
-            assert(!isConcreteControl || repeatControl.getSize == 0)
-            assert(!isMustGenerateTemplate)
-
+        if (! bodyRepeated) {
             // What we do here is replay the body to /dev/null in order to find and output the begin delimiter (but not
             // the other delimiters)
             outputInterceptor.setForward(false)
@@ -187,7 +182,7 @@ class XFormsRepeatHandler extends XFormsControlLifecyleHandler(true, true) { // 
 
         // 4. Delimiter: end repeat
         if (isMustGenerateBeginEndDelimiters)
-            outputDelimiter("xforms-repeat-begin-end", "repeat-end-" + XFormsUtils.namespaceId(containingDocument, effectiveId))
+            outputDelimiter("xforms-repeat-begin-end", "repeat-end-" + namespacedId)
     }
 
     // Don't output any LHHA

src/java/org/orbeon/oxf/xforms/analysis/XFormsAnnotatorContentHandler.java
@@ -212,9 +212,18 @@ public class XFormsAnnotatorContentHandler extends XMLReceiverAdapter {
                         (existingAppearance != null ? existingAppearance + " " : "") + XFormsConstants.XXFORMS_SEPARATOR_APPEARANCE_QNAME.getQualifiedName());
                 startElement(true, uri, localname, qName, attributes);
             } else if (isXForms && "repeat".equals(localname)) {
-                // Leave element untouched (except for the id attribute)
+                // Add separator appearance
+                if (isClosestXHTMLAncestorTableContainer()) {
+                    final String existingAppearance = attributes.getValue("appearance");
+                    attributes = XMLUtils.addOrReplaceAttribute(attributes, "", "", XFormsConstants.APPEARANCE_QNAME.getName(),
+                            (existingAppearance != null ? existingAppearance + " " : "") + XFormsConstants.XXFORMS_SEPARATOR_APPEARANCE_QNAME.getQualifiedName());
+                }
+
+                // Start xforms:repeat
                 startElement(true, uri, localname, qName, attributes);
-                // Use xforms:repeat-iteration instead of xxforms:iteration so we don't have to deal with a new namespace
+
+                // Start xforms:repeat-iteration
+                // NOTE: Use xforms:repeat-iteration instead of xxforms:iteration so we don't have to deal with a new namespace
                 reusableAttributes.clear();
                 reusableAttributes.addAttribute("", "id", "id", ContentHandlerHelper.CDATA, xformsElementId + "~iteration");
                 final Attributes repeatIterationAttributes = getAttributesGatherNamespaces(qName, reusableAttributes, reusableStringArray, 0);

src/java/org/orbeon/oxf/xforms/processor/handlers/OutputInterceptor.java
@@ -13,7 +13,6 @@
  */
 package org.orbeon.oxf.xforms.processor.handlers;
 
-import org.apache.commons.lang3.StringUtils;
 import org.orbeon.oxf.pipeline.api.XMLReceiver;
 import org.orbeon.oxf.xml.*;
 import org.xml.sax.*;
@@ -24,9 +23,11 @@ import org.xml.sax.helpers.AttributesImpl;
  */
 public class OutputInterceptor extends ForwardingXMLReceiver {
 
-    private Listener beginDelimiterListener;
+    private final String spanQName;
+    private final Listener beginDelimiterListener;
+    private final boolean isAroundTableOrListElement;
 
-    private String spanQName;
+    private boolean gotElements = false;
 
     private String delimiterNamespaceURI;
     private String delimiterPrefix;
@@ -34,31 +35,51 @@ public class OutputInterceptor extends ForwardingXMLReceiver {
 
     private String addedClasses;
 
-    private boolean mustGenerateFirstDelimiters = true ;
+    private boolean mustGenerateFirstDelimiters = true;
 
-    private int level;
-    private boolean isCharacters;
+    private int level = 0;
     private StringBuilder currentCharacters = new StringBuilder();
 
     protected AttributesImpl reusableAttributes = new AttributesImpl();
-    public OutputInterceptor(XMLReceiver output, String spanQName, Listener beginDelimiterListener) {
+
+    public OutputInterceptor(XMLReceiver output, String spanQName, Listener beginDelimiterListener, boolean isAroundTableOrListElement) {
         super(output);
         this.spanQName = spanQName;
         this.beginDelimiterListener = beginDelimiterListener;
+        this.isAroundTableOrListElement = isAroundTableOrListElement;
+
+        // Default to <xhtml:span>
+        delimiterNamespaceURI = XMLConstants.XHTML_NAMESPACE_URI;
+        delimiterPrefix       = XMLUtils.prefixFromQName(spanQName);
+        delimiterLocalName    = XMLUtils.localNameFromQName(spanQName);
+    }
+
+    public boolean isGotElements() {
+        return gotElements;
     }
 
     public void startElement(String uri, String localname, String qName, Attributes attributes) throws SAXException {
 
         level++;
-        final boolean topLevel = level == 1;
+        final boolean topLevelElement = level == 1;
 
-        flushCharacters(false, topLevel);
+        if (! gotElements) {
+            // Override default as we just go an element
 
-        // The first element received determines the type of separator
-        checkDelimiters(uri, qName, topLevel);
+            assert topLevelElement;
+
+            delimiterNamespaceURI = uri;
+            delimiterPrefix       = XMLUtils.prefixFromQName(qName);
+            delimiterLocalName    = XMLUtils.localNameFromQName(qName);
+
+            gotElements = true;
+        }
+
+        flushCharacters(false, topLevelElement);
+        generateFirstDelimitersIfNeeded();
 
         // Add or update classes on element if needed
-        super.startElement(uri, localname, qName, topLevel ? getAttributesWithClass(attributes) : attributes);
+        super.startElement(uri, localname, qName, topLevelElement ? getAttributesWithClass(attributes) : attributes);
     }
 
     public void endElement(String uri, String localname, String qName) throws SAXException {
@@ -71,45 +92,39 @@ public class OutputInterceptor extends ForwardingXMLReceiver {
 
     public void characters(char[] chars, int start, int length) {
         currentCharacters.append(chars, start, length);
-        isCharacters = true;
     }
 
-    public void flushCharacters(boolean finalFlush, boolean topLevel) throws SAXException {
+    public void flushCharacters(boolean finalFlush, boolean topLevelCharacters) throws SAXException {
 
-        if (currentCharacters.length() > 0) {
+        final String currentString = currentCharacters.toString();
 
-            final String currentString = currentCharacters.toString();
+        if (topLevelCharacters && ! isAroundTableOrListElement) {
+            // We handle top-level characters specially and wrap them in a span so we can hide them
+            generateTopLevelSpanWithCharacters(currentCharacters.toString());
+        } else {
+            // Just output characters as is in deeper levels, or when around at table or list element
             final char[] chars = currentString.toCharArray();
-            if (StringUtils.isBlank(currentString) || !topLevel) {
-                // Just output whitespace as is
-                super.characters(chars, 0, chars.length);
-            } else {
-
-                // The first element received determines the type of separator
-                checkDelimiters(XMLConstants.XHTML_NAMESPACE_URI, spanQName, topLevel);
-
-                // Wrap any other text within an xhtml:span
-                super.startElement(XMLConstants.XHTML_NAMESPACE_URI, "span", spanQName, getAttributesWithClass(XMLUtils.EMPTY_ATTRIBUTES));
-                super.characters(chars, 0, chars.length);
-                super.endElement(XMLConstants.XHTML_NAMESPACE_URI, "span", spanQName);
-            }
-
-            isCharacters = false;
-            currentCharacters.setLength(0);
+            super.characters(chars, 0, chars.length);
         }
+
+        currentCharacters.setLength(0);
 
         if (finalFlush)
-            checkDelimiters(XMLConstants.XHTML_NAMESPACE_URI, spanQName, topLevel);
+            generateFirstDelimitersIfNeeded();
     }
 
-    private void checkDelimiters(String uri, String qName, boolean topLevel) throws SAXException {
+    private void generateTopLevelSpanWithCharacters(String characters) throws SAXException {
+        // The first element received determines the type of separator
+        generateFirstDelimitersIfNeeded();
 
-        if (topLevel && delimiterNamespaceURI == null) {
-            delimiterNamespaceURI = uri;
-            delimiterPrefix = XMLUtils.prefixFromQName(qName);
-            delimiterLocalName = XMLUtils.localNameFromQName(qName);
-        }
+        // Wrap any other text within an xhtml:span
+        super.startElement(XMLConstants.XHTML_NAMESPACE_URI, "span", spanQName, getAttributesWithClass(XMLUtils.EMPTY_ATTRIBUTES));
+        final char[] chars = characters.toCharArray();
+        super.characters(chars, 0, chars.length);
+        super.endElement(XMLConstants.XHTML_NAMESPACE_URI, "span", spanQName);
+    }
 
+    private void generateFirstDelimitersIfNeeded() throws SAXException {
         if (mustGenerateFirstDelimiters) {
             // Generate first delimiter
             beginDelimiterListener.generateFirstDelimiter(this);
@@ -134,18 +149,6 @@ public class OutputInterceptor extends ForwardingXMLReceiver {
             return originalAttributes;
     }
 
-    public String getDelimiterNamespaceURI() {
-        return delimiterNamespaceURI;
-    }
-
-    public String getDelimiterPrefix() {
-        return delimiterPrefix;
-    }
-
-    public String getDelimiterLocalName() {
-        return delimiterLocalName;
-    }
-
     public boolean isMustGenerateFirstDelimiters() {
         return mustGenerateFirstDelimiters;
     }
@@ -154,7 +157,7 @@ public class OutputInterceptor extends ForwardingXMLReceiver {
         this.addedClasses = addedClasses;
     }
 
-    public void outputDelimiter(ContentHandler contentHandler, String delimiterNamespaceURI, String delimiterPrefix, String delimiterLocalName, String classes, String id) throws SAXException {
+    public void outputDelimiter(ContentHandler contentHandler, String classes, String id) throws SAXException {
 
         reusableAttributes.clear();
         if (id != null)

src/java/org/orbeon/oxf/xforms/processor/handlers/xhtml/XFormsCaseHandler.java
@@ -13,7 +13,9 @@
  */
 package org.orbeon.oxf.xforms.processor.handlers.xhtml;
 
+import org.orbeon.oxf.xforms.XFormsConstants;
 import org.orbeon.oxf.xforms.XFormsUtils;
+import org.orbeon.oxf.xforms.analysis.controls.AppearanceTrait;
 import org.orbeon.oxf.xforms.control.XFormsControl;
 import org.orbeon.oxf.xforms.control.controls.XFormsCaseControl;
 import org.orbeon.oxf.xforms.processor.handlers.OutputInterceptor;
@@ -90,12 +92,11 @@ public class XFormsCaseHandler extends XFormsControlLifecyleHandler {
                 public void generateFirstDelimiter(OutputInterceptor outputInterceptor) throws SAXException {
                     if (isMustGenerateBeginEndDelimiters) {
                         // Delimiter: begin case
-                        outputInterceptor.outputDelimiter(currentSavedOutput, outputInterceptor.getDelimiterNamespaceURI(),
-                                outputInterceptor.getDelimiterPrefix(), outputInterceptor.getDelimiterLocalName(), firstDelimiterClasses,
+                        outputInterceptor.outputDelimiter(currentSavedOutput, firstDelimiterClasses,
                                 "xforms-case-begin-" + XFormsUtils.namespaceId(containingDocument, effectiveId));
                     }
                 }
-            });
+            }, ((AppearanceTrait) staticControl().parent().get()).jAppearances().contains(XFormsConstants.XXFORMS_SEPARATOR_APPEARANCE_QNAME));
 
             final String controlClasses; {
                 final StringBuilder classes = new StringBuilder(isVisible ? "xforms-case-selected" : "xforms-case-deselected");
@@ -131,19 +132,13 @@ public class XFormsCaseHandler extends XFormsControlLifecyleHandler {
 
             final boolean isMustGenerateBeginEndDelimiters = !handlerContext.isFullUpdateTopLevelControl(effectiveId);
             if (isMustGenerateBeginEndDelimiters) {
-                if (currentOutputInterceptor.getDelimiterNamespaceURI() != null) {
-                    // Output end delimiter
-                    currentOutputInterceptor.outputDelimiter(currentSavedOutput, currentOutputInterceptor.getDelimiterNamespaceURI(),
-                        currentOutputInterceptor.getDelimiterPrefix(), currentOutputInterceptor.getDelimiterLocalName(), "xforms-case-begin-end",
-                            "xforms-case-end-" + XFormsUtils.namespaceId(containingDocument, effectiveId));
-                } else {
-                    // Output start and end delimiter using xhtml:span
-                    final String xhtmlPrefix = handlerContext.findXHTMLPrefix();
-                    currentOutputInterceptor.outputDelimiter(currentSavedOutput, XMLConstants.XHTML_NAMESPACE_URI,
-                        xhtmlPrefix, "span", "xforms-case-begin-end", "xforms-case-begin-" + XFormsUtils.namespaceId(containingDocument, effectiveId));
-                    currentOutputInterceptor.outputDelimiter(currentSavedOutput, XMLConstants.XHTML_NAMESPACE_URI,
-                        xhtmlPrefix, "span", "xforms-case-begin-end", "xforms-case-end-" + XFormsUtils.namespaceId(containingDocument, effectiveId));
+                final String namespacedId = XFormsUtils.namespaceId(containingDocument, effectiveId);
+                if (! currentOutputInterceptor.isGotElements()) {
+                    // Output start delimiter using xhtml:span
+                    currentOutputInterceptor.outputDelimiter(currentSavedOutput, "xforms-case-begin-end", "xforms-case-begin-" + namespacedId);
                 }
+                // Output end delimiter
+                currentOutputInterceptor.outputDelimiter(currentSavedOutput, "xforms-case-begin-end", "xforms-case-end-" + namespacedId);
             }
         } else if (!isVisible) {
             // Case not visible, restore output

src/java/org/orbeon/oxf/xforms/processor/handlers/xhtml/XFormsControlLifecyleHandler.java
@@ -16,6 +16,7 @@ package org.orbeon.oxf.xforms.processor.handlers.xhtml;
 import org.apache.commons.lang3.StringUtils;
 import org.orbeon.oxf.xforms.StaticStateGlobalOps;
 import org.orbeon.oxf.xforms.XFormsConstants;
+import org.orbeon.oxf.xforms.analysis.ElementAnalysis;
 import org.orbeon.oxf.xforms.analysis.controls.LHHAAnalysis;
 import org.orbeon.oxf.xforms.control.LHHASupport;
 import org.orbeon.oxf.xforms.control.XFormsControl;
@@ -203,6 +204,11 @@ public abstract class XFormsControlLifecyleHandler extends XFormsBaseHandlerXHTM
         final StaticStateGlobalOps globalOps = containingDocument.getStaticOps();
         final LHHAAnalysis analysis = globalOps.getLabel(getPrefixedId());
         return analysis != null && analysis.isLocal();
+    }
+
+    protected ElementAnalysis staticControl() {
+        final StaticStateGlobalOps globalOps = containingDocument.getStaticOps();
+        return globalOps.getControlAnalysis(getPrefixedId());
     }
 
     private boolean hasLocalHint() {

src/java/org/orbeon/oxf/xforms/processor/handlers/xhtml/XFormsGroupSeparatorHandler.java
@@ -78,12 +78,11 @@ public class XFormsGroupSeparatorHandler extends XFormsGroupHandler {
                 public void generateFirstDelimiter(OutputInterceptor outputInterceptor) throws SAXException {
                     // Delimiter: begin group
                     if (isMustGenerateBeginEndDelimiters) {
-                        outputInterceptor.outputDelimiter(currentSavedOutput, outputInterceptor.getDelimiterNamespaceURI(),
-                                outputInterceptor.getDelimiterPrefix(), outputInterceptor.getDelimiterLocalName(), firstDelimiterClasses,
+                        outputInterceptor.outputDelimiter(currentSavedOutput, firstDelimiterClasses,
                                 "group-begin-" + XFormsUtils.namespaceId(containingDocument, effectiveId));
                     }
                 }
-            });
+            }, true);
             
             controller.setOutput(new DeferredXMLReceiverImpl(outputInterceptor));
 
@@ -110,8 +109,7 @@ public class XFormsGroupSeparatorHandler extends XFormsGroupHandler {
 
             final boolean isMustGenerateBeginEndDelimiters = !handlerContext.isFullUpdateTopLevelControl(effectiveId);
             if (isMustGenerateBeginEndDelimiters) {
-                outputInterceptor.outputDelimiter(currentSavedOutput, outputInterceptor.getDelimiterNamespaceURI(),
-                        outputInterceptor.getDelimiterPrefix(), outputInterceptor.getDelimiterLocalName(), "xforms-group-begin-end",
+                outputInterceptor.outputDelimiter(currentSavedOutput, "xforms-group-begin-end",
                         "group-end-" + XFormsUtils.namespaceId(containingDocument, effectiveId));
             }
         } else if (isNonRelevant(control)) {

src/resources-packaged/ops/unit-tests/xforms-server/tests-xforms-misc.xml
@@ -740,8 +740,11 @@
                          </xhtml:span>
                          <xhtml:span id="repeat-begin-xf-1" class="xforms-repeat-begin-end"/>
                          <xhtml:span class="xforms-repeat-delimiter"/>
+                         <xhtml:span class="xforms-repeat-selected-item-1"/>
                          <xhtml:span class="xforms-repeat-delimiter"/>
+                         <xhtml:span/>
                          <xhtml:span class="xforms-repeat-delimiter"/>
+                         <xhtml:span class="xforms-repeat-template"/>
                          <xhtml:span id="repeat-end-xf-1" class="xforms-repeat-begin-end"/>
                      </xhtml:form>
                  </xhtml:body>

commit 315b302cbb7484c52a6cc9d44d4678a80014ea4e

Fix exception in noscript mode caused by 38bf40ca809796c0cd7b784be32f9a904dfc87e0

src/scala/org/orbeon/oxf/xforms/processor/handlers/xhtml/XFormsRepeatHandler.scala
@@ -169,7 +169,7 @@ class XFormsRepeatHandler extends XFormsControlLifecyleHandler(true, true) { // 
         }
 
         // 3. Handle case where no delimiter was output by repeat iterations or template
-        if (! bodyRepeated) {
+        if (isMustGenerateTemplate && ! bodyRepeated) {
             // What we do here is replay the body to /dev/null in order to find and output the begin delimiter (but not
             // the other delimiters)
             outputInterceptor.setForward(false)
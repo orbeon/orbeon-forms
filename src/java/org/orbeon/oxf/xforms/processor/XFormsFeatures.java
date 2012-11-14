0469902abcac7489b33eef2f0ef60532eeae7fbf
avernet <avernet@orbeon.com>
2012-11-14 10:49:40 -0800

    Upgrading jQuery from 1.7.1 to 1.8.2

--- src/java/org/orbeon/oxf/xforms/processor/XFormsFeatures.java
+++ src/java/org/orbeon/oxf/xforms/processor/XFormsFeatures.java
@@ -62,7 +62,7 @@ public class XFormsFeatures {
 
     private static final ResourceConfig[] scripts = {
             // jQuery
-            new ResourceConfig("/ops/jquery/jquery-1.7.1.js", "/ops/jquery/jquery-1.7.1.min.js"),
+            new ResourceConfig("/ops/jquery/jquery-1.8.2.js", "/ops/jquery/jquery-1.8.2.min.js"),
             // Yahoo UI Library
             new ResourceConfig("/ops/yui/yahoo/yahoo.js", "/ops/yui/yahoo/yahoo-min.js"),
             new ResourceConfig("/ops/yui/event/event.js", "/ops/yui/event/event-min.js"),

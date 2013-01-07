2d5a00ea2acc1d78250d1d8e1b8435894bacb370
avernet <avernet@orbeon.com>
2013-01-07 11:46:35 -0800

    IE do our own parsing to preserve whitespace, fixes #682

--- src/resources-packaged/ops/javascript/orbeon/xforms/server/AjaxServer.js
+++ src/resources-packaged/ops/javascript/orbeon/xforms/server/AjaxServer.js
@@ -625,10 +625,16 @@
                 ORBEON.xforms.Init.document();
             }
         } else {
-            if (o.responseText &&
-                    (!responseXML || (responseXML && responseXML.documentElement && responseXML.documentElement.tagName.toLowerCase() == "html"))) {
-                // The XML document does not come in o.responseXML: parse o.responseText.
-                // This happens in particular when we get a response after a background upload.
+            if (o.responseText
+                    && (
+                        // Don't rely on IE's XML parsing, as it doesn't preserve white spaces
+                        $.browser.msie
+                        // Handle cases where somehoe, the XML document does not come in o.responseXML: parse o.responseText.
+                        // This happens in particular when we get a response after a background upload.
+                        || !responseXML
+                        // Suspecting this wa put there as the browser doesn't necessarily parse the response if it is HTML (e.g. an error)
+                        || (responseXML && responseXML.documentElement && responseXML.documentElement.tagName.toLowerCase() == "html")
+                    )) {
                 var xmlString = o.responseText.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&");
                 responseXML = ORBEON.util.Dom.stringToDom(xmlString);
             }

orbeon/orbeon-forms

commit 8452756d29ab0f4724aa1c90b90dfc215a0a9a50

Fix #857 "Exception when renaming section"
- fix xforms-insert event when root element replaced
- an incorrect event here caused the id index to get out of sync

src/java/org/orbeon/oxf/xforms/action/actions/XFormsInsertAction.java
@@ -431,8 +431,23 @@ public class XFormsInsertAction extends XFormsAction {
         // "4. If the insert is successful, the event xforms-insert is dispatched."
         // XFormsInstance handles index and repeat items updates 
         if (doDispatch && modifiedInstance != null) {
+
+            // Adjust insert location node and before/after/into in case the root element was replaced
+            final NodeInfo adjustedInsertLocationNodeInfo;
+            final String adjustedBeforeAfterInto;
+
+            final NodeInfo parent = insertedNodeInfos.get(0).getParent(); // we know there is at least one because modifiedInstance != null
+            if (parent != null && parent.equals(parent.getDocumentRoot())) {
+                // Node was inserted under document node
+                adjustedInsertLocationNodeInfo = parent.getDocumentRoot();
+                adjustedBeforeAfterInto = "into";
+            } else {
+                adjustedInsertLocationNodeInfo = insertLocationNodeInfo;
+                adjustedBeforeAfterInto = beforeAfterInto;
+            }
+
             Dispatch.dispatchEvent(
-                    new XFormsInsertEvent(modifiedInstance, insertedNodeInfos, originItems, insertLocationNodeInfo, beforeAfterInto));
+                    new XFormsInsertEvent(modifiedInstance, insertedNodeInfos, originItems, adjustedInsertLocationNodeInfo, adjustedBeforeAfterInto));
         }
 
         return insertedNodeInfos;

commit 9e1eafab91f6176301c39641ab776ec40d4641e5

Fix exception when inserting namespace node
Regression from 8452756d29ab0f4724aa1c90b90dfc215a0a9a50

src/java/org/orbeon/oxf/xforms/action/actions/XFormsInsertAction.java
@@ -436,7 +436,7 @@ public class XFormsInsertAction extends XFormsAction {
             final NodeInfo adjustedInsertLocationNodeInfo;
             final String adjustedBeforeAfterInto;
 
-            final NodeInfo parent = insertedNodeInfos.get(0).getParent();
+            final NodeInfo parent = insertedNodeInfos.get(0).getNodeKind() == org.w3c.dom.Node.ELEMENT_NODE ? insertedNodeInfos.get(0).getParent() : null;
             if (parent != null && parent.equals(parent.getDocumentRoot())) {
                 // Node was inserted under document node
                 adjustedInsertLocationNodeInfo = parent.getDocumentRoot();
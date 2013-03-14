3d80ed56ba6db8bfb4fae546b55e6b94e6db02e3
ebruchez <ebruchez@orbeon.com>
2013-03-14 12:17:08 -0700

    Server-side for #870 "Ability to enter HTML labels in the builder"

--- src/main/scala/org/orbeon/oxf/fb/ControlOps.scala
+++ src/main/scala/org/orbeon/oxf/fb/ControlOps.scala
@@ -627,4 +627,16 @@ object ControlOps {
 
     def buildControlAbsoluteId(inDoc: NodeInfo, staticId: String) =
         buildControlEffectiveId(inDoc, staticId) map effectiveIdToAbsoluteId
+
+    // For a given control and LHHA type, set the mediatype on the LHHA to be HTML or plain text
+    def setControlLHHAMediatype(inDoc: NodeInfo, controlName: String, lhha: String, isHTML: Boolean): Unit =
+        findControlByName(inDoc, controlName).toList child lhha foreach { lhhaElement ⇒
+            val isCurrentlyHTML = (lhhaElement attValue "mediatype") == "text/html"
+            if (isHTML != isCurrentlyHTML) {
+                if (! isHTML)
+                    insert(into = lhhaElement, origin = attributeInfo("mediatype", "text/html"))
+                else
+                    delete(lhhaElement \@ "mediatype")
+            }
+        }
 }

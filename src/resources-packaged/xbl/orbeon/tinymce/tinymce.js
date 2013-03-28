8ccb4009f03d7bee815e88095e1005369af8b9ef
tjoris <tars.joris@inventivedesigners.com>
2013-03-28 16:25:52 +0100

    Fix basepath of Tiny MCE.

--- src/resources-packaged/xbl/orbeon/tinymce/tinymce.js
+++ src/resources-packaged/xbl/orbeon/tinymce/tinymce.js
@@ -35,7 +35,7 @@
             var baseURLa = YAHOO.util.Dom.getElementsByClassName('tinymce-base-url', null, this.container)[0];
             // Remove the magic number and extension at the end of the URL. The magic number was added to allow for
             // URL post-processing for portlets. The extension is added so that the version number is added to the URL.
-            var baseURL = baseURLa.href.substr(0, baseURLa.href.length - '1b713b2e6d7fd45753f4b8a6270b776e.js'.length);
+            var baseURL = baseURLa.href.substr(0, baseURLa.href.length - '/1b713b2e6d7fd45753f4b8a6270b776e.js'.length);
             tinymce.baseURL = baseURL;
 
             // Create TinyMCE editor instance

8ecec12c972784e155d8304e62f384957750486e
ebruchez <ebruchez@orbeon.com>
2013-04-17 10:38:48 -0700

    Add contributed numberer for Swedish
    Also adapted the unit tests from Groovy to Scala.

---
+++ src/main/java/org/orbeon/saxon/number/Numberer_sv.java
@@ -0,0 +1,303 @@
+package org.orbeon.saxon.number;
+
+/**
+ * @author Karel Goossens, BTR-Services, Belgium
+ * @author Hakan Soderstrom
+ *         <p/>
+ *         Numberer class for the Swedish language.
+ * @see <a href="http://en.wikipedia.org/wiki/Swedish_grammar">http://en.wikipedia.org/wiki/Swedish_grammar</a>
+ * @see <a href="http://www2.hhs.se/isa/swedish/chap4.htm">http://www2.hhs.se/isa/swedish/chap4.htm</a>
+ */
+public class Numberer_sv extends AbstractNumberer {
+
+    private static final long serialVersionUID = 1L;
+
+    private static String[] swedishOrdinalUnits = {
+            "", "f\u00f6rsta", "andra", "tredje", "fj\u00e4rde", "femte",
+            "sj\u00e4tte", "sjunde", "\u00e5ttonde", "nionde",
+            "tionde", "elfte", "tolfte", "trettonde", "fjortonde", "femtonde",
+            "sextonde", "sjuttonde", "artonde", "nittonde"};
+
+    private static String[] swedishOrdinalTens = {
+            "", "tionde", "tjugonde", "trettionde", "fyrtionde", "femtionde",
+            "sextionde", "sjuttionde", "\u00e5ttionde", "nittionde"};
+
+    private static String[] swedishUnits = {
+            "", "ett", "tv\u00e5", "tre", "fyra", "fem", "sex", "sju", "\u00e5tta", "nio", "tio",
+            "elva", "tolv", "tretton", "fjorton", "femton", "sexton", "sjutton", "arton", "nitton"};
+
+    private static String[] swedishTens = {
+            "", "tio", "tjugo", "trettio", "fyrtio", "femtio",
+            "sextio", "sjuttio", "\u00e5ttio", "nittio"};
+
+    private static long ONE_HUNDRED = 100L;
+    private static long ONE_THOUSAND = 1000L;
+    private static long ONE_MILLION = 1000000L;
+    private static long ONE_BILLION = 1000000000L; // Swedish definition 10^9
+
+    /**
+     * Show an ordinal number as swedish words in a requested case (for example, Twentyfirst)
+     *
+     * @param ordinalParam not used.
+     * @param number       the number to be converted to a word.
+     * @param wordCase     UPPER_CASE or LOWER_CASE.
+     * @return String representing the number in words.
+     */
+    public String toOrdinalWords(String ordinalParam, long number, int wordCase) {
+        String s = (number == 0) ? "nollte" : toOrdinalWordsPriv(number);
+
+        if (wordCase == UPPER_CASE) {
+            return s.toUpperCase();
+        } else if (wordCase == LOWER_CASE) {
+            return s.toLowerCase();
+        } else {
+            return s;
+        }
+    }
+
+    private String toOrdinalWordsPriv(long number) {
+        String s = "";
+
+        if (number >= ONE_BILLION) {
+            s = toBillionOrdinalWordsPriv(number) + toOrdinalWordsPriv(number % ONE_BILLION);
+        } else if (number >= ONE_MILLION) {
+            s = toMillionOrdinalWordsPriv(number) + toOrdinalWordsPriv(number % ONE_MILLION);
+        } else if (number >= ONE_THOUSAND) {
+            s = toThousandOrdinalWordsPriv(number) + toOrdinalWordsPriv(number % ONE_THOUSAND);
+        } else if (number >= ONE_HUNDRED) {
+            s = toHundredOrdinalWordsPriv(number) + toOrdinalWordsPriv(number % ONE_HUNDRED);
+        } else {
+            if (number < 20) {
+                s = swedishOrdinalUnits[(int) number];
+            } else {
+                int rem = (int) (number % 10);
+                if (rem == 0) {
+                    s = swedishOrdinalTens[(int) number / 10];
+                } else {
+                    s = swedishTens[(int) number / 10] + swedishOrdinalUnits[rem];
+                }
+            }
+        }
+
+        return s;
+    }
+
+    private String toBillionOrdinalWordsPriv(long number) {
+        String result = "";
+        long units = number / ONE_BILLION;
+        long rem = number % ONE_BILLION;
+        if (units > 0) {
+            if (rem == 0) {
+                result = (units == 1) ? "miljardte" : (toWordsPriv(units) + " miljardte");
+            } else {
+                if (units == 1) {
+                    result = "en miljard ";
+                } else {
+                    result = toWordsPriv(units) + " miljarder ";
+                    ;
+                }
+            }
+        }
+        return result;
+    }
+
+    private String toMillionOrdinalWordsPriv(long number) {
+        String result = "";
+        long units = number / ONE_MILLION;
+        long rem = number % ONE_MILLION;
+        if (units > 0) {
+            if (rem == 0) {
+                result = (units == 1) ? "miljonte" : (toWordsPriv(units) + " miljonte");
+            } else {
+                if (units == 1) {
+                    result = "en miljon ";
+                } else {
+                    result = toWordsPriv(units) + " miljoner ";
+                }
+            }
+        }
+        return result;
+    }
+
+    private String toThousandOrdinalWordsPriv(long number) {
+        String result = "";
+        long units = number / ONE_THOUSAND;
+        long rem = number % ONE_THOUSAND;
+        if (units > 0) {
+            if (rem == 0) {
+                result = (units == 1) ? "tusende" : (toWordsPriv(units) + "tusende");
+            } else {
+                if (units == 1) {
+                    result = "ettusen ";
+                } else {
+                    result = toWordsPriv(units) + "tusen ";
+                }
+            }
+        }
+        return result;
+    }
+
+    private String toHundredOrdinalWordsPriv(long number) {
+        String result = "";
+        long units = number / ONE_HUNDRED;
+        long rem = number % ONE_HUNDRED;
+        if (units > 0) {
+            if (rem == 0) {
+                result = toWordsPriv(units) + "hundrade";
+            } else {
+                if (units == 1) {
+                    result = "etthundra";
+                } else {
+                    result = toWordsPriv(units) + "hundra";
+                }
+            }
+        }
+        return result;
+    }
+
+    public String toWords(long number) {
+        return (number == 0) ? "noll" : toWordsPriv(number);
+    }
+
+    private String toWordsPriv(long number) {
+        String result = "";
+        if (number >= ONE_BILLION) {
+            result = toBillionWordsPriv(number) + toWordsPriv(number % ONE_BILLION);
+        } else if (number >= ONE_MILLION) {
+            result = toMillionWordsPriv(number) + toWordsPriv(number % ONE_MILLION);
+        } else if (number >= ONE_THOUSAND) {
+            result = toThousandWordsPriv(number) + toWordsPriv(number % ONE_THOUSAND);
+        } else if (number >= ONE_HUNDRED) {
+            result = toHundredWordsPriv(number) + toWordsPriv(number % ONE_HUNDRED);
+        } else {
+            if (number < 20) return swedishUnits[(int) number];
+            int rem = (int) (number % 10);
+            return swedishTens[(int) number / 10] + swedishUnits[rem];
+        }
+
+        return result;
+    }
+
+    private String toBillionWordsPriv(long number) {
+        String result = "";
+        long units = number / ONE_BILLION;
+        if (units > 0) {
+            result = (units == 1) ? "en miljard" : toWordsPriv(units) + " miljarder";
+            if (number % ONE_BILLION > 0) result += ' ';
+        }
+        return result;
+    }
+
+    private String toMillionWordsPriv(long number) {
+        String result = "";
+        long units = number / ONE_MILLION;
+        if (units > 0) {
+            result = (units == 1) ? "en miljon" : toWordsPriv(units) + " miljoner";
+            if (number % ONE_MILLION > 0) result += ' ';
+        }
+        return result;
+    }
+
+    private String toThousandWordsPriv(long number) {
+        String result = "";
+        long units = number / ONE_THOUSAND;
+        if (units > 0) {
+            result = (units == 1) ? "ettusen" : toWordsPriv(units) + "tusen";
+            if (number % ONE_THOUSAND > 0) result += ' ';
+        }
+        return result;
+    }
+
+    private String toHundredWordsPriv(long number) {
+        String result = "";
+        long units = number / ONE_HUNDRED;
+        if (units > 0) {
+            result = (units == 1) ? "etthundra" : toWordsPriv(units) + "hundra";
+        }
+        return result;
+    }
+
+    public String toWords(long number, int wordCase) {
+        String s;
+        if (number == 0) {
+            s = "noll";
+        } else {
+            s = toWords(number);
+        }
+        if (wordCase == UPPER_CASE) {
+            return s.toUpperCase();
+        } else if (wordCase == LOWER_CASE) {
+            return s.toLowerCase();
+        } else {
+            return s;
+        }
+    }
+
+
+    private static String[] swedishMonths = {
+            "januari", "februari", "mars", "april", "maj", "juni",
+            "juli", "augusti", "september", "oktober", "november", "december"
+    };
+
+    /**
+     * Get a month name or abbreviation
+     *
+     * @param month    The month number (1=January, 12=December)
+     * @param minWidth The minimum number of characters
+     * @param maxWidth The maximum number of characters
+     */
+    //@Override
+    public String monthName(int month, int minWidth, int maxWidth) {
+        String name = swedishMonths[month - 1];
+        if (maxWidth < 3) {
+            maxWidth = 3;
+        }
+        if (name.length() > maxWidth) {
+            name = name.substring(0, maxWidth);
+        }
+        while (name.length() < minWidth) {
+            name = name + ' ';
+        }
+        return name;
+    }
+
+    /**
+     * Get a day name or abbreviation
+     *
+     * @param day      The day of the week (1=Monday, 7=Sunday)
+     * @param minWidth The minimum number of characters
+     * @param maxWidth The maximum number of characters
+     */
+    public String dayName(int day, int minWidth, int maxWidth) {
+        String name = swedishDays[day - 1];
+        if (maxWidth < 2) {
+            maxWidth = 2;
+        }
+        if (name.length() > maxWidth) {
+            name = swedishDayAbbreviations[day - 1];
+            if (name.length() > maxWidth) {
+                name = name.substring(0, maxWidth);
+            }
+        }
+        while (name.length() < minWidth) {
+            name = name + ' ';
+        }
+        if (minWidth == 1 && maxWidth == 2) {
+            // special case
+            name = name.substring(0, minUniqueDayLength[day - 1]);
+        }
+        return name;
+    }
+
+    private static String[] swedishDays = {
+            "m\u00e5ndag", "tisdag", "onsdag", "torsdag", "fredag", "l\u00f6rdag", "s\u00f6ndag"
+    };
+
+    private static String[] swedishDayAbbreviations = {
+            "m\u00e5nd", "tisd", "onsd", "tors", "fred", "l\u00f6rd", "s\u00f6nd"
+    };
+
+    private static int[] minUniqueDayLength = {
+            1, 2, 1, 2, 1, 1, 1
+    };
+}
---
+++ src/test/scala/org/orbeon/saxon/Numberer_svTest.scala
@@ -0,0 +1,268 @@
+package org.orbeon.saxon
+
+import org.orbeon.saxon.number.{AbstractNumberer, Numberer_sv}
+import org.junit.Test
+import org.scalatest.junit.AssertionsForJUnit
+
+/**
+ * @author Karel Goossens, BTR-Services, Belgium
+ * @author Hakan Soderstrom
+ * @author Erik Bruchez
+ *         <p/>
+ *         Numberer class for the Swedish language.
+ * @see <a href="http://en.wikipedia.org/wiki/Swedish_grammar">http://en.wikipedia.org/wiki/Swedish_grammar</a>
+ * @see <a href="http://www2.hhs.se/isa/swedish/chap4.htm">http://www2.hhs.se/isa/swedish/chap4.htm</a>
+ */
+class Numberer_svTest extends AssertionsForJUnit {
+
+    val N = new Numberer_sv()
+    val ONE_MILLION = 1000000L
+    val ONE_BILLION = 1000000000L // Swedish definition 10^9
+
+    private def check(number: Long, words: String, ordinalWords: String): Unit = {
+        assert(words === N.toWords(number))
+        assert(ordinalWords === N.toOrdinalWords("", number, AbstractNumberer.LOWER_CASE))
+    }
+
+    @Test def testLowNumbers(): Unit = {
+        check(0L, "noll", "nollte")
+        check(1L, "ett", "första")
+        check(2L, "två", "andra")
+        check(3L, "tre", "tredje")
+        check(4L, "fyra", "fjärde")
+        check(5L, "fem", "femte")
+        check(6L, "sex", "sjätte")
+        check(7L, "sju", "sjunde")
+        check(8L, "åtta", "åttonde")
+        check(9L, "nio", "nionde")
+        check(10L, "tio", "tionde")
+        check(11L, "elva", "elfte")
+        check(12L, "tolv", "tolfte")
+        check(13L, "tretton", "trettonde")
+        check(14L, "fjorton", "fjortonde")
+        check(15L, "femton", "femtonde")
+        check(16L, "sexton", "sextonde")
+        check(17L, "sjutton", "sjuttonde")
+        check(18L, "arton", "artonde")
+        check(19L, "nitton", "nittonde")
+        check(20L, "tjugo", "tjugonde")
+    }
+
+    @Test def testEvenTens(): Unit = {
+        check(10L, "tio", "tionde")
+        check(20L, "tjugo", "tjugonde")
+        check(30L, "trettio", "trettionde")
+        check(40L, "fyrtio", "fyrtionde")
+        check(50L, "femtio", "femtionde")
+        check(60L, "sextio", "sextionde")
+        check(70L, "sjuttio", "sjuttionde")
+        check(80L, "åttio", "åttionde")
+        check(90L, "nittio", "nittionde")
+        check(100L, "etthundra", "etthundrade")
+    }
+
+    @Test def testEvenPowersOfTen(): Unit = {
+        check(1000L, "ettusen", "tusende")
+        check(10000L, "tiotusen", "tiotusende")
+        check(100000L, "etthundratusen", "etthundratusende")
+        check(ONE_MILLION, "en miljon", "miljonte")
+        check(10 * ONE_MILLION, "tio miljoner", "tio miljonte")
+        check(100 * ONE_MILLION, "etthundra miljoner", "etthundra miljonte")
+        check(ONE_BILLION, "en miljard", "miljardte")
+        check(10 * ONE_BILLION, "tio miljarder", "tio miljardte")
+        check(100 * ONE_BILLION, "etthundra miljarder", "etthundra miljardte")
+    }
+
+    @Test def testPowersOfTenMultiples(): Unit = {
+        check(2000L, "tvåtusen", "tvåtusende")
+        check(20000L, "tjugotusen", "tjugotusende")
+        check(200000L, "tvåhundratusen", "tvåhundratusende")
+        check(2 * ONE_MILLION, "två miljoner", "två miljonte")
+        check(20 * ONE_MILLION, "tjugo miljoner", "tjugo miljonte")
+        check(200 * ONE_MILLION, "tvåhundra miljoner", "tvåhundra miljonte")
+        check(2 * ONE_BILLION, "två miljarder", "två miljardte")
+        check(20 * ONE_BILLION, "tjugo miljarder", "tjugo miljardte")
+        check(200 * ONE_BILLION, "tvåhundra miljarder", "tvåhundra miljardte")
+        check(3000L, "tretusen", "tretusende")
+        check(30000L, "trettiotusen", "trettiotusende")
+        check(300000L, "trehundratusen", "trehundratusende")
+        check(3 * ONE_MILLION, "tre miljoner", "tre miljonte")
+        check(30 * ONE_MILLION, "trettio miljoner", "trettio miljonte")
+        check(300 * ONE_MILLION, "trehundra miljoner", "trehundra miljonte")
+        check(3 * ONE_BILLION, "tre miljarder", "tre miljardte")
+        check(30 * ONE_BILLION, "trettio miljarder", "trettio miljardte")
+        check(300 * ONE_BILLION, "trehundra miljarder", "trehundra miljardte")
+        check(8000L, "åttatusen", "åttatusende")
+        check(80000L, "åttiotusen", "åttiotusende")
+        check(800000L, "åttahundratusen", "åttahundratusende")
+        check(8 * ONE_MILLION, "åtta miljoner", "åtta miljonte")
+        check(80 * ONE_MILLION, "åttio miljoner", "åttio miljonte")
+        check(800 * ONE_MILLION, "åttahundra miljoner", "åttahundra miljonte")
+        check(8 * ONE_BILLION, "åtta miljarder", "åtta miljardte")
+        check(80 * ONE_BILLION, "åttio miljarder", "åttio miljardte")
+        check(800 * ONE_BILLION, "åttahundra miljarder", "åttahundra miljardte")
+    }
+
+    @Test def testSmallComposites(): Unit = {
+        check(21L, "tjugoett", "tjugoförsta")
+        check(32L, "trettiotvå", "trettioandra")
+        check(43L, "fyrtiotre", "fyrtiotredje")
+        check(54L, "femtiofyra", "femtiofjärde")
+        check(65L, "sextiofem", "sextiofemte")
+        check(76L, "sjuttiosex", "sjuttiosjätte")
+        check(87L, "åttiosju", "åttiosjunde")
+        check(98L, "nittioåtta", "nittioåttonde")
+        check(89L, "åttionio", "åttionionde")
+        check(78L, "sjuttioåtta", "sjuttioåttonde")
+        check(67L, "sextiosju", "sextiosjunde")
+        check(56L, "femtiosex", "femtiosjätte")
+        check(45L, "fyrtiofem", "fyrtiofemte")
+        check(34L, "trettiofyra", "trettiofjärde")
+        check(23L, "tjugotre", "tjugotredje")
+        check(123L, "etthundratjugotre", "etthundratjugotredje")
+        check(234L, "tvåhundratrettiofyra", "tvåhundratrettiofjärde")
+        check(345L, "trehundrafyrtiofem", "trehundrafyrtiofemte")
+        check(456L, "fyrahundrafemtiosex", "fyrahundrafemtiosjätte")
+        check(567L, "femhundrasextiosju", "femhundrasextiosjunde")
+        check(678L, "sexhundrasjuttioåtta", "sexhundrasjuttioåttonde")
+        check(789L, "sjuhundraåttionio", "sjuhundraåttionionde")
+        check(901L, "niohundraett", "niohundraförsta")
+        check(197L, "etthundranittiosju", "etthundranittiosjunde")
+        check(286L, "tvåhundraåttiosex", "tvåhundraåttiosjätte")
+        check(375L, "trehundrasjuttiofem", "trehundrasjuttiofemte")
+        check(464L, "fyrahundrasextiofyra", "fyrahundrasextiofjärde")
+        check(553L, "femhundrafemtiotre", "femhundrafemtiotredje")
+        check(642L, "sexhundrafyrtiotvå", "sexhundrafyrtioandra")
+        check(731L, "sjuhundratrettioett", "sjuhundratrettioförsta")
+    }
+
+    @Test def testMediumComposites(): Unit = {
+        check(1234L, "ettusen tvåhundratrettiofyra", "ettusen tvåhundratrettiofjärde")
+        check(2345L, "tvåtusen trehundrafyrtiofem", "tvåtusen trehundrafyrtiofemte")
+        check(3456L, "tretusen fyrahundrafemtiosex", "tretusen fyrahundrafemtiosjätte")
+        check(4567L, "fyratusen femhundrasextiosju", "fyratusen femhundrasextiosjunde")
+        check(5678L, "femtusen sexhundrasjuttioåtta", "femtusen sexhundrasjuttioåttonde")
+        check(6789L, "sextusen sjuhundraåttionio", "sextusen sjuhundraåttionionde")
+        check(7890L, "sjutusen åttahundranittio", "sjutusen åttahundranittionde")
+        check(9876L, "niotusen åttahundrasjuttiosex", "niotusen åttahundrasjuttiosjätte")
+        check(8765L, "åttatusen sjuhundrasextiofem", "åttatusen sjuhundrasextiofemte")
+        check(7654L, "sjutusen sexhundrafemtiofyra", "sjutusen sexhundrafemtiofjärde")
+        check(6543L, "sextusen femhundrafyrtiotre", "sextusen femhundrafyrtiotredje")
+        check(5432L, "femtusen fyrahundratrettiotvå", "femtusen fyrahundratrettioandra")
+        check(4321L, "fyratusen trehundratjugoett", "fyratusen trehundratjugoförsta")
+        check(1357L, "ettusen trehundrafemtiosju", "ettusen trehundrafemtiosjunde")
+        check(2468L, "tvåtusen fyrahundrasextioåtta", "tvåtusen fyrahundrasextioåttonde")
+        check(3579L, "tretusen femhundrasjuttionio", "tretusen femhundrasjuttionionde")
+        check(4680L, "fyratusen sexhundraåttio", "fyratusen sexhundraåttionde")
+        check(5791L, "femtusen sjuhundranittioett", "femtusen sjuhundranittioförsta")
+        check(6801L, "sextusen åttahundraett", "sextusen åttahundraförsta")
+        check(7913L, "sjutusen niohundratretton", "sjutusen niohundratrettonde")
+        check(8024L, "åttatusen tjugofyra", "åttatusen tjugofjärde")
+        check(9135L, "niotusen etthundratrettiofem", "niotusen etthundratrettiofemte")
+    }
+
+    @Test def testLargeComposites(): Unit = {
+        check(111111L, "etthundraelvatusen etthundraelva", "etthundraelvatusen etthundraelfte")
+        check(1111111L, "en miljon etthundraelvatusen etthundraelva",
+            "en miljon etthundraelvatusen etthundraelfte")
+        check(11111111L, "elva miljoner etthundraelvatusen etthundraelva",
+            "elva miljoner etthundraelvatusen etthundraelfte")
+        check(111111111L, "etthundraelva miljoner etthundraelvatusen etthundraelva",
+            "etthundraelva miljoner etthundraelvatusen etthundraelfte")
+        check(1111111111L, "en miljard etthundraelva miljoner etthundraelvatusen etthundraelva",
+            "en miljard etthundraelva miljoner etthundraelvatusen etthundraelfte")
+        check(11111111111L, "elva miljarder etthundraelva miljoner etthundraelvatusen etthundraelva",
+            "elva miljarder etthundraelva miljoner etthundraelvatusen etthundraelfte")
+        check(111111111111L,
+            "etthundraelva miljarder etthundraelva miljoner etthundraelvatusen etthundraelva",
+            "etthundraelva miljarder etthundraelva miljoner etthundraelvatusen etthundraelfte")
+        check(222222L, "tvåhundratjugotvåtusen tvåhundratjugotvå", "tvåhundratjugotvåtusen tvåhundratjugoandra")
+        check(2222222L, "två miljoner tvåhundratjugotvåtusen tvåhundratjugotvå",
+            "två miljoner tvåhundratjugotvåtusen tvåhundratjugoandra")
+        check(22222222L, "tjugotvå miljoner tvåhundratjugotvåtusen tvåhundratjugotvå",
+            "tjugotvå miljoner tvåhundratjugotvåtusen tvåhundratjugoandra")
+        check(222222222L, "tvåhundratjugotvå miljoner tvåhundratjugotvåtusen tvåhundratjugotvå",
+            "tvåhundratjugotvå miljoner tvåhundratjugotvåtusen tvåhundratjugoandra")
+        check(2222222222L, "två miljarder tvåhundratjugotvå miljoner tvåhundratjugotvåtusen tvåhundratjugotvå",
+            "två miljarder tvåhundratjugotvå miljoner tvåhundratjugotvåtusen tvåhundratjugoandra")
+        check(22222222222L,
+            "tjugotvå miljarder tvåhundratjugotvå miljoner tvåhundratjugotvåtusen tvåhundratjugotvå",
+            "tjugotvå miljarder tvåhundratjugotvå miljoner tvåhundratjugotvåtusen tvåhundratjugoandra")
+        check(222222222222L,
+            "tvåhundratjugotvå miljarder tvåhundratjugotvå miljoner tvåhundratjugotvåtusen tvåhundratjugotvå",
+            "tvåhundratjugotvå miljarder tvåhundratjugotvå miljoner tvåhundratjugotvåtusen tvåhundratjugoandra")
+    }
+
+    @Test def testWithZeroes(): Unit = {
+        check(101L, "etthundraett", "etthundraförsta")
+        check(202L, "tvåhundratvå", "tvåhundraandra")
+        check(707L, "sjuhundrasju", "sjuhundrasjunde")
+        check(1001L, "ettusen ett", "ettusen första")
+        check(1002L, "ettusen två", "ettusen andra")
+        check(10001L, "tiotusen ett", "tiotusen första")
+        check(10011L, "tiotusen elva", "tiotusen elfte")
+        check(10002L, "tiotusen två", "tiotusen andra")
+        check(10022L, "tiotusen tjugotvå", "tiotusen tjugoandra")
+        check(1000001L, "en miljon ett", "en miljon första")
+        check(1000011L, "en miljon elva", "en miljon elfte")
+        check(1000004L, "en miljon fyra", "en miljon fjärde")
+        check(1000044L, "en miljon fyrtiofyra", "en miljon fyrtiofjärde")
+        check(100000001L, "etthundra miljoner ett", "etthundra miljoner första")
+        check(200000002L, "tvåhundra miljoner två", "tvåhundra miljoner andra")
+        check(100800001L, "etthundra miljoner åttahundratusen ett", "etthundra miljoner åttahundratusen första")
+        check(200800002L, "tvåhundra miljoner åttahundratusen två", "tvåhundra miljoner åttahundratusen andra")
+        check(1000800001L, "en miljard åttahundratusen ett", "en miljard åttahundratusen första")
+        check(2000800002L, "två miljarder åttahundratusen två", "två miljarder åttahundratusen andra")
+        check(1000807001L, "en miljard åttahundrasjutusen ett", "en miljard åttahundrasjutusen första")
+        check(2000807002L, "två miljarder åttahundrasjutusen två", "två miljarder åttahundrasjutusen andra")
+    }
+
+    @Test def testVarious(): Unit = {
+        // The largest int
+        check(2147483647L,
+            "två miljarder etthundrafyrtiosju miljoner fyrahundraåttiotretusen sexhundrafyrtiosju",
+            "två miljarder etthundrafyrtiosju miljoner fyrahundraåttiotretusen sexhundrafyrtiosjunde")
+    }
+
+    @Test def testDateRelated(): Unit = {
+        assert("jan" === N.monthName(1, 3, 3))
+        assert("januari" === N.monthName(1, 3, 10))
+        assert("dec" === N.monthName(12, 3, 3))
+        assert("december" === N.monthName(12, 3, 10))
+        assert("må" === N.dayName(1, 2, 2))
+        assert("m" === N.dayName(1, 1, 2))
+        assert("mån" === N.dayName(1, 3, 3))
+        assert("månd" === N.dayName(1, 4, 4))
+        assert("måndag" === N.dayName(1, 2, 10))
+        assert("ti" === N.dayName(2, 2, 2))
+        assert("ti" === N.dayName(2, 1, 2))
+        assert("tis" === N.dayName(2, 3, 3))
+        assert("tisd" === N.dayName(2, 4, 4))
+        assert("tisdag" === N.dayName(2, 2, 10))
+        assert("on" === N.dayName(3, 2, 2))
+        assert("o" === N.dayName(3, 1, 2))
+        assert("ons" === N.dayName(3, 3, 3))
+        assert("onsd" === N.dayName(3, 4, 4))
+        assert("onsdag" === N.dayName(3, 2, 10))
+        assert("to" === N.dayName(4, 2, 2))
+        assert("to" === N.dayName(4, 1, 2))
+        assert("tor" === N.dayName(4, 3, 3))
+        assert("tors" === N.dayName(4, 4, 4))
+        assert("torsdag" === N.dayName(4, 2, 10))
+        assert("fr" === N.dayName(5, 2, 2))
+        assert("f" === N.dayName(5, 1, 2))
+        assert("fre" === N.dayName(5, 3, 3))
+        assert("fred" === N.dayName(5, 4, 4))
+        assert("fredag" === N.dayName(5, 2, 10))
+        assert("lö" === N.dayName(6, 2, 2))
+        assert("l" === N.dayName(6, 1, 2))
+        assert("lör" === N.dayName(6, 3, 3))
+        assert("lörd" === N.dayName(6, 4, 4))
+        assert("lördag" === N.dayName(6, 2, 10))
+        assert("sö" === N.dayName(7, 2, 2))
+        assert("s" === N.dayName(7, 1, 2))
+        assert("sön" === N.dayName(7, 3, 3))
+        assert("sönd" === N.dayName(7, 4, 4))
+        assert("söndag" === N.dayName(7, 2, 10))
+    }
+}

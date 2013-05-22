d9cab0b13ab7293df40f4250b08b18a54bd0b8e0
ebruchez <ebruchez@orbeon.com>
2013-05-22 14:19:10 -0700

    Fix compilation issue
    Error was:
    
    error: reference to XMLUtils is ambiguous;
    it is imported twice in the same scope by
    import _root_.java.util._
    and import _root_.org.orbeon.oxf.xml._

--- src/main/java/org/orbeon/oxf/processor/generator/URLGenerator.java
+++ src/main/java/org/orbeon/oxf/processor/generator/URLGenerator.java
@@ -41,7 +41,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Generates SAX events from a document fetched from an URL.

0441d5b0ce8d3bff32d12bef73f6a3c812046130
ebruchez <ebruchez@orbeon.com>
2013-06-26 13:38:50 -0700

    New fr:tabbable component
    This depends on #1094.

---
+++ src/resources-packaged/xbl/orbeon/tabbable/tabbable.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2013 Orbeon, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+YAHOO.namespace("xbl.fr");
+YAHOO.xbl.fr.Tabbable = function() {};
+ORBEON.xforms.XBL.declareClass(YAHOO.xbl.fr.Tabbable, "xbl-fr-tabbable");
+YAHOO.xbl.fr.Tabbable.prototype = {
+
+    init: function() {
+        $(this.container).find('.nav-tabs a').click(function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+        })
+    }
+};

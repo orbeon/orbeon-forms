2f6df385a44b467730a8870948239950e75e9000
avernet <avernet@orbeon.com>
2013-09-20 17:35:48 -0700

    DDL for DB2

---
+++ src/resources/apps/fr/persistence/relational/ddl/db2-4_3.sql
@@ -0,0 +1,49 @@
+create table orbeon_form_definition (
+    created             timestamp,
+    last_modified_time  timestamp,
+    last_modified_by    varchar(255),
+    app                 varchar(255),
+    form                varchar(255),
+    deleted             character(1),
+    xml                 xml
+);
+
+create table orbeon_form_definition_attach (
+    created             timestamp,
+    last_modified_time  timestamp,
+    last_modified_by    varchar(255),
+    app                 varchar(255),
+    form                varchar(255),
+    deleted             character(1),
+    file_name           varchar(255),
+    file_content        blob(1048576)
+);
+
+create table orbeon_form_data (
+    created             timestamp,
+    last_modified_time  timestamp,
+    last_modified_by    varchar(255),
+    username            varchar(255),
+    groupname           varchar(255),
+    app                 varchar(255),
+    form                varchar(255),
+    document_id         varchar(255),
+    deleted             character(1),
+    draft               character(1),
+    xml                 xml
+);
+
+create table orbeon_form_data_attach (
+    created             timestamp,
+    last_modified_time  timestamp,
+    last_modified_by    varchar(255),
+    username            varchar(255),
+    groupname           varchar(255),
+    app                 varchar(255),
+    form                varchar(255),
+    document_id         varchar(255),
+    deleted             character(1),
+    draft               character(1),
+    file_name           varchar(255),
+    file_content        blob(2097152)
+);

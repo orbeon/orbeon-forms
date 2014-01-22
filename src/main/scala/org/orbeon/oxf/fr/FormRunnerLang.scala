orbeon/orbeon-forms

commit 14b9278b031dad01c34dcb11429942973a32dc52

Implement #1122 "Use Liferay language as requested Form Runner language"
- FR
  - support Orbeon-Liferay-Language
  - "cleans" language, i.e. "en_US" becomes "en"
  - don't show language selector if Orbeon-Liferay-Language provided
- proxy portlet
  - send Orbeon-Liferay-Language if configured
  - preferences: dropdowns and checkboxes, refactor, style
  - support FR Home page
- add ExternalContextOps
- comments

src/main/scala/org/orbeon/oxf/fr/FormRunnerLang.scala
@@ -19,8 +19,9 @@ import org.orbeon.oxf.util.NetUtils
 import org.orbeon.saxon.om.Item
 import collection.JavaConverters._
 import org.orbeon.oxf.pipeline.api.ExternalContext.Request
-import org.orbeon.scaxon.XML._
+import org.orbeon.oxf.externalcontext.ExternalContextOps._
 
+// NOTE: Language is currently assumed to be only the plain language part, e.g. "en", "it", "zh".
 trait FormRunnerLang {
 
     import FormRunner._
@@ -77,7 +78,7 @@ trait FormRunnerLang {
     // Public for unit tests
     def getDefaultLang(appForm: Option[AppForm]): String = {
         val suffix = appForm.toList flatMap (_.toList)
-        Option(properties.getString("oxf.fr.default-language" :: suffix mkString ".")) getOrElse "en"
+        Option(properties.getString("oxf.fr.default-language" :: suffix mkString ".")) map  cleanLanguage getOrElse "en"
     }
 
     // Return a predicate telling whether a language is allowed based on properties. If app/form are specified, then the
@@ -85,7 +86,7 @@ trait FormRunnerLang {
     // Public for unit tests
     def isAllowedLang(appForm: Option[AppForm]): String ⇒ Boolean = {
         val suffix = appForm.toList flatMap (_.toList)
-        val set    = stringOptionToSet(Option(properties.getString("oxf.fr.available-languages" :: suffix mkString ".")))
+        val set    = stringOptionToSet(Option(properties.getString("oxf.fr.available-languages" :: suffix mkString "."))) map cleanLanguage
         // If none specified via property or property contains a wildcard, all languages are considered available
         if (set.isEmpty || set("*")) _ ⇒ true else set
     }
@@ -95,14 +96,21 @@ trait FormRunnerLang {
     def findRequestedLang(appForm: Option[AppForm], requestedLang: String): Option[String] = {
         val request = NetUtils.getExternalContext.getRequest
 
-        def fromRequest = Option(request.getParameterMap.get(LanguageParam)) flatMap (_.lift(0)) map (_.toString)
+        def fromHeader  = request.getFirstHeader("orbeon-liferay-language") map cleanLanguage
+        def fromRequest = request.getFirstHeader(LanguageParam) map cleanLanguage
         def fromSession = stringFromSession(request, LanguageParam)
 
         nonEmptyOrNone(requestedLang) orElse
+            fromHeader orElse
             fromRequest orElse
             fromSession orElse
             Option(getDefaultLang(appForm))
     }
+
+    // Support e.g. en_US with underscore, but normalize to en-US and only keep the language part for now. Later we can
+    // support country/variant etc. See Java Locale and comments in XXFormsFormatMessage.
+    private def cleanLanguage(lang: String) =
+        split[List](lang, "-_").head
 
     private def selectLangUseDefault(appForm: Option[AppForm], requestedLang: Option[String], availableLangs: List[String]) = {
         def matchingLanguage = availableLangs intersect requestedLang.toList headOption

descriptors/proxy-portlet/portlet.xml
@@ -26,6 +26,10 @@
             <name>read-only</name>
             <value>false</value>
         </init-param>
+        <init-param>
+            <name>send-liferay-language</name>
+            <value>false</value>
+        </init-param>
         
         <supports>
             <mime-type>text/html</mime-type>

src/main/scala/org/orbeon/oxf/externalcontext/ExternalContextOps.scala
@@ -1,0 +1,32 @@
+/**
+ * Copyright (C) 2013 Orbeon, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+package org.orbeon.oxf.externalcontext
+
+import org.orbeon.oxf.pipeline.api.ExternalContext.Request
+import collection.JavaConverters._
+
+object ExternalContextOps {
+    implicit class RequestOps(request: Request) {
+
+        // NOTE: Ideally would return immutable.Map
+        def parameters: collection.Map[String, Array[AnyRef]] =
+            request.getParameterMap.asScala
+
+        def getFirstParamAsString(name: String) =
+            Option(request.getParameterMap.get(name)) flatMap (_ collectFirst { case s: String ⇒ s })
+
+        def getFirstHeader(name: String) =
+            Option(request.getHeaderValuesMap.get(name)) flatMap (_.lift(0))
+    }
+}

src/main/scala/org/orbeon/oxf/portlet/OrbeonProxyPortlet.scala
@@ -26,6 +26,7 @@ import collection.breakOut
 import com.liferay.portal.util.PortalUtil
 import java.util.{Enumeration ⇒ JEnumeration}
 import scala.util.control.NonFatal
+import com.liferay.portal.kernel.language.LanguageUtil
 
 /**
  * Orbeon Forms Form Runner proxy portlet.
@@ -68,11 +69,18 @@ class OrbeonProxyPortlet extends GenericPortlet with ProxyPortletEdit with Buffe
         getHttpServletRequest flatMap (f ⇒ Option(f(request)))
 
     // Immutable information about an outgoing request
-    case class RequestDetails(content: Option[Content], session: PortletSession, url: String, namespace: String, headers: Seq[(String, String)], params: Seq[(String, String)])
+    case class RequestDetails(
+        content: Option[Content],
+        session: PortletSession,
+        url: String,
+        namespace: String,
+        headers: Seq[(String, String)],
+        params: Seq[(String, String)]
+    )
 
     object RequestDetails {
 
-        def apply(request: PortletRequest, content: Option[Content], session: PortletSession, url: String, namespace: String): RequestDetails = {
+        def apply(request: PortletRequest, content: Option[Content], url: String, namespace: String): RequestDetails = {
 
             def headerPairs =
                 for {
@@ -98,10 +106,15 @@ class OrbeonProxyPortlet extends GenericPortlet with ProxyPortletEdit with Buffe
                 } yield
                     pair
 
-            RequestDetails(content, session, url, namespace, headersToForward.toList, paramsToForward)
+            // E.g. en_US. Pass this as fr-language to Form Runner.
+            val language = nonEmptyOrNone(LanguageUtil.getLanguageId(request))
+            def languageHeader =  language map ("Orbeon-Liferay-Language" →)
+
+            RequestDetails(content, request.getPortletSession(true), url, namespace, headersToForward.toList ++ languageHeader.toList, paramsToForward)
         }
     }
 
+    private val FormRunnerHome = """/fr/(\?(.*))?""".r
     private val FormRunnerPath = """/fr/([^/]+)/([^/]+)/(new|summary)(\?(.*))?""".r
     private val FormRunnerDocumentPath = """/fr/([^/]+)/([^/]+)/(new|edit|view)/([^/?]+)?(\?(.*))?""".r
 
@@ -141,10 +154,10 @@ class OrbeonProxyPortlet extends GenericPortlet with ProxyPortletEdit with Buffe
 
     private def directServeResource(request: ResourceRequest, response: ResourceResponse): Unit = {
         val resourceId = request.getResourceID
-        val url = buildFormRunnerURL(getPreference(request, FormRunnerURL), resourceId)
+        val url = buildFormRunnerURL(getPreference(request, FormRunnerURL), resourceId, embeddable = false)
 
         val namespace = response.getNamespace
-        val connection = connectURL(RequestDetails(request, contentFromRequest(request, namespace), request.getPortletSession(true), url, namespace))
+        val connection = connectURL(RequestDetails(request, contentFromRequest(request, namespace), url, namespace))
 
         useAndClose(connection.getInputStream) { is ⇒
             propagateResponseHeaders(connection, response)
@@ -156,33 +169,39 @@ class OrbeonProxyPortlet extends GenericPortlet with ProxyPortletEdit with Buffe
 
     private def createRequestDetails(request: PortletRequest, namespace: String): RequestDetails = {
         // Determine URL based on preferences and request
-        val url = {
-            val pathParameter = request.getParameter(WSRPURLRewriter.PathParameterName)
+        val path = {
 
-            if (pathParameter == "/xforms-server-submit")
-                // XForms server submit
-                buildFormRunnerURL(getPreference(request, FormRunnerURL), pathParameter)
-            else {
-                // Form Runner path
-                def filterAction(action: String) =
-                    if (getPreference(request, ReadOnly) == "true" && action == "edit") "view" else action
+            def pathParameterOpt =
+                Option(request.getParameter(WSRPURLRewriter.PathParameterName))
 
-                val (appName, formName, action, documentId, query) = pathParameter match {
-                    // Incoming path is Form Runner path without document id
-                    case FormRunnerPath(appName, formName, action, _, query) ⇒ (appName, formName, filterAction(action), None, Option(query))
-                    // Incoming path is Form Runner path with document id
-                    case FormRunnerDocumentPath(appName, formName, action, documentId, _, query) ⇒ (appName, formName, filterAction(action), Some(documentId), Option(query))
-                    // No incoming path, use preferences
-                    case null ⇒ (getPreference(request, AppName), getPreference(request, FormName), getPreference(request, Action), None, None)
-                    // Unsupported path
-                    case otherPath ⇒ throw new PortletException("Unsupported path: " + otherPath)
-                }
+            def defaultPath =
+                if (getPreference(request, Page) == "home")
+                    buildFormRunnerHomePath(None)
+                else
+                    buildFormRunnerPath(getPreference(request, AppName), getPreference(request, FormName), getPreference(request, Page), None, None)
 
-                buildFormRunnerURL(getPreference(request, FormRunnerURL), appName, formName, action, documentId, query)
+            def filterAction(action: String) =
+                if (getPreference(request, ReadOnly) == "true" && action == "edit") "view" else action
+
+            pathParameterOpt getOrElse defaultPath match {
+                case path @ "/xforms-server-submit" ⇒
+                    path
+                // Incoming path is Form Runner path without document id
+                case FormRunnerPath(appName, formName, action, _, query) ⇒
+                    buildFormRunnerPath(appName, formName, filterAction(action), None, Option(query))
+                // Incoming path is Form Runner path with document id
+                case FormRunnerDocumentPath(appName, formName, action, documentId, _, query) ⇒
+                    buildFormRunnerPath(appName, formName, filterAction(action), Some(documentId), Option(query))
+                // Incoming path is Form Runner Home page
+                case FormRunnerHome(_, query) ⇒
+                    buildFormRunnerHomePath(Option(query))
+                // Unsupported path
+                case otherPath ⇒
+                    throw new PortletException("Unsupported path: " + otherPath)
             }
         }
 
-        RequestDetails(request, contentFromRequest(request, namespace), request.getPortletSession(true), url, namespace)
+        RequestDetails(request, contentFromRequest(request, namespace), buildFormRunnerURL(getPreference(request, FormRunnerURL), path, embeddable = true), namespace)
     }
 
     private def contentFromRequest(request: PortletRequest, namespace: String): Option[Content] = {
@@ -317,11 +336,12 @@ class OrbeonProxyPortlet extends GenericPortlet with ProxyPortletEdit with Buffe
             case _ ⇒
         }
 
-    private def buildFormRunnerURL(baseURL: String, app: String, form: String, action: String, documentId: Option[String], query: Option[String]) =
-        dropTrailingSlash(baseURL) + "/fr/" + app + "/" + form + "/" + action +
-            (documentId map ("/" + _) getOrElse "") +
-            (query map ("?" + _ + "&") getOrElse "?") + "orbeon-embeddable=true"
+    private def buildFormRunnerPath(app: String, form: String, action: String, documentId: Option[String], query: Option[String]) =
+        NetUtils.appendQueryString("/fr/" + app + "/" + form + "/" + action + (documentId map ("/" +) getOrElse ""), query getOrElse "")
 
-    private def buildFormRunnerURL(baseURL: String, path: String) =
-        dropTrailingSlash(baseURL) + path
+    private def buildFormRunnerHomePath(query: Option[String]) =
+        NetUtils.appendQueryString("/fr/", query getOrElse "")
+
+    private def buildFormRunnerURL(baseURL: String, path: String, embeddable: Boolean) =
+        NetUtils.appendQueryString(dropTrailingSlash(baseURL) + path, if(embeddable) "orbeon-embeddable=true" else "")
 }

src/main/scala/org/orbeon/oxf/portlet/ProxyPortletEdit.scala
@@ -16,31 +16,76 @@ package org.orbeon.oxf.portlet
 import org.orbeon.oxf.util.ScalaUtils._
 import javax.portlet._
 import org.apache.log4j.Logger
+import scala.xml.Elem
 
 // Preference editor for the proxy portlet
 trait ProxyPortletEdit extends GenericPortlet {
 
     implicit def logger: Logger
+    
+    case class NameLabel(name: String, label: String)
 
-    sealed trait Preference { val name: String }
-    case object FormRunnerURL extends Preference { val name = "form-runner-url" }
-    case object AppName       extends Preference { val name = "app-name" }
-    case object FormName      extends Preference { val name = "form-name" }
-    case object Action        extends Preference { val name = "action" }
-    case object ReadOnly      extends Preference { val name = "read-only" }
+    sealed trait ControlType { def render(pref: Pref, value: String): Elem }
 
-    private val PreferenceLabels = Seq(
-        FormRunnerURL → "Form Runner URL",
-        AppName       → "Form Runner app name",
-        FormName      → "Form Runner form name",
-        Action        → "Form Runner action",
-        ReadOnly      → "Read-Only access"
+    case object InputControl extends ControlType {
+        def render(pref: Pref, value: String) =
+            <label>{pref.nameLabel.label}
+                <input type="text" name={pref.nameLabel.name} value={value}/>
+            </label>
+    }
+
+    case object CheckboxControl extends ControlType {
+        def render(pref: Pref, value: String) =
+            <label>{pref.nameLabel.label}
+                {
+                    val template = <input type="checkbox" name={pref.nameLabel.name} value="true" checked="checked"/>
+                    if (value == "true") template else template.copy(attributes = template.attributes.remove("checked"))
+                }
+            </label>
+    }
+    
+    case class SelectControl(nameLabels: List[NameLabel]) extends ControlType {
+        def render(pref: Pref, value: String) =
+            <label>{pref.nameLabel.label}
+                <select name={pref.nameLabel.name}>
+                    {
+                        for (nameLabel ← nameLabels) yield {
+                            val template = <option value={nameLabel.name} selected="selected">{nameLabel.label}</option>
+                            if (value == nameLabel.name) template else template.copy(attributes = template.attributes.remove("selected"))
+                        }
+                    }
+                </select>
+            </label>
+    }
+
+    sealed trait Pref { val tpe: ControlType; val nameLabel: NameLabel }
+
+    val PageNameLabels = List(
+        NameLabel("home", "Home Page"),
+        NameLabel("summary", "Summary Page"),
+        NameLabel("new", "New Page")
+    )
+
+    case object FormRunnerURL        extends Pref { val tpe = InputControl;                      val nameLabel = NameLabel("form-runner-url", "Form Runner URL") }
+    case object AppName              extends Pref { val tpe = InputControl;                      val nameLabel = NameLabel("app-name", "Form Runner app name") }
+    case object FormName             extends Pref { val tpe = InputControl;                      val nameLabel = NameLabel("form-name", "Form Runner form name") }
+    case object ReadOnly             extends Pref { val tpe = CheckboxControl;                   val nameLabel = NameLabel("read-only", "Readonly access") }
+    case object SendLiferayLanguage  extends Pref { val tpe = CheckboxControl;                   val nameLabel = NameLabel("send-liferay-language", "Send Liferay language") }
+    case object Page                 extends Pref { val tpe = new SelectControl(PageNameLabels); val nameLabel = NameLabel("action", "Form Runner page") }
+
+    val AllPreferences = Seq(
+        Page,
+        FormRunnerURL,
+        AppName,
+        FormName,
+        ReadOnly,
+        SendLiferayLanguage
     )
 
     // Return the value of the preference if set, otherwise the value of the initialization parameter
     // NOTE: We should be able to use portlet.xml portlet-preferences/preference, but somehow this doesn't work properly
-    def getPreference(request: PortletRequest, pref: Preference) =
-        request.getPreferences.getValue(pref.name, getPortletConfig.getInitParameter(pref.name))
+    def getPreference(request: PortletRequest, pref: Pref) =
+        request.getPreferences.getValue(pref.nameLabel.name, getPortletConfig.getInitParameter(pref.nameLabel.name))
 
     // Very simple preferences editor
     override def doEdit(request: RenderRequest, response: RenderResponse): Unit =
@@ -50,19 +95,26 @@ trait ProxyPortletEdit extends GenericPortlet {
             response.getWriter write
                 <div>
                     <style>
-                        .orbeon-pref-form label {{display: block; font-weight: bold}}
-                        .orbeon-pref-form input {{display: block; width: 20em }}
+                        .orbeon-pref-form label {{ display: block; clear:both; font-weight: bold }}
+                        .orbeon-pref-form input[type=text] {{ display: block; width: 300px; margin-bottom: 10px }}
+                        .orbeon-pref-form input[type=checkbox] {{ float: left; margin-right: 6px; margin-bottom: 10px }}
+                        .orbeon-pref-form select {{ display: block; width: auto; margin-bottom: 10px }}
+                        .orbeon-pref-form hr {{ clear: both }}
+                        .orbeon-pref-form fieldset {{ padding-bottom: 0 }}
                     </style>
                     <form action={response.createActionURL.toString} method="post" class="orbeon-pref-form">
-                        {
-                            for ((pref, label) ← PreferenceLabels) yield
-                                <label>{label}: <input name={pref.name} value={getPreference(request, pref)}/></label>
-                        }
-                        <hr/>
-                        <p>
-                            <button name="save" value="save">Save</button>
-                            <button name="cancel" value="cancel">Cancel</button>
-                        </p>
+                        <fieldset>
+                            <legend>Form Runner Portlet Settings</legend>
+                            {
+                                for (pref ← AllPreferences) yield
+                                    pref.tpe.render(pref, getPreference(request, pref))
+                            }
+                            <hr/>
+                            <div>
+                                <button name="save"   value="save">Save</button>
+                                <button name="cancel" value="cancel">Cancel</button>
+                            </div>
+                        </fieldset>
                     </form>
                 </div>.toString
         }
@@ -72,10 +124,11 @@ trait ProxyPortletEdit extends GenericPortlet {
         withRootException("view action", new PortletException(_)) {
             request.getParameter("save") match {
                 case "save" ⇒
-                    def setPreference(pref: Preference, value: String) = request.getPreferences.setValue(pref.name, value)
+                    def setPreference(pref: Pref, value: String) =
+                        request.getPreferences.setValue(pref.nameLabel.name, value)
 
-                    for ((pref, label) ← PreferenceLabels)
-                        setPreference(pref, request.getParameter(pref.name))
+                    for (pref ← AllPreferences)
+                        setPreference(pref, request.getParameter(pref.nameLabel.name))
 
                     request.getPreferences.store()
                 case _ ⇒

src/main/scala/org/orbeon/oxf/xforms/function/xxforms/XXFormsFormatMessage.scala
@@ -38,8 +38,19 @@ class XXFormsFormatMessage extends XFormsFunction with FunctionSupport {
         val format =
             elementAnalysisForSource flatMap (XXFormsLang.resolveXMLangHandleAVTs(getContainingDocument, _)) match {
                 case Some(lang) ⇒
-                    // Really not sure how xml:lang should be parsed, see:
+                    // Not sure how xml:lang should be parsed, see:
                     //
+                    // XML spec points to:
+                    //
+                    // - http://tools.ietf.org/html/rfc4646
+                    // - http://tools.ietf.org/html/rfc4647
+                    //
+                    // NOTES:
+                    //
+                    // - IETF BCP 47 replaces RFC 4646 (and includes RFC 5646 and RFC 4647)
+                    // - Java 7 has an improved Locale class which supports parsing BCP 47
+                    //
+                    // http://docs.oracle.com/javase/7/docs/api/java/util/Locale.html#forLanguageTag(java.lang.String)
                     // http://www.w3.org/International/articles/language-tags/
                     // http://sites.google.com/site/openjdklocale/design-specification
                     // IETF BCP 47: http://www.rfc-editor.org/rfc/bcp/bcp47.txt

src/resources/apps/fr/components/view.xsl
@@ -310,7 +310,7 @@
 
             <!-- Don't display language selector if there is only one language -->
             <!-- NOTE: Resolve model here, as for now model within XBL component won't resolve -->
-            <xf:group id="fr-language-selector" model="fr-resources-model" ref=".[count($available-languages) gt 1]">
+            <xf:group id="fr-language-selector" model="fr-resources-model" ref=".[count($available-languages) gt 1 and normalize-space(xxf:get-request-header('orbeon-liferay-language')) = '']">
                 <fr:link-select1 ref="$fr-selector-lang">
                     <xf:itemset ref="$available-languages">
                         <xf:label ref="(xxf:instance('fr-languages-instance')/language[@code = context()]/@native-name, context())[1]"/>

commit 968c4f2166e8c80cdffc7e730de5c8a8f6c06ffc

Fix #1422 "fr-language on the URL no longer works"
Regression from 14b9278b031dad01c34dcb11429942973a32dc52.

src/main/scala/org/orbeon/oxf/fr/FormRunnerLang.scala
@@ -97,7 +97,7 @@ trait FormRunnerLang {
         val request = NetUtils.getExternalContext.getRequest
 
         def fromHeader  = request.getFirstHeader("orbeon-liferay-language") map cleanLanguage
-        def fromRequest = request.getFirstHeader(LanguageParam) map cleanLanguage
+        def fromRequest = request.getFirstParamAsString(LanguageParam) map cleanLanguage
         def fromSession = stringFromSession(request, LanguageParam)
 
         nonEmptyOrNone(requestedLang) orElse
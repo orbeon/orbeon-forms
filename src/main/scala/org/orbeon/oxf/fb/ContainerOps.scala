orbeon/orbeon-forms

commit eb1fdfe39574e59e949d3b47bb8c6d2261c77e0a

Fix #1548 "Section template with nested section causes error"
And also add proper support for nested sections within section templates.

src/main/scala/org/orbeon/oxf/fb/ResourcesOps.scala
@@ -16,7 +16,7 @@ package org.orbeon.oxf.fb
 import org.orbeon.scaxon.XML._
 import org.orbeon.oxf.xforms.action.XFormsAPI._
 import org.orbeon.oxf.fr.FormRunner._
-import org.orbeon.saxon.om.NodeInfo
+import org.orbeon.saxon.om.{SequenceIterator, NodeInfo}
 import org.apache.commons.lang3.StringUtils._
 import collection.JavaConverters._
 import java.{util ⇒ ju}
@@ -26,11 +26,14 @@ trait ResourcesOps extends BaseOps {
 
     def currentResources      = asNodeInfo(topLevelModel("fr-form-model").get.getVariable("current-resources"))
     def currentLang           = currentResources attValue "*:lang"
-    def allResources          = currentResources parent * child "resource"
-    def allLangs              = allResources att "*:lang" map (_.stringValue)
-    def allLangsWithResources = allLangs zip allResources
+    def resourcesRoot         = currentResources parent * head
 
-    def allLangsXPath         = allLangs.asJava
+    def allResources(resources: NodeInfo)  = resources child "resource"
+    def allLangs(resources: NodeInfo)      = allResources(resources) attValue "*:lang"
+    def allLangsXPath(resources: NodeInfo) = allLangs(resources).asJava
+
+    def allLangsWithResources(resources: NodeInfo) =
+        allLangs(resources) zip allResources(resources)
 
     // Find the current resource holder for the given name
     def findCurrentResourceHolder(controlName: String) =
@@ -47,9 +50,9 @@ trait ResourcesOps extends BaseOps {
             (n ⇒ n \ resourceName)
 
     // NOTE: Doesn't enforce that the same number of e.g. <alert> elements are present per lang
-    def getControlResourcesWithLang(controlName: String, resourceName: String, langs: Seq[String] = allLangs) = {
+    def getControlResourcesWithLang(controlName: String, resourceName: String, langs: Seq[String] = allLangs(resourcesRoot)) = {
         val langsSet = langs.toSet
-        findResourceHoldersWithLang(controlName) collect {
+        findResourceHoldersWithLang(controlName, resourcesRoot) collect {
             case (lang, holder) if langsSet(lang) ⇒ lang → (holder child resourceName)
         }
     }
@@ -87,7 +90,9 @@ trait ResourcesOps extends BaseOps {
     // Get the control's items for all languages
     def getControlItemsGroupedByValue(controlName: String): ju.List[NodeInfo] = {
 
-        val holdersWithLang = findResourceHoldersWithLang(controlName)
+        val localResourcesRoot = resourcesRoot
+
+        val holdersWithLang = findResourceHoldersWithLang(controlName, localResourcesRoot)
 
         // All unique values in the order they appear
         val distinctValues = (
@@ -106,7 +111,7 @@ trait ResourcesOps extends BaseOps {
             holder / "item" find (_ / "value" === value)
 
         def lhhaForLangAndValue(lang: String, value: String, lhha: String) = (
-            findResourceHoldersForLang(controlName, lang)
+            findResourceHolderForLang(controlName, lang, localResourcesRoot)
             map (holderItemForValue(_, value).toList / lhha stringValue)
             getOrElse ""
         )
@@ -147,7 +152,7 @@ trait ResourcesOps extends BaseOps {
 
         val addHints = hasItemHintEditor(controlName)
 
-        for ((lang, holder) ← findResourceHoldersWithLang(controlName)) {
+        for ((lang, holder) ← findResourceHoldersWithLang(controlName, resourcesRoot)) {
 
             delete(holder / "item")
 
@@ -193,7 +198,7 @@ trait ResourcesOps extends BaseOps {
 
     // NOTE: Assume enclosing control resource holder already exists
     def ensureResourceHoldersForLang(controlName: String, resourceName: String, count: Int, lang: String) = {
-        val controlHolder = findResourceHoldersForLang(controlName, lang).get
+        val controlHolder = findResourceHolderForLang(controlName, lang, resourcesRoot).get
 
         val existing = controlHolder child resourceName
 
@@ -207,32 +212,35 @@ trait ResourcesOps extends BaseOps {
     }
 
     // NOTE: Assume enclosing control resource holder already exists
-    def ensureResourceHoldersForLangs(controlName: String, resourceName: String, count: Int = 1, langs: Seq[String] = allLangs) =
-        allLangs map (lang ⇒ lang → ensureResourceHoldersForLang(controlName, resourceName, count, lang))
+    def ensureResourceHoldersForLangs(controlName: String, resourceName: String, count: Int = 1, langs: Seq[String] = allLangs(resourcesRoot)) =
+        allLangs(resourcesRoot) map (lang ⇒ lang → ensureResourceHoldersForLang(controlName, resourceName, count, lang))
 
-    def findResourceHoldersForLang(controlName: String, lang: String) =
-        findResourceHoldersWithLang(controlName) collectFirst { case (`lang`, holder) ⇒ holder }
+    def findResourceHolderForLang(controlName: String, lang: String, resources: NodeInfo) =
+        findResourceHoldersWithLang(controlName, resources) collectFirst { case (`lang`, holder) ⇒ holder }
 
     // Find control resource holders
     def findResourceHolders(controlName: String): Seq[NodeInfo] =
-        findResourceHoldersWithLang(controlName) map (_._2)
+        findResourceHoldersWithLang(controlName, resourcesRoot) map (_._2)
 
     // Find control resource holders with their language
-    def findResourceHoldersWithLang(controlName: String): Seq[(String, NodeInfo)] =
+    def findResourceHoldersWithLang(controlName: String, resources: NodeInfo): Seq[(String, NodeInfo)] =
         for {
-            (lang, resource) ← allLangsWithResources
+            (lang, resource) ← allLangsWithResources(resources)
             holder           ← resource child controlName headOption // there *should* be only one
         } yield
             (lang, holder)
 
+    // For the given bind and lang, find all associated resource holders
+    def iterateSelfAndDescendantBindsResourceHolders(rootBind: NodeInfo, lang: String, resources: NodeInfo) =
+        FormBuilder.iterateSelfAndDescendantBinds(rootBind) map findBindName flatMap (findResourceHolderForLang(_, lang, resources))
+
+    // Same for XPath callers
+    def iterateSelfAndDescendantBindsResourceHoldersXPath(rootBind: NodeInfo, lang: String, resources: NodeInfo): SequenceIterator =
+        iterateSelfAndDescendantBindsResourceHolders(rootBind, lang, resources)
+
     // Same as above but doesn't require a Form Builder context
     def findResourceHoldersWithLangUseDoc(inDoc: NodeInfo, controlName: String): Seq[(String, NodeInfo)] =
-        for {
-            resource ← resourcesInstanceRoot(inDoc) child "resource"
-            lang     = resource attValue "*:lang"
-            holder   ← resource child controlName headOption // there *should* be only one
-        } yield
-            (lang, holder)
+        findResourceHoldersWithLang(controlName, resourcesInstanceRoot(inDoc))
 
     def lhhaHoldersForAllLangsUseDoc(inDoc: NodeInfo, controlName: String, lhha: String) =
         for {

src/main/scala/org/orbeon/oxf/fb/AlertsAndConstraintsOps.scala
@@ -131,7 +131,7 @@ trait AlertsAndConstraintsOps extends ControlOps {
 
                 def messagesForAllLangs(a: AlertDetails) = {
                     val messagesMap = a.messages.toMap
-                    allLangs map { lang ⇒ lang → messagesMap.getOrElse(lang, "") }
+                    allLangs(resourcesRoot) map { lang ⇒ lang → messagesMap.getOrElse(lang, "") }
                 }
 
                 val messagesByLang = (
@@ -144,7 +144,7 @@ trait AlertsAndConstraintsOps extends ControlOps {
                 // Make sure we have a default for all languages if there are no alerts or if some languages are missing
                 // from the alerts. We do want to update all languages on write, including removing unneeded <alert>
                 // elements.
-                val defaultMessages = allLangs map (_ → Nil)
+                val defaultMessages = allLangs(resourcesRoot) map (_ → Nil)
 
                 defaultMessages.toMap ++ messagesByLang toList
             }

src/main/scala/org/orbeon/oxf/fb/ControlOps.scala
@@ -32,7 +32,6 @@ import org.orbeon.oxf.xml.dom4j.Dom4jUtils
 import org.orbeon.oxf.fr.FormRunner._
 import org.orbeon.oxf.xforms.XFormsUtils._
 import org.orbeon.oxf.util.ScalaUtils._
-import org.orbeon.oxf.xforms.xbl.BindingDescriptor._
 
 /*
  * Form Builder: operations on controls.
@@ -137,6 +136,20 @@ trait ControlOps extends SchemaOps with ResourcesOps {
 
         // Start with top-level
         ensureBind(topLevelBind, names.toIterator)
+    }
+
+    // Iterate over the given bind followed by all of its descendants, depth-first
+    def iterateSelfAndDescendantBinds(rootBind: NodeInfo): Iterator[NodeInfo] = {
+
+        def selfAndDescendant(bind: NodeInfo): Iterator[NodeInfo] = {
+
+            val childrenIterator    = bind / "*:bind" iterator
+            val descendantsIterator = childrenIterator flatMap selfAndDescendant
+
+            Iterator.single(bind) ++ descendantsIterator
+        }
+
+        selfAndDescendant(rootBind)
     }
 
     // Delete the controls in the given grid cell, if any

src/main/scala/org/orbeon/oxf/fb/ToolboxOps.scala
@@ -348,7 +348,7 @@ object ToolboxOps {
         findControlByName(doc, name) foreach { controlElement ⇒
 
             // Create <resource xml:lang="..."> containers
-            val resourcesWithLang = findResourceHoldersWithLang(name) map {
+            val resourcesWithLang = findResourceHoldersWithLang(name, resourcesRoot) map {
                 case (lang, holder) ⇒ elementInfo("resource", attributeInfo(XML_URI → "lang", lang) ++ holder)
             }
 

src/resources-packaged/xbl/fb/dialog-itemsets/dialog-itemsets.xbl
@@ -122,7 +122,7 @@
                         <xf:var
                             name="available-languages"
                             model="fr-resources-model"
-                            value="frf:getFormLangSelection('orbeon', 'builder', fbf:allLangsXPath())"/>
+                            value="frf:getFormLangSelection('orbeon', 'builder', fbf:allLangsXPath($resources/..))"/>
 
                         <!-- Don't display language selector if there is only one language -->
                         <xf:group ref=".[count($available-languages) gt 1 and normalize-space(xxf:get-request-header('orbeon-liferay-language')) = '']">

src/resources/forms/orbeon/builder/service/form-to-xbl.xsl
@@ -98,7 +98,7 @@
                 </icon>
             </metadata>
 
-            <xsl:apply-templates select="/xh:html/xh:body//fr:section"/>
+            <xsl:apply-templates select="/xh:html/xh:body//fr:section[empty(ancestor::fr:section)]"/>
         </xbl:xbl>
     </xsl:template>
 
@@ -124,7 +124,7 @@
         <xsl:attribute name="{name(.)}" select="(fr:value-except(p:split(), 'fr-form-model'), $model-id)"/>
     </xsl:template>
 
-    <!-- Generate one component per section -->
+    <!-- Generate one component per top-level section -->
     <xsl:template match="/xh:html/xh:body//fr:section">
 
         <!-- ==== Section information ============================================================================== -->
@@ -143,7 +143,6 @@
 
         <!-- Section instance data element -->
         <!-- NOTE: could also gather ancestor-or-self::xf:bind/@ref and evaluate expression to be more generic -->
-        <!-- TODO: What do do with custom data model? -->
         <xsl:variable name="section-data" select="$fr-form-instance/*/*[name() = $section-name]" as="element()"/>
 
         <!-- Binding ids must be unique as the xbl:xbl of the section templates are embedded into the published forms.
@@ -236,12 +235,12 @@
                     <!-- Sections resources: include resource for section and for all leaf controls -->
                     <xf:instance id="fr-form-resources">
                         <resources xmlns="">
-                            <xsl:for-each select="$fr-resources-instance/*/resource">
+                            <xsl:variable name="resources" select="$fr-resources-instance/*"/>
+                            <xsl:for-each select="$resources/resource">
                                 <xsl:variable name="lang" select="@xml:lang" as="xs:string"/>
 
                                 <resource xml:lang="{$lang}">
-                                    <xsl:copy-of select="*[name() = $section-name]"/>
-                                    <xsl:copy-of select="*[name() = (for $n in $section-data//*[not(*)] return name($n))]"/>
+                                    <xsl:copy-of select="fbf:iterateSelfAndDescendantBindsResourceHoldersXPath($section-bind, $lang, $resources)"/>
                                 </resource>
                             </xsl:for-each>
                         </resources>
@@ -323,8 +322,8 @@
                         value="instance('fr-form-resources')/(resource[@xml:lang = xxf:instance('fr-language-instance')], resource[1])[1]" as="element(resource)"/>
 
                 <xf:group appearance="xxf:internal">
-                    <!-- Copy grids within section -->
-                    <xsl:copy-of select="$fr-section/(fr:grid | fb:grid)"/>
+                    <!-- Copy section content -->
+                    <xsl:copy-of select="$fr-section/*"/>
                 </xf:group>
             </xbl:template>
         </xbl:binding>

commit 51ff0c66f82112ed08a1c2a641e35b9da42c028d

Fix #1687 "Some forms fail to load and show empty"
- occurred if some binds in a library didn't have `name` attributes
- probably regressed with eb1fdfe39574e59e949d3b47bb8c6d2261c77e0a
- findBindName → getBindNameOrEmpty

src/main/scala/org/orbeon/oxf/fb/ContainerOps.scala
@@ -323,7 +323,7 @@ trait ContainerOps extends ControlOps {
 
         def holderForBind(bind: NodeInfo): NodeInfo = {
 
-            val controlName = findBindName(bind)
+            val controlName = getBindNameOrEmpty(bind)
 
             val fromBinding =
                 findCurrentBindingByName(inDoc, controlName, bindings) map

src/main/scala/org/orbeon/oxf/fb/ResourcesOps.scala
@@ -21,6 +21,7 @@ import org.apache.commons.lang3.StringUtils._
 import collection.JavaConverters._
 import java.{util ⇒ ju}
 import org.orbeon.oxf.util.ScalaUtils._
+import org.apache.commons.lang3.StringUtils
 
 trait ResourcesOps extends BaseOps {
 
@@ -217,7 +218,7 @@ trait ResourcesOps extends BaseOps {
         allLangs(resourcesRoot) map (lang ⇒ lang → ensureResourceHoldersForLang(controlName, resourceName, count, lang))
 
     def findResourceHolderForLang(controlName: String, lang: String, resources: NodeInfo) =
-        findResourceHoldersWithLang(controlName, resources) collectFirst { case (`lang`, holder) ⇒ holder }
+        findResourceHoldersWithLang(controlName ensuring (StringUtils.isNotBlank(_)), resources) collectFirst { case (`lang`, holder) ⇒ holder }
 
     // Find control resource holders
     def findResourceHolders(controlName: String): Seq[NodeInfo] =
@@ -233,7 +234,12 @@ trait ResourcesOps extends BaseOps {
 
     // For the given bind and lang, find all associated resource holders
     def iterateSelfAndDescendantBindsResourceHolders(rootBind: NodeInfo, lang: String, resources: NodeInfo) =
-        FormBuilder.iterateSelfAndDescendantBinds(rootBind) map findBindName flatMap (findResourceHolderForLang(_, lang, resources))
+        for {
+            bindNode ← FormBuilder.iterateSelfAndDescendantBinds(rootBind)
+            bindName ← findBindName(bindNode)
+            holder   ← findResourceHolderForLang(bindName, lang, resources)
+        } yield
+            holder
 
     // Same for XPath callers
     def iterateSelfAndDescendantBindsResourceHoldersXPath(rootBind: NodeInfo, lang: String, resources: NodeInfo): SequenceIterator =

src/main/scala/org/orbeon/oxf/fr/FormRunnerContainerOps.scala
@@ -99,5 +99,5 @@ trait FormRunnerContainerOps extends FormRunnerControlOps {
             bind          ← findBindByName(inDoc, controlName)
             iterationBind ← bind / "*:bind" headOption // there should be only a single nested bind
         } yield
-            findBindName(iterationBind)
+            getBindNameOrEmpty(iterationBind)
 }

src/main/scala/org/orbeon/oxf/fr/FormRunnerControlOps.scala
@@ -93,8 +93,11 @@ trait FormRunnerControlOps extends FormRunnerBaseOps {
         hasIdValue(bind, bindId(name)) || bindRefOrNodeset(bind) == Some(name) // also check ref/nodeset in case id is not present
 
     // Canonical way: use the `name` attribute
-    def findBindName(bind: NodeInfo) =
+    def getBindNameOrEmpty(bind: NodeInfo) =
         bind attValue "name"
+
+    def findBindName(bind: NodeInfo) =
+        bind attValueOpt "name"
 
     def hasHTMLMediatype(nodes: Seq[NodeInfo]) =
         nodes exists (element ⇒ (element attValue "mediatype") == "text/html")

src/main/scala/org/orbeon/scaxon/XML.scala
@@ -289,6 +289,7 @@ object XML {
         def \\(test: Test): Seq[NodeInfo] = find(Axis.DESCENDANT, test)
 
         // Return an element's attributes
+        // Q: Should functions taking a String match on no namespace only?
         def /@(attName: String): Seq[NodeInfo] = /@(new NodeLocalNameTest(attName, Some(Type.ATTRIBUTE)))
         def \@(attName: String): Seq[NodeInfo] = /@(attName)
         def /@(attName: QName): Seq[NodeInfo] = /@(new NodeQNameTest((attName.getNamespaceURI, attName.getName), Some(Type.ATTRIBUTE)))
@@ -317,6 +318,18 @@ object XML {
         def attTokens(attName: String) = stringOptionToSet(Some(attValue(attName)))
         def attClasses = attTokens("class")
         def id = attValue("id")
+
+        def attValueOpt(attName: String) = /@(attName) match {
+            case Seq() ⇒ None
+            case s     ⇒ Some(s.stringValue)
+        }
+
+        def attValueOpt(attName: QName) = /@(attName) match {
+            case Seq() ⇒ None
+            case s     ⇒ Some(s.stringValue)
+        }
+
+        def idOpt = attValueOpt("id")
 
         def self(test: Test) = find(Axis.SELF, test)
         def parent(test: Test) = find(Axis.PARENT, test)
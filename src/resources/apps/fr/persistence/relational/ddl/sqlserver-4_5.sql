da778e7c7a3ed697360f7a7c3e2df2c252b66ce3
avernet <avernet@orbeon.com>
2014-04-29 15:09:58 -0700

    Switching to nvarchar for better Unicode support

--- src/resources/apps/fr/persistence/relational/ddl/sqlserver-4_5.sql
+++ src/resources/apps/fr/persistence/relational/ddl/sqlserver-4_5.sql
@@ -1,9 +1,9 @@
 create table orbeon_form_definition (
     created            datetime,
     last_modified_time datetime,
-    last_modified_by   varchar(255),
-    app                varchar(255),
-    form               varchar(255),
+    last_modified_by   nvarchar(255),
+    app                nvarchar(255),
+    form               nvarchar(255),
     form_version       int not null,
     deleted            char(1) not null,
     xml                xml
@@ -12,25 +12,25 @@ create table orbeon_form_definition (
 create table orbeon_form_definition_attach (
     created            datetime,
     last_modified_time datetime,
-    last_modified_by   varchar(255),
-    app                varchar(255),
-    form               varchar(255),
+    last_modified_by   nvarchar(255),
+    app                nvarchar(255),
+    form               nvarchar(255),
     form_version       int not null,
     deleted            char(1) not null,
-    file_name          varchar(255),
+    file_name          nvarchar(255),
     file_content       varbinary(max)
 );
 
 create table orbeon_form_data (
     created            datetime,
     last_modified_time datetime,
-    last_modified_by   varchar(255),
-    username           varchar(255),
-    groupname          varchar(255),
-    app                varchar(255),
-    form               varchar(255),
+    last_modified_by   nvarchar(255),
+    username           nvarchar(255),
+    groupname          nvarchar(255),
+    app                nvarchar(255),
+    form               nvarchar(255),
     form_version       int not null,
-    document_id        varchar(255),
+    document_id        nvarchar(255),
     draft              char(1) not null,
     deleted            char(1) not null,
     xml                xml
@@ -39,15 +39,15 @@ create table orbeon_form_data (
 create table orbeon_form_data_attach (
     created            datetime,
     last_modified_time datetime,
-    last_modified_by   varchar(255),
-    username           varchar(255),
-    groupname          varchar(255),
-    app                varchar(255),
-    form               varchar(255),
+    last_modified_by   nvarchar(255),
+    username           nvarchar(255),
+    groupname          nvarchar(255),
+    app                nvarchar(255),
+    form               nvarchar(255),
     form_version       int not null,
-    document_id        varchar(255),
+    document_id        nvarchar(255),
     draft              char(1) not null,
     deleted            char(1) not null,
-    file_name          varchar(255),
+    file_name          nvarchar(255),
     file_content       varbinary(max)
 );

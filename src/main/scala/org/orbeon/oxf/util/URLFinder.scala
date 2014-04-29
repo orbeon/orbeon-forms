ab82c91eeec2f559ab6c9cc582da8aac5e4da275
ebruchez <ebruchez@orbeon.com>
2014-04-29 16:00:54 -0700

    More readable regex for #1694
    - also relax domain names

--- src/main/scala/org/orbeon/oxf/util/URLFinder.scala
+++ src/main/scala/org/orbeon/oxf/util/URLFinder.scala
@@ -39,11 +39,63 @@ object URLFinder {
     // - https://gist.github.com/gruber/8891611
     // - https://gist.github.com/winzig/8894715
     // - https://gist.github.com/dperini/729294
+    // - https://gist.github.com/HenkPoley/8899766
     // - http://mathiasbynens.be/demo/url-regex
     // - http://developer.android.com/reference/android/text/util/Linkify.html
     // - https://github.com/android/platform_frameworks_base/blob/master/core/java/android/util/Patterns.java
     // - http://blog.codinghorror.com/the-problem-with-urls/
 
+    private val RegexpDomains =
+        """[a-z]{2,13}"""
+
+    private val BalancedParensOneLevelDeep =
+        """\([^\s()]*?\([^\s()]+\)[^\s()]*?\)"""
+
+    private val BalancedParens =
+        """\([^\s]+?\)"""
+
+    // NOTE: String interpolation works bizarrely with triple quotes: backslash escapes are interpreted. So we escape
+    // the backslashes.
     private val URLMatchRegex =
-        """(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))""".r
+        s"""(?xi)
+            \\b
+            (
+              (?:
+                https?:
+                (?:
+                  /{1,3}
+                  |
+                  [a-z0-9%]
+                )
+                |
+                [a-z0-9.\\-]+[.]
+                (?:$RegexpDomains)
+                /
+              )
+              (?:
+                [^\\s()<>{}\\[\\]]+
+                |
+                $BalancedParensOneLevelDeep
+                |
+                $BalancedParens
+              )+
+              (?:
+                $BalancedParensOneLevelDeep
+                |
+                $BalancedParens
+                |
+                [^\\s`!()\\[\\]{};:'".,<>?«»“”‘’]
+              )
+              |
+              (?:
+                (?<!@)
+                [a-z0-9]+
+                (?:[.\\-][a-z0-9]+)*
+                [.]
+                (?:$RegexpDomains)
+                \\b
+                /?
+                (?!@)
+              )
+            )""".r
 }

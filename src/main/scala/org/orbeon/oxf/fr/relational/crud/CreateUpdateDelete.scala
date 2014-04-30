orbeon/orbeon-forms

commit 74b621b511e4b9db6ce679f29533d6764fa12e15

For SQL Server #697, use inner join instead of IN

src/main/scala/org/orbeon/oxf/fr/relational/crud/CreateUpdateDelete.scala
@@ -67,21 +67,21 @@ trait CreateUpdateDelete extends RequestResponse with Common {
         val table  = tableName(req)
         val resultSet = {
             val ps = connection.prepareStatement(
-                s"""|select created ${req.forData.option(", username , groupname, form_version").mkString}
-                    |  from $table
-                    | where (last_modified_time, $idCols)
-                    |       in
+                s"""|SELECT created
+                    |       ${if (req.forData) ", username , groupname, form_version" else ""}
+                    |FROM   $table t,
                     |       (
-                    |             select max(last_modified_time) last_modified_time, $idCols
-                    |               from $table
-                    |              where app  = ?
+                    |           SELECT   max(last_modified_time) last_modified_time, ${idCols.mkString(", ")}
+                    |           FROM     $table
+                    |           WHERE    app  = ?
                     |                    and form = ?
                     |                    ${if (! req.forData)     "and form_version = ?" else ""}
                     |                    ${if (req.forData)       "and document_id  = ?" else ""}
                     |                    ${if (req.forAttachment) "and file_name    = ?" else ""}
-                    |           group by $idCols
-                    |       )
-                    |       and deleted = 'N'
+                    |           GROUP BY ${idCols.mkString(", ")}
+                    |       ) m
+                    |WHERE  ${joinColumns("last_modified_time" +: idCols, "t", "m")}
+                    |       AND deleted = 'N'
                     |""".stripMargin)
             val position = Iterator.from(1)
             ps.setString(position.next(), req.app)
@@ -115,28 +115,28 @@ trait CreateUpdateDelete extends RequestResponse with Common {
         locally {
             val xmlCol = if (req.provider == "oracle") "xml_clob" else "xml"
             val ps = connection.prepareStatement(
-                s"""|insert into $table
-                    |(
-                    |                                   created, last_modified_time, last_modified_by
-                    |                                 , app, form, form_version
-                    |    ${if (req.forData)          ", document_id"             else ""}
-                    |                                 , deleted
-                    |    ${if (req.forData)          ", draft"                   else ""}
-                    |    ${if (req.forAttachment)    ", file_name, file_content" else ""}
-                    |    ${if (! req.forAttachment) s", $xmlCol"                 else ""}
-                    |    ${if (req.forData)          ", username, groupname"     else ""}
-                    |)
-                    |values
-                    |(
-                    |                               ?, ?, ?
-                    |                               , ?, ?, ?
-                    |    ${if (req.forData)         ", ?"    else ""}
-                    |                               , ${if (delete) "'Y'" else "'N'"}
-                    |    ${if (req.forData)         ", ?"    else ""}
-                    |    ${if (req.forAttachment)   ", ?, ?" else ""}
-                    |    ${if (! req.forAttachment) ", ?" else ""}
-                    |    ${if (req.forData)         ", ?, ?" else ""}
-                    |)
+                s"""|INSERT INTO $table
+                    |   (
+                    |                                      created, last_modified_time, last_modified_by
+                    |                                    , app, form, form_version
+                    |       ${if (req.forData)          ", document_id"             else ""}
+                    |                                    , deleted
+                    |       ${if (req.forData)          ", draft"                   else ""}
+                    |       ${if (req.forAttachment)    ", file_name, file_content" else ""}
+                    |       ${if (! req.forAttachment) s", $xmlCol"                 else ""}
+                    |       ${if (req.forData)          ", username, groupname"     else ""}
+                    |   )
+                    |VALUE
+                    |   (
+                    |                                  ?, ?, ?
+                    |                                  , ?, ?, ?
+                    |       ${if (req.forData)         ", ?"    else ""}
+                    |                                  , ${if (delete) "'Y'" else "'N'"}
+                    |       ${if (req.forData)         ", ?"    else ""}
+                    |       ${if (req.forAttachment)   ", ?, ?" else ""}
+                    |       ${if (! req.forAttachment) ", ?" else ""}
+                    |       ${if (req.forData)         ", ?, ?" else ""}
+                    |   )
                     |""".stripMargin)
 
             val position = Iterator.from(1)
@@ -169,12 +169,11 @@ trait CreateUpdateDelete extends RequestResponse with Common {
         if (req.forData && ! req.dataPart.get.isDraft && ! req.forAttachment) {
             for (table ←  Set("orbeon_form_data", "orbeon_form_data_attach")) {
                 val ps = connection.prepareStatement(
-                    s"""|delete from $table
-                        |where
-                        |    app             = ?
-                        |    and form        = ?
-                        |    and document_id = ?
-                        |    and draft       = 'Y'
+                    s"""|DELETE FROM $table
+                        |WHERE  app             = ?
+                        |       AND form        = ?
+                        |       AND document_id = ?
+                        |       AND draft       = 'Y'
                         |""".stripMargin)
                 val position = Iterator.from(1)
                 ps.setString(position.next(), req.app)

src/main/scala/org/orbeon/oxf/fr/relational/crud/Common.scala
@@ -43,18 +43,18 @@ trait Common extends RequestResponse with FormRunnerPersistence {
         val versionResult = {
             val table = s"orbeon_form_${if (docId.isEmpty) "definition" else "data"}"
             val ps = connection.prepareStatement(
-                s"""|select max(form_version)
-                    |  from $table
-                    | where (last_modified_time, app, form, form_version) in
+                s"""|SELECT max(t.form_version)
+                    |FROM   $table t,
                     |       (
-                    |             select max(last_modified_time) last_modified_time, app, form, form_version
-                    |               from $table
-                    |              where app = ?
-                    |                    and form = ?
+                    |           SELECT   max(last_modified_time) last_modified_time, app, form, form_version
+                    |           FROM     $table
+                    |           WHERE    app = ?
+                    |                    AND form = ?
                     |                    ${docId.map(_ ⇒ "and document_id = ?").getOrElse("")}
-                    |           group by app, form, form_version
-                    |       )
-                    |   and deleted = 'N'
+                    |           GROUP BY app, form, form_version
+                    |       ) m
+                        |WHERE  ${joinColumns(Seq("last_modified_time", "app", "form", "form_version"), "t", "m")}
+                    |       AND t.deleted = 'N'
                     |""".stripMargin)
                           ps.setString(1, app)
                           ps.setString(2, form)
@@ -84,14 +84,17 @@ trait Common extends RequestResponse with FormRunnerPersistence {
     }
 
     // List of columns that identify a row
-    def idColumns(req: Request): String =
-        Seq(
+    def idColumns(req: Request): List[String] =
+        List(
             Some("app"), Some("form"),
             req.forForm       option "form_version",
             req.forData       option "document_id",
             req.forData       option "draft",
             req.forAttachment option "file_name"
-        ).flatten.mkString(", ")
+        ).flatten
+
+    def idColumnsList(req: Request): String = idColumns(req).mkString(", ")
+    def joinColumns(cols: Seq[String], t1: String, t2: String) = cols.map(c ⇒ s"$t1.$c = $t2.$c").mkString(" AND ")
 
     // Given a user/group name coming from the data, tells us what operations we can do in this data, assuming that
     // it is for the current request app/form

src/main/scala/org/orbeon/oxf/fr/relational/crud/Read.scala
@@ -40,24 +40,22 @@ trait Read extends RequestResponse with Common with FormRunnerPersistence {
                     case _        ⇒ "t.xml"
                 }
                 val ps = connection.prepareStatement(
-                    s"""|select
-                        |    last_modified_time
-                        |    ${if (req.forAttachment) ", file_content"          else s", $xmlCol xml"}
-                        |    ${if (req.forData)       ", username, groupname"   else ""}
-                        |    , form_version, deleted
-                        |from $table t
-                        |    where (last_modified_time, $idCols)
-                        |          in
-                        |          (
-                        |              select max(last_modified_time) last_modified_time, $idCols
-                        |                from $table
-                        |               where app  = ?
+                    s"""|SELECT  t.last_modified_time
+                        |        ${if (req.forAttachment) ", t.file_content"            else s", $xmlCol xml"}
+                        |        ${if (req.forData)       ", t.username, t.groupname"   else ""}
+                        |        , t.form_version, t.deleted
+                        |FROM    $table t,
+                        |        (
+                        |            SELECT   max(last_modified_time) last_modified_time, ${idCols.mkString(", ")}
+                        |              FROM   $table
+                        |             WHERE   app  = ?
                         |                     and form = ?
                         |                     ${if (req.forForm)       "and form_version = ?"              else ""}
                         |                     ${if (req.forData)       "and document_id = ? and draft = ?" else ""}
                         |                     ${if (req.forAttachment) "and file_name = ?"                 else ""}
-                        |            group by $idCols
-                        |          )
+                        |            GROUP BY ${idCols.mkString(", ")}
+                        |        ) m
+                        |WHERE   ${joinColumns("last_modified_time" +: idCols, "t", "m")}
                         |""".stripMargin)
 
                 val position = Iterator.from(1)

commit 0cc59e910273f2afc46758d7701b52cdc5d924ae

Fix typo introduced in 74b621b5.

src/main/scala/org/orbeon/oxf/fr/relational/crud/CreateUpdateDelete.scala
@@ -126,7 +126,7 @@ trait CreateUpdateDelete extends RequestResponse with Common {
                     |       ${if (! req.forAttachment) s", $xmlCol"                 else ""}
                     |       ${if (req.forData)          ", username, groupname"     else ""}
                     |   )
-                    |VALUE
+                    |VALUES
                     |   (
                     |                                  ?, ?, ?
                     |                                  , ?, ?, ?
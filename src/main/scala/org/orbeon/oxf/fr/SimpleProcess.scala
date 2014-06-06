380706e69693acb6997c2404838dfa71fb7c579a
ebruchez <ebruchez@orbeon.com>
2014-06-06 09:34:09 -0700

    Backward compatibility trampoline for SimpleProcess

---
+++ src/main/scala/org/orbeon/oxf/fr/SimpleProcess.scala
@@ -0,0 +1,12 @@
+package org.orbeon.oxf.fr
+import process.{SimpleProcess ⇒ NewSimpleProcess}
+
+// Trampoline for backward compatibility. See also:
+// https://github.com/orbeon/orbeon-forms/issues/1095
+object SimpleProcess {
+    def runProcessByName(scope: String, name: String) =
+        NewSimpleProcess.runProcessByName(scope, name)
+
+    def runProcess(scope: String, process: String) =
+        NewSimpleProcess.runProcess(scope, process)
+}

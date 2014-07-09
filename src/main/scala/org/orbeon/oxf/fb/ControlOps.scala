orbeon/orbeon-forms

commit 675de9649ea9a2c7b4d006956ade265d8909395a

Make control children point to resources, for #507

src/main/scala/org/orbeon/oxf/fb/ControlOps.scala
@@ -32,6 +32,7 @@ import org.orbeon.oxf.xml.dom4j.Dom4jUtils
 import org.orbeon.oxf.fr.FormRunner._
 import org.orbeon.oxf.xforms.XFormsUtils._
 import org.orbeon.oxf.util.ScalaUtils._
+import org.orbeon.oxf.fr.FormRunner
 
 /*
  * Form Builder: operations on controls.
@@ -215,15 +216,18 @@ trait ControlOps extends SchemaOps with ResourcesOps {
             setvalue(controlElement \@ attName, makeInstanceExpression(templateId(newName)))
 
         // Set xf:label, xf:hint, xf:help and xf:alert @ref if present
-        // FIXME: This code is particularly ugly!
-        controlElement \ * filter (e ⇒ LHHANames(e.localname)) map
-            (e ⇒ (e \@ "ref", e.localname)) filter
-                (_._1 nonEmpty) filter { e ⇒
-                    val ref = e._1.stringValue
-                    ref.isEmpty || ref.startsWith("$form-resources")
-                } foreach { e ⇒
-                    setvalue(e._1, s"$$form-resources/$newName/${e._2}")
-                }
+        val resourcesNames = currentResources.child(newName).child(*).map(_.localname).toSet
+        for {
+            resourcePointer ← controlElement.child(*)
+            resourceName = resourcePointer.localname
+            if resourcesNames(resourceName)                             // We have a resource for this sub-element
+            ref = resourcePointer.att("ref").headOption
+            if ref.nonEmpty                                             // If no ref, value might be inline
+            refVal = ref.head.stringValue
+            if refVal.isEmpty || refVal.startsWith("$form-resources")   // Don't overwrite ref pointing somewhere else
+        } locally {
+            setvalue(ref.toSeq, s"$$form-resources/$newName/$resourceName")
+        }
 
         // If using a static itemset editor, set xf:itemset/@ref xf:itemset/@nodeset value
         if (hasEditor(controlElement, "static-itemset"))

src/main/scala/org/orbeon/oxf/fb/ToolboxOps.scala
@@ -69,44 +69,44 @@ object ToolboxOps {
                 // Set default pointer to resources if there is an xf:alert
                 setvalue(newControlElement \ "*:alert" \@ "ref", OldStandardAlertRef)
 
-                // Adjust bindings on newly inserted control
-                renameControlByElement(newControlElement, newControlName)
-
                 // Data holder may contain file attributes
                 val dataHolder = newDataHolder(newControlName, binding)
 
-                val formLanguages = allLangs(formResourcesRoot)
-                val resourceHolders = formLanguages map { formLang ⇒
+                // Create resource holder for all form languages
+                val resourceHolders = {
+                    val formLanguages = allLangs(formResourcesRoot)
+                    formLanguages map { formLang ⇒
                     // Elements for LHHA resources, only keeping those referenced from the view (e.g. a button has no hint)
-                    val lhhaResourceEls = {
-                        val lhhaNames = newControlElement \ * map (_.localname) filter LHHAResourceNamesToInsert
-                        lhhaNames map (elementInfo(_))
-                    }
-
-                    // Resource holders from XBL metadata
-                    val xblResourceEls = binding / "*:metadata" / "*:templates" / "*:resources" / *
-
-                    // Template items, if needed
-                    val itemsResourceEls =
-                        if (hasEditor(newControlElement, "static-itemset")) {
-                            val fbResourceInFormLang = FormRunner.formResourcesInLang(formLang)
-                            val originalTemplateItems = fbResourceInFormLang \ "template" \ "items" \ "item"
-                            if (hasEditor(newControlElement, "item-hint")) {
-                                // Supports hint: keep hint we have in the resources.xml
-                                originalTemplateItems
-                            }  else {
-                                // Hint not supported: <hint> in each <item>
-                                originalTemplateItems map { item ⇒
-                                    val newLHHA = (item / *) filter (_.localname != "hint")
-                                    elementInfo("item", newLHHA)
-                                }
-                            }
-                        } else {
-                            Seq.empty
+                        val lhhaResourceEls = {
+                            val lhhaNames = newControlElement \ * map (_.localname) filter LHHAResourceNamesToInsert
+                            lhhaNames map (elementInfo(_))
                         }
 
-                    val resourceEls = lhhaResourceEls ++ xblResourceEls ++ itemsResourceEls
-                    formLang → elementInfo(newControlName, resourceEls)
+                        // Resource holders from XBL metadata
+                        val xblResourceEls = binding / "*:metadata" / "*:templates" / "*:resources" / *
+
+                        // Template items, if needed
+                        val itemsResourceEls =
+                            if (hasEditor(newControlElement, "static-itemset")) {
+                                val fbResourceInFormLang = FormRunner.formResourcesInLang(formLang)
+                                val originalTemplateItems = fbResourceInFormLang \ "template" \ "items" \ "item"
+                                if (hasEditor(newControlElement, "item-hint")) {
+                                    // Supports hint: keep hint we have in the resources.xml
+                                    originalTemplateItems
+                                }  else {
+                                    // Hint not supported: <hint> in each <item>
+                                    originalTemplateItems map { item ⇒
+                                        val newLHHA = (item / *) filter (_.localname != "hint")
+                                        elementInfo("item", newLHHA)
+                                    }
+                                }
+                            } else {
+                                Seq.empty
+                            }
+
+                        val resourceEls = lhhaResourceEls ++ xblResourceEls ++ itemsResourceEls
+                        formLang → elementInfo(newControlName, resourceEls)
+                    }
                 }
 
                 // Insert data and resource holders
@@ -116,6 +116,10 @@ object ToolboxOps {
                     resourceHolders,
                     precedingControlNameInSectionForControl(newControlElement)
                 )
+
+                // Adjust bindings on newly inserted control, done after the control is added as
+                // renameControlByElement() expects resources to be present
+                renameControlByElement(newControlElement, newControlName)
 
                 // Insert the bind element
                 val bind = ensureBinds(doc, findContainerNames(gridTd) :+ newControlName)

src/test/scala/org/orbeon/oxf/fb/FormBuilderFunctionsTest.scala
@@ -25,6 +25,7 @@ import org.orbeon.saxon.om._
 import org.orbeon.scaxon.XML._
 import org.scalatest.junit.AssertionsForJUnit
 import org.orbeon.oxf.xml.TransformerUtils
+import org.orbeon.oxf.fr.FormRunner
 
 class FormBuilderFunctionsTest extends DocumentTestBase with FormBuilderSupport with AssertionsForJUnit {
 
@@ -156,15 +157,28 @@ class FormBuilderFunctionsTest extends DocumentTestBase with FormBuilderSupport 
         withActionAndFBDoc(TemplateDoc) { doc ⇒
 
             // Insert explanation control
-            val selectionControls = TransformerUtils.urlToTinyTree("oxf:/xbl/orbeon/explanation/explanation.xbl")
-            val explanationBinding = selectionControls.rootElement.child("binding").head
-            ToolboxOps.insertNewControl(doc, explanationBinding)
+            val frExplanation = {
+                val selectionControls = TransformerUtils.urlToTinyTree("oxf:/xbl/orbeon/explanation/explanation.xbl")
+                val explanationBinding = selectionControls.rootElement.child("binding").head
+                ToolboxOps.insertNewControl(doc, explanationBinding)
+                doc.descendant("*:explanation").head
+            }
 
             // Check resource holder just contains <text>, taken from the XBL metadata
-            val explanationResourceHolder = FormBuilder.resourcesRoot.child("resource").child(*).last
-            val actual   = <holder> { explanationResourceHolder.child(*) map nodeInfoToElem } </holder>
-            val expected = <holder><text/></holder>
-            assertXMLDocuments(actual, expected)
+            locally {
+                val explanationResourceHolder = FormBuilder.resourcesRoot.child("resource").child(*).last
+                val actual   = <holder> { explanationResourceHolder.child(*) map nodeInfoToElem } </holder>
+                val expected = <holder><text/></holder>
+                assertXMLDocuments(actual, expected)
+            }
+
+            // Check that the <fr:text ref=""> points to the corresponding <text> resource
+            locally {
+                val controlName = FormRunner.controlName(frExplanation.attValue("id"))
+                val actualRef = frExplanation.child("*:text").head.attValue("ref")
+                val expectedRef = "$form-resources/" ++ controlName ++ "/text"
+                assert(actualRef === expectedRef)
+            }
         }
 
     @Test def insertRepeat(): Unit =

commit 8e2acccc42269f20027418ae02f36d168cd0fa66

Fix #1820 "Copy/paste of control doesn't copy LHHA references correctly"
- regressed with 675de9649ea9a2c7b4d006956ade265d8909395a
- control w/ new name didn’t have resources yet, so resource names were empty
- add example of xcv instance

src/main/scala/org/orbeon/oxf/fb/ControlOps.scala
@@ -32,7 +32,6 @@ import org.orbeon.oxf.xml.dom4j.Dom4jUtils
 import org.orbeon.oxf.fr.FormRunner._
 import org.orbeon.oxf.xforms.XFormsUtils._
 import org.orbeon.oxf.util.ScalaUtils._
-import org.orbeon.oxf.fr.FormRunner
 
 /*
  * Form Builder: operations on controls.
@@ -202,10 +201,13 @@ trait ControlOps extends SchemaOps with ResourcesOps {
         }
 
     def renameControl(inDoc: NodeInfo, oldName: String, newName: String): Unit =
-        findControlByName(inDoc, oldName) foreach (renameControlByElement(_, newName))
+        findControlByName(inDoc, oldName) foreach (renameControlByElement(_, newName, resourceNamesInUseForControl(newName)))
+
+    def resourceNamesInUseForControl(controlName: String) =
+        currentResources.child(controlName).child(*).map(_.localname).to[Set]
 
     // Rename the control (but NOT its holders, binds, etc.)
-    def renameControlByElement(controlElement: NodeInfo, newName: String): Unit = {
+    def renameControlByElement(controlElement: NodeInfo, newName: String, resourcesNames: Set[String]): Unit = {
 
         // Set @id in any case, @ref value if present, @bind value if present
         ensureAttribute(controlElement, "id", controlId(newName))
@@ -216,7 +218,6 @@ trait ControlOps extends SchemaOps with ResourcesOps {
             setvalue(controlElement \@ attName, makeInstanceExpression(templateId(newName)))
 
         // Set xf:label, xf:hint, xf:help and xf:alert @ref if present
-        val resourcesNames = currentResources.child(newName).child(*).map(_.localname).toSet
         for {
             resourcePointer ← controlElement.child(*)
             resourceName = resourcePointer.localname

src/main/scala/org/orbeon/oxf/fb/ToolboxOps.scala
@@ -119,7 +119,7 @@ object ToolboxOps {
 
                 // Adjust bindings on newly inserted control, done after the control is added as
                 // renameControlByElement() expects resources to be present
-                renameControlByElement(newControlElement, newControlName)
+                renameControlByElement(newControlElement, newControlName, resourceNamesInUseForControl(newControlName))
 
                 // Insert the bind element
                 val bind = ensureBinds(doc, findContainerNames(gridTd) :+ newControlName)
@@ -415,7 +415,38 @@ object ToolboxOps {
 
             val xvc = asNodeInfo(topLevelModel("fr-form-model").get.getVariable("xcv"))
 
+            /* Example layout:
+            <xcv>
+                <control>
+                    <xf:input id="control-1-control" bind="control-1-bind">
+                        <xf:label ref="$form-resources/control-1/label"/>
+                        <xf:hint ref="$form-resources/control-1/hint"/>
+                        <xf:alert ref="$fr-resources/detail/labels/alert"/>
+                    </xf:input>
+                </control>
+                <holder>
+                    <control-1/>
+                </holder>
+                <resources>
+                    <resource xml:lang="en">
+                        <control-1>
+                            <label>My label</label>
+                            <hint/>
+                            <alert/>
+                        </control-1>
+                    </resource>
+                </resources>
+                <bind>
+                    <xf:bind id="control-1-bind" name="control-1" ref="control-1"/>
+                </bind>
+            </xcv>
+            */
+
             (xvc \ "control" \ * headOption) foreach { control ⇒
+
+                def holders   = xvc \ "holder" \ *
+                def resources = xvc \ "resources" \ "resource" \ *
+
                 val name = {
                     val requestedName = getControlName(control)
 
@@ -425,10 +456,11 @@ object ToolboxOps {
                         val newName = controlNameFromId(nextId(td, "control"))
 
                         // Rename everything
-                        // TODO: don't rename in place
-                        renameControlByElement(control, newName)
+                        // LATER: Don't rename in place as it would be better to keep the original name when possible,
+                        // e.g. copy, paste remove first control, paste again.
+                        renameControlByElement(control, newName, resources \ * map (_.localname) toSet)
 
-                        (xvc \ "holder" \ *) ++ (xvc \ "resources" \ "resource" \ *) foreach
+                        holders ++ resources foreach
                             (rename(_, newName))
 
                         (xvc \ "bind" \ * headOption) foreach
@@ -443,8 +475,8 @@ object ToolboxOps {
                 val newControlElement = insert(into = gridTd, origin = control).head
                 insertHolders(
                     newControlElement,
-                    xvc \ "holder" \ * head,
-                    xvc \ "resources" \ "resource" map (r ⇒ (r \@ "*:lang" stringValue, r \ * head)),
+                    holders.head,
+                    xvc \ "resources" \ "resource" map (r ⇒ (r attValue "*:lang", r \ * head)),
                     precedingControlNameInSectionForControl(newControlElement)
                 )
 
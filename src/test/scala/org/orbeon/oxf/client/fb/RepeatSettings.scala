0664c78ecea769ea5deda0c1c0de008c0e21765d
avernet <avernet@orbeon.com>
2014-11-06 17:07:34 -0800

    Fix test after implementing #635

--- src/test/scala/org/orbeon/oxf/client/fb/RepeatSettings.scala
+++ src/test/scala/org/orbeon/oxf/client/fb/RepeatSettings.scala
@@ -22,42 +22,49 @@ trait RepeatSettings extends AssertionsForJUnit with FormBuilderOps with XFormsO
 
     private object Private {
 
-        val GridDetailsDialogPrefix = "dialog-grid-details≡"
+        val GridDetailsDialogPrefix    = "dialog-container-details≡"
+        val RepeatedContentTab         = s"${GridDetailsDialogPrefix}tabs≡repeat-tab-grid≡"
+        val SwitchToRepeatedContentTab = "a[href = '#dialog-container-details≡tabs≡tab-pane-2']"
 
-        val MinRepeatSelect1Id      = s"${GridDetailsDialogPrefix}min-repeat"
-        val MaxRepeatSelect1Id      = s"${GridDetailsDialogPrefix}max-repeat"
+        val MinRepeatSelect1Id         = s"${RepeatedContentTab}min-repeat"
+        val MaxRepeatSelect1Id         = s"${RepeatedContentTab}max-repeat"
 
-        val SaveButtonSelector      = cssSelector(s"#${GridDetailsDialogPrefix}save-button button")
-        val MinInputSelector        = cssSelector(s"#${GridDetailsDialogPrefix}min-input")
-        val MaxInputSelector        = cssSelector(s"#${GridDetailsDialogPrefix}max-input")
+        val SaveButtonSelector         = cssSelector(s"#${GridDetailsDialogPrefix}save-button button")
+        val MinInputSelector           = cssSelector(s"#${RepeatedContentTab}min-input")
+        val MaxInputSelector           = cssSelector(s"#${RepeatedContentTab}max-input")
     }
 
     import Private._
 
     @Test def minMaxSettings(): Unit = {
 
+        def openGridDetails()  = Builder.openGridDetails("fb≡section-1-control≡grid-3-grid")
+        def closeGridDetails() = clickOn(SaveButtonSelector)
+
         def saveAndCheck(min: Int, max: Int) =
             for {
+                _ ← openGridDetails()
+                _ ← clickOn(cssSelector(SwitchToRepeatedContentTab))
                 _ ← clickOn(XForms.fullItemSelector(MinRepeatSelect1Id, min))
                 _ ← clickOn(XForms.fullItemSelector(MaxRepeatSelect1Id, max))
-                minValue      = min.toString
-                maxValue      = ((min + 1) max 1).toString
+                minValue = min.toString
+                maxValue = ((min + 1) max 1).toString
                 checkMinField = min >= 2
                 checkMaxField = max >= 1
                 _ ← if (checkMinField) MinInputSelector.element.replaceFieldText(minValue)
                 _ ← if (checkMaxField) MaxInputSelector.element.replaceFieldText(maxValue)
-                _ ← clickOn(SaveButtonSelector)
-                _ ← Builder.openGridDetails("fb≡section-1-control≡grid-3-grid")
+                _ ← closeGridDetails()
+                _ ← openGridDetails()
                 _ ← assert(radioButton(XForms.fullItemSelector(MinRepeatSelect1Id, min)).isSelected)
                 _ ← assert(radioButton(XForms.fullItemSelector(MaxRepeatSelect1Id, max)).isSelected)
                 _ ← if (checkMinField) assert(minValue === MinInputSelector.element.fieldText)
                 _ ← if (checkMaxField) assert(maxValue === MaxInputSelector.element.fieldText)
+                _ ← closeGridDetails()
             }()
 
         Builder.onNewForm {
             for {
                 _ ← Builder.insertNewRepeatedGrid()
-                _ ← Builder.openGridDetails("fb≡section-1-control≡grid-3-grid")
             }()
 
             for {

43623fcaeddc78cc3b62c6545469bf0c056e2098
ebruchez <ebruchez@orbeon.com>
2015-04-01 17:25:24 -0700

    Minor: indent

--- src/main/scala/org/orbeon/oxf/xforms/action/actions/XFormsSetindexAction.scala
+++ src/main/scala/org/orbeon/oxf/xforms/action/actions/XFormsSetindexAction.scala
@@ -15,11 +15,12 @@ package org.orbeon.oxf.xforms.action.actions
 
 import org.dom4j.Element
 import org.orbeon.oxf.common.OXFException
-import org.orbeon.oxf.xforms.control.controls.XFormsRepeatControl
 import org.orbeon.oxf.xforms.action.{DynamicActionContext, XFormsAction, XFormsActionInterpreter}
-import org.orbeon.oxf.xforms.event.events.XXFormsSetindexEvent
+import org.orbeon.oxf.xforms.control.controls.XFormsRepeatControl
 import org.orbeon.oxf.xforms.control.{Focus, XFormsControl}
 import org.orbeon.oxf.xforms.event.Dispatch
+import org.orbeon.oxf.xforms.event.events.XXFormsSetindexEvent
+
 import scala.util.control.NonFatal
 
 /**
@@ -27,7 +28,7 @@ import scala.util.control.NonFatal
  */
 class XFormsSetindexAction extends XFormsAction {
 
-    override def execute(context: DynamicActionContext) {
+    override def execute(context: DynamicActionContext): Unit = {
 
         val interpreter = context.interpreter
         val element = context.analysis.element
@@ -35,14 +36,20 @@ class XFormsSetindexAction extends XFormsAction {
         // Get the repeat static id
         val repeatStaticId =
             resolveStringAVT("repeat")(context) getOrElse
-                (throw new OXFException("Cannot resolve mandatory '" + "repeat" + "' attribute on " + context.actionName + " action."))
+                (throw new OXFException(s"Cannot resolve mandatory 'repeat' attribute on ${context.actionName} action."))
 
         // Determine the index
         val index = {
 
             val indexXPath = element.attributeValue("index")
             val contextStack = interpreter.actionXPathContext
-            val indexString = interpreter.evaluateAsString(element, contextStack.getCurrentBindingContext.nodeset, contextStack.getCurrentBindingContext.position, "number(" + indexXPath + ")")
+
+            val indexString = interpreter.evaluateAsString(
+                element,
+                contextStack.getCurrentBindingContext.nodeset,
+                contextStack.getCurrentBindingContext.position,
+                "number(" + indexXPath + ")"
+            )
 
             try indexString.toInt
             catch { case NonFatal(_) ⇒ return } // "If the index evaluates to NaN the action has no effect."
@@ -55,7 +62,13 @@ class XFormsSetindexAction extends XFormsAction {
 
 object XFormsSetindexAction {
     
-    def executeSetindexAction(interpreter: XFormsActionInterpreter, actionElement: Element, repeatStaticId: String, index: Int) = {
+    def executeSetindexAction(
+        interpreter    : XFormsActionInterpreter,
+        actionElement  : Element,
+        repeatStaticId : String,
+        index          : Int
+    ): Int = {
+
         val indentedLogger = interpreter.indentedLogger
         
         // "This XForms Action begins by invoking the deferred update behavior."
@@ -81,14 +94,19 @@ object XFormsSetindexAction {
                 // Handle focus changes
                 Focus.updateFocusWithEvents(focusedBefore)
                 
-                // However at this time return the index only for repeat controls as we don't have a generic way to figure this out yet
+                // However at this time return the index only for repeat controls as we don't have a generic way to
+                // figure this out yet
                 repeatControl map (_.getIndex) getOrElse -1
                 
             case _ ⇒
-                // "If there is a null search result for the target object and the source object is an XForms action such as
-                // dispatch, send, setfocus, setindex or toggle, then the action is terminated with no effect."
+                // "If there is a null search result for the target object and the source object is an XForms action
+                // such as dispatch, send, setfocus, setindex or toggle, then the action is terminated with no effect."
                 if (indentedLogger.isDebugEnabled)
-                    indentedLogger.logDebug("xf:setindex", "index does not refer to an existing xf:repeat element, ignoring action", "repeat id", repeatStaticId)
+                    indentedLogger.logDebug(
+                        "xf:setindex",
+                        "index does not refer to an existing xf:repeat element, ignoring action",
+                        "repeat id", repeatStaticId
+                    )
                 
                 -1
         }

orbeon/orbeon-forms

commit 3b01f23e09c819366ab5861ccc2da9f5105c1c5c

For #1978: new element layout
- also fixes issue of missing required asterisk in Form Builder
- move from `xf:validation` to distinct element names
- don't use custom MIP for fb:required anymore in Form Builder
- hide alerts in Form Builder
- update Schema generator accordingly
- renaming for clarity

src/resources/forms/orbeon/builder/form/annotate.xpl
@@ -158,21 +158,25 @@
                 </xsl:template>
 
                 <!-- Convert MIP names (attributes and nested elements) -->
+                <!-- NOTE: We leave custom MIPs as they are. The user must not use fb:* custom MIPs. -->
                 <xsl:template match="xf:bind/@relevant
                                    | xf:bind/@readonly
-                                   | xf:bind/@required
                                    | xf:bind/@constraint
                                    | xf:bind/@calculate
-                                   | xf:bind/@xxf:default
-                                   | xf:validation/@relevant
-                                   | xf:validation/@readonly
-                                   | xf:validation/@required
-                                   | xf:validation/@constraint
-                                   | xf:validation/@calculate
-                                   | xf:validation/@xxf:default"
+                                   | xf:bind/@xxf:default"
                               mode="within-model">
-                    <!-- Further below we only allow fb:required to be interpreted as a custom MIP -->
                     <xsl:attribute name="fb:{local-name()}" select="."/>
+                </xsl:template>
+
+                <xsl:template match="xf:bind/xf:relevant
+                                   | xf:bind/xf:readonly
+                                   | xf:bind/xf:constraint
+                                   | xf:bind/xf:calculate
+                                   | xf:bind/xxf:default"
+                              mode="within-model">
+                    <xsl:element name="fb:{local-name()}">
+                        <xsl:apply-templates select="@* | node()" mode="#current"/>
+                    </xsl:element>
                 </xsl:template>
 
                 <!-- Add model actions -->
@@ -180,14 +184,8 @@
 
                     <xsl:copy>
                         <!-- Namespace for fb:required -->
-                        <xsl:namespace name="fb" select="'http://orbeon.org/oxf/xml/form-builder'"/>
-                        <xsl:apply-templates select="@* except @xxf:custom-mips" mode="within-model"/>
-                        <!-- Only let fb:required act as a custom MIP. All other non-standard bind attributes, including
-                             @fb:relevant, are ignored. We don't need the result of these rewritten MIPs at design time,
-                             and this helps with performance in case there is a large number of binds. -->
-                        <xsl:attribute name="xxf:custom-mips" select="string-join((@xxf:custom-mips, 'fb:required'), ' ')"/>
-
-                        <xsl:apply-templates select="node()" mode="within-model"/>
+                        <!--<xsl:namespace name="fb" select="'http://orbeon.org/oxf/xml/form-builder'"/>-->
+                        <xsl:apply-templates select="@* | node()" mode="within-model"/>
 
                         <!-- Upon model creation, recalculation and revalidation, notify Form Builder -->
                         <xsl:for-each select="('xforms-model-construct', 'xforms-recalculate', 'xforms-revalidate', 'xxforms-xpath-error')">
@@ -397,19 +395,40 @@
                     </xsl:copy>
                 </xsl:template>
 
-                <!-- Convert xf:constraint/@value to xf:validation/@fb:constraint, etc. -->
-                <xsl:template match="xf:bind/xf:relevant
-                                   | xf:bind/xf:readonly
-                                   | xf:bind/xf:required
-                                   | xf:bind/xf:constraint
-                                   | xf:bind/xf:calculate
-                                   | xf:bind/xxf:default"
+                <!-- For a while (not in a release) we supported xf:validation/@* (but not custom MIPs on them) -->
+                <xsl:template match="xf:bind/xf:validation[@relevant | @readonly | @constraint | @calculate | @xxf:default]"
                               mode="within-model">
-                    <xsl:element name="xf:validation">
-                        <xsl:if test="@value">
-                            <xsl:attribute name="fb:{local-name()}" select="@value"/>
-                        </xsl:if>
-                        <xsl:apply-templates select="(@* except @value) | node()" mode="#current"/>
+
+                    <xsl:variable
+                        name="validation"
+                        select="."/>
+
+                    <xsl:variable
+                        name="atts"
+                        select="@relevant | @readonly | @constraint | @calculate | @xxf:default"/>
+
+                    <!-- XForms supported more than one attribute, but FB only generated one -->
+                    <xsl:for-each select="$atts[1]">
+                        <xsl:element name="fb:{local-name()}">
+                            <xsl:attribute name="value" select="."/>
+                            <xsl:apply-templates select="$validation/@* except $atts" mode="#current"/>
+                        </xsl:element>
+                    </xsl:for-each>
+                </xsl:template>
+
+                <xsl:template match="xf:bind/xf:validation[@type]"
+                              mode="within-model">
+                    <xsl:element name="xf:type">
+                        <xsl:apply-templates select="@* except @type" mode="#current"/>
+                        <xsl:value-of select="@type"/>
+                    </xsl:element>
+                </xsl:template>
+
+                <xsl:template match="xf:bind/xf:validation[@required]"
+                              mode="within-model">
+                    <xsl:element name="xf:required">
+                        <xsl:attribute name="value" select="@required"/>
+                        <xsl:apply-templates select="@* except @required" mode="#current"/>
                     </xsl:element>
                 </xsl:template>
 

src/resources/forms/orbeon/builder/form/deannotate.xpl
@@ -86,26 +86,26 @@
                 <!-- Convert MIP names (attributes and nested elements) -->
                 <xsl:template match="xf:bind/@fb:relevant
                                    | xf:bind/@fb:readonly
-                                   | xf:bind/@fb:required
                                    | xf:bind/@fb:constraint
-                                   | xf:bind/@fb:calculate
-                                   | xf:validation/@fb:relevant
-                                   | xf:validation/@fb:readonly
-                                   | xf:validation/@fb:required
-                                   | xf:validation/@fb:constraint
-                                   | xf:validation/@fb:calculate">
+                                   | xf:bind/@fb:calculate">
                     <xsl:attribute name="{local-name()}" select="."/>
                 </xsl:template>
-                <xsl:template match="xf:bind/@fb:default | xf:validation/@fb:default">
+                <xsl:template match="xf:bind/@fb:default">
                     <xsl:attribute name="xxf:{local-name()}" select="."/>
                 </xsl:template>
 
-                <!-- Restore xxf:custom-mips -->
-                <xsl:template match="xh:head/xf:model[@id = 'fr-form-model']/@xxf:custom-mips">
-                    <xsl:variable name="tokens" select="p:split()[. != 'fb:required']" xmlns:p="http://www.orbeon.com/oxf/pipeline"/>
-                    <xsl:if test="exists($tokens)">
-                        <xsl:attribute name="xxf:custom-mips" select="string-join($tokens, ' ')"/>
-                    </xsl:if>
+                <xsl:template match="xf:bind/fb:relevant
+                                   | xf:bind/fb:readonly
+                                   | xf:bind/fb:constraint
+                                   | xf:bind/fb:calculate">
+                    <xsl:element name="xf:{local-name()}">
+                        <xsl:apply-templates select="@* | node()" mode="#current"/>
+                    </xsl:element>
+                </xsl:template>
+                <xsl:template match="xf:bind/fb:default">
+                    <xsl:element name="xxf:{local-name()}">
+                        <xsl:apply-templates select="@* | node()" mode="#current"/>
+                    </xsl:element>
                 </xsl:template>
 
                 <!-- Remove model actions -->

src/main/java/org/orbeon/oxf/xforms/XFormsConstants.java
@@ -234,8 +234,13 @@ public class XFormsConstants {
     public static final QName XXFORMS_DEFAULT_QNAME = new QName("default", XXFORMS_NAMESPACE);
     public static final QName XXFORMS_DEFAULTS_QNAME = new QName("defaults", XXFORMS_NAMESPACE);
     public static final QName XXFORMS_DEFERRED_QNAME = new QName("deferred", XXFORMS_NAMESPACE);
+
     public static final QName VALIDATION_QNAME = new QName("validation");
     public static final QName XFORMS_VALIDATION_QNAME = new QName("validation", XFORMS_NAMESPACE);
+
+    public static final QName XFORMS_TYPE_QNAME = new QName("type", XFORMS_NAMESPACE);
+    public static final QName XFORMS_RELEVANT_QNAME = new QName("relevant", XFORMS_NAMESPACE);
+    public static final QName XFORMS_REQUIRED_QNAME = new QName("required", XFORMS_NAMESPACE);
 
     public static final QName XXFORMS_READONLY_ATTRIBUTE_QNAME = new QName(READONLY_ATTRIBUTE_NAME, XXFORMS_NAMESPACE);
     public static final QName XXFORMS_INDEX_QNAME = new QName("index", XXFORMS_NAMESPACE);

src/main/scala/org/orbeon/oxf/fb/AlertsAndConstraintsOps.scala
@@ -31,8 +31,6 @@ trait AlertsAndConstraintsOps extends ControlOps {
 
     self: GridOps ⇒ // funky dependency, to resolve at some point
 
-    private val XFValidationQName: QName = XF → "validation"
-
     private val OldAlertRefMatcher = """\$form-resources/([^/]+)/alert(\[(\d+)\])?""".r
     private val NewAlertRefMatcher = """xxf:r\('([^.]+)\.alert(\.(\d+))?'\)""".r
 
@@ -87,7 +85,7 @@ trait AlertsAndConstraintsOps extends ControlOps {
             writeValidations(
                 inDoc,
                 controlName,
-                Required.name,
+                Required,
                 List(v)
             )
         }
@@ -102,7 +100,7 @@ trait AlertsAndConstraintsOps extends ControlOps {
             writeValidations(
                 inDoc,
                 controlName,
-                Type.name,
+                Type,
                 List(v)
             )
         }
@@ -111,7 +109,7 @@ trait AlertsAndConstraintsOps extends ControlOps {
         writeValidations(
             inDoc,
             controlName,
-            Constraint.name,
+            Constraint,
             allValidations collect { case v: ConstraintValidation ⇒ v }
         )
 
@@ -126,16 +124,16 @@ trait AlertsAndConstraintsOps extends ControlOps {
     private def writeValidations(
         inDoc       : NodeInfo,
         controlName : String,
-        mipName     : String,
+        mip         : MIP,
         validations : List[Validation]
     ): Unit = {
 
         val bind = findBindByName(inDoc, controlName).get
 
-        val mipAttQName = mipNameToFBMIPQname(mipName)
+        val existingAttributeValidations = mipAtts (bind, mip)
+        val existingElementValidations   = mipElems(bind, mip)
 
-        val existingAttributeValidations = bind /@ mipAttQName
-        val existingElementValidations   = bind /  XFValidationQName filter (_ /@ mipAttQName nonEmpty)
+        val (_, mipElemQName) = mipToFBMIPQNames(mip)
 
         validations match {
             case Nil ⇒
@@ -145,7 +143,7 @@ trait AlertsAndConstraintsOps extends ControlOps {
                 // Single validation without custom alert: set @fb:mipAttName and remove all nested elements
                 // See also: https://github.com/orbeon/orbeon-forms/issues/1829
                 // NOTE: We could optimize further by taking this branch if there is no type or required validation.
-                updateMip(inDoc, controlName, mipName, value)
+                updateMipAsAttributeOnly(inDoc, controlName, mip.name, value)
                 delete(existingElementValidations)
             case _ ⇒
                 val nestedValidations =
@@ -154,22 +152,20 @@ trait AlertsAndConstraintsOps extends ControlOps {
                         nonEmptyOrNone(value) match {
                             case Some(nonEmptyValue) ⇒
 
-                                val validationElem =
-                                    <xf:validation
+                                val prefix = mipElemQName.getNamespaceURI match {
+                                    case FB ⇒ "fb" // also covers the case of `xxf:default` (Form Builder names here)
+                                    case XF ⇒ "xf" // case of `xf:type`, `xf:required`
+                                }
+
+                                val dummyMIPElem =
+                                    <xf:dummy
                                         id={idOpt.orNull}
                                         level={if (level != ErrorLevel) level.name else null}
+                                        value={if (mip != Type) nonEmptyValue else null}
                                         xmlns:xf={XF}
-                                        xmlns:fb={FB}/>
+                                        xmlns:fb={FB}>{if (mip == Type) nonEmptyValue else null}</xf:dummy>
 
-                                val mipAtt =
-                                    sx.Attribute(
-                                        mipAttQName.getNamespacePrefix,
-                                        mipAttQName.getName,
-                                        sx.Text(nonEmptyValue),
-                                        sx.Null
-                                    )
-
-                                List(validationElem % mipAtt: NodeInfo)
+                                List(dummyMIPElem.copy(prefix = prefix, label = mipElemQName.getName): NodeInfo)
                             case None ⇒
                                 Nil
                         }
@@ -290,15 +286,12 @@ trait AlertsAndConstraintsOps extends ControlOps {
 
         val DefaultRequireValidation = RequiredValidation(None, Left(false), None)
 
-        def fromForm(inDoc: NodeInfo, controlName: String): RequiredValidation = {
-            val mipAttQName = mipNameToFBMIPQname(Required.name)
-
-            findMIPs(inDoc, controlName, mipAttQName).headOption map {
+        def fromForm(inDoc: NodeInfo, controlName: String): RequiredValidation =
+            findMIPs(inDoc, controlName, Required).headOption map {
                 case (idOpt, _, value, alertOpt) ⇒
                     RequiredValidation(idOpt, xpathOptToEither(Some(value)), alertOpt)
             } getOrElse
                 DefaultRequireValidation
-        }
 
         def fromXML(validationElem: NodeInfo, newIds: Iterator[String]): Option[RequiredValidation] = {
             require(validationElem /@ "type" === Required.name)
@@ -397,9 +390,7 @@ trait AlertsAndConstraintsOps extends ControlOps {
                     Right(qName)
             }
             
-            val mipAttQName = mipNameToFBMIPQname(Type.name)
-
-            findMIPs(inDoc, controlName, mipAttQName).headOption map {
+            findMIPs(inDoc, controlName, Type).headOption map {
                 case (idOpt, _, value, alertOpt) ⇒
                     DatatypeValidation(idOpt, builtinOrSchemaType(value), alertOpt)
             } getOrElse
@@ -488,7 +479,7 @@ trait AlertsAndConstraintsOps extends ControlOps {
     object ConstraintValidation {
 
         def fromForm(inDoc: NodeInfo, controlName: String): List[ConstraintValidation] =
-            findMIPs(inDoc, controlName, mipNameToFBMIPQname(Constraint.name)) map {
+            findMIPs(inDoc, controlName, Constraint) map {
                 case (idOpt, level, value, alertOpt) ⇒
                     ConstraintValidation(idOpt, level, value, alertOpt)
             }
@@ -612,7 +603,7 @@ trait AlertsAndConstraintsOps extends ControlOps {
         }
     }
 
-    private def findMIPs(inDoc: NodeInfo, controlName: String, attQName: QName) = {
+    private def findMIPs(inDoc: NodeInfo, controlName: String, mip: MIP) = {
 
         val bind            = findBindByName(inDoc, controlName).get // require the bind
         val supportedAlerts = AlertDetails.fromForm(inDoc, controlName)
@@ -635,17 +626,20 @@ trait AlertsAndConstraintsOps extends ControlOps {
             (
                 nonEmptyOrNone(id),
                 nonEmptyOrNone(e attValue LEVEL_QNAME) map LevelByName getOrElse ErrorLevel,
-                e attValue attQName,
+                if (mip == Type) e.getStringValue else e attValue VALUE_QNAME,
                 findAlertForId(id)
             )
         }
 
-        // Gather all validations (in fb:*)
-        def attributeValidations = bind /@ attQName map fromAttribute
-        def elementValidations   = bind /  XFValidationQName filter (_ /@ attQName nonEmpty) map fromElement
+        // Gather all validations (in fb:* except for type)
+        def attributeValidations = mipAtts (bind, mip) map fromAttribute
+        def elementValidations   = mipElems(bind, mip) map fromElement
 
         attributeValidations ++ elementValidations toList
     }
+
+    private def mipAtts (bind: NodeInfo, mip: MIP) = bind /@ mipToFBMIPQNames(mip)._1
+    private def mipElems(bind: NodeInfo, mip: MIP) = bind /  mipToFBMIPQNames(mip)._2
 
     private def alertOrPlaceholder(alert: Option[AlertDetails], forLang: String) =
         alert orElse Some(AlertDetails(None, List(currentLang → ""), global = false)) map (_.toXML(forLang)) get

src/main/scala/org/orbeon/oxf/fb/ControlOps.scala
@@ -41,8 +41,8 @@ trait ControlOps extends SchemaOps with ResourcesOps {
 
     val FB = "http://orbeon.org/oxf/xml/form-builder"
 
-    private val MIPsToRewrite = AllMIPs - Type
-    private val RewrittenMIPs = MIPsToRewrite map (mip ⇒ mip.name → toQName(FB → ("fb:" + mip.name))) toMap
+    private val MIPsToRewrite = AllMIPs - Type - Required
+    private val RewrittenMIPs = MIPsToRewrite map (mip ⇒ mip → toQName(FB → ("fb:" + mip.name))) toMap
 
     val BindElementTest: Test = XF → "bind"
 
@@ -361,10 +361,10 @@ trait ControlOps extends SchemaOps with ResourcesOps {
 
     // Update a mip for the given control, grid or section id
     // The bind is created if needed
-    def updateMip(inDoc: NodeInfo, controlName: String, mipName: String, mipValue: String): Unit = {
+    def updateMipAsAttributeOnly(inDoc: NodeInfo, controlName: String, mipName: String, mipValue: String): Unit = {
 
         require(Model.AllMIPNames(mipName))
-        val mipQName = mipNameToFBMIPQname(mipName)
+        val (mipAttQName, _) = mipToFBMIPQNames(AllMIPsByName(mipName))
 
         findControlByName(inDoc, controlName) foreach { control ⇒
 
@@ -386,9 +386,9 @@ trait ControlOps extends SchemaOps with ResourcesOps {
 
             nonEmptyOrNone(mipValue) match {
                 case Some(normalizedMipValue) if ! mustRemoveAttribute(normalizedMipValue) ⇒
-                    ensureAttribute(bind, mipQName, normalizedMipValue)
+                    ensureAttribute(bind, mipAttQName, normalizedMipValue)
                 case _ ⇒
-                    delete(bind /@ mipQName)
+                    delete(bind /@ mipAttQName)
             }
         }
     }
@@ -424,21 +424,23 @@ trait ControlOps extends SchemaOps with ResourcesOps {
     }
 
     // Get the value of a MIP attribute if present
-    def getMip(inDoc: NodeInfo, controlName: String, mipName: String) = {
+    def readMipAsAttributeOnly(inDoc: NodeInfo, controlName: String, mipName: String) = {
         require(Model.AllMIPNames(mipName))
-        val mipQName = mipNameToFBMIPQname(mipName)
+        val (mipAttQName, _) = mipToFBMIPQNames(AllMIPsByName(mipName))
 
-        findBindByName(inDoc, controlName) flatMap (_ attValueOpt mipQName)
+        findBindByName(inDoc, controlName) flatMap (_ attValueOpt mipAttQName)
     }
 
-    def mipNameToFBMIPQname(mipName: String) =
-        RewrittenMIPs.get(mipName)                 orElse
-        (AllMIPsByName.get(mipName) map (_.aName)) getOrElse
-        (throw new IllegalArgumentException)
-
     // XForms callers: find the value of a MIP or null (the empty sequence)
-    def getMipOrEmpty(inDoc: NodeInfo, controlName: String, mipName: String) =
-        getMip(inDoc, controlName, mipName).orNull
+    def readMipAsAttributeOnlyOrEmpty(inDoc: NodeInfo, controlName: String, mipName: String) =
+        readMipAsAttributeOnly(inDoc, controlName, mipName).orNull
+
+    // Return (attQName, elemQName)
+    def mipToFBMIPQNames(mip: MIP) =
+        RewrittenMIPs.get(mip) match {
+            case Some(qn) ⇒ qn        → qn
+            case None     ⇒ mip.aName → mip.eName
+        }
 
     // Get all control names by inspecting all elements with an id that converts to a valid name
     def getAllControlNames(inDoc: NodeInfo) =

src/main/scala/org/orbeon/oxf/fr/schema/SchemaGenerator.scala
@@ -97,23 +97,32 @@ object SchemaGenerator {
             def unapply(bind: NodeInfo): Option[BindInfo] = {
                 val repeatGridNode = findControlNodeForBind(bind, "*:grid") filter FormRunner.isRepeat toList
 
-                // Also support nested xf:validation elements
-                val bindAndValidations = bind +: (bind / XFORMS_VALIDATION_QNAME)
+                // NOTE: Don't support deprecated xf:validation elements, as they were not in a release. But support
+                // xf:relevant, xf:required, and xf:type.
+
+                val hasRelevant =
+                    bind ++ (bind / XFORMS_RELEVANT_QNAME) /@ "has-relevant" nonEmpty
 
                 val maybeRequired =
-                    bindAndValidations map (_ /@ "required") exists (_.stringValue != "false()")
+                    (bind /@ REQUIRED_QNAME) ++ (bind / XFORMS_REQUIRED_QNAME /@ VALUE_QNAME) exists (_.stringValue != "false()")
+
+                def typeFromBind =
+                    (bind /@ TYPE_QNAME headOption) map (_.stringValue)
+
+                def typeFromChildElem =
+                    (bind / XFORMS_TYPE_QNAME headOption) map (_.stringValue)
 
                 val elemType = (
-                    (bindAndValidations /@ "type" headOption)
-                    map (_.stringValue)
-                    map bind.resolveQName
+                    typeFromBind
+                    orElse    typeFromChildElem
+                    map       bind.resolveQName
                     filterNot Set(XS_STRING_QNAME, XFORMS_STRING_QNAME)
                 )
 
                 Some(BindInfo(
                     elemName      = bind /@ ("ref" || "nodeset"),
                     maybeRequired = maybeRequired,
-                    hasRelevant   = bindAndValidations /@ "has-relevant" nonEmpty,
+                    hasRelevant   = hasRelevant,
                     elemType      = elemType,
                     repeated      = repeatGridNode nonEmpty,
                     min           = repeatGridNode /@ "min",

commit 309649fb28e607415b0bdb958de8de990ad84e93

FB: Fix performance regression with custom MIPs
- need to have xxf:custom-mips attribute or all fb:* MIPs are evaluated
- regressed with 3b01f23e09c819366ab5861ccc2da9f5105c1c5c

src/resources/forms/orbeon/builder/form/annotate.xpl
@@ -183,9 +183,15 @@
                 <xsl:template match="xf:model[generate-id() = generate-id($model)]">
 
                     <xsl:copy>
-                        <!-- Namespace for fb:required -->
-                        <!--<xsl:namespace name="fb" select="'http://orbeon.org/oxf/xml/form-builder'"/>-->
-                        <xsl:apply-templates select="@* | node()" mode="within-model"/>
+                        <!-- Make sure xxf:custom-mips is not missing otherwise all fb:* MIPs are evaluated -->
+                        <xsl:if test="empty(@xxf:custom-mips)">
+                            <xsl:attribute name="xxf:custom-mips"/>
+                            <xsl:attribute name="fb:added-empty-custom-mips" select="'true'"/>
+                        </xsl:if>
+                        
+                        <xsl:apply-templates
+                            select="(@* except (@xxf:custom-mips, @fb:added-empty-custom-mips)) | node()"
+                            mode="within-model"/>
 
                         <!-- Upon model creation, recalculation and revalidation, notify Form Builder -->
                         <xsl:for-each select="('xforms-model-construct', 'xforms-recalculate', 'xforms-revalidate', 'xxforms-xpath-error')">

src/resources/forms/orbeon/builder/form/deannotate.xpl
@@ -107,7 +107,18 @@
                         <xsl:apply-templates select="@* | node()" mode="#current"/>
                     </xsl:element>
                 </xsl:template>
-
+                
+                <!-- Restore xxf:custom-mips if we had added it -->
+                <xsl:template match="
+                    xh:head/xf:model[
+                        @id = 'fr-form-model'
+                        and @fb:added-empty-custom-mips = 'true'
+                    ]/@xxf:custom-mips"/>
+                <xsl:template match="
+                    xh:head/xf:model[
+                        @id = 'fr-form-model'
+                    ]/@fb:added-empty-custom-mips"/>
+                
                 <!-- Remove model actions -->
                 <xsl:template match="xh:head/xf:model[@id = 'fr-form-model']/*[p:has-class('fb-annotation')]"/>
 
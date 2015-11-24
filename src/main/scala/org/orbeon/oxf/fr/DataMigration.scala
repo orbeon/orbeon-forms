2e98476663b1e192c8ece6a940ed8f81ad41d497
ebruchez <ebruchez@orbeon.com>
2015-11-24 15:25:07 -0800

    Fix #2441 "Repeated fr:grid migration doesn't handle zero iteration case"

--- src/main/scala/org/orbeon/oxf/fr/DataMigration.scala
+++ src/main/scala/org/orbeon/oxf/fr/DataMigration.scala
@@ -13,7 +13,7 @@
  */
 package org.orbeon.oxf.fr
 
-import org.orbeon.oxf.fr.FormRunner._
+import org.orbeon.oxf.util.ScalaUtils._
 import org.orbeon.oxf.xml.TransformerUtils
 import org.orbeon.saxon.om.{DocumentInfo, NodeInfo}
 import org.orbeon.scaxon.XML
@@ -21,121 +21,154 @@ import org.orbeon.scaxon.XML._
 
 object DataMigration {
 
-    // NOTE: We could build this with JSON objects instead
-    def encodeMigrationsToJSON(migrations: Seq[(String, String)]) =
-        migrations map { case (path, iterationName) ⇒
-            s"""{ "path": "$path", "iteration-name": "$iterationName" }"""
-        } mkString ("[", ",", "]")
+  // NOTE: We could build this with JSON objects instead
+  def encodeMigrationsToJSON(migrations: Seq[(String, String)]) =
+    migrations map { case (path, iterationName) ⇒
+      s"""{ "path": "$path", "iteration-name": "$iterationName" }"""
+    } mkString ("[", ",", "]")
 
-    // Ouch, a lot of boilerplate! (Rapture JSON might provide nicer syntax)
-    def decodeMigrationsFromJSON(jsonMigrationMap: String): List[(String, String)] = {
+  // Ouch, a lot of boilerplate! (Rapture JSON might provide nicer syntax)
+  def decodeMigrationsFromJSON(jsonMigrationMap: String): List[(String, String)] = {
 
-        import spray.json._
-        val json = jsonMigrationMap.parseJson
+    import spray.json._
+    val json = jsonMigrationMap.parseJson
 
-        val names =
-            Iterator(json) collect {
-                case JsArray(migrations) ⇒
-                    migrations.iterator collect {
-                        case JsObject(fields) ⇒
-                            (
-                                fields("path").asInstanceOf[JsString].value,
-                                fields("iteration-name").asInstanceOf[JsString].value
-                            )
-                    }
-            }
+    val names =
+      Iterator(json) collect {
+        case JsArray(migrations) ⇒
+          migrations.iterator collect {
+            case JsObject(fields) ⇒
+              (
+                fields("path").asInstanceOf[JsString].value,
+                fields("iteration-name").asInstanceOf[JsString].value
+              )
+          }
+      }
 
-        names.flatten.toList
-    }
+    names.flatten.toList
+  }
 
-    private def partitionNodes(
-        mutableData : NodeInfo,
-        migration   : List[(String, String)]
-    ): List[(List[NodeInfo], String)] =
-        migration flatMap {
-            case (path, iterationName) ⇒
+  private val TrimPathElementRE = """\s*\(?([^)]+)\)?\s*""".r
 
-                // NOTE: Use collect, but we know they are nodes if the JSON is correct and contains paths
-                val nodes = XML.eval(mutableData.rootElement, path) collect {
-                    case node: NodeInfo ⇒ node
-                }
+  private def partitionNodes(
+    mutableData : NodeInfo,
+    migration   : List[(String, String)]
+  ): List[(NodeInfo, List[NodeInfo], String, String)] =
+    migration flatMap {
+      case (path, iterationName) ⇒
 
-                // NOTE: Should ideally test on uriQualifiedName instead. The data in practice has elements which
-                // in no namespaces, and if they were in a namespace, the prefixes would likely be unique.
-                val firsts = nodes filter (node ⇒ node.precedingSibling(node.name).isEmpty)
+        val (pathToParentNodes, pathToChildNodes) = {
+          // NOTE: The format of the path is like `(section-3)/(section-3-iteration)/(grid-4)`. Form Builder
+          // puts parentheses for the abandoned case of a custom XML format, and we kept that when producing
+          // the migration data. As long as we know that there are no `/` within the parentheses we are fine.
+          val parts                          = split[List](path, "/")
+          val TrimPathElementRE(trimmedLast) = parts.last
 
-                firsts map { first ⇒
-                    (first :: first.followingSibling(first.name).toList, iterationName)
-                }
+          (parts.init mkString "/", trimmedLast)
         }
 
-    import org.orbeon.oxf.xforms.action.XFormsAPI._
-
-    //@XPathFunction
-    def dataMaybeMigratedFrom(data: DocumentInfo, metadata: Option[DocumentInfo]) =
-        dataMaybeMigratedFromTo(data, metadata, migrateDataFrom)
-
-    //@XPathFunction
-    def dataMaybeMigratedTo(data: DocumentInfo, metadata: Option[DocumentInfo]) =
-        dataMaybeMigratedFromTo(data, metadata, migrateDataTo)
-
-    private def dataMaybeMigratedFromTo(
-        data     : DocumentInfo,
-        metadata : Option[DocumentInfo],
-        migrate  : (DocumentInfo, String) ⇒ DocumentInfo
-    ) =
-        for {
-            metadata  ← metadata
-            migration ← migrationMapFromMetadata(metadata.rootElement)
-        } yield
-            migrate(data, migration)
-
-    def migrationMapFromMetadata(metadataRootElement: NodeInfo) =
-        metadataRootElement firstChild "migration" filter (_.attValue("version") == "4.8.0") map (_.stringValue)
-
-    def migrateDataTo(data: DocumentInfo, jsonMigrationMap: String): DocumentInfo = {
-
-        val mutableData = TransformerUtils.extractAsMutableDocument(data)
-
-        partitionNodes(mutableData, decodeMigrationsFromJSON(jsonMigrationMap)) foreach {
-            case (iterations, iterationName) ⇒
-
-                val iterationContent = iterations map (_ / Node toList) // force
-
-                delete(iterations.head / Node, doDispatch = false)
-                delete(iterations.tail,        doDispatch = false)
-
-                insert(
-                    into       = iterations.head,
-                    origin     = iterationContent map (elementInfo(iterationName, _)),
-                    doDispatch = false
-                )
+        // NOTE: Use collect, but we know they are nodes if the JSON is correct and contains paths
+        val parentNodes = XML.eval(mutableData.rootElement, pathToParentNodes) collect {
+          case node: NodeInfo ⇒ node
         }
 
-        mutableData
-    }
+        parentNodes map { parentNode ⇒
 
-    def migrateDataFrom(data: DocumentInfo, jsonMigrationMap: String): DocumentInfo = {
+          val nodes = XML.eval(parentNode, pathToChildNodes) collect {
+            case node: NodeInfo ⇒ node
+          }
 
-        val mutableData = TransformerUtils.extractAsMutableDocument(data)
-
-        partitionNodes(mutableData, decodeMigrationsFromJSON(jsonMigrationMap)) foreach {
-            case (iterations, iterationName) ⇒
-
-                assert(iterations.tail.isEmpty)
-                val container = iterations.head
-
-                val iterationContent = (container / * toList) map (_ / Node toList) // force
-
-                insert(
-                    after      = container,
-                    origin     = iterationContent map (elementInfo(container.name, _)),
-                    doDispatch = false
-                )
-
-                delete(container, doDispatch = false)
+          // NOTE: Should ideally test on uriQualifiedName instead. The data in practice has elements which
+          // in no namespaces, and if they were in a namespace, the prefixes would likely be unique.
+          (parentNode, nodes.to[List], pathToChildNodes, iterationName)
         }
-
-        mutableData
     }
+
+  import org.orbeon.oxf.xforms.action.XFormsAPI._
+
+  //@XPathFunction
+  def dataMaybeMigratedFrom(data: DocumentInfo, metadata: Option[DocumentInfo]) =
+    dataMaybeMigratedFromTo(data, metadata, migrateDataFrom)
+
+  //@XPathFunction
+  def dataMaybeMigratedTo(data: DocumentInfo, metadata: Option[DocumentInfo]) =
+    dataMaybeMigratedFromTo(data, metadata, migrateDataTo)
+
+  private def dataMaybeMigratedFromTo(
+    data     : DocumentInfo,
+    metadata : Option[DocumentInfo],
+    migrate  : (DocumentInfo, String) ⇒ DocumentInfo
+  ) =
+    for {
+      metadata  ← metadata
+      migration ← migrationMapFromMetadata(metadata.rootElement)
+    } yield
+      migrate(data, migration)
+
+  def migrationMapFromMetadata(metadataRootElement: NodeInfo) =
+    metadataRootElement firstChild "migration" filter (_.attValue("version") == "4.8.0") map (_.stringValue)
+
+  def migrateDataTo(data: DocumentInfo, jsonMigrationMap: String): DocumentInfo = {
+
+    val mutableData = TransformerUtils.extractAsMutableDocument(data)
+
+    partitionNodes(mutableData, decodeMigrationsFromJSON(jsonMigrationMap)) foreach {
+      case (parentNode, iterations, repeatName, iterationName) ⇒
+
+        iterations match {
+          case Nil ⇒
+            // Issue: we don't know, based just on the migration map, where to insert container elements to
+            // follow bind order. This is not a new problem as we don't enforce order, see:
+            //
+            //     https://github.com/orbeon/orbeon-forms/issues/443.
+            //
+            // For now we choose to add after the last element.
+            //
+            // BTW at runtime `fr:grid[@repeat = 'true']` inserts iterations before the first element.
+            insert(
+              into       = parentNode,
+              after      = parentNode / *,
+              origin     = elementInfo(repeatName, Nil),
+              doDispatch = false
+            )
+          case _ ⇒
+            val iterationContent = iterations map (_ / Node toList) // force
+
+            delete(iterations.head / Node, doDispatch = false)
+            delete(iterations.tail,        doDispatch = false)
+
+            insert(
+              into       = iterations.head,
+              origin     = iterationContent map (elementInfo(iterationName, _)),
+              doDispatch = false
+            )
+        }
+    }
+
+    mutableData
+  }
+
+  def migrateDataFrom(data: DocumentInfo, jsonMigrationMap: String): DocumentInfo = {
+
+    val mutableData = TransformerUtils.extractAsMutableDocument(data)
+
+    partitionNodes(mutableData, decodeMigrationsFromJSON(jsonMigrationMap)) foreach {
+      case (_, iterations, _, _) ⇒
+
+        assert(iterations.tail.isEmpty)
+        val container = iterations.head
+
+        val iterationContent = (container / * toList) map (_ / Node toList) // force
+
+        insert(
+          after      = container,
+          origin     = iterationContent map (elementInfo(container.name, _)),
+          doDispatch = false
+        )
+
+        delete(container, doDispatch = false)
+    }
+
+    mutableData
+  }
 }
--- src/test/scala/org/orbeon/oxf/fb/MigrationTest.scala
+++ src/test/scala/org/orbeon/oxf/fb/MigrationTest.scala
@@ -25,255 +25,327 @@ import org.orbeon.scaxon.XML._
 
 class MigrationTest extends DocumentTestBase with FormBuilderSupport with XMLSupport with AssertionsForJUnit {
 
-    val MigrationJSON =
-        """
-          |[
-          |  {
-          |    "path": "(section-3)/(section-3-iteration)/(grid-4)",
-          |    "iteration-name": "grid-4-iteration"
-          |  },
-          |  {
-          |    "path": "(section-13)/(grid-6)",
-          |    "iteration-name": "grid-6-iteration"
-          |  },
-          |  {
-          |    "path": "(section-13)/(grid-14)",
-          |    "iteration-name": "grid-14-iteration"
-          |  },
-          |  {
-          |    "path": "(section-8)/(grid-3)",
-          |    "iteration-name": "my-custom-grid-3-iteration"
-          |  },
-          |  {
-          |    "path": "(section-23)/(grid-3)",
-          |    "iteration-name": "my-custom-grid-3-iteration"
-          |  }
-          |]
-        """.stripMargin
+  val MigrationJSON =
+    """
+      [
+        {
+          "path": "(section-3)/(section-3-iteration)/(grid-4)",
+          "iteration-name": "grid-4-iteration"
+        },
+        {
+          "path": "(section-13)/(grid-6)",
+          "iteration-name": "grid-6-iteration"
+        },
+        {
+          "path": "(section-13)/(grid-14)",
+          "iteration-name": "grid-14-iteration"
+        },
+        {
+          "path": "(section-8)/(grid-3)",
+          "iteration-name": "my-custom-grid-3-iteration"
+        },
+        {
+          "path": "(section-23)/(grid-3)",
+          "iteration-name": "my-custom-grid-3-iteration"
+        }
+      ]
+    """
 
-    @Test def buildGridMigrationMap(): Unit = {
+  @Test def buildGridMigrationMap(): Unit = {
 
-        val form    = readURLAsImmutableXMLDocument("oxf:/org/orbeon/oxf/fb/form-to-migrate.xhtml")
-        val toolbox = readURLAsImmutableXMLDocument("oxf:/org/orbeon/oxf/fb/form-to-migrate-library.xml")
+    val form    = readURLAsImmutableXMLDocument("oxf:/org/orbeon/oxf/fb/form-to-migrate.xhtml")
+    val toolbox = readURLAsImmutableXMLDocument("oxf:/org/orbeon/oxf/fb/form-to-migrate-library.xml")
 
-        val result =
-            MigrationOps.buildGridMigrationMap(form, Some(toolbox), legacyGridsOnly = false)
+    val result =
+      MigrationOps.buildGridMigrationMap(form, Some(toolbox), legacyGridsOnly = false)
 
-        import spray.json._
-        assert(MigrationJSON.parseJson === result.parseJson)
+    import spray.json._
+    assert(MigrationJSON.parseJson === result.parseJson)
+  }
+
+  val DataOrbeonForms47: NodeInfo =
+    <form>
+      <section-1>
+        <control-1/>
+      </section-1>
+      <section-3>
+        <section-3-iteration>
+          <control-6/>
+          <grid-4>
+            <control-7/>
+          </grid-4>
+          <grid-4>
+            <control-7/>
+          </grid-4>
+          <grid-4>
+            <control-7/>
+          </grid-4>
+        </section-3-iteration>
+        <section-3-iteration>
+          <control-6/>
+          <grid-4>
+            <control-7/>
+          </grid-4>
+          <grid-4>
+            <control-7/>
+          </grid-4>
+          <grid-4>
+            <control-7/>
+          </grid-4>
+        </section-3-iteration>
+        <section-3-iteration>
+          <control-6/>
+        </section-3-iteration>
+      </section-3>
+      <section-13>
+        <grid-6>
+          <control-10/>
+          <control-11/>
+          <control-12/>
+          <control-13/>
+        </grid-6>
+        <grid-6>
+          <control-10/>
+          <control-11/>
+          <control-12/>
+          <control-13/>
+        </grid-6>
+        <grid-6>
+          <control-10/>
+          <control-11/>
+          <control-12/>
+          <control-13/>
+        </grid-6>
+        <grid-14>
+          <control-16/>
+        </grid-14>
+        <grid-14>
+          <control-16/>
+        </grid-14>
+        <grid-14>
+          <control-16/>
+        </grid-14>
+      </section-13>
+      <section-8>
+        <control-1/>
+        <grid-3>
+          <control-8/>
+          <control-9/>
+        </grid-3>
+        <grid-3>
+          <control-8/>
+          <control-9/>
+        </grid-3>
+        <grid-3>
+          <control-8/>
+          <control-9/>
+        </grid-3>
+      </section-8>
+      <section-23>
+        <control-1/>
+        <grid-3>
+          <control-8/>
+          <control-9/>
+        </grid-3>
+        <grid-3>
+          <control-8/>
+          <control-9/>
+        </grid-3>
+        <grid-3>
+          <control-8/>
+          <control-9/>
+        </grid-3>
+      </section-23>
+    </form>
+
+  val DataOrbeonForms48: NodeInfo =
+    <form>
+      <section-1>
+        <control-1/>
+      </section-1>
+      <section-3>
+        <section-3-iteration>
+          <control-6/>
+          <grid-4>
+            <grid-4-iteration>
+              <control-7/>
+            </grid-4-iteration>
+            <grid-4-iteration>
+              <control-7/>
+            </grid-4-iteration>
+            <grid-4-iteration>
+              <control-7/>
+            </grid-4-iteration>
+          </grid-4>
+        </section-3-iteration>
+        <section-3-iteration>
+          <control-6/>
+          <grid-4>
+            <grid-4-iteration>
+              <control-7/>
+            </grid-4-iteration>
+            <grid-4-iteration>
+              <control-7/>
+            </grid-4-iteration>
+            <grid-4-iteration>
+              <control-7/>
+            </grid-4-iteration>
+          </grid-4>
+        </section-3-iteration>
+        <section-3-iteration>
+          <control-6/>
+          <grid-4/>
+        </section-3-iteration>
+      </section-3>
+      <section-13>
+        <grid-6>
+          <grid-6-iteration>
+            <control-10/>
+            <control-11/>
+            <control-12/>
+            <control-13/>
+          </grid-6-iteration>
+          <grid-6-iteration>
+            <control-10/>
+            <control-11/>
+            <control-12/>
+            <control-13/>
+          </grid-6-iteration>
+          <grid-6-iteration>
+            <control-10/>
+            <control-11/>
+            <control-12/>
+            <control-13/>
+          </grid-6-iteration>
+        </grid-6>
+        <grid-14>
+          <grid-14-iteration>
+            <control-16/>
+          </grid-14-iteration>
+          <grid-14-iteration>
+            <control-16/>
+          </grid-14-iteration>
+          <grid-14-iteration>
+            <control-16/>
+          </grid-14-iteration>
+        </grid-14>
+      </section-13>
+      <section-8>
+        <control-1/>
+        <grid-3>
+          <my-custom-grid-3-iteration>
+            <control-8/>
+            <control-9/>
+          </my-custom-grid-3-iteration>
+          <my-custom-grid-3-iteration>
+            <control-8/>
+            <control-9/>
+          </my-custom-grid-3-iteration>
+          <my-custom-grid-3-iteration>
+            <control-8/>
+            <control-9/>
+          </my-custom-grid-3-iteration>
+        </grid-3>
+      </section-8>
+      <section-23>
+        <control-1/>
+        <grid-3>
+          <my-custom-grid-3-iteration>
+            <control-8/>
+            <control-9/>
+          </my-custom-grid-3-iteration>
+          <my-custom-grid-3-iteration>
+            <control-8/>
+            <control-9/>
+          </my-custom-grid-3-iteration>
+          <my-custom-grid-3-iteration>
+            <control-8/>
+            <control-9/>
+          </my-custom-grid-3-iteration>
+        </grid-3>
+      </section-23>
+    </form>
+
+  val DataOrbeonForms47EmptyIterations: NodeInfo =
+    <form>
+      <section-1>
+        <control-1/>
+      </section-1>
+      <section-3>
+        <section-3-iteration>
+          <control-6/>
+        </section-3-iteration>
+      </section-3>
+      <section-13/>
+      <section-8>
+        <control-1/>
+      </section-8>
+      <section-23>
+        <control-1/>
+      </section-23>
+    </form>
+
+  val DataOrbeonForms48EmptyIterations: NodeInfo =
+    <form>
+      <section-1>
+        <control-1/>
+      </section-1>
+      <section-3>
+        <section-3-iteration>
+          <control-6/>
+          <grid-4/>
+        </section-3-iteration>
+      </section-3>
+      <section-13>
+        <grid-6/>
+        <grid-14/>
+      </section-13>
+      <section-8>
+        <control-1/>
+        <grid-3/>
+      </section-8>
+      <section-23>
+        <control-1/>
+        <grid-3/>
+      </section-23>
+    </form>
+
+  @Test def migrateDataTo(): Unit =
+    for {
+      (from, to) ← List(
+        DataOrbeonForms47                → DataOrbeonForms48,
+        DataOrbeonForms47EmptyIterations → DataOrbeonForms48EmptyIterations
+      )
+    } locally {
+      assertXMLDocumentsIgnoreNamespacesInScope(
+        to.root,
+        DataMigration.migrateDataTo(from.root, MigrationJSON)
+      )
     }
 
-    val Data47: NodeInfo =
-        <form>
-            <section-1>
-                <control-1/>
-            </section-1>
-            <section-3>
-                <section-3-iteration>
-                    <control-6/>
-                    <grid-4>
-                        <control-7/>
-                    </grid-4>
-                    <grid-4>
-                        <control-7/>
-                    </grid-4>
-                    <grid-4>
-                        <control-7/>
-                    </grid-4>
-                </section-3-iteration>
-                <section-3-iteration>
-                    <control-6/>
-                    <grid-4>
-                        <control-7/>
-                    </grid-4>
-                    <grid-4>
-                        <control-7/>
-                    </grid-4>
-                    <grid-4>
-                        <control-7/>
-                    </grid-4>
-                </section-3-iteration>
-            </section-3>
-            <section-13>
-                <grid-6>
-                    <control-10/>
-                    <control-11/>
-                    <control-12/>
-                    <control-13/>
-                </grid-6>
-                <grid-6>
-                    <control-10/>
-                    <control-11/>
-                    <control-12/>
-                    <control-13/>
-                </grid-6>
-                <grid-6>
-                    <control-10/>
-                    <control-11/>
-                    <control-12/>
-                    <control-13/>
-                </grid-6>
-                <grid-14>
-                    <control-16/>
-                </grid-14>
-                <grid-14>
-                    <control-16/>
-                </grid-14>
-                <grid-14>
-                    <control-16/>
-                </grid-14>
-            </section-13>
-            <section-8>
-                <control-1/>
-                <grid-3>
-                    <control-8/>
-                    <control-9/>
-                </grid-3>
-                <grid-3>
-                    <control-8/>
-                    <control-9/>
-                </grid-3>
-                <grid-3>
-                    <control-8/>
-                    <control-9/>
-                </grid-3>
-            </section-8>
-            <section-23>
-                <control-1/>
-                <grid-3>
-                    <control-8/>
-                    <control-9/>
-                </grid-3>
-                <grid-3>
-                    <control-8/>
-                    <control-9/>
-                </grid-3>
-                <grid-3>
-                    <control-8/>
-                    <control-9/>
-                </grid-3>
-            </section-23>
-        </form>
+  @Test def migrateDataFrom(): Unit =
+    for {
+      (from, to) ← List(
+        DataOrbeonForms47                → DataOrbeonForms48,
+        DataOrbeonForms47EmptyIterations → DataOrbeonForms48EmptyIterations
+      )
+    } locally {
+      assertXMLDocumentsIgnoreNamespacesInScope(
+        from.root,
+        DataMigration.migrateDataFrom(to.root, MigrationJSON)
+      )
+    }
 
-    val Data48: NodeInfo =
-        <form>
-            <section-1>
-                <control-1/>
-            </section-1>
-            <section-3>
-                <section-3-iteration>
-                    <control-6/>
-                    <grid-4>
-                        <grid-4-iteration>
-                            <control-7/>
-                        </grid-4-iteration>
-                        <grid-4-iteration>
-                            <control-7/>
-                        </grid-4-iteration>
-                        <grid-4-iteration>
-                            <control-7/>
-                        </grid-4-iteration>
-                    </grid-4>
-                </section-3-iteration>
-                <section-3-iteration>
-                    <control-6/>
-                    <grid-4>
-                        <grid-4-iteration>
-                            <control-7/>
-                        </grid-4-iteration>
-                        <grid-4-iteration>
-                            <control-7/>
-                        </grid-4-iteration>
-                        <grid-4-iteration>
-                            <control-7/>
-                        </grid-4-iteration>
-                    </grid-4>
-                </section-3-iteration>
-            </section-3>
-            <section-13>
-                <grid-6>
-                    <grid-6-iteration>
-                        <control-10/>
-                        <control-11/>
-                        <control-12/>
-                        <control-13/>
-                    </grid-6-iteration>
-                    <grid-6-iteration>
-                        <control-10/>
-                        <control-11/>
-                        <control-12/>
-                        <control-13/>
-                    </grid-6-iteration>
-                    <grid-6-iteration>
-                        <control-10/>
-                        <control-11/>
-                        <control-12/>
-                        <control-13/>
-                    </grid-6-iteration>
-                </grid-6>
-                <grid-14>
-                    <grid-14-iteration>
-                        <control-16/>
-                    </grid-14-iteration>
-                    <grid-14-iteration>
-                        <control-16/>
-                    </grid-14-iteration>
-                    <grid-14-iteration>
-                        <control-16/>
-                    </grid-14-iteration>
-                </grid-14>
-            </section-13>
-            <section-8>
-                <control-1/>
-                <grid-3>
-                    <my-custom-grid-3-iteration>
-                        <control-8/>
-                        <control-9/>
-                    </my-custom-grid-3-iteration>
-                    <my-custom-grid-3-iteration>
-                        <control-8/>
-                        <control-9/>
-                    </my-custom-grid-3-iteration>
-                    <my-custom-grid-3-iteration>
-                        <control-8/>
-                        <control-9/>
-                    </my-custom-grid-3-iteration>
-                </grid-3>
-            </section-8>
-            <section-23>
-                <control-1/>
-                <grid-3>
-                    <my-custom-grid-3-iteration>
-                        <control-8/>
-                        <control-9/>
-                    </my-custom-grid-3-iteration>
-                    <my-custom-grid-3-iteration>
-                        <control-8/>
-                        <control-9/>
-                    </my-custom-grid-3-iteration>
-                    <my-custom-grid-3-iteration>
-                        <control-8/>
-                        <control-9/>
-                    </my-custom-grid-3-iteration>
-                </grid-3>
-            </section-23>
-        </form>
-
-    @Test def migrateDataTo(): Unit =
-        assertXMLDocumentsIgnoreNamespacesInScope(
-            Data48.root,
-            DataMigration.migrateDataTo(Data47.root, MigrationJSON)
-        )
-
-    @Test def migrateDataFrom(): Unit =
-        assertXMLDocumentsIgnoreNamespacesInScope(
-            Data47.root,
-            DataMigration.migrateDataFrom(Data48.root, MigrationJSON)
-        )
-
-    @Test def roundTripData(): Unit =
-        assertXMLDocumentsIgnoreNamespacesInScope(
-            Data47.root,
-            DataMigration.migrateDataFrom(DataMigration.migrateDataTo(Data47.root, MigrationJSON), MigrationJSON)
-        )
+  @Test def roundTripData(): Unit =
+    for {
+      (from, to) ← List(
+        DataOrbeonForms47                → DataOrbeonForms48,
+        DataOrbeonForms47EmptyIterations → DataOrbeonForms48EmptyIterations
+      )
+    } locally {
+      assertXMLDocumentsIgnoreNamespacesInScope(
+        from.root,
+        DataMigration.migrateDataFrom(DataMigration.migrateDataTo(from.root, MigrationJSON), MigrationJSON)
+      )
+    }
 }

69017c89e5fce6cef130fcd3c12c2ebbe424f0c9
ebruchez <ebruchez@orbeon.com>
2016-03-12 23:02:20 -0800

    For #2164: move PropertyStore to Scala and don't use XPath
    - using XPathCache requires that properties are initialized

---
+++ src/main/scala/org/orbeon/oxf/properties/PropertyStore.scala
@@ -0,0 +1,147 @@
+/**
+ * Copyright (C) 2016 Orbeon, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+package org.orbeon.oxf.properties
+
+import java.net.{URI, URISyntaxException}
+import java.{lang ⇒ jl, util ⇒ ju}
+
+import org.dom4j.{Document, Element, QName}
+import org.orbeon.oxf.common.ValidationException
+import org.orbeon.oxf.util.{DateUtils, ScalaUtils, XPath}
+import org.orbeon.oxf.xml.XMLConstants._
+import org.orbeon.oxf.xml.dom4j.{Dom4jUtils, LocationData}
+import org.orbeon.saxon.dom4j.DocumentWrapper
+import org.orbeon.saxon.om.Name10Checker
+import org.orbeon.scaxon.XML._
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+
+// Represent property sets grouped as global and per-processor properties.
+class PropertyStore(propertiesDocument: Document) {
+
+  private val globalPropertySet = new PropertySet
+  private val processorPropertySets = mutable.HashMap[QName, PropertySet]()
+
+  locally {
+
+    val dw = new DocumentWrapper(propertiesDocument, null, XPath.GlobalConfiguration)
+
+    // NOTE: The use of `attribute` is for special use of the property store by certain processors.
+    // NOTE: Don't use XPathCache, because that depends on properties, which we are initializing here!
+    for (node ← (dw descendant "property") ++ (dw descendant "attribute")) {
+
+      val propertyElement = unsafeUnwrapElement(node)
+
+      if (Option(propertyElement.attributeValue("as")).isDefined) {
+
+        val typeQName = Dom4jUtils.extractAttributeValueQName(propertyElement, "as")
+
+        if (! PropertyStore.SupportedTypes.contains(typeQName))
+          throw new ValidationException(s"Invalid as attribute: ${typeQName.getQualifiedName}", propertyElement.getData.asInstanceOf[LocationData])
+
+        val name = propertyElement.attributeValue("name")
+
+        val value =
+          Option(propertyElement.attributeValue("value")) match {
+            case Some(valueFromAttribute) ⇒valueFromAttribute
+            case None                     ⇒ ScalaUtils.trimAllToEmpty(propertyElement.getText)
+          }
+
+        Option(propertyElement.attributeValue("processor-name")) match {
+          case Some(processorName) ⇒
+            val processorQName = Dom4jUtils.extractAttributeValueQName(propertyElement, "processor-name")
+            getProcessorPropertySet(processorQName).setProperty(propertyElement, name, typeQName, value)
+          case None ⇒
+            getGlobalPropertySet.setProperty(propertyElement, name, typeQName, value)
+        }
+      }
+    }
+  }
+
+  def getGlobalPropertySet: PropertySet = globalPropertySet
+
+  def getProcessorPropertySet(processorQName: QName): PropertySet =
+    processorPropertySets.getOrElseUpdate(processorQName, new PropertySet)
+}
+
+private object PropertyStore {
+
+  private val SupportedTypes = Map[QName, (String, Element) ⇒ AnyRef](
+    XS_STRING_QNAME             → convertString,
+    XS_INTEGER_QNAME            → convertInteger,
+    XS_BOOLEAN_QNAME            → convertBoolean,
+    XS_DATE_QNAME               → convertDate,
+    XS_DATETIME_QNAME           → convertDate,
+    XS_QNAME_QNAME              → convertQName,
+    XS_ANYURI_QNAME             → convertURI,
+    XS_NCNAME_QNAME             → convertNCName,
+    XS_NMTOKEN_QNAME            → convertNMTOKEN,
+    XS_NMTOKENS_QNAME           → convertNMTOKENS,
+    XS_NONNEGATIVEINTEGER_QNAME → convertNonNegativeInteger
+  )
+
+  /**
+    * Convert a property's string value to an object.
+    *
+    * @param element Element on which the property is defined. Used for QName resolution if needed.
+    * @return        object or null
+    */
+  def getObjectFromStringValue(stringValue: String, typ: QName, element: Element): AnyRef =
+    SupportedTypes.get(typ) map (_(stringValue, element)) orNull
+
+  def convertString (value: String, element: Element) = value
+  def convertInteger(value: String, element: Element) = new jl.Integer(value)
+  def convertBoolean(value: String, element: Element) = jl.Boolean.valueOf(value)
+  def convertDate   (value: String, element: Element) = new ju.Date(DateUtils.parseISODateOrDateTime(value))
+
+  def convertQName(value: String, element: Element): QName =
+    Dom4jUtils.extractAttributeValueQName(element, "value")
+
+  def convertURI(value: String, element: Element): URI =
+    try {
+      new URI(value)
+    } catch {
+      case e: URISyntaxException ⇒
+        throw new ValidationException(e, null)
+    }
+
+  def convertNCName(value: String, element: Element): String = {
+    if (! Name10Checker.getInstance.isValidNCName(value))
+      throw new ValidationException("Not an NCName: " + value, null)
+    value
+  }
+
+  def convertNMTOKEN(value: String, element: Element): String = {
+    if (! Name10Checker.getInstance.isValidNmtoken(value))
+      throw new ValidationException("Not an NMTOKEN: " + value, null)
+    value
+  }
+
+  def convertNMTOKENS(value: String, element: Element): ju.Set[String] = {
+    val tokens = ScalaUtils.split[Set](value)
+    for (token ← tokens) {
+      if (! Name10Checker.getInstance.isValidNmtoken(token))
+        throw new ValidationException(s"Not an NMTOKENS: $value" , null)
+    }
+    tokens.asJava
+  }
+
+  def convertNonNegativeInteger(value: String, element: Element): jl.Integer = {
+    val ret = convertInteger(value, element)
+    if (ret < 0)
+      throw new ValidationException(s"Not a non-negative integer: $value", null)
+    ret
+  }
+}

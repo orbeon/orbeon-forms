663563ab03da561c371b361ae0f7c0b224d5c315
ebruchez <ebruchez@orbeon.com>
2016-06-03 14:17:54 -0700

    For #2232: move test file to module

--- common/src/test/scala/org/orbeon/oxf/util/ScalaUtilsTest.scala
+++ common/src/test/scala/org/orbeon/oxf/util/ScalaUtilsTest.scala
@@ -1,23 +1,23 @@
 /**
- *  Copyright (C) 2012 Orbeon, Inc.
+ * Copyright (C) 2012 Orbeon, Inc.
  *
- *  This program is free software; you can redistribute it and/or modify it under the terms of the
- *  GNU Lesser General Public License as published by the Free Software Foundation; either version
- *  2.1 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- *  The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
- *  package org.orbeon.oxf.util
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
  */
 package org.orbeon.oxf.util
 
 import org.junit.Test
-import org.scalatest.junit.AssertionsForJUnit
 import org.orbeon.oxf.util.ScalaUtils._
-import collection.mutable
+import org.scalatest.junit.AssertionsForJUnit
+
+import scala.collection.mutable
 
 class ScalaUtilsTest extends AssertionsForJUnit {
 

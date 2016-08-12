7b5c4057fc422eca344c5aa87b115d0afce3634e
avernet <avernet@orbeon.com>
2016-08-12 15:26:54 -0700

    Add missing index tables from upgrade script

--- form-runner/src/main/resources/apps/fr/persistence/relational/ddl/db2-4_6-to-2016_2.sql
+++ form-runner/src/main/resources/apps/fr/persistence/relational/ddl/db2-4_6-to-2016_2.sql
@@ -15,3 +15,26 @@ UPDATE orbeon_form_data SET id = DEFAULT;
 
 ALTER TABLE orbeon_form_data
 ADD PRIMARY KEY (id);
+
+CREATE TABLE orbeon_i_current (
+    data_id             INT           NOT NULL,
+    created             TIMESTAMP     NOT NULL,
+    last_modified_time  TIMESTAMP     NOT NULL,
+    last_modified_by    VARCHAR(255)          ,
+    username            VARCHAR(255)          ,
+    groupname           VARCHAR(255)          ,
+    app                 VARCHAR(255)  NOT NULL,
+    form                VARCHAR(255)  NOT NULL,
+    form_version        INT           NOT NULL,
+    document_id         VARCHAR(255)  NOT NULL,
+    draft               CHARACTER(1)  NOT NULL,
+    FOREIGN KEY         (data_id)     REFERENCES orbeon_form_data(id)
+);
+
+CREATE TABLE orbeon_i_control_text (
+    data_id             INT           NOT NULL,
+    pos                 INT           NOT NULL,
+    control             VARCHAR(255)  NOT NULL,
+    val                 CLOB          NOT NULL,
+    FOREIGN KEY         (data_id)     REFERENCES orbeon_form_data(id)
+);

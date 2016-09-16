15d54703e4724914cda63b7b751e0c6f49a43d7a
ebruchez <ebruchez@orbeon.com>
2016-09-16 11:45:18 -0700

    For #2926: don't name parameter

--- src/main/scala/org/orbeon/oxf/xforms/event/ClientEvents.scala
+++ src/main/scala/org/orbeon/oxf/xforms/event/ClientEvents.scala
@@ -90,7 +90,7 @@ object ClientEvents extends Logging with XMLReceiverSupport {
 
       // Decode encrypted server events
       def decodeServerEvents(text: String) =
-        Dom4j.elements(decodeXML(text, forceEncryption = true).getRootElement, XXFORMS_EVENT_QNAME) map
+        Dom4j.elements(decodeXML(text, true).getRootElement, XXFORMS_EVENT_QNAME) map
           (LocalEvent(_, trusted = true)) toList
 
       // All global server events

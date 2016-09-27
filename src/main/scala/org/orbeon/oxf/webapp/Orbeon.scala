bac64f82122df742701cbcf6a6489c0a09a46cf5
ebruchez <ebruchez@orbeon.com>
2016-09-27 16:08:19 -0700

    Better logging of resource manager configuration

--- src/main/scala/org/orbeon/oxf/webapp/Orbeon.scala
+++ src/main/scala/org/orbeon/oxf/webapp/Orbeon.scala
@@ -59,7 +59,20 @@ object Orbeon {
       { case (name, value) ⇒ name.startsWith("oxf.resources.")} updated
         (WebAppResourceManagerImpl.WEB_APP_CONTEXT_KEY, context) asJava
 
-    logger.info("Initializing Resource Manager with: " + properties)
+    locally {
+
+      val propertiesAsScala = properties.asScala
+
+      val maxKeyLength =
+        if (propertiesAsScala.nonEmpty) propertiesAsScala.keysIterator.maxBy(_.length).length else 0
+
+      val sorted = propertiesAsScala.to[List].sortBy(_._1).collect{
+        case (key, value: String) ⇒ s""" "$key":${" " * (maxKeyLength + 1 - key.length)}"$value" """.trim
+      }.mkString("{\n  ", ",\n  ", "\n}")
+
+      logger.info(s"Initializing Resource Manager with: $sorted")
+    }
+
     ResourceManagerWrapper.init(properties)
 
     // 2. Initialize properties

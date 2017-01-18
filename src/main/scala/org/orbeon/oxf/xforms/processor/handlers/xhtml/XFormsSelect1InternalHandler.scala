88ee6dd2c6d119979bf6d5321d2209c48c4b9278
ebruchez <ebruchez@orbeon.com>
2017-01-18 11:10:30 -0800

    Move to Scala

---
+++ src/main/scala/org/orbeon/oxf/xforms/processor/handlers/xhtml/XFormsSelect1InternalHandler.scala
@@ -0,0 +1,23 @@
+/**
+  * Copyright (C) 2010 Orbeon, Inc.
+  *
+  * This program is free software; you can redistribute it and/or modify it under the terms of the
+  * GNU Lesser General Public License as published by the Free Software Foundation; either version
+  * 2.1 of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  * See the GNU Lesser General Public License for more details.
+  *
+  * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+  */
+package org.orbeon.oxf.xforms.processor.handlers.xhtml
+
+import org.orbeon.oxf.xforms.control.XFormsControl
+import org.xml.sax.Attributes
+
+// Internal appearance handler: do not output control or LHHA
+class XFormsSelect1InternalHandler extends XFormsControlLifecyleHandler(false, true) {
+  override protected def isMustOutputControl(control: XFormsControl) = false
+  protected def handleControlStart(uri: String, localname: String, qName: String, attributes: Attributes, effectiveId: String, control: XFormsControl) = ()
+}

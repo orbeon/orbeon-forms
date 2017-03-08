7de48dbcffdde680030a243ad0bd49a617d622fa
ebruchez <ebruchez@orbeon.com>
2017-03-08 15:54:27 -0800

    For #2996: more clean-up

--- xforms/js/src/main/scala/org/orbeon/xforms/UploadServer.scala
+++ xforms/js/src/main/scala/org/orbeon/xforms/UploadServer.scala
@@ -31,9 +31,10 @@ object UploadServer {
   val uploadEventQueue = new ExecutionQueue(asyncUploadRequestSetPromise)
 
   // While an upload is in progress:
-  var yuiConnectionOpt         : Option[js.Object]     = None // connection object
-  var processingEventOpt       : Option[UploadEvent]   = None // event for the field being uploaded
+  var currentEventOpt          : Option[UploadEvent]   = None // event for the field being uploaded
   var remainingEvents          : List[UploadEvent]     = Nil  // events for the fields that are left to be uploaded
+  var yuiConnectionOpt         : Option[js.Object]     = None // connection object
+
   var executionQueuePromiseOpt : Option[Promise[Unit]] = None // promise to complete when we are done processing all events
 
   // Method called by YUI when the upload ends successfully.
@@ -49,7 +50,7 @@ object UploadServer {
     (xhr.responseText: js.UndefOr[String]).toOption match {
       case Some(text) if (text ne null) && text.startsWith("&lt;xxf:event-response") ⇒
         // Clear upload field we just uploaded, otherwise subsequent uploads will upload the same data again
-        processingEventOpt foreach (_.upload.clear())
+        currentEventOpt foreach (_.upload.clear())
         // The Ajax response typically contains information about each file (name, size, etc)
         AjaxServer.handleResponseAjax(xhr)
         // Are we done, or do we still need to handle events for other forms?
@@ -64,9 +65,9 @@ object UploadServer {
    * remaining events.
    */
   def continueWithRemainingEvents(): Unit = {
-    processingEventOpt foreach (_.upload.uploadDone())
+    currentEventOpt foreach (_.upload.uploadDone())
     yuiConnectionOpt = None
-    processingEventOpt = None
+    currentEventOpt = None
     if (remainingEvents.isEmpty) {
       executionQueuePromiseOpt.foreach (_.success(()))
       executionQueuePromiseOpt = None
@@ -88,20 +89,20 @@ object UploadServer {
   private def asyncUploadRequest(events: List[UploadEvent]): Unit = events match {
 
     case Nil ⇒ throw new IllegalArgumentException
-    case processingEvent :: tail ⇒
+    case currentEvent :: tail ⇒
 
-      processingEventOpt = events.headOption
+      currentEventOpt = Some(currentEvent)
       remainingEvents = tail
 
       // Switch the upload to progress state, so users can't change the file and know the upload is in progress
-      processingEvent.upload.setState("progress")
+      currentEvent.upload.setState("progress")
 
       // Tell server we're starting uploads
       AjaxServer.fireEvents(
         js.Array(
           new AjaxServer.Event(
             new js.Object {
-              val targetId     = processingEvent.upload.container.id
+              val targetId     = currentEvent.upload.container.id
               val eventName    = EventNames.XXFormsUploadStart
               val showProgress = false
             }
@@ -114,11 +115,11 @@ object UploadServer {
 
       val newlyDisabledElems = {
 
-        def isUuidInput (e: html.Input) = e.name == "$uuid"                   // use `name` as `id` might be prefixed when embedding
-        def isFieldset  (e: html.Input) = e.tagName.toLowerCase == "fieldset" // disabling fieldsets disables all nested controls
-        def isThisUpload(e: html.Input) = $(e).hasClass(controls.Upload.UploadSelectClass) && $.contains(processingEvent.upload.container, e)
+        def isUuidInput (e: html.Input) = e.name == "$uuid"                   // `id` might be prefixed when embedding
+        def isFieldset  (e: html.Input) = e.tagName.toLowerCase == "fieldset" // disabling fieldsets disables nested controls
+        def isThisUpload(e: html.Input) = $(e).hasClass(controls.Upload.UploadSelectClass) && $.contains(currentEvent.upload.container, e)
 
-        processingEvent.form.elements.iterator collect {
+        currentEvent.form.elements.iterator collect {
           case e: html.Input ⇒ e
         } filterNot { e ⇒
           e.disabled.toOption.contains(true) || isUuidInput(e) || isFieldset(e) || isThisUpload(e)
@@ -128,20 +129,21 @@ object UploadServer {
       newlyDisabledElems foreach (_.disabled = true)
 
       // Trigger actual upload through a form POST and start asking server for progress
-      YUIConnect.setForm(processingEvent.form, isUpload = true, secureUri = true)
+      YUIConnect.setForm(currentEvent.form, isUpload = true, secureUri = true)
 
       yuiConnectionOpt =
         Some(
           YUIConnect.asyncRequest(
             "POST",
-            Globals.xformsServerUploadURL(processingEvent.form.id),
+            Globals.xformsServerUploadURL(currentEvent.form.id),
             new YUICallback {
               val upload: js.Function = uploadSuccess _
-              // Failure isn't called; instead we detect if an upload is interrupted through the progress-state="interrupted" in the Ajax response
+              // Failure isn't called; instead we detect if an upload is interrupted through
+              // `progress-state="interrupted"` in the Ajax response.
               val failure: js.Function = () ⇒ ()
               val argument = new js.Object {
                 val isUpload = true
-                val formId   = processingEvent.form.id
+                val formId   = currentEvent.form.id
               }
             }
           )
@@ -152,16 +154,14 @@ object UploadServer {
       newlyDisabledElems foreach (_.disabled = false)
   }
 
-  /**
-   * While there is a file upload going, this method runs at a regular interval and keeps asking the server for
-   * the status of the upload. Initially, it is called by the UploadServer when it sends the file. Then, it is called
-   * by the upload control, when it receives a progress update, as we only want to ask for an updated progress after
-   * we get an answer from the server.
-   */
+  // While there is a file upload going, this method runs at a regular interval and keeps asking the server for
+  // the status of the upload. Initially, it is called by `UploadServer` when it sends the file. Then, it is called
+  // by the upload control, when it receives a progress update, as we only want to ask for an updated progress after
+  // we get an answer from the server.
   def askForProgressUpdate(): Unit =
     // Keep asking for progress update at regular interval until there is no upload in progress
     js.timers.setTimeout(Properties.delayBeforeUploadProgressRefresh.get()) {
-      processingEventOpt foreach { processingEvent ⇒
+      currentEventOpt foreach { processingEvent ⇒
         AjaxServer.fireEvents(
           js.Array(
             new AjaxServer.Event(
@@ -178,18 +178,16 @@ object UploadServer {
       }
     }
 
-  /**
-   * Cancels the uploads currently in process. This can be called by the control, which delegates canceling to the
-   * UploadServer as it can't know about other controls being "uploaded" at the same time. Indeed, we can have
-   * uploads for multiple files at the same time, and for each one of the them, we want to clear the upload field,
-   * and switch back to the empty state so users can again select a file to upload.
-   */
+  // Cancel the uploads currently in process. This can be called by the control, which delegates canceling to
+  // `UploadServer` as it can't know about other controls being "uploaded" at the same time. Indeed, we can have
+  // uploads for multiple files at the same time, and for each one of the them, we want to clear the upload field,
+  // and switch back to the empty state so users can again select a file to upload.
   def cancel(doAbort: Boolean, event: String): Unit = {
 
     if (doAbort)
       yuiConnectionOpt foreach (YUIConnect.abort(_))
 
-    processingEventOpt foreach { processingEvent ⇒
+    currentEventOpt foreach { processingEvent ⇒
 
       AjaxServer.fireEvents(
         js.Array(

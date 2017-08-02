0ccc37eeb33724dc504d239b2e61329e0385d2ec
ebruchez <ebruchez@orbeon.com>
2017-08-02 13:31:36 -0700

    Comment

--- src/main/scala/org/orbeon/scaxon/SimplePath.scala
+++ src/main/scala/org/orbeon/scaxon/SimplePath.scala
@@ -25,6 +25,8 @@ import org.orbeon.saxon.tinytree.TinyTree
 import scala.collection.JavaConverters._
 import scala.collection._
 
+// The idea of this is to provide simple path navigation operations on Saxon nodes, without creating
+// full XPath expressions, as those need to be compiled, cached, and run, and can be very complex.
 object SimplePath {
 
   import Private._

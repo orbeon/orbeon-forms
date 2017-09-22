e61561a6b5a30117f4d0b9f8d217b8d8e16cd8db
avernet <avernet@orbeon.com>
2017-09-21 17:18:11 -0700

    Minor: copyright date

--- form-runner/js/src/main/scala/org/orbeon/fr/Grid.scala
+++ form-runner/js/src/main/scala/org/orbeon/fr/Grid.scala
@@ -1,5 +1,5 @@
 /**
-  * Copyright (C) 2007 Orbeon, Inc.
+  * Copyright (C) 2017 Orbeon, Inc.
   *
   * This program is free software; you can redistribute it and/or modify it under the terms of the
   * GNU Lesser General Public License as published by the Free Software Foundation; either version

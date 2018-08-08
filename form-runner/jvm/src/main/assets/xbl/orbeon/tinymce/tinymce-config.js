b5b625fa6cb16d2b70f95eea830cd7cb50222fab
ebruchez <ebruchez@orbeon.com>
2018-08-08 16:10:33 -0700

    For #1753: remove no longer supported configurations

--- form-runner/jvm/src/main/assets/xbl/orbeon/tinymce/tinymce-config.js
+++ form-runner/jvm/src/main/assets/xbl/orbeon/tinymce/tinymce-config.js
@@ -5,23 +5,12 @@ YAHOO.xbl.fr.Tinymce.DefaultConfig = {
     menubar:                                false,
     plugins:                                "lists link",
     toolbar:                                "bold italic | bullist numlist outdent indent | link",
-    theme_advanced_buttons2:                "",
-    theme_advanced_buttons3:                "",
-    theme_advanced_buttons4:                "",
-    theme_advanced_toolbar_location:        "top",
-    theme_advanced_toolbar_align:           "left",
-    theme_advanced_resizing:                true,
-    theme_advanced_blockformats:            "p,h1,h2,h3",
     gecko_spellcheck:                       true,
     doctype:                                '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
     encoding:                               "xml",
     entity_encoding:                        "raw",
     forced_root_block:                      'div',
-    remove_redundant_brs:                   true,
     verify_html:                            true,
-    editor_css:                             "",      // don't let the editor load UI CSS because that fails in portlets
-    theme_advanced_statusbar_location:      "none",
-    theme_advanced_path:                    false,
     // Override default TinyMCE class on tables, which adds borders. We can't leave this just empty, otherwise
     // TinyMCE puts its own CSS class.
     visual_table_class:                     "fr-tinymce-table",

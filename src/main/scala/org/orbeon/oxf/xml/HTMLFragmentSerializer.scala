ff107b71fc0a45b059f9bf7dbd15c86e0a03f585
ebruchez <ebruchez@orbeon.com>
2018-11-09 14:56:50 -0800

    Fix #3817 "CP-1252 filtering should also happen in `HTMLFragmentSerializer`"

--- src/main/scala/org/orbeon/oxf/xml/HTMLFragmentSerializer.scala
+++ src/main/scala/org/orbeon/oxf/xml/HTMLFragmentSerializer.scala
@@ -14,19 +14,33 @@
 package org.orbeon.oxf.xml
 
 import java.io.Writer
-import javax.xml.transform.stream.StreamResult
 
+import javax.xml.transform.stream.StreamResult
+import org.orbeon.dom.QName
+import org.orbeon.oxf.processor.converter.{TextConverterBase, XMLConverter}
+import org.orbeon.oxf.properties.Properties
+import org.orbeon.oxf.xml.dom4j.Dom4jUtils
 import org.xml.sax.Attributes
 
 object HTMLFragmentSerializer {
-  
+
   def create(writer: Writer, skipRootElement: Boolean) = {
-    
+
     val identity = TransformerUtils.getIdentityTransformerHandler
-    
+
     TransformerUtils.applyOutputProperties(
       identity.getTransformer,
-      "html",
+      Dom4jUtils.qNameToExplodedQName(
+        Properties.instance.getPropertySet(
+          QName(
+            "html-converter",
+            XMLConstants.OXF_PROCESSORS_NAMESPACE
+          )
+        ).getQName(
+          TextConverterBase.DEFAULT_METHOD_PROPERTY_NAME,
+          XMLConverter.DEFAULT_METHOD
+        )
+      ),
       null,
       null,
       null,
@@ -38,32 +52,32 @@ object HTMLFragmentSerializer {
     )
 
     identity.setResult(new StreamResult(writer))
-    
+
     val htmlReceiver = new PlainHTMLOrXHTMLReceiver("", identity)
-    
+
     if (skipRootElement)
       new SkipRootElement(htmlReceiver)
     else
       htmlReceiver
-      
+
   }
-  
+
   class SkipRootElement(receiver: XMLReceiver) extends ForwardingXMLReceiver(receiver) {
-  
+
     var level = 0
-  
+
     override def startElement(uri: String, localname: String, qName: String, attributes: Attributes): Unit = {
-      
+
       if (level > 0)
         super.startElement(uri, localname, qName, attributes)
-      
+
       level += 1
     }
-  
+
     override def endElement(uri: String, localname: String, qName: String): Unit = {
-      
+
       level -= 1
-      
+
       if (level > 0)
         super.endElement(uri, localname, qName)
     }

fdac8ef3aff22c86834fee22d07f3162ddb6fe7d
ebruchez <ebruchez@orbeon.com>
2018-11-19 11:45:05 -0800

    Following #3630: test for sibling position

--- src/test/scala/org/orbeon/dom/saxon/NodeWrapperTest.scala
+++ src/test/scala/org/orbeon/dom/saxon/NodeWrapperTest.scala
@@ -1,7 +1,5 @@
-package org.orbeon.dom.saxon
-
 /**
-  * Copyright (C) 2007 Orbeon, Inc.
+  * Copyright (C) 2018 Orbeon, Inc.
   *
   * This program is free software; you can redistribute it and/or modify it under the terms of the
   * GNU Lesser General Public License as published by the Free Software Foundation; either version
@@ -13,6 +11,64 @@ package org.orbeon.dom.saxon
   *
   * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
   */
-class NodeWrapperTest {
+package org.orbeon.dom.saxon
+
+import org.orbeon.dom.{DocumentFactory, Namespace}
+import org.orbeon.oxf.util.XPath
+import org.orbeon.saxon.om.SiblingCountingNode
+import org.scalatest.FunSpec
+
+class NodeWrapperTest extends FunSpec {
+
+  val XS = Namespace("xs", "http://www.w3.org/2001/XMLSchema")
+  val XF = Namespace("xf", "http://www.w3.org/2002/xforms")
+
+  describe("Sibling position") {
+
+    val doc = DocumentFactory.createDocument("root")
+
+    val rootElem = doc.getRootElement
+
+    rootElem.add(XS)
+    rootElem.add(XF)
+
+    import org.orbeon.oxf.util.CoreUtils._
+
+    val text1  = DocumentFactory.createText("before")          |!> rootElem.add
+    val child1 = DocumentFactory.createElement("elem1")        |!> rootElem.add
+    val text2  = DocumentFactory.createText("between")         |!> rootElem.add
+    val child2 = DocumentFactory.createElement("elem2")        |!> rootElem.add
+    val text3  = DocumentFactory.createText("after")           |!> rootElem.add
+
+    val att1   = DocumentFactory.createAttribute("att1", "v1") |!> child1.add
+    val att2   = DocumentFactory.createAttribute("att2", "v2") |!> child1.add
+
+    val att3   = DocumentFactory.createAttribute("att3", "v3") |!> child2.add
+    val att4   = DocumentFactory.createAttribute("att4", "v4") |!> child2.add
+
+    val expected = List(
+      (doc     , 0),
+      (rootElem, 0),
+
+      (text1   , 2),
+      (child1  , 3), // 2 namespace nodes before (BAD!)
+      (text2   , 4),
+      (child2  , 5),
+      (text3   , 6),
+
+      (att1    , 0),
+      (att2    , 1),
+
+      (att3    , 0),
+      (att4    , 1)
+    )
+
+    val docWrapper = new DocumentWrapper(doc, null, XPath.GlobalConfiguration)
+
+    for (((node, expected), index) ← expected.zipWithIndex)
+      it(s"for index $index") {
+        assert(docWrapper.wrap(node).asInstanceOf[SiblingCountingNode].getSiblingPosition === expected)}
+
+  }
 
 }

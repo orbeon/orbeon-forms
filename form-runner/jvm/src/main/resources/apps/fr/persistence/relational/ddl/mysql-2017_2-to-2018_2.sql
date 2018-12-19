3ad0671694e144cfc950db350d6245696a0c2e14
avernet <avernet@orbeon.com>
2018-12-19 11:02:50 -0800

    For #3813: add missing index

--- form-runner/jvm/src/main/resources/apps/fr/persistence/relational/ddl/mysql-2017_2-to-2018_2.sql
+++ form-runner/jvm/src/main/resources/apps/fr/persistence/relational/ddl/mysql-2017_2-to-2018_2.sql
@@ -60,4 +60,5 @@ MODIFY control             VARCHAR(255)          CHARACTER SET utf8mb4 COLLATE u
 MODIFY val                 MEDIUMTEXT            CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NOT NULL;
 
 CREATE        INDEX orbeon_form_data_i2           ON orbeon_form_data              (document_id);
+CREATE        INDEX orbeon_from_data_attach_i2    ON orbeon_form_data_attach       (document_id);
 CREATE        INDEX orbeon_i_current_i2           ON orbeon_i_current              (app, form, draft);
--- form-runner/jvm/src/main/resources/apps/fr/persistence/relational/ddl/oracle-2017_2-to-2018_2.sql
+++ form-runner/jvm/src/main/resources/apps/fr/persistence/relational/ddl/oracle-2017_2-to-2018_2.sql
@@ -1,2 +1,3 @@
 CREATE        INDEX orbeon_form_data_i2           ON orbeon_form_data              (document_id);
+CREATE        INDEX orbeon_from_data_attach_i2    ON orbeon_form_data_attach       (document_id);
 CREATE        INDEX orbeon_i_current_i2           ON orbeon_i_current              (app, form, draft);

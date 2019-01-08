fd6e24e1320163cdfdd6a860f01031e5467af15e
ebruchez <ebruchez@orbeon.com>
2019-01-08 08:54:07 -0800

    For #233: missing file

---
+++ xforms/shared/src/main/scala/org/orbeon/xforms/rpc/definitions.scala
@@ -0,0 +1,27 @@
+/**
+  * Copyright (C) 2019 Orbeon, Inc.
+  *
+  * This program is free software; you can redistribute it and/or modify it under the terms of the
+  * GNU Lesser General Public License as published by the Free Software Foundation; either version
+  *  2.1 of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  * See the GNU Lesser General Public License for more details.
+  *
+  * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+  */
+
+package org.orbeon.xforms.rpc
+
+import org.orbeon.oxf.util.Modifier
+
+
+case class Control(id: String, valueOpt: Option[String])
+
+case class Controls(controls: List[Control])
+
+
+case class KeyListener(eventName: Set[String], observer: String, keyText: String, modifiers: Set[Modifier])
+
+case class KeyListeners(listeners: List[KeyListener])

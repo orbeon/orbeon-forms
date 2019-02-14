8bc0675215706d29ce4def5d05cac6945298cd27
ebruchez <ebruchez@orbeon.com>
2019-02-14 11:05:00 -0800

    Move to Scala

---
+++ xforms/jvm/src/main/scala/org/orbeon/oxf/xforms/submission/ClientGetAllSubmission.scala
@@ -0,0 +1,43 @@
+/**
+  * Copyright (C) 2009 Orbeon, Inc.
+  *
+  * This program is free software; you can redistribute it and/or modify it under the terms of the
+  * GNU Lesser General Public License as published by the Free Software Foundation; either version
+  * 2.1 of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  * See the GNU Lesser General Public License for more details.
+  *
+  * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+  */
+package org.orbeon.oxf.xforms.submission
+
+import org.orbeon.oxf.util.PathUtils
+import org.orbeon.oxf.xforms.action.actions.XFormsLoadAction
+
+/**
+  * Client-side GET all submission. It stores the resulting location directly into the containing document.
+  */
+class ClientGetAllSubmission(submission: XFormsModelSubmission)
+  extends BaseSubmission(submission) {
+
+  def getType = "get all"
+
+  def isMatch(p: SubmissionParameters, p2: SecondPassParameters, sp: SerializationParameters): Boolean =
+    p.isHandlingClientGetAll
+
+  def connect(p: SubmissionParameters, p2: SecondPassParameters, sp: SerializationParameters): SubmissionResult = {
+    XFormsLoadAction.resolveStoreLoadValue(
+      containingDocument,
+      submission.getSubmissionElement,
+      true,
+      PathUtils.appendQueryString(p2.actionOrResource, Option(sp.queryString) getOrElse ""),
+      null,
+      null,
+      p.urlNorewrite,
+      Option(submission.getActiveSubmissionParameters) map (_.xxfShowProgress) getOrElse true
+    )
+    null
+  }
+}

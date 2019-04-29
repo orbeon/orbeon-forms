d46dafe406897a1b736607d991a2c70bf509abf5
ebruchez <ebruchez@orbeon.com>
2019-04-29 10:26:01 -0700

    For #4011: refactor: split into traits

--- src/main/scala/org/orbeon/oxf/xml/ElementHandlerController.scala
+++ src/main/scala/org/orbeon/oxf/xml/ElementHandlerController.scala
@@ -37,6 +37,10 @@ import scala.util.control.NonFatal
   *
   * Q: Should use pools of handlers to reduce memory consumption?
   */
+class ElementHandlerController
+  extends ElementHandlerControllerXMLReceiver
+     with ElementHandlerControllerHandlers
+
 object ElementHandlerController {
 
   type Matcher[T <: AnyRef] = (Attributes, AnyRef) ⇒ T
@@ -44,7 +48,7 @@ object ElementHandlerController {
   // For Java callers
   abstract class Function2Base[V1, V2, R] extends ((V1, V2) ⇒ R)
 
-  private case class HandlerInfo(level: Int, elementHandler: ElementHandler, locator: Locator) {
+  private[xml] case class HandlerInfo(level: Int, elementHandler: ElementHandler, locator: Locator) {
 
     val saxStore: Option[SAXStore] = elementHandler.isRepeating option new SAXStore
 
@@ -53,14 +57,14 @@ object ElementHandlerController {
       saxStore foreach (_.setDocumentLocator(locator))
   }
 
-  private val AllMatcher: Matcher[jl.Boolean] = (_: Attributes, _: AnyRef) ⇒ jl.Boolean.TRUE
+  private[xml] val AllMatcher: Matcher[jl.Boolean] = (_: Attributes, _: AnyRef) ⇒ jl.Boolean.TRUE
 
-  private type HandlerAndMatcher = (String, Matcher[_ <: AnyRef])
+  private[xml] type HandlerAndMatcher = (String, Matcher[_ <: AnyRef])
 
   // `Class.forName` is expensive, so we cache mappings
   private val classNameToHandlerClass = new ju.concurrent.ConcurrentHashMap[String, Constructor[ElementHandler]]
 
-  private def getHandlerByClassName(
+  private[xml] def getHandlerByClassName(
     handlerClassName : String,
     uri              : String,
     localname        : String,
@@ -90,7 +94,7 @@ object ElementHandlerController {
     }
   }
 
-  private def withWrapThrowable[T](thunk: ⇒ T)(implicit locator: OutputLocator): T =
+  private[xml] def withWrapThrowable[T](thunk: ⇒ T)(implicit locator: OutputLocator): T =
     try {
       thunk
     } catch {
@@ -99,7 +103,7 @@ object ElementHandlerController {
     }
 }
 
-class ElementHandlerController extends XMLReceiver {
+trait ElementHandlerControllerHandlers extends XMLReceiver {
 
   import ElementHandlerController._
 
@@ -108,10 +112,9 @@ class ElementHandlerController extends XMLReceiver {
   private val _customMatchers  = new ju.ArrayList[HandlerAndMatcher]
 
   private val _handlerInfos = new ju.Stack[HandlerInfo]
-  private var _currentHandlerInfo: HandlerInfo = null
-
-  private var _isFillingUpSAXStore = false
-  private var _level = 0
+  def currentHandlerInfoOpt                : Option[HandlerInfo] = ! _handlerInfos.isEmpty option _handlerInfos.peek
+  def pushHandler(handlerInfo: HandlerInfo): Unit = _handlerInfos.push(handlerInfo)
+  def popHandler ()                        : Unit = _handlerInfos.pop()
 
   private var _elementHandlerContext: AnyRef = null
   def setElementHandlerContext(elementHandlerContext: AnyRef): Unit = _elementHandlerContext = elementHandlerContext
@@ -120,11 +123,9 @@ class ElementHandlerController extends XMLReceiver {
   def getOutput: DeferredXMLReceiver = _output
   def setOutput(output: DeferredXMLReceiver): Unit = _output = output
 
-  private val _namespaceContext = new NamespaceContext
-  def namespaceContext: NamespaceContext = _namespaceContext
-
-  private implicit var _locator: OutputLocator = null
-  def locator: Locator = _locator
+  // Implemented by `ElementHandlerControllerXMLReceiver`
+  implicit def locator: OutputLocator
+  def level: Int
 
   // Register a handler. The handler can match, in order:
   //
@@ -159,129 +160,111 @@ class ElementHandlerController extends XMLReceiver {
 
     // Replay content of current SAXStore
 
-    val beforeLocatorCount = if (_locator ne null) _locator.size else 0
+    val beforeLocatorCount = if (locator ne null) locator.size else 0
 
-    _currentHandlerInfo.saxStore foreach (_.replay(this))
+    currentHandlerInfoOpt flatMap(_.saxStore) foreach (_.replay(this))
 
-    val afterLocatorCount = if (_locator ne null) _locator.size else 0
+    val afterLocatorCount = if (locator ne null) locator.size else 0
 
     if (beforeLocatorCount != afterLocatorCount) {
       // This means that the SAXStore replay called `setDocumentLocator()`
       assert(afterLocatorCount == beforeLocatorCount + 1, "incorrect locator stack state")
-      _locator.pop()
+      locator.pop()
     }
   }
 
-  // A handler may call this to start providing new dynamic content to process.
-  def startBody(): Unit = {
-    // Just push null so that the content is not subject to the `isForwarding` test.
-    _handlerInfos.push(null)
-    _currentHandlerInfo = null
-  }
+  // A handler may call this to start providing new dynamic content to process
+  // TODO: Not great: "just push null so that the content is not subject to the `isForwarding` test".
+  def startBody(): Unit =
+    pushHandler(null)
 
-  // A handler may call this to end providing new dynamic content to process.
-  def endBody(): Unit = {
-    _handlerInfos.pop()
-    _currentHandlerInfo = if (! _handlerInfos.isEmpty) _handlerInfos.peek else null
-  }
+  // A handler may call this to end providing new dynamic content to process
+  def endBody(): Unit =
+    popHandler()
 
-  def startDocument(): Unit = {
-    withWrapThrowable {
-      _output.startDocument()
-    }
-  }
+  def findHandlerFromElem(element: Element, handlerContext: AnyRef): Option[ElementHandler] =
+    findHandler(
+      element.getNamespaceURI,
+      element.getName,
+      element.getQualifiedName,
+      Dom4jUtils.getSAXAttributes(element)
+    ) map
+      (_.elementHandler)
 
-  def endDocument(): Unit = {
-    withWrapThrowable {
-      _output.endDocument()
-    }
-  }
+  def findHandler(
+    uri            : String,
+    localname      : String,
+    qName          : String,
+    attributes     : Attributes
+  ): Option[HandlerInfo] = {
 
-  def startElement(uri: String, localname: String, qName: String, attributes: Attributes): Unit =
-    withWrapThrowable {
+    def fromCustomMatchers =
+      findWithMatchers(_customMatchers, uri, localname, qName, attributes, _elementHandlerContext)
 
-      // Increment level before, so that if callees like start() and startElement() use us, the level is correct
-      _level += 1
-
-      _namespaceContext.startElement()
-
-      if (_isFillingUpSAXStore) {
-        _currentHandlerInfo.saxStore foreach (_.startElement(uri, localname, qName, attributes))
-      } else if ((_currentHandlerInfo ne null) && !_currentHandlerInfo.elementHandler.isForwarding) {
-        // The current handler doesn't want forwarding
-        // Just ignore content
-      } else { // Look for a new handler
-        findHandler(uri, localname, qName, attributes, _elementHandlerContext) match {
-          case Some(handlerInfo) ⇒
-            // New handler found
-            val elementHandler = handlerInfo.elementHandler
-            // Push current handler
-            _currentHandlerInfo = handlerInfo
-            _handlerInfos.push(_currentHandlerInfo)
-            if (elementHandler.isRepeating) {
-              // Repeating handler will process its body later
-              _isFillingUpSAXStore = true
-            } else {
-              // Non-repeating handler processes its body immediately
-              // Signal init/start to current handler
-              elementHandler.start()
-            }
-          case None ⇒
-            // New handler not found, send to output
-            _output.startElement(uri, localname, qName, attributes)
-        }
-      }
-    }
-
-  def endElement(uri: String, localname: String, qName: String): Unit =
-    withWrapThrowable {
-      if ((_currentHandlerInfo ne null) && _currentHandlerInfo.level == _level) {
-        // End of current handler
-        if (_isFillingUpSAXStore) {
-          // Was filling-up SAXStore
-          _isFillingUpSAXStore = false
-          // Process body once
-          _currentHandlerInfo.elementHandler.start()
-          _currentHandlerInfo.elementHandler.end()
-        } else {
-          // Signal end to current handler
-          _currentHandlerInfo.elementHandler.end()
-        }
-        // Pop current handler
-        _handlerInfos.pop()
-        _currentHandlerInfo = if (! _handlerInfos.isEmpty) _handlerInfos.peek else null
-      } else if (_isFillingUpSAXStore) {
-        _currentHandlerInfo.saxStore foreach (_.endElement(uri, localname, qName))
-      } else if ((_currentHandlerInfo ne null) && ! _currentHandlerInfo.elementHandler.isForwarding) {
-        // NOP
-      } else {
-        // Just forward
-        _output.endElement(uri, localname, qName)
+    def fromFullMatchers =
+      Option(_handlerMatchers.get(XMLUtils.buildExplodedQName(uri, localname))) flatMap { handlerMatchers ⇒
+        findWithMatchers(handlerMatchers, uri, localname, qName, attributes, _elementHandlerContext)
       }
 
-      _namespaceContext.endElement()
+    def fromUriBasedHandler =
+      Option(_uriHandlers.get(uri)) map { uriHandlerClassName ⇒
+        HandlerInfo(
+          level,
+          getHandlerByClassName(uriHandlerClassName, uri, localname, qName, attributes, null, _elementHandlerContext),
+          locator
+        )
+      }
 
-      _level -= 1
+    fromCustomMatchers orElse fromFullMatchers orElse fromUriBasedHandler
+  }
+
+  private def findWithMatchers(
+    matchers       : ju.List[HandlerAndMatcher],
+    uri            : String,
+    localname      : String,
+    qName          : String,
+    attributes     : Attributes,
+    handlerContext : AnyRef
+  ): Option[HandlerInfo] =
+    matchers.asScala.iterator map {
+      case (handlerClassName, matcher) ⇒
+        handlerClassName → matcher(attributes, _elementHandlerContext)
+    } collectFirst {
+      case (handlerClassName, matched) if matched ne null ⇒
+        HandlerInfo(
+          level,
+          getHandlerByClassName(handlerClassName, uri, localname, qName, attributes, matched, handlerContext),
+          locator
+        )
     }
+}
 
-  def startPrefixMapping(prefix: String, uri: String): Unit =
-    fillOrForward { r ⇒
-      _namespaceContext.startPrefixMapping(prefix, uri)
-      r.startPrefixMapping(prefix, uri)
-    }
+trait ElementHandlerControllerXMLReceiver extends XMLReceiver {
 
-  def endPrefixMapping     (s: String)                                  : Unit = fillOrForward(_.endPrefixMapping(s))
-  def characters           (chars: Array[Char], start: Int, length: Int): Unit = fillOrForward(_.characters(chars, start, length))
-  def ignorableWhitespace  (ch: Array[Char], start: Int, length: Int)   : Unit = fillOrForward(_.ignorableWhitespace(ch, start, length))
-  def processingInstruction(target: String, data: String)               : Unit = fillOrForward(_.processingInstruction(target, data))
-  def skippedEntity(name: String)                                       : Unit = fillOrForward(_.skippedEntity(name))
-  def startDTD   (name: String, publicId: String, systemId: String)     : Unit = fillOrForward(_.startDTD(name, publicId, systemId))
-  def endDTD     ()                                                     : Unit = fillOrForward(_.endDTD())
-  def startEntity(name: String)                                         : Unit = fillOrForward(_.startEntity(name))
-  def endEntity  (name: String)                                         : Unit = fillOrForward(_.endEntity(name))
-  def startCDATA ()                                                     : Unit = fillOrForward(_.startCDATA())
-  def endCDATA   ()                                                     : Unit = fillOrForward(_.endCDATA())
-  def comment    (ch: Array[Char], start: Int, length: Int)             : Unit = fillOrForward(_.comment(ch, start, length))
+  import ElementHandlerController._
+
+  private var _locator: OutputLocator = null
+  implicit def locator: OutputLocator = _locator
+
+  private val _namespaceContext = new NamespaceContext
+  def namespaceContext: NamespaceContext = _namespaceContext
+
+  private var _isFillingUpSAXStore = false
+  private var _level = 0
+  def level: Int = _level
+
+  // Implemented by `ElementHandlerControllerHandlers`
+  def getOutput: XMLReceiver
+  def currentHandlerInfoOpt: Option[HandlerInfo]
+  def pushHandler(handlerInfo: HandlerInfo): Unit
+  def popHandler(): Unit
+
+  def findHandler(
+    uri            : String,
+    localname      : String,
+    qName          : String,
+    attributes     : Attributes
+  ): Option[HandlerInfo]
 
   // NOTE: This is called by the outer caller. Then it can be called by repeat or component body replay, which
   // recursively hit this controller. The outer caller may or may not call setDocumentLocator() once. If there is
@@ -303,69 +286,107 @@ class ElementHandlerController extends XMLReceiver {
       }
     }
 
-  def findHandlerFromElem(element: Element, handlerContext: AnyRef): Option[ElementHandler] =
-    findHandler(
-      element.getNamespaceURI,
-      element.getName,
-      element.getQualifiedName,
-      Dom4jUtils.getSAXAttributes(element),
-      handlerContext
-    ) map
-      (_.elementHandler)
-
-  private def findHandler(
-    uri            : String,
-    localname      : String,
-    qName          : String,
-    attributes     : Attributes,
-    handlerContext : AnyRef
-  ): Option[HandlerInfo] = {
-
-    def fromCustomMatchers =
-      findWithMatchers(_customMatchers, uri, localname, qName, attributes, handlerContext)
-
-    def fromFullMatchers =
-      Option(_handlerMatchers.get(XMLUtils.buildExplodedQName(uri, localname))) flatMap { handlerMatchers ⇒
-        findWithMatchers(handlerMatchers, uri, localname, qName, attributes, handlerContext)
-      }
-
-    def fromUriBasedHandler =
-      Option(_uriHandlers.get(uri)) map { uriHandlerClassName ⇒
-        HandlerInfo(
-          _level,
-          getHandlerByClassName(uriHandlerClassName, uri, localname, qName, attributes, null, handlerContext),
-          _locator
-        )
-      }
-
-    fromCustomMatchers orElse fromFullMatchers orElse fromUriBasedHandler
-  }
-
-  private def findWithMatchers(
-    matchers       : ju.List[HandlerAndMatcher],
-    uri            : String,
-    localname      : String,
-    qName          : String,
-    attributes     : Attributes,
-    handlerContext : AnyRef
-  ): Option[HandlerInfo] =
-    matchers.asScala.iterator map {
-      case (handlerClassName, matcher) ⇒
-        handlerClassName → matcher(attributes, _elementHandlerContext)
-    } collectFirst {
-      case (handlerClassName, matched) if matched ne null ⇒
-        HandlerInfo(
-          _level,
-          getHandlerByClassName(handlerClassName, uri, localname, qName, attributes, matched, handlerContext),
-          _locator
-        )
+  def startDocument(): Unit =
+    withWrapThrowable {
+      getOutput.startDocument()
     }
+
+  def endDocument(): Unit =
+    withWrapThrowable {
+      getOutput.endDocument()
+    }
+
+  def startElement(uri: String, localname: String, qName: String, attributes: Attributes): Unit =
+    withWrapThrowable {
+
+      // Increment level before, so that if callees like `start()` and `startElement()` use us, the level is correct
+      _level += 1
+
+      _namespaceContext.startElement()
+
+      currentHandlerInfoOpt match {
+        case Some(currentHandlerInfo) if _isFillingUpSAXStore ⇒
+          currentHandlerInfo.saxStore foreach (_.startElement(uri, localname, qName, attributes))
+        case Some(currentHandlerInfo) if ! currentHandlerInfo.elementHandler.isForwarding ⇒
+          // NOP
+        case _ ⇒
+          // Look for a new handler
+          findHandler(uri, localname, qName, attributes) match {
+            case Some(handlerInfo) ⇒
+              // New handler found
+              pushHandler(handlerInfo)
+              if (handlerInfo.elementHandler.isRepeating) {
+                // Repeating handler will process its body later
+                _isFillingUpSAXStore = true
+              } else {
+                // Non-repeating handler processes its body immediately
+                // Signal init/start to current handler
+                handlerInfo.elementHandler.start()
+              }
+            case None ⇒
+              // New handler not found, send to output
+              getOutput.startElement(uri, localname, qName, attributes)
+          }
+      }
+    }
+
+  def endElement(uri: String, localname: String, qName: String): Unit =
+    withWrapThrowable {
+      currentHandlerInfoOpt match {
+        case Some(currentHandlerInfo) if currentHandlerInfo.level == _level ⇒
+          // End of current handler
+          if (_isFillingUpSAXStore) {
+            // Was filling-up SAXStore
+            _isFillingUpSAXStore = false
+            // Process body once
+            currentHandlerInfo.elementHandler.start()
+            currentHandlerInfo.elementHandler.end()
+          } else {
+            // Signal end to current handler
+            currentHandlerInfo.elementHandler.end()
+          }
+          popHandler()
+        case Some(currentHandlerInfo) if _isFillingUpSAXStore ⇒
+          currentHandlerInfo.saxStore foreach (_.endElement(uri, localname, qName))
+        case Some(currentHandlerInfo) if ! currentHandlerInfo.elementHandler.isForwarding ⇒
+          // NOP
+        case _ ⇒
+          // Just forward
+          getOutput.endElement(uri, localname, qName)
+      }
+
+      _namespaceContext.endElement()
+
+      _level -= 1
+    }
+
+  def startPrefixMapping(prefix: String, uri: String): Unit =
+    fillOrForward { r ⇒
+      _namespaceContext.startPrefixMapping(prefix, uri)
+      r.startPrefixMapping(prefix, uri)
+    }
+
+  def endPrefixMapping     (s: String)                                       : Unit = fillOrForward(_.endPrefixMapping(s))
+  def characters           (chars: Array[Char], start: Int, length: Int)     : Unit = fillOrForward(_.characters(chars, start, length))
+  def processingInstruction(target: String, data: String)                    : Unit = fillOrForward(_.processingInstruction(target, data))
+  def comment              (ch: Array[Char], start: Int, length: Int)        : Unit = fillOrForward(_.comment(ch, start, length))
+
+  // NOTE: We don't expect calls to any of the following
+  def ignorableWhitespace  (ch: Array[Char], start: Int, length: Int)        : Unit = fillOrForward(_.ignorableWhitespace(ch, start, length))
+  def skippedEntity        (name: String)                                    : Unit = fillOrForward(_.skippedEntity(name))
+  def startDTD             (name: String, publicId: String, systemId: String): Unit = fillOrForward(_.startDTD(name, publicId, systemId))
+  def endDTD               ()                                                : Unit = fillOrForward(_.endDTD())
+  def startEntity          (name: String)                                    : Unit = fillOrForward(_.startEntity(name))
+  def endEntity            (name: String)                                    : Unit = fillOrForward(_.endEntity(name))
+  def startCDATA           ()                                                : Unit = fillOrForward(_.startCDATA())
+  def endCDATA             ()                                                : Unit = fillOrForward(_.endCDATA())
 
   private def fillOrForward[T](thunk: XMLReceiver ⇒ T): Unit =
     withWrapThrowable {
       if (_isFillingUpSAXStore)
-        _currentHandlerInfo.saxStore foreach thunk
-      else if ((_currentHandlerInfo eq null) || _currentHandlerInfo.elementHandler.isForwarding)
-        thunk(_output)
+        currentHandlerInfoOpt flatMap (_.saxStore) foreach thunk
+      else if (currentHandlerInfoOpt forall (_.elementHandler.isForwarding))
+        thunk(getOutput)
     }
+
 }

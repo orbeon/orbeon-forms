5c414f06eebe3c0d4f53e2796cbeb00fc4900a52
ebruchez <ebruchez@orbeon.com>
2019-05-15 17:12:59 -0700

    Indent, types

--- xforms/jvm/src/main/scala/org/orbeon/oxf/xforms/control/LHHAValue.scala
+++ xforms/jvm/src/main/scala/org/orbeon/oxf/xforms/control/LHHAValue.scala
@@ -1,16 +1,16 @@
 /**
- * Copyright (C) 2013 Orbeon, Inc.
- *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU Lesser General Public License as published by the Free Software Foundation; either version
- * 2.1 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
- */
+  * Copyright (C) 2013 Orbeon, Inc.
+  *
+  * This program is free software; you can redistribute it and/or modify it under the terms of the
+  * GNU Lesser General Public License as published by the Free Software Foundation; either version
+  * 2.1 of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  * See the GNU Lesser General Public License for more details.
+  *
+  * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+  */
 package org.orbeon.oxf.xforms.control
 
 import org.apache.commons.lang3.StringUtils
@@ -22,18 +22,18 @@ import org.orbeon.oxf.xml.dom4j.LocationData
 
 case class LHHAValue(label: String, isHTML: Boolean) {
 
-    def streamAsHTML(ch: XMLReceiverHelper, locationData: LocationData): Unit =
-        if (isHTML)
-            streamHTMLFragment(ch.getXmlReceiver, label, locationData, "")
-        else
-            ch.text(StringUtils.defaultString(label))
+  def streamAsHTML(ch: XMLReceiverHelper, locationData: LocationData): Unit =
+    if (isHTML)
+      streamHTMLFragment(ch.getXmlReceiver, label, locationData, "")
+    else
+      ch.text(StringUtils.defaultString(label))
 
-    def htmlValue(locationData: LocationData) =
-        if (isHTML)
-            getEscapedHTMLValue(locationData, label)
-        else
-          MarkupUtils.escapeXMLMinimal(label)
+  def htmlValue(locationData: LocationData): String =
+    if (isHTML)
+      getEscapedHTMLValue(locationData, label)
+    else
+      MarkupUtils.escapeXMLMinimal(label)
 
-    def javaScriptValue(locationData: LocationData) =
-        escapeJavaScript(htmlValue(locationData))
+  def javaScriptValue(locationData: LocationData): String =
+    escapeJavaScript(htmlValue(locationData))
 }

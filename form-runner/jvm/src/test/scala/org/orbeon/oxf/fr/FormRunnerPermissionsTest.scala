f88db723906ddf104cbab35b39d92aff5207fcc8
avernet <avernet@orbeon.com>
2019-09-14 17:56:13 -0700

    For #4173: fix test

--- form-runner/jvm/src/test/scala/org/orbeon/oxf/fr/FormRunnerPermissionsTest.scala
+++ form-runner/jvm/src/test/scala/org/orbeon/oxf/fr/FormRunnerPermissionsTest.scala
@@ -14,6 +14,7 @@
 package org.orbeon.oxf.fr
 
 import org.orbeon.oxf.externalcontext.{Credentials, Organization, ParametrizedRole, SimpleRole}
+import org.orbeon.oxf.fr.permission.Operation.{Create, Read, Update}
 import org.orbeon.oxf.fr.permission.PermissionsAuthorization._
 import org.orbeon.oxf.fr.permission._
 import org.scalatest.FunSpec

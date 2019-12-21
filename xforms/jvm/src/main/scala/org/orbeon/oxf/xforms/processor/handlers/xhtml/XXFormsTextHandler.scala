5887af81becdc62fd676d89a72a16ca18beddd07
ebruchez <ebruchez@orbeon.com>
2019-12-20 16:09:55 -0800

    Move `XXFormsTextHandler to Scala

---
+++ xforms/jvm/src/main/scala/org/orbeon/oxf/xforms/processor/handlers/xhtml/XXFormsTextHandler.scala
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2009 Orbeon, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+package org.orbeon.oxf.xforms.processor.handlers.xhtml
+
+import org.orbeon.oxf.xforms.control.controls.XXFormsTextControl
+import org.xml.sax.Attributes
+
+class XXFormsTextHandler(uri: String, localname: String, qName: String, attributes: Attributes, matched: Any, handlerContext: Any)
+  extends XFormsBaseHandlerXHTML(uri, localname, qName, attributes, matched, handlerContext, repeating = false, forwarding = false) {
+
+  override def start(): Unit = {
+    val effectiveId    = xformsHandlerContext.getEffectiveId(attributes)
+    val textControl    = containingDocument.getControlByEffectiveId(effectiveId).asInstanceOf[XXFormsTextControl]
+    val contentHandler = xformsHandlerContext.getController.getOutput
+
+    val externalValue = textControl.getExternalValue()
+
+    if (externalValue != null && externalValue.length > 0)
+      contentHandler.characters(externalValue.toCharArray, 0, externalValue.length)
+  }
+}

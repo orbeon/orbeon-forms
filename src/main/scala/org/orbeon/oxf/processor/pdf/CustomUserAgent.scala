4b332e9b81644c740d4c5420f52b56950f899a6f
Erik Bruchez <erik@bruchez.org>
2020-05-22 09:53:41 -0700

    For #4442: refactor and clean-up

--- src/main/scala/org/orbeon/oxf/processor/pdf/CustomUserAgent.scala
+++ src/main/scala/org/orbeon/oxf/processor/pdf/CustomUserAgent.scala
@@ -1,27 +1,19 @@
-/*
- * Copyright (c) 2004, 2005 Torbjoern Gannholm
- * Copyright (c) 2006 Wisconsin Court System
+/**
+ * Copyright (C) 2020 Orbeon, Inc.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ *  2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
  */
 package org.orbeon.oxf.processor.pdf
 
-import java.awt.Color
-import java.awt.geom.AffineTransform
-import java.awt.image.{AffineTransformOp, BufferedImage}
-import java.io.{BufferedInputStream, InputStream}
+import java.io.InputStream
 import java.net.URI
 
 import com.lowagie.text.Image
@@ -30,22 +22,20 @@ import org.orbeon.io.IOUtils
 import org.orbeon.oxf.externalcontext.{ExternalContext, URLRewriter}
 import org.orbeon.oxf.http.{Headers, HttpMethod}
 import org.orbeon.oxf.pipeline.api.PipelineContext
+import org.orbeon.oxf.processor.pdf.ImageSupport._
 import org.orbeon.oxf.util.CoreUtils._
 import org.orbeon.oxf.util.TryUtils._
-import org.orbeon.oxf.util.{Connection, ConnectionResult, ImageMetadata, IndentedLogger, NetUtils, URLRewriterUtils}
+import org.orbeon.oxf.util.{Connection, ConnectionResult, IndentedLogger, NetUtils, URLRewriterUtils}
 import org.xhtmlrenderer.layout.SharedContext
 import org.xhtmlrenderer.pdf.{ITextFSImage, ITextOutputDevice, PDFAsImage}
 import org.xhtmlrenderer.resource.ImageResource
 import org.xhtmlrenderer.swing.NaiveUserAgent
-import org.xhtmlrenderer.util.{Configuration, ContentTypeDetectingInputStreamWrapper, ImageUtil, XRLog}
+import org.xhtmlrenderer.util.{ContentTypeDetectingInputStreamWrapper, ImageUtil, XRLog}
 
 import scala.collection.mutable
 import scala.util.Try
 import scala.util.control.NonFatal
 
-object CustomUserAgent {
-  private val IMAGE_CACHE_CAPACITY = 32
-}
 
 class CustomUserAgent(
   pipelineContext : PipelineContext,
@@ -53,16 +43,9 @@ class CustomUserAgent(
   sharedContext   : SharedContext)(implicit
   externalContext : ExternalContext,
   indentedLogger  : IndentedLogger
-) extends NaiveUserAgent(
-    Configuration.valueAsInt(
-      "xr.image.cache-capacity",
-      CustomUserAgent.IMAGE_CACHE_CAPACITY
-    )
-  ) {
+) extends NaiveUserAgent {
 
   import Private._
-
-  private val requestOpt = Option(externalContext) flatMap (ctx => Option(ctx.getRequest))
 
   override def getImageResource(originalUriString: String): ImageResource = {
 
@@ -218,6 +201,8 @@ class CustomUserAgent(
 
   private object Private {
 
+    val requestOpt = Option(externalContext) flatMap (ctx => Option(ctx.getRequest))
+
     // See https://github.com/orbeon/orbeon-forms/issues/1996
     // Use our own local cache (`NaiveUserAgent` has one too) so that we can cache against the absolute URL
     // yet pass a local URL to `super.getImageResource()`. This doesn't live beyond the production of this
@@ -240,96 +225,6 @@ class CustomUserAgent(
       val factor = sharedContext.getDotsPerPixel
       if (factor != 1.0f)
         image.scaleAbsolute(image.getPlainWidth * factor, image.getPlainHeight * factor)
-    }
-
-    val MaxExifSize: Int = 64 * 1024
-
-    def findImageOrientation(is: InputStream): (BufferedInputStream, Option[Int]) = {
-
-      val bis = new BufferedInputStream(is, MaxExifSize)
-      bis.mark(MaxExifSize)
-
-      val orientationOpt =
-        ImageMetadata.findKnownMetadata(bis, ImageMetadata.MetadataType.Orientation) collect {
-          case i: Int => i.intValue
-        }
-
-      bis.reset()
-
-      (bis, orientationOpt)
-    }
-
-    def findTransformation(orientation: Int, width: Int, height: Int): Option[AffineTransform] =
-      orientation match {
-        case 2 =>
-          // Horizontal flip
-          Some(
-            new AffineTransform            |!>
-              (_.scale(-1.0, 1.0))         |!>
-              (_.translate(-width, 0))
-          )
-        case 3 =>
-          // 180 degree rotation
-          Some(
-            new AffineTransform            |!>
-              (_.translate(width, height)) |!>
-              (_.rotate(Math.PI))
-          )
-        case 4 =>
-          // Vertical flip
-          Some(
-            new AffineTransform            |!>
-              (_.scale(1.0, -1.0))         |!>
-              (_.translate(0, -height))
-          )
-        case 5 =>
-          Some(
-            new AffineTransform            |!>
-              (_.rotate(-Math.PI / 2))     |!>
-              (_.scale(-1.0, 1.0))
-          )
-        case 6 =>
-          // 90 degree rotation
-          Some(
-            new AffineTransform            |!>
-              (_.translate(height, 0))     |!>
-              (_.rotate(Math.PI / 2))
-          )
-        case 7 =>
-          Some(
-            new AffineTransform            |!>
-              (_.scale(-1.0, 1.0))         |!>
-              (_.translate(-height, 0))    |!>
-              (_.translate(0, width))      |!>
-              (_.rotate(  3 * Math.PI / 2))
-          )
-        case 8 =>
-          // 270 degree rotation
-          Some(
-            new AffineTransform            |!>
-              (_.translate(0, width))      |!>
-              (_.rotate(  3 * Math.PI / 2))
-          )
-        case _ =>
-          None
-      }
-
-    def transformImage(sourceImage: BufferedImage, transform: AffineTransform): BufferedImage = {
-
-      val op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BICUBIC)
-
-      val destinationImage =
-        op.createCompatibleDestImage(
-          sourceImage,
-          if (sourceImage.getType == BufferedImage.TYPE_BYTE_GRAY) sourceImage.getColorModel else null
-        )
-
-      // Not sure we need to clear
-      val g = destinationImage.createGraphics
-      g.setBackground(Color.WHITE)
-      g.clearRect(0, 0, destinationImage.getWidth, destinationImage.getHeight)
-
-      op.filter(sourceImage, destinationImage)
     }
   }
 }
---
+++ src/main/scala/org/orbeon/oxf/processor/pdf/ImageSupport.scala
@@ -0,0 +1,115 @@
+/**
+ * Copyright (C) 2020 Orbeon, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ *  2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+package org.orbeon.oxf.processor.pdf
+
+import java.awt.Color
+import java.awt.geom.AffineTransform
+import java.awt.image.{AffineTransformOp, BufferedImage}
+import java.io.{BufferedInputStream, InputStream}
+
+import org.orbeon.oxf.util.CoreUtils._
+import org.orbeon.oxf.util.ImageMetadata
+
+object ImageSupport {
+
+  private val MaxExifSize: Int = 64 * 1024
+
+  def findImageOrientation(is: InputStream): (BufferedInputStream, Option[Int]) = {
+
+    val bis = new BufferedInputStream(is, MaxExifSize)
+    bis.mark(MaxExifSize)
+
+    val orientationOpt =
+      ImageMetadata.findKnownMetadata(bis, ImageMetadata.MetadataType.Orientation) collect {
+        case i: Int => i.intValue
+      }
+
+    bis.reset()
+
+    (bis, orientationOpt)
+  }
+
+  def findTransformation(orientation: Int, width: Int, height: Int): Option[AffineTransform] =
+    orientation match {
+      case 2 =>
+        // Horizontal flip
+        Some(
+          new AffineTransform            |!>
+            (_.scale(-1.0, 1.0))         |!>
+            (_.translate(-width, 0))
+        )
+      case 3 =>
+        // 180 degree rotation
+        Some(
+          new AffineTransform            |!>
+            (_.translate(width, height)) |!>
+            (_.rotate(Math.PI))
+        )
+      case 4 =>
+        // Vertical flip
+        Some(
+          new AffineTransform            |!>
+            (_.scale(1.0, -1.0))         |!>
+            (_.translate(0, -height))
+        )
+      case 5 =>
+        Some(
+          new AffineTransform            |!>
+            (_.rotate(-Math.PI / 2))     |!>
+            (_.scale(-1.0, 1.0))
+        )
+      case 6 =>
+        // 90 degree rotation
+        Some(
+          new AffineTransform            |!>
+            (_.translate(height, 0))     |!>
+            (_.rotate(Math.PI / 2))
+        )
+      case 7 =>
+        Some(
+          new AffineTransform            |!>
+            (_.scale(-1.0, 1.0))         |!>
+            (_.translate(-height, 0))    |!>
+            (_.translate(0, width))      |!>
+            (_.rotate(  3 * Math.PI / 2))
+        )
+      case 8 =>
+        // 270 degree rotation
+        Some(
+          new AffineTransform            |!>
+            (_.translate(0, width))      |!>
+            (_.rotate(  3 * Math.PI / 2))
+        )
+      case _ =>
+        None
+    }
+
+  def transformImage(sourceImage: BufferedImage, transform: AffineTransform): BufferedImage = {
+
+    val op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BICUBIC)
+
+    val destinationImage =
+      op.createCompatibleDestImage(
+        sourceImage,
+        if (sourceImage.getType == BufferedImage.TYPE_BYTE_GRAY) sourceImage.getColorModel else null
+      )
+
+    // Not sure we need to clear
+    val g = destinationImage.createGraphics
+    g.setBackground(Color.WHITE)
+    g.clearRect(0, 0, destinationImage.getWidth, destinationImage.getHeight)
+
+    op.filter(sourceImage, destinationImage)
+  }
+}

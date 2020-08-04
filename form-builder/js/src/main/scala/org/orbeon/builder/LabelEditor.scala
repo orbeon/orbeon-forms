aa367da68b76177758e8e8f4157ab42b6aa5ff17
avernet <avernet@orbeon.com>
2020-08-04 15:47:37 -0700

    Fix #4634: enter not to open Form Settings

--- form-builder/js/src/main/scala/org/orbeon/builder/LabelEditor.scala
+++ form-builder/js/src/main/scala/org/orbeon/builder/LabelEditor.scala
@@ -81,7 +81,13 @@ object LabelEditor {
           val labelInput = $("<input class='fb-edit-section-label'/>")
           $(".fb-main").append(labelInput)
           labelInput.on("blur", () => asUnit { if (labelInput.is(":visible")) sendNewLabelValue() })
-          labelInput.on(EventNames.KeyPress, (e: JQueryEventObject) => asUnit { if (e.which == 13) sendNewLabelValue() })
+          labelInput.on(EventNames.KeyPress, (e: JQueryEventObject) => asUnit {
+            if (e.which == 13) {
+              // Avoid "enter" from being dispatched to other control that might get the focus
+              e.preventDefault()
+              sendNewLabelValue()
+            }
+          })
           AjaxClient.ajaxResponseProcessed.add(_ => labelInput.hide())
           labelInputOpt = labelInput
           labelInput

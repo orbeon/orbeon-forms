9b5b63851c17097cc161ff2cfe920d50c013e344
Erik Bruchez <erik@bruchez.org>
2020-08-21 15:42:46 -0700

    Refactor: move to `xformsCommon` project

---
+++ xforms-common/shared/src/main/scala/org/orbeon/xforms/EventNames.scala
@@ -0,0 +1,53 @@
+package org.orbeon.xforms
+
+/**
+ * Copyright (C) 2007 Orbeon, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ */
+object EventNames {
+
+  private val Prefix = "xxforms-upload-"
+
+  val XXFormsUploadStart = Prefix + "start"
+  val XXFormsUploadProgress = Prefix + "progress"
+  val XXFormsUploadCancel = Prefix + "cancel"
+  val XXFormsUploadDone = Prefix + "done"
+  val XXFormsUploadError = Prefix + "error"
+
+  val XXFormsAllEventsRequired = "xxforms-all-events-required"
+  val XXFormsSessionHeartbeat = "xxforms-session-heartbeat"
+  val XXFormsServerEvents = "xxforms-server-events"
+  val XXFormsPoll = "xxforms-poll"
+  val XXFormsValue = "xxforms-value"
+  val XXFormsRpcRequest = "xxforms-rpc-request"
+  val XXFormsDnD = "xxforms-dnd"
+
+  val XFormsFocus = "xforms-focus"
+
+  val Change = "change"
+  val KeyPress = "keypress"
+  val KeyDown = "keydown"
+  val KeyUp = "keyup"
+  val TouchStart = "touchstart"
+  val FocusIn = "focusin"
+  val FocusOut = "focusout"
+  val DOMContentLoaded = "DOMContentLoaded"
+  val DOMActivate = "DOMActivate"
+
+  val KeyTextPropertyName = "text"
+  val KeyModifiersPropertyName = "modifiers"
+
+  val KeyboardEvents: Set[String] = Set(KeyPress, KeyDown, KeyUp)
+
+  val InteractiveReadyState = "interactive"
+  val CompleteReadyState = "complete"
+}

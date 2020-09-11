2b0ad19400556309d8a98de7e86e4d09f749fdf3
Erik Bruchez <erik@bruchez.org>
2020-09-10 23:37:42 -0700

    Minor: imports

--- xforms/jvm/src/main/scala/org/orbeon/oxf/xforms/xbl/XBLTransformer.scala
+++ xforms/jvm/src/main/scala/org/orbeon/oxf/xforms/xbl/XBLTransformer.scala
@@ -15,7 +15,7 @@ package org.orbeon.oxf.xforms.xbl
 
 import java.{util => ju}
 
-import cats.implicits.catsSyntaxOptionId
+import cats.syntax.option._
 import org.orbeon.dom._
 import org.orbeon.dom.saxon.DocumentWrapper
 import org.orbeon.oxf.util.StringUtils._
@@ -25,7 +25,6 @@ import org.orbeon.oxf.xforms.analysis.controls.LHHA
 import org.orbeon.oxf.xforms.event.EventHandlerImpl
 import org.orbeon.oxf.xforms.{PartAnalysis, XFormsUtils}
 import org.orbeon.oxf.xml.dom.Extensions._
-import org.orbeon.oxf.xml.dom.IOSupport
 import org.orbeon.saxon.om.NodeInfo
 import org.orbeon.scaxon.NodeConversions._
 import org.orbeon.xforms.XFormsNames._

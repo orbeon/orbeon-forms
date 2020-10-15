22c85cb3aa5ceac95ef88c724d5f11992688d482
Erik Bruchez <erik@bruchez.org>
2020-10-15 10:23:22 -0700

    Move to Scala

---
+++ src/main/scala/org/orbeon/oxf/xml/XMLUtils.scala
@@ -0,0 +1,36 @@
+package org.orbeon.oxf.xml
+
+
+object XMLUtils {
+
+  def prefixFromQName(qName: String): String = {
+    val colonIndex = qName.indexOf(':')
+    if (colonIndex == -1)
+      ""
+    else
+      qName.substring(0, colonIndex)
+  }
+
+  def localNameFromQName(qName: String): String = {
+    val colonIndex = qName.indexOf(':')
+    if (colonIndex == -(1))
+      qName
+    else
+      qName.substring(colonIndex + 1)
+  }
+
+  def buildQName(prefix: String, localname: String): String =
+    if (prefix == null || prefix == "")
+      localname
+    else
+      prefix + ":" + localname
+
+  /**
+   * Encode a URI and local name to an exploded QName (also known as a "Clark name") String.
+   */
+  def buildExplodedQName(uri: String, localname: String): String =
+    if ("" == uri)
+      localname
+    else
+      "{" + uri + '}' + localname
+}

92b053d1e57e92f093751955a5c2ef9127482154
Erik Bruchez <erik@bruchez.org>
2020-11-16 15:33:59 -0800

    Use `CoreCrossPlatformSupport`

--- xforms-runtime/shared/src/main/scala/org/orbeon/oxf/xforms/action/actions/XXFormsJoinSubmissions.scala
+++ xforms-runtime/shared/src/main/scala/org/orbeon/oxf/xforms/action/actions/XXFormsJoinSubmissions.scala
@@ -2,7 +2,7 @@ package org.orbeon.oxf.xforms.action.actions
 
 import org.orbeon.dom.Element
 import org.orbeon.oxf.common.OXFException
-import org.orbeon.oxf.common.Version
+import org.orbeon.oxf.util.CoreCrossPlatformSupport
 import org.orbeon.oxf.xforms.action.XFormsAction
 import org.orbeon.oxf.xforms.action.XFormsActionInterpreter
 import org.orbeon.xforms.xbl.Scope
@@ -19,7 +19,7 @@ class XXFormsJoinSubmissions extends XFormsAction {
     overriddenContext    : om.Item
   ): Unit = {
     // Process all pending async submissions. The action will block until the method returns.
-    if (Version.isPE) {
+    if (CoreCrossPlatformSupport.isPE) {
       // Only supported in PE version
       val manager = actionInterpreter.containingDocument.getAsynchronousSubmissionManager(false)
       if (manager ne null)

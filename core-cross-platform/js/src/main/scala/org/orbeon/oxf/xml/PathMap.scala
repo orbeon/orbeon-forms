4a07e86c204893035a1fd687c6a7d5562136877c
Erik Bruchez <erik@bruchez.org>
2020-11-16 11:13:21 -0800

    Update `PathMap` stub

--- core-cross-platform/js/src/main/scala/org/orbeon/oxf/xml/PathMap.scala
+++ core-cross-platform/js/src/main/scala/org/orbeon/oxf/xml/PathMap.scala
@@ -2,4 +2,10 @@ package org.orbeon.saxon.expr
 
 
 // On the Scala.js side, this is just a marker trait. There will never be an instance of it.
-trait PathMap
+trait PathMap {
+  def setInvalidated(b: Boolean): Unit = ()
+}
+
+object PathMap {
+  trait PathMapNodeSet
+}
--- xforms-web/src/main/scala/org/orbeon/xforms/analysis/PathMap.scala
+++
@@ -1,5 +0,0 @@
-package org.orbeon.xforms.analysis
-
-
-// On the JS side, just use a trait, which will never have a concrete value in `PathMapXPathAnalysis`.
-trait PathMap

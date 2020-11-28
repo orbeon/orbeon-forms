8af1dd187349109706e32eda6ccf160256fe8d34
Erik Bruchez <erik@bruchez.org>
2020-11-27 21:26:26 -0800

    Placeholders

--- common/js/src/main/scala/org/orbeon/oxf/common/OrbeonLocationException.scala
+++ common/js/src/main/scala/org/orbeon/oxf/common/OrbeonLocationException.scala
@@ -4,8 +4,6 @@ import org.orbeon.datatypes.LocationData
 
 
 object OrbeonLocationException extends OrbeonLocationExceptionTrait {
-
-  def getAllLocationData(throwable: Throwable): List[LocationData] = ???
-
-  def getRootLocationData(throwable: Throwable): Option[LocationData] = ???
+  def getAllLocationData(throwable: Throwable): List[LocationData] = Nil // XXX TODO
+  def getRootLocationData(throwable: Throwable): Option[LocationData] = None // XXX TODO
 }

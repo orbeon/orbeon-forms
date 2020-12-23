e6a481455de4fbc3d098f09c8eddae6fc674e404
Erik Bruchez <erik@bruchez.org>
2020-12-22 23:32:21 -0800

    Fix `xxf:property()` function for `oxf.xforms.` properties

--- xforms-runtime/js/src/main/scala/org/orbeon/oxf/xforms/library/XXFormsFunctionLibrary.scala
+++ xforms-runtime/js/src/main/scala/org/orbeon/oxf/xforms/library/XXFormsFunctionLibrary.scala
@@ -1,14 +1,17 @@
 package org.orbeon.oxf.xforms.library
 
+import cats.syntax.option._
 import org.orbeon.macros.XPathFunction
+import org.orbeon.oxf.util.CoreUtils.BooleanOps
+import org.orbeon.oxf.util.StringUtils._
+import org.orbeon.oxf.xforms.function.XFormsFunction
 import org.orbeon.oxf.xforms.function.XFormsFunction._
-import org.orbeon.saxon.IndependentRequestFunctions
 import org.orbeon.oxf.xml.{OrbeonFunctionLibrary, SaxonUtils}
 import org.orbeon.saxon.expr.XPathContext
-import org.orbeon.xforms.{Namespaces, XFormsNames}
-import org.orbeon.saxon.om
-import org.orbeon.saxon.value.AtomicValue
 import org.orbeon.saxon.function.CoreSupport
+import org.orbeon.saxon.{IndependentRequestFunctions, om}
+import org.orbeon.saxon.value.AtomicValue
+import org.orbeon.xforms.{Namespaces, XFormsNames}
 
 
 object XXFormsFunctionLibrary
@@ -25,7 +28,17 @@ object XXFormsFunctionLibrary
       .headOption.toList.flatMap(_.bindingEvenIfNonRelevant)
 
   @XPathFunction
-  def property(propertyName: String): Option[AtomicValue] =
-    CoreSupport.property(propertyName)
+  def property(propertyName: String)(implicit xfc: XFormsFunction.Context): Option[AtomicValue] = {
 
+    def fromContainingDocumentProperties =
+      propertyName.startsWith(XFormsPropertyPrefix) option {
+        val shortName = propertyName.substringAfter(XFormsPropertyPrefix)
+        SaxonUtils.convertJavaObjectToSaxonObject(xfc.containingDocument.getProperty(shortName)).asInstanceOf[AtomicValue]
+      }
+
+    def fromOtherProperties =
+      CoreSupport.property(propertyName)
+
+    fromContainingDocumentProperties orElse fromOtherProperties
+  }
 }

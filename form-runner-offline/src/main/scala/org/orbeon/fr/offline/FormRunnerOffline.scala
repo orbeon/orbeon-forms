9ad69d66aaa0366061ae4482cb52362908c251dc
Erik Bruchez <erik@bruchez.org>
2020-12-31 14:52:45 -0800

    API to pass compiled Form Runner form

--- form-runner-offline/src/main/scala/org/orbeon/fr/offline/FormRunnerOffline.scala
+++ form-runner-offline/src/main/scala/org/orbeon/fr/offline/FormRunnerOffline.scala
@@ -3,11 +3,13 @@ package org.orbeon.fr.offline
 import org.orbeon.dom.{Document, Element}
 import org.orbeon.oxf.fr.library.{FormRunnerDateSupportFunctionLibrary, FormRunnerFunctionLibrary, FormRunnerInternalFunctionLibrary}
 import org.orbeon.oxf.http.BasicCredentials
+import org.orbeon.oxf.util.CoreCrossPlatformSupport
 import org.orbeon.xforms.App
 import org.orbeon.xforms.offline.demo.OfflineDemo
 import org.orbeon.oxf.xforms.processor.XFormsURIResolver
 import org.orbeon.saxon.om.NodeInfo
 import org.orbeon.saxon.utils.Configuration
+import org.orbeon.xforms.offline.demo.OfflineDemo.CompiledForm
 import org.scalajs.dom.{XMLHttpRequest, html}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -39,44 +41,55 @@ object FormRunnerOffline extends App {
     OfflineDemo.onPageContainsFormsMarkup()
 
   @JSExport
-  def renderDemoForm(
+  def renderForm(
+    container    : html.Element,
+    compiledForm : CompiledForm,
+    appName      : String,
+    formName     : String,
+    mode         : String
+  ): Unit =
+    OfflineDemo.renderCompiledForm(
+      container,
+      compiledForm,
+      List(FormRunnerFunctionLibrary, FormRunnerInternalFunctionLibrary, FormRunnerDateSupportFunctionLibrary),
+      Some(
+        new XFormsURIResolver {
+          def readAsDom4j(urlString: String, credentials: BasicCredentials): Document =
+            urlString match {
+              case "input:instance" =>
+
+                val root = Element("request")
+
+                root.addElement("app").addText(appName)
+                root.addElement("form").addText(formName)
+                root.addElement("form-version").addText("1") // TODO
+//                  root.addElement("document")
+                root.addElement("document").addText(CoreCrossPlatformSupport.randomHexId) // temp as mode is not read correctly!
+                root.addElement("mode").addText(mode)
+
+                Document(root)
+              case _ =>
+                throw new UnsupportedOperationException(s"resolving `$urlString")
+            }
+
+          def readAsTinyTree(configuration: Configuration, urlString: String, credentials: BasicCredentials): NodeInfo = {
+            throw new UnsupportedOperationException(s"resolving readonly `$urlString")
+          }
+        }
+      )
+    )
+
+  @JSExport
+  def testLoadAndRenderForm(
     container : html.Element,
     appName   : String,
     formName  : String,
     mode      : String
-  ): Unit = {
-
-//    fetchCompiledForm(s"http://localhost:9090/orbeon/fr/service/$appName/$formName/compile") foreach { text =>
-    fetchCompiledForm(s"http://localhost:9090/orbeon/xforms-compiler/service/compile/date.xhtml") foreach { text =>
-      OfflineDemo.renderCompiledForm(
-        container,
-        text,
-        List(FormRunnerFunctionLibrary, FormRunnerInternalFunctionLibrary, FormRunnerDateSupportFunctionLibrary),
-        Some(
-          new XFormsURIResolver {
-            def readAsDom4j(urlString: String, credentials: BasicCredentials): Document =
-              urlString match {
-                case "input:instance" =>
-
-                  val root = Element("request")
-
-                  root.addElement("app").addText(appName)
-                  root.addElement("form").addText(formName)
-                  root.addElement("form-version").addText("1") // TODO
-                  root.addElement("document")
-                  root.addElement("mode").addText(mode)
-
-                  Document(root)
-                case _ =>
-                  throw new UnsupportedOperationException(s"resolving `$urlString")
-              }
-
-            def readAsTinyTree(configuration: Configuration, urlString: String, credentials: BasicCredentials): NodeInfo = ???
-          }
-        )
-      )
+  ): Unit =
+//    fetchCompiledForm(s"http://localhost:9090/orbeon/xforms-compiler/service/compile/date.xhtml") foreach { text =>
+    fetchCompiledForm(s"http://localhost:9090/orbeon/fr/service/$appName/$formName/compile") foreach { compiledForm =>
+      renderForm(container, compiledForm, appName, formName, mode)
     }
-  }
 
   private def fetchCompiledForm(url: String): Future[String] = {
     val p = Promise[String]()

e57106387807966367645488735f76de9220ba4e
avernet <avernet@orbeon.com>
2021-01-07 17:22:06 -0800

    Fix #4745: when a String is passed, `File` expects a path, not a URI

--- xforms/jvm/src/main/scala/org/orbeon/oxf/xforms/upload/UploaderServer.scala
+++ xforms/jvm/src/main/scala/org/orbeon/oxf/xforms/upload/UploaderServer.scala
@@ -40,6 +40,7 @@ import org.orbeon.oxf.xforms.upload.api.{FileScan, FileScanProvider, FileScanSta
 import org.orbeon.xforms.Constants
 import org.slf4j.LoggerFactory
 
+import java.net.URI
 import scala.collection.JavaConverters._
 import scala.collection.{mutable => m}
 import scala.util.{Failure, Success, Try}
@@ -357,7 +358,7 @@ object UploaderServer {
     // case is that of a zero-length file, which will probably not be created by `DiskFileItem` as nothing
     // is written.
     def fileFromFileItemCreateIfNeeded(fileItem: DiskFileItem): java.io.File =
-      new java.io.File(RequestGenerator.urlForFileItemCreateIfNeeded(fileItem, NetUtils.REQUEST_SCOPE))
+      new java.io.File(new URI(RequestGenerator.urlForFileItemCreateIfNeeded(fileItem, NetUtils.REQUEST_SCOPE)))
 
     def withFileScanCall(block: => FileScanStatus): FileScanStatus =
       Try(block) match {

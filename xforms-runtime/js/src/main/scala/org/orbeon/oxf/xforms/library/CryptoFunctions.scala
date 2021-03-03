f26ef8778886f0da7d3fc961eb2eea50d0d36607
Erik Bruchez <erik@bruchez.org>
2021-03-03 10:43:03 -0800

    Use JavaScript `Crypto` API to produce `random()`

--- xforms-runtime/js/src/main/scala/org/orbeon/oxf/xforms/library/CryptoFunctions.scala
+++ xforms-runtime/js/src/main/scala/org/orbeon/oxf/xforms/library/CryptoFunctions.scala
@@ -2,12 +2,28 @@ package org.orbeon.oxf.xforms.library
 
 import org.orbeon.macros.XPathFunction
 import org.orbeon.oxf.xml.OrbeonFunctionLibrary
-import org.orbeon.saxon.function.RandomSupport
+import org.scalajs.dom.crypto.GlobalCrypto
+
+import scala.scalajs.js.typedarray.{DataView, Int8Array}
 
 
 trait CryptoFunctions extends OrbeonFunctionLibrary {
-
   @XPathFunction
   def random(isSeed: Boolean = true): Double =
-    RandomSupport.evaluate(isSeed = true)
+    CryptoFunctions.random()
+}
+
+
+object CryptoFunctions {
+  def random(): Double = {
+
+    val ints = new Int8Array(8)
+    GlobalCrypto.crypto.getRandomValues(ints)
+
+    // https://stackoverflow.com/questions/34575635/cryptographically-secure-float
+    ints(7) = 63
+    ints(6) = (ints(6) | 0xf0).toByte
+
+    new DataView(ints.buffer).getFloat64(0, littleEndian = true) - 1
+  }
 }

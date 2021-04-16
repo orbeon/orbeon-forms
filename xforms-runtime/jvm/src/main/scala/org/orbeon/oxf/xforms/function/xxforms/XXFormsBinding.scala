9f923c7313839e142040d1b6635dde7734a4558b
Erik Bruchez <erik@bruchez.org>
2021-04-15 18:30:48 -0700

    Implement #4856: `xxf:binding()` must support XPath dependencies

--- xforms-runtime/jvm/src/main/scala/org/orbeon/oxf/xforms/function/xxforms/XXFormsBinding.scala
+++ xforms-runtime/jvm/src/main/scala/org/orbeon/oxf/xforms/function/xxforms/XXFormsBinding.scala
@@ -13,15 +13,56 @@
  */
 package org.orbeon.oxf.xforms.function.xxforms
 
+import org.orbeon.oxf.xforms.analysis.ElementAnalysis.ancestorsIterator
+import org.orbeon.oxf.xforms.analysis.controls.ComponentControl
+import org.orbeon.oxf.xforms.analysis.{ElementAnalysisTreeXPathAnalyzer, PathMapXPathAnalysis}
 import org.orbeon.oxf.xforms.function.XFormsFunction
-import org.orbeon.saxon.expr.XPathContext
+import org.orbeon.saxon.expr.{PathMap, StringLiteral, XPathContext}
 import org.orbeon.saxon.om.SequenceIterator
 import org.orbeon.scaxon.Implicits._
+
 
 class XXFormsBinding extends XFormsFunction {
 
   override def iterate(xpathContext: XPathContext): SequenceIterator =
     findControls(0, followIndexes = true)(xpathContext).headOption map (_.bindingEvenIfNonRelevant)
 
-  // TODO: PathMap
+  override def addToPathMap(pathMap: PathMap, pathMapNodeSet: PathMap.PathMapNodeSet): PathMap.PathMapNodeSet = {
+
+    arguments.head match {
+      case s: StringLiteral =>
+
+        val staticId = s.getStringValue
+
+        pathMap.getPathMapContext match {
+          case context: ElementAnalysisTreeXPathAnalyzer.SimplePathMapContext =>
+
+            val boundElemOpt =
+              ancestorsIterator(context.element, includeSelf = false) collectFirst {
+                case c: ComponentControl => c
+              } filter { c =>
+                c.commonBinding.modeBinding && c.commonBinding.bindingElemId.contains(staticId)
+              }
+
+            boundElemOpt.flatMap(_.bindingAnalysis) match {
+              case Some(analysis: PathMapXPathAnalysis) =>
+                // TODO: review this!
+                val clonedVariablePathMap = analysis.pathmap.get.clone
+                pathMap.addRoots(clonedVariablePathMap.getPathMapRoots)
+                clonedVariablePathMap.findFinalNodes
+              case _ =>
+                // Invalidate
+                pathMap.setInvalidated(true)
+                null
+            }
+
+          case _ =>
+            throw new IllegalStateException("Can't process PathMap because context is not of expected type.")
+        }
+
+      case _ =>
+        pathMap.setInvalidated(true)
+        null
+    }
+  }
 }

72a1c7c7884ef1eb3bb55e4304d402a0f8a4edae
Erik Bruchez <erik@bruchez.org>
2021-10-13 15:33:15 -0700

    Refactor: use existing functions

--- src/main/scala/org/orbeon/oxf/xml/EncodeDecode.scala
+++ src/main/scala/org/orbeon/oxf/xml/EncodeDecode.scala
@@ -14,11 +14,9 @@
 package org.orbeon.oxf.xml
 
 import org.orbeon.dom.Document
-import org.orbeon.dom.io.DocumentSource
 import org.orbeon.oxf.common.OXFException
 import org.orbeon.oxf.util.CoreUtils.BooleanOps
 import org.orbeon.oxf.util.{Compressor, SecureUtils}
-import org.orbeon.oxf.xml.dom4j.{LocationDocumentResult, LocationDocumentSource}
 
 
 object EncodeDecode {
@@ -28,15 +26,8 @@ object EncodeDecode {
 
     // Get SAXStore
     // TODO: This is not optimal since we create a second in-memory representation. Should stream instead.
-    val saxStore = new SAXStore
-    // NOTE: We don't encode XML comments and use only the ContentHandler interface
-    val source =
-      if (location)
-        new LocationDocumentSource(document)
-      else
-        new DocumentSource(document)
-
-    TransformerUtils.sourceToSAX(source, saxStore)
+    // NOTE: We don't encode XML comments and use only the `ContentHandler` interface
+    val saxStore = TransformerUtils.dom4jToSAXStore(document, location)
 
     // Serialize SAXStore to bytes
     // TODO: This is not optimal since we create a third in-memory representation. Should stream instead.
@@ -44,6 +35,18 @@ object EncodeDecode {
 
     // Encode bytes
     encodeBytes(bytes, compress, encrypt)
+  }
+
+  def decodeXML(encodedXML: String, forceEncryption: Boolean): Document = {
+
+    val bytes = decodeBytes(encodedXML, forceEncryption)
+
+    // Deserialize bytes to SAXStore
+    // TODO: This is not optimal
+    val saxStore = SAXStoreBinaryFormat.deserialize(bytes)
+
+    // Deserialize `SAXStore` to `Document`
+    TransformerUtils.saxStoreToDom4jDocument(saxStore)
   }
 
   // 2016-09-14: `encrypt = false` only when encoding static state when using server-side state handling, and
@@ -62,22 +65,6 @@ object EncodeDecode {
       case (false, None)                => "X3" + org.orbeon.oxf.util.Base64.encode(bytesToEncode, useLineBreaks = false)
       case (false, Some(gzipByteArray)) => "X4" + org.orbeon.oxf.util.Base64.encode(gzipByteArray, useLineBreaks = false)
     }
-  }
-
-  def decodeXML(encodedXML: String, forceEncryption: Boolean): Document = {
-
-    val bytes = decodeBytes(encodedXML, forceEncryption)
-
-    // Deserialize bytes to SAXStore
-    // TODO: This is not optimal
-    val saxStore = SAXStoreBinaryFormat.deserialize(bytes)
-
-    // Deserialize SAXStore to dom4j document
-    val identity = TransformerUtils.getIdentityTransformerHandler
-    val result = new LocationDocumentResult
-    identity.setResult(result)
-    saxStore.replay(identity)
-    result.getDocument
   }
 
   def decodeBytes(encoded: String, forceEncryption: Boolean): Array[Byte] = {

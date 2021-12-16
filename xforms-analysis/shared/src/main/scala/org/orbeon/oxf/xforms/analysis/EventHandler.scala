2a184ce1c7064f3e38039ad93e78e6f1850b8e04
Erik Bruchez <erik@bruchez.org>
2021-12-15 16:15:30 -0800

    Minor: import

--- xforms-analysis/shared/src/main/scala/org/orbeon/oxf/xforms/analysis/EventHandler.scala
+++ xforms-analysis/shared/src/main/scala/org/orbeon/oxf/xforms/analysis/EventHandler.scala
@@ -19,7 +19,6 @@ import org.orbeon.oxf.util.{IndentedLogger, Modifier}
 import org.orbeon.oxf.xforms.analysis.ElementAnalysis.attSet
 import org.orbeon.oxf.xforms.analysis.EventHandler.{ObserverIsPrecedingSibling, TargetIsObserver, isDispatchAction}
 import org.orbeon.oxf.xforms.analysis.controls.{ActionTrait, RepeatIterationControl}
-import org.orbeon.xforms.XFormsId
 import org.orbeon.xforms.XFormsNames._
 import org.orbeon.xforms.analysis.{Perform, Propagate}
 import org.orbeon.xforms.xbl.Scope

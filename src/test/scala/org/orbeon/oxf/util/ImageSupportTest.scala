bcc127281e3afda72e673946022772720f8676d0
Erik Bruchez <erik@bruchez.org>
2022-01-26 09:39:40 -0800

    For #3061: indent

--- src/test/scala/org/orbeon/oxf/util/ImageSupportTest.scala
+++ src/test/scala/org/orbeon/oxf/util/ImageSupportTest.scala
@@ -26,21 +26,21 @@ class ImageSupportTest
 
     val Expected = List(
       (jpgImg, None,     None,     jpegMediatype.some, None),
-      (jpgImg, None,     None,     pngMediatype.some,  Some((pngMediatype,  400,368))),
+      (jpgImg, None,     None,     pngMediatype.some,  Some((pngMediatype,  400, 368))),
       (jpgImg, 200.some, None,     None,               Some((jpegMediatype, 200, 184))),
       (jpgImg, None,     200.some, None,               Some((jpegMediatype, 217, 200))),
       (jpgImg, 200.some, None,     pngMediatype.some,  Some((pngMediatype,  200, 184))),
       (jpgImg, None,     200.some, pngMediatype.some,  Some((pngMediatype,  217, 200))),
 
-      (pngImg, None,     None,     jpegMediatype.some, Some((jpegMediatype, 400,368))),
+      (pngImg, None,     None,     jpegMediatype.some, Some((jpegMediatype, 400, 368))),
       (pngImg, None,     None,     pngMediatype.some,  None),
       (pngImg, 200.some, None,     None,               Some((pngMediatype,  200, 184))),
       (pngImg, None,     200.some, None,               Some((pngMediatype,  217, 200))),
-      (pngImg, 200.some, None,     pngMediatype.some,  Some((pngMediatype, 200, 184))),
-      (pngImg, None,     200.some, pngMediatype.some,  Some((pngMediatype, 217, 200))),
+      (pngImg, 200.some, None,     pngMediatype.some,  Some((pngMediatype,  200, 184))),
+      (pngImg, None,     200.some, pngMediatype.some,  Some((pngMediatype,  217, 200))),
     )
 
-    for ((imgUri, maxWidthOpt, maxHeightOpt, mediatypeOpt, expectedOpt) <- Expected) {
+    for ((imgUri, maxWidthOpt, maxHeightOpt, mediatypeOpt, expectedOpt) <- Expected)
       it(s"must pass for $imgUri/$maxWidthOpt/$maxHeightOpt/$mediatypeOpt") {
         PipelineSupport.withTestExternalContext() { implicit ec =>
 
@@ -63,6 +63,5 @@ class ImageSupportTest
           assert(expectedOpt == actualOpt)
         }
       }
-    }
   }
 }

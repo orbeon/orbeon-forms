130096e8f13564fd8af234b4f7d223cf4a099fc4
tiz170 <jiaojiao1210@gmail.com>
2022-05-31 18:46:07 -0700

    For #1498/#5342: fix style issues

--- src/main/scala/org/orbeon/oxf/processor/pdf/XHTMLToPDFProcessor.scala
+++ src/main/scala/org/orbeon/oxf/processor/pdf/XHTMLToPDFProcessor.scala
@@ -13,12 +13,8 @@
  */
 package org.orbeon.oxf.processor.pdf
 
-import com.lowagie.text.pdf.BaseFont
-import com.openhtmltopdf.pdfboxout.PdfRendererBuilder.PdfAConformance
-
 import java.io.{File, OutputStream}
-import com.openhtmltopdf.pdfboxout.{PdfBoxFastOutputDevice, PdfBoxRenderer, PdfBoxSlowOutputDevice, PdfBoxUserAgent, PdfRendererBuilder}
-import com.openhtmltopdf.swing.NaiveUserAgent
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
 import org.orbeon.io.IOUtils
 import org.orbeon.oxf.externalcontext.ExternalContext
 import org.orbeon.oxf.pipeline.api.PipelineContext
@@ -29,12 +25,10 @@ import org.orbeon.oxf.properties.Properties
 import org.orbeon.oxf.resources.ResourceManagerWrapper
 import org.orbeon.oxf.util.StringUtils._
 import org.orbeon.oxf.util._
-import org.xhtmlrenderer.pdf.ITextRenderer
 
-import scala.util.Try
 import scala.util.control.NonFatal
 
-// XHTML to PDF converter using the Flying Saucer library.
+// XHTML to PDF converter using the Open HTML To PDF library.
 private object XHTMLToPDFProcessor {
 
   val logger = LoggerFactory.createLogger(classOf[XHTMLToPDFProcessor])
@@ -43,9 +37,7 @@ private object XHTMLToPDFProcessor {
   val DefaultDotsPerPoint = 20f * 4f / 3f
   val DefaultDotsPerPixel = 14
 
-  // tiz170 TODO: update the method to set properties for OHTP
-  def embedFontsConfiguredInProperties(renderer: ITextRenderer, pdfRendererBuilder: PdfRendererBuilder): Unit = {
-
+  def embedFontsConfiguredInProperties(pdfRendererBuilder: PdfRendererBuilder): Unit = {
     // tiz170: get global properties
     val props = Properties.instance.getPropertySet
 
@@ -67,10 +59,6 @@ private object XHTMLToPDFProcessor {
             case _          => throw new IllegalStateException
           }
 
-        // tiz170: add font
-        //        renderer.getFontResolver.addFont(absolutePath, familyOpt.orNull, BaseFont.IDENTITY_H, BaseFont.EMBEDDED, null)
-        // tiz170 TODO: add OHTP version of add font
-        // the corresponding function is useFont
         pdfRendererBuilder.useFont(new File(absolutePath), familyOpt.orNull)
       } catch {
         case NonFatal(_) =>
@@ -88,22 +76,15 @@ class XHTMLToPDFProcessor() extends HttpBinarySerializer {
 
   protected def getDefaultContentType: String = XHTMLToPDFProcessor.DefaultContentType
 
-  protected def readInput(
-                           pipelineContext : PipelineContext,
-                           input           : ProcessorInput,
-                           config          : HttpSerializerBase.Config,
-                           outputStream    : OutputStream
-                         ): Unit = {
-
+  protected def readInput(pipelineContext : PipelineContext,
+                          input           : ProcessorInput,
+                          config          : HttpSerializerBase.Config,
+                          outputStream    : OutputStream): Unit = {
     implicit val externalContext         : ExternalContext               = NetUtils.getExternalContext
     implicit val indentedLogger          : IndentedLogger                = new IndentedLogger(XHTMLToPDFProcessor.logger)
     implicit val coreCrossPlatformSupport: CoreCrossPlatformSupportTrait = CoreCrossPlatformSupport
 
     val requestOpt  = Option(externalContext) flatMap (ctx => Option(ctx.getRequest))
-    // tiz170: this is where it initiates fly saucer ITextRenderer
-    val renderer    = new ITextRenderer(DefaultDotsPerPoint, DefaultDotsPerPixel)
-    // tiz170 TODO: initialize OHTP correspondence
-    // the correspondence is PdfRendererBuilder
     val pdfRendererBuilder = new PdfRendererBuilder()
     pdfRendererBuilder.useFastMode();
     //    pdfRendererBuilder.useSlowMode();
@@ -111,43 +92,17 @@ class XHTMLToPDFProcessor() extends HttpBinarySerializer {
     //    pdfRendererBuilder.usePdfAConformance(PdfRendererBuilder.PdfAConformance.PDFA_3_A)
 
     try {
-      embedFontsConfiguredInProperties(renderer, pdfRendererBuilder)
+      embedFontsConfiguredInProperties(pdfRendererBuilder)
 
-      //      // NOTE: Default compression level is 0.75:
-      //      // https://docs.oracle.com/javase/8/docs/api/javax/imageio/plugins/jpeg/JPEGImageWriteParam.html#JPEGImageWriteParam-java.util.Locale-
-      //      val jpegCompressionLevel =
-      //        Properties.instance.getPropertySet.getNonBlankString("oxf.fr.pdf.jpeg.compression") flatMap
-      //          (s => Try(java.lang.Float.parseFloat(s)).toOption) filter
-      //          (f => f >= 0f && f <= 1.0f) getOrElse
-      //          0.9f
-
-      //       tiz170: set callback mechanism for Flying Saucer to interact with a user agent
-      //      renderer.getSharedContext.setUserAgentCallback(
-      //        new CustomUserAgent(
-      //          jpegCompressionLevel,
-      //          pipelineContext,
-      //          renderer.getSharedContext
-      //        )
-      //      )
-
-      ////       tiz170: this is where data is passed into flying-saucer
-      //      renderer.setDocument(
-      //        readInputAsDOM(pipelineContext, input),
-      //        requestOpt map (_.getRequestURL) orNull // no base URL if can't get request URL from context
-      //      )
-      //      renderer.layout()
-
-      // tiz170: this is custom IO util
       IOUtils.useAndClose(outputStream) { os =>
-        // tiz170 TODO: OHTP version of setting document
         pdfRendererBuilder.toStream(os)
         pdfRendererBuilder.withW3cDocument(
           readInputAsDOM(pipelineContext, input),
           requestOpt map (_.getRequestURL) orNull // no base URL if can't get request URL from context
         )
-        //        pdfRendererBuilder.useUriResolver(new CustomFSUriResolver())
         val pdfBoxRenderer = pdfRendererBuilder.buildPdfRenderer()
-        //         tiz170 TODO: set callback mechanism for OHTP to interact with a user agent
+
+        // set user agent callback
         val userAgent = new CustomUserAgentOHTP(pdfBoxRenderer.getOutputDevice(), pipelineContext)
         userAgent.setSharedContext(pdfBoxRenderer.getSharedContext)
         pdfBoxRenderer.getSharedContext.setUserAgentCallback(userAgent)
@@ -155,21 +110,11 @@ class XHTMLToPDFProcessor() extends HttpBinarySerializer {
 
         // Page count might be zero!
         // Q: Log if no pages?
-        // tiz170 TODO: PdfRendererBuilder has a buildPdfRenderer method which returns a PdfBoxRenderer.
-        // PdfBoxRenderer is OHTP version of ITextRenderer in flying saucer
-        //        val hasPages = Option(renderer.getRootBox.getLayer.getPages) exists (_.size > 0)
         val hasPages = Option(pdfBoxRenderer.getRootBox.getLayer.getPages) exists (_.size > 0)
         if (hasPages) {
-          // tiz170: this is where pdf is created using flyer saucer
-          //          renderer.createPDF(os)
-          // tiz170 TODO: add OHTP correspondence of creating PDF
           pdfBoxRenderer.createPDF()
         }
       }
-
-    } finally {
-      // Free resources associated with the rendering context
-      //      renderer.getSharedContext.reset()
     }
   }
 }

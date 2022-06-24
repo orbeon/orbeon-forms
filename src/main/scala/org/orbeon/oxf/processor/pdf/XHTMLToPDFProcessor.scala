5a2367b1d0fba3bb5f3123cfe8f92600dadfec99
tiz170 <jiaojiao1210@gmail.com>
2022-06-24 15:13:47 -0700

    For #1498/#5342: add debugging code

--- src/main/scala/org/orbeon/oxf/processor/pdf/XHTMLToPDFProcessor.scala
+++ src/main/scala/org/orbeon/oxf/processor/pdf/XHTMLToPDFProcessor.scala
@@ -25,7 +25,11 @@ import org.orbeon.oxf.properties.Properties
 import org.orbeon.oxf.resources.ResourceManagerWrapper
 import org.orbeon.oxf.util.StringUtils._
 import org.orbeon.oxf.util._
+import org.w3c.dom.Document
 
+import javax.xml.transform.dom.DOMSource
+import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.{OutputKeys, TransformerException, TransformerFactory}
 import scala.util.Try
 import scala.util.control.NonFatal
 
@@ -89,9 +93,9 @@ class XHTMLToPDFProcessor() extends HttpBinarySerializer {
     val pdfRendererBuilder = new PdfRendererBuilder()
     pdfRendererBuilder.useFastMode();
 
-    //    pdfRendererBuilder.useSlowMode();
-    pdfRendererBuilder.usePdfUaAccessbility(true) // java.lang.IndexOutOfBoundsException if uncomment
-    pdfRendererBuilder.usePdfAConformance(PdfRendererBuilder.PdfAConformance.PDFA_3_U)
+//    pdfRendererBuilder.useSlowMode();
+//    pdfRendererBuilder.usePdfUaAccessbility(true) // java.lang.IndexOutOfBoundsException if uncomment
+//    pdfRendererBuilder.usePdfAConformance(PdfRendererBuilder.PdfAConformance.PDFA_3_U)
 
     embedFontsConfiguredInProperties(pdfRendererBuilder)
 
@@ -105,15 +109,20 @@ class XHTMLToPDFProcessor() extends HttpBinarySerializer {
 
     IOUtils.useAndClose(outputStream) { os =>
       pdfRendererBuilder.toStream(os)
+      val document = readInputAsDOM(pipelineContext, input)
+
+      // used for debbugging. will remove later
+      val documentString = getStringFromDocument(document)
+
       pdfRendererBuilder.withW3cDocument(
-        readInputAsDOM(pipelineContext, input),
+        document,
         requestOpt map (_.getRequestURL) orNull // no base URL if can't get request URL from context
       )
       val pdfBoxRenderer = pdfRendererBuilder.buildPdfRenderer()
 
       try {
         // set user agent callback
-        val userAgent = new CustomUserAgentOHTP(jpegCompressionLevel, pdfBoxRenderer.getOutputDevice(), pipelineContext,  pdfBoxRenderer.getSharedContext)
+        val userAgent = new CustomUserAgentOHTP(jpegCompressionLevel, pdfBoxRenderer.getOutputDevice(), pipelineContext, pdfBoxRenderer.getSharedContext)
         userAgent.setSharedContext(pdfBoxRenderer.getSharedContext)
         pdfBoxRenderer.getSharedContext.setUserAgentCallback(userAgent)
         pdfBoxRenderer.layout()
@@ -129,5 +138,17 @@ class XHTMLToPDFProcessor() extends HttpBinarySerializer {
         pdfBoxRenderer.close()
       }
     }
+  }
+
+  // used for debbugging. will remove later
+  import java.io.StringWriter
+  def getStringFromDocument(doc: Document): String = {
+    val domSource = new DOMSource(doc)
+    val writer = new StringWriter
+    val result = new StreamResult(writer)
+    val tf = TransformerFactory.newInstance
+    val transformer = tf.newTransformer
+    transformer.transform(domSource, result)
+    writer.toString
   }
 }

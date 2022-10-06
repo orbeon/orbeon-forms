50b28602daac6e00354aaea5edb09143c66dd81c
Erik Bruchez <ebruchez@orbeon.com>
2022-10-06 15:05:21 -0700

    For #2331: new and updated tests

--- form-runner/jvm/src/test/scala/org/orbeon/oxf/servlet/FormRunnerRequestFilterTest.scala
+++ form-runner/jvm/src/test/scala/org/orbeon/oxf/servlet/FormRunnerRequestFilterTest.scala
@@ -15,13 +15,13 @@ package org.orbeon.oxf.servlet
 
 import org.mockito.invocation.InvocationOnMock
 import org.mockito.{ArgumentMatchers, Mockito}
+import org.orbeon.oxf.externalcontext.{Credentials, ServletPortletRequest, SimpleRole, UserAndGroup}
 import org.orbeon.oxf.http.Headers
 import org.orbeon.oxf.test.ResourceManagerSupport
 import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatestplus.mockito.MockitoSugar
 
 import javax.servlet.http.{HttpServletRequest, HttpServletRequestWrapper, HttpSession}
-import scala.collection.immutable.TreeMap
 import scala.collection.mutable
 import scala.jdk.CollectionConverters._
 
@@ -30,49 +30,71 @@ class FormRunnerRequestFilterTest extends ResourceManagerSupport with AnyFunSpec
 
   describe("The servlet filter's `amendRequest()` function") {
 
-    // Initial headers
-    val initialHeaders = Map(
-      "p1" -> List("v1a", "v1b"),
-      // NOTE: Just use these header names because that's what's configured in the properties for the Liferay test
-      "Orbeon-Liferay-User-Email".toLowerCase -> List("test@orbeon.com"),
-      "Orbeon-Liferay-User-Roles".toLowerCase -> List("manager,employee")
-    )
-
-    val sessionAttributes = mutable.Map[String, AnyRef]()
-    val mockSession = mock[HttpSession]
-    Mockito when mockSession.getAttribute(ArgumentMatchers.anyString) thenAnswer
-      ((invocation: InvocationOnMock) => sessionAttributes.get(invocation.getArguments()(0).asInstanceOf[String]).orNull)
-    Mockito when mockSession.setAttribute(ArgumentMatchers.anyString, ArgumentMatchers.any) thenAnswer
-      ((invocation: InvocationOnMock) => sessionAttributes += invocation.getArguments()(0).asInstanceOf[String] -> invocation.getArguments()(1))
-
-    // Request with initial headers
-    val mockRequest = new HttpServletRequestWrapper(mock[HttpServletRequest]) {
-      override def getHeader(name: String) = initialHeaders.get(name) map (_.head) orNull
-      override def getHeaders(name: String) =
-        (initialHeaders.get(name) map (_.iterator) getOrElse Iterator.empty).asJavaEnumeration
-      override def getHeaderNames = initialHeaders.keysIterator.asJavaEnumeration
-      override def getSession(create: Boolean) = mockSession
-      override def getPathInfo: String = "/fr/orbeon/controls/new"
+    val mockSession = {
+      val sessionAttributes = mutable.Map[String, AnyRef]()
+      val mockSession = mock[HttpSession]
+      Mockito when mockSession.getAttribute(ArgumentMatchers.anyString) thenAnswer
+        ((invocation: InvocationOnMock) => sessionAttributes.get(invocation.getArguments()(0).asInstanceOf[String]).orNull)
+      Mockito when mockSession.setAttribute(ArgumentMatchers.anyString, ArgumentMatchers.any) thenAnswer
+        ((invocation: InvocationOnMock) => sessionAttributes += invocation.getArguments()(0).asInstanceOf[String] -> invocation.getArguments()(1))
+      mockSession
     }
 
-    val amendedRequest = FormRunnerAuthFilter.amendRequest(mockRequest)
+    def headersFromRequest(req: HttpServletRequest): Map[String, List[String]] =
+      req.getHeaderNames.asScala map
+        (n => n -> req.getHeaders(n).asScala.toList) toMap
 
-    // NOTE: Use Seq or List but not Array for comparison, because Array's == doesn't work as expected in Scala
-    val expectedHeaders = initialHeaders ++ List(
+    // Request with initial headers
+    def newAmendedRequest(path: String, headers: Map[String, List[String]]) = {
+
+      val mockRequest =
+        new HttpServletRequestWrapper(mock[HttpServletRequest]) {
+          override def getHeader(name: String) = headers.get(name) map (_.head) orNull
+          override def getHeaders(name: String) =
+            (headers.get(name) map (_.iterator) getOrElse Iterator.empty).asJavaEnumeration
+          override def getHeaderNames = headers.keysIterator.asJavaEnumeration
+          override def getSession(create: Boolean) = mockSession
+          override def getPathInfo: String = path
+        }
+
+      FormRunnerAuthFilter.amendRequest(mockRequest)
+    }
+
+    val testCredentials =
+      Credentials(
+        UserAndGroup("test@orbeon.com", None),
+        List(SimpleRole("manager"), SimpleRole("employee")),
+        Nil
+      )
+
+    val testOrbeonHeaders = Map(
       Headers.OrbeonCredentialsLower -> List("""{"username":"test%40orbeon.com","groups":[],"roles":[{"name":"manager"},{"name":"employee"}],"organizations":[]}"""),
       Headers.OrbeonUsernameLower    -> List("test@orbeon.com"),
       Headers.OrbeonRolesLower       -> List("manager", "employee")
     )
 
-    // NOTE: Use asInstanceOf because servlet API doesn't have generics
-    val actualHeaders = amendedRequest.getHeaderNames.asScala map
-      (n => n -> amendedRequest.getHeaders(n).asScala.toList) toMap
+    it("must set authentication headers based on incoming headers") {
 
-    // Compare using TreeMap to get a reliable order
-    def toTreeMap[K, V](map: Map[K, V])(implicit ord: Ordering[K]) = TreeMap[K, V]() ++ map
+      // Initial headers
+      val initialHeaders = Map(
+        "p1" -> List("v1a", "v1b"),
+        // NOTE: Just use these header names because that's what's configured in the properties for the Liferay test
+        "Orbeon-Liferay-User-Email".toLowerCase -> List("test@orbeon.com"),
+        "Orbeon-Liferay-User-Roles".toLowerCase -> List("manager,employee")
+      )
 
-    it ("must set authentication headers based on incoming headers") {
-      assert(toTreeMap(expectedHeaders) === toTreeMap(actualHeaders))
+      val amendedRequest = newAmendedRequest("/fr/orbeon/controls/new", initialHeaders)
+
+      assert((initialHeaders ++ testOrbeonHeaders) == headersFromRequest(amendedRequest))
+      assert(ServletPortletRequest.findCredentialsInSession(new ServletSessionImpl(mockSession)).contains(testCredentials))
+    }
+
+    it("must keep authentication headers for services and store credentials") {
+
+      val amendedRequest = newAmendedRequest("/fr/service/acme", testOrbeonHeaders)
+
+      assert(testOrbeonHeaders == headersFromRequest(amendedRequest))
+      assert(ServletPortletRequest.findCredentialsInSession(new ServletSessionImpl(mockSession)).contains(testCredentials))
     }
   }
 }

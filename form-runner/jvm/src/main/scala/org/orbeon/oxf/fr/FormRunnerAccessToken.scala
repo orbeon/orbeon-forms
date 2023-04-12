d626ebf13ff997be77977faf9b223bc120ff322a
Erik Bruchez <ebruchez@orbeon.com>
2023-04-11 21:28:04 -0700

    For #5437: no CE implementation

--- form-runner/jvm/src/main/scala/org/orbeon/oxf/fr/FormRunnerAccessToken.scala
+++ form-runner/jvm/src/main/scala/org/orbeon/oxf/fr/FormRunnerAccessToken.scala
@@ -1,6 +1,7 @@
 package org.orbeon.oxf.fr
 
 import org.orbeon.io.CharsetNames
+import org.orbeon.oxf.common.Version
 import org.orbeon.oxf.fr.permission.Operations
 import org.orbeon.oxf.util.SecureUtils
 import org.orbeon.oxf.util.StringUtils._
@@ -11,8 +12,15 @@ import scala.util.Try
 // CE doesn't support encryption/decryption
 object FormRunnerAccessToken extends FormRunnerAccessTokenTrait {
 
-  def encryptToken(tokenHmac: TokenHmac, tokenPayload: TokenPayload): Option[String] = None
-  def decryptToken(tokenHmac: TokenHmac, token: String): Try[TokenPayload] = ???
+  def encryptToken(tokenHmac: TokenHmac, tokenPayload: TokenPayload): Option[String] = {
+    Version.instance.requirePEFeature("Token-based authentication")
+    None
+  }
+
+  def decryptToken(tokenHmac: TokenHmac, token: String): Try[TokenPayload] = {
+    Version.instance.requirePEFeature("Token-based authentication")
+    throw new UnsupportedOperationException
+  }
 
   def encryptOperations(operationsTokens: Set[String]): String =
     SecureUtils.encrypt(operationsTokens.mkString(" ").getBytes(CharsetNames.Utf8))

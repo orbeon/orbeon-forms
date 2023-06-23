b9e3ba7becd1e1ca16f6589f3f0e4b0b9e9cc1cd
avernet <avernet@orbeon.com>
2023-06-23 16:10:37 -0700

    Fix test: `dummyimage.com` producing webp, not png
    - `dummyimage.com` is producing a webp, even when asked to produce a png
    - `placeholder.com` is working again, and produces the correct
      image type

--- form-runner/jvm/src/test/scala/org/orbeon/oxf/fr/ActionsFormat20182Test.scala
+++ form-runner/jvm/src/test/scala/org/orbeon/oxf/fr/ActionsFormat20182Test.scala
@@ -40,7 +40,7 @@ class ActionsFormat20182Test
         withTestExternalContext { _ =>
           withFormRunnerDocument(processorService, doc) {
             for {
-              (expectedSizeOpt, i) <- List(Some(392), None, Some(408), None).zipWithIndex
+              (expectedSizeOpt, i) <- List(Some(1426), None, Some(1162), None).zipWithIndex
               control              = resolveObject[XFormsValueControl]("my-attachment-control", indexes = List(i + 1)).get
               value                = control.getValue
             } locally {

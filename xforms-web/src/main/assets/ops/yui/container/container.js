e0c977e26740a86a31965976ffbccf10963c1a04
avernet <avernet@orbeon.com>
2023-08-25 13:17:18 -0700

    For #779: don't steal focus from Select2

--- xforms-web/src/main/assets/ops/yui/container/container.js
+++ xforms-web/src/main/assets/ops/yui/container/container.js
@@ -5866,11 +5866,11 @@ version: 2.8.1
                     doc = document.documentElement,
                     insideDoc = (target !== doc && target !== window);
 
-                var isMceToolbar = target.closest(".tox-tinymce") !== null;
+                var isMceToolbarOrSelect2 = target.closest(".tox-tinymce, .select2-container") !== null;
 
                 // mask and documentElement checks added for IE, which focuses on the mask when it's clicked on, and focuses on
                 // the documentElement, when the document scrollbars are clicked on
-                if (insideDoc && target !== this.element && target !== this.mask && !Dom.isAncestor(this.element, target) && !isMceToolbar) {
+                if (insideDoc && target !== this.element && target !== this.mask && !Dom.isAncestor(this.element, target) && ! isMceToolbarOrSelect2) {
                     try {
                         if (this.firstElement) {
                             this.firstElement.focus();

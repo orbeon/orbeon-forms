8d9b69061b4c6382170ee28baf37d50a59f9e598
Erik Bruchez <ebruchez@orbeon.com>
2023-08-31 17:09:11 -0700

    For #5952: initial implementation

--- form-runner-web/src/main/scala/org/orbeon/fr/FormRunnerEmbeddingAPI.scala
+++ form-runner-web/src/main/scala/org/orbeon/fr/FormRunnerEmbeddingAPI.scala
@@ -3,7 +3,9 @@ package org.orbeon.fr
 
 import org.orbeon.oxf.fr.ControlOps
 import org.orbeon.xforms
-import org.orbeon.xforms.{$, XFormsId}
+
+import org.orbeon.xforms.{$, DocumentAPI, XFormsId, XFormsXbl}
+
 import org.scalajs.dom
 import org.scalajs.dom.html
 import org.scalajs.dom.html.Element
@@ -49,4 +51,30 @@ class FormRunnerForm(private val form: xforms.Form) extends js.Object {
       // Check the id matches the requested name
       e => (e.id ne null) && (ControlOps.controlNameFromIdOpt(XFormsId.getStaticIdFromId(e.id)) contains controlName)
     } toJSArray
+
+  def setControlValue(controlName: String, controlValue: String): Unit =
+    findControlsByName(controlName).headOption.foreach {
+      case e if e.classList.contains("xbl-fr-dropdown-select1") =>
+        DocumentAPI.setValue(e.querySelector(".xforms-select1").asInstanceOf[html.Element], controlValue, form.elem)
+      case e if XFormsXbl.isJavaScriptLifecycle(e) =>
+        DocumentAPI.setValue(e, controlValue, form.elem)
+      case e if XFormsXbl.isComponent(e) =>
+        // NOP, as the server will reject the value anyway in this case
+      case e =>
+        DocumentAPI.setValue(e, controlValue, form.elem)
+    }
+
+  def activateControl(controlName: String): Unit =
+    findControlsByName(controlName).headOption.foreach { e =>
+
+      val elemToClickOpt =
+        if (e.classList.contains("xforms-trigger"))
+          Option(e.querySelector("button").asInstanceOf[html.Element])
+        else if (e.classList.contains("xforms-input"))
+          Option(e.querySelector("input").asInstanceOf[html.Element])
+        else
+          None
+
+      elemToClickOpt.foreach(_.click())
+    }
 }

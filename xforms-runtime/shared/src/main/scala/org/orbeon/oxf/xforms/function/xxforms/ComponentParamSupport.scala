52a5c1e5d56344425565e9802394aab958bedb11
avernet <avernet@orbeon.com>
2023-10-23 19:48:10 -0700

    Fix #6027: don't trim property value

--- xforms-runtime/shared/src/main/scala/org/orbeon/oxf/xforms/function/xxforms/ComponentParamSupport.scala
+++ xforms-runtime/shared/src/main/scala/org/orbeon/oxf/xforms/function/xxforms/ComponentParamSupport.scala
@@ -103,7 +103,7 @@ object ComponentParamSupport {
     // such a way. So if the value is a blank string (which means the value is actually a blank `xs:string` or maybe
     // `xs:anyURI`), consider the property missing. We could revise this in the future to make a distinction between
     // a blank or empty string and a missing property.
-    propertyNameOpt flatMap property filter (_.getStringValue.nonAllBlank)
+    propertyNameOpt flatMap property filter (_.getStringValue.nonEmpty)
   }
 
   // For example `xbl.fr.number.decimal-separator`

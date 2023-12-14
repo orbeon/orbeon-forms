6f0123bd9c58646a5f4f8d44a07935b677915cc9
Olivier Bruchez <olivier@bruchez.org>
2023-12-14 16:41:54 +0100

    For #4885: simplify HttpServletRequest/Response

--- servlet-support/src/main/scala/org/orbeon/oxf/servlet/HttpServletResponse.scala
+++ servlet-support/src/main/scala/org/orbeon/oxf/servlet/HttpServletResponse.scala
@@ -13,7 +13,6 @@
  */
 package org.orbeon.oxf.servlet
 
-import java.io.PrintWriter
 import java.{util => ju}
 
 object HttpServletResponse {
@@ -27,21 +26,10 @@ trait HttpServletResponse extends ServletResponse {
   def addIntHeader(name: String, value: Int): Unit
   def flushBuffer(): Unit
   def getBufferSize: Int
-  def getCharacterEncoding: String
-  def getContentType: String
-  def getLocale: ju.Locale
-  def getOutputStream: ServletOutputStream
-  def getWriter: PrintWriter
-  def isCommitted: Boolean
-  def reset(): Unit
-  def resetBuffer(): Unit
   def sendError(sc: Int): Unit
   def sendError(sc: Int, msg: String): Unit
   def sendRedirect(location: String): Unit
   def setBufferSize(size: Int): Unit
-  def setCharacterEncoding(charset: String): Unit
-  def setContentLength(len: Int): Unit
-  def setContentType(`type`: String): Unit
   def setDateHeader(name: String, date: Long): Unit
   def setHeader(name: String, value: String): Unit
   def setIntHeader(name: String, value: Int): Unit
@@ -50,7 +38,7 @@ trait HttpServletResponse extends ServletResponse {
   def setStatus(sc: Int, sm: String): Unit
 }
 
-class JavaxHttpServletResponse(httpServletResponse: javax.servlet.http.HttpServletResponse) extends HttpServletResponse {
+class JavaxHttpServletResponse(httpServletResponse: javax.servlet.http.HttpServletResponse) extends JavaxServletResponse(httpServletResponse) with HttpServletResponse {
   override def getNativeServletResponse: javax.servlet.http.HttpServletResponse = httpServletResponse
 
   override def addDateHeader(name: String, date: Long): Unit = httpServletResponse.addDateHeader(name, date)
@@ -58,21 +46,10 @@ class JavaxHttpServletResponse(httpServletResponse: javax.servlet.http.HttpServl
   override def addIntHeader(name: String, value: Int): Unit = httpServletResponse.addIntHeader(name, value)
   override def flushBuffer(): Unit = httpServletResponse.flushBuffer()
   override def getBufferSize: Int = httpServletResponse.getBufferSize
-  override def getCharacterEncoding: String = httpServletResponse.getCharacterEncoding
-  override def getContentType: String = httpServletResponse.getContentType
-  override def getLocale: ju.Locale = httpServletResponse.getLocale
-  override def getOutputStream: ServletOutputStream = ServletOutputStream(httpServletResponse.getOutputStream)
-  override def getWriter: PrintWriter = httpServletResponse.getWriter
-  override def isCommitted: Boolean = httpServletResponse.isCommitted
-  override def reset(): Unit = httpServletResponse.reset()
-  override def resetBuffer(): Unit = httpServletResponse.resetBuffer()
   override def sendError(sc: Int): Unit = httpServletResponse.sendError(sc)
   override def sendError(sc: Int, msg: String): Unit = httpServletResponse.sendError(sc, msg)
   override def sendRedirect(location: String): Unit = httpServletResponse.sendRedirect(location)
   override def setBufferSize(size: Int): Unit = httpServletResponse.setBufferSize(size)
-  override def setCharacterEncoding(charset: String): Unit = httpServletResponse.setCharacterEncoding(charset)
-  override def setContentLength(len: Int): Unit = httpServletResponse.setContentLength(len)
-  override def setContentType(`type`: String): Unit = httpServletResponse.setContentType(`type`)
   override def setDateHeader(name: String, date: Long): Unit = httpServletResponse.setDateHeader(name, date)
   override def setHeader(name: String, value: String): Unit = httpServletResponse.setHeader(name, value)
   override def setIntHeader(name: String, value: Int): Unit = httpServletResponse.setIntHeader(name, value)
@@ -81,7 +58,7 @@ class JavaxHttpServletResponse(httpServletResponse: javax.servlet.http.HttpServl
   override def setStatus(sc: Int, sm: String): Unit = httpServletResponse.setStatus(sc, sm)
 }
 
-class JakartaHttpServletResponse(httpServletResponse: jakarta.servlet.http.HttpServletResponse) extends HttpServletResponse {
+class JakartaHttpServletResponse(httpServletResponse: jakarta.servlet.http.HttpServletResponse) extends JakartaServletResponse(httpServletResponse) with HttpServletResponse {
   override def getNativeServletResponse: jakarta.servlet.http.HttpServletResponse = httpServletResponse
 
   override def addDateHeader(name: String, date: Long): Unit = httpServletResponse.addDateHeader(name, date)
@@ -89,21 +66,10 @@ class JakartaHttpServletResponse(httpServletResponse: jakarta.servlet.http.HttpS
   override def addIntHeader(name: String, value: Int): Unit = httpServletResponse.addIntHeader(name, value)
   override def flushBuffer(): Unit = httpServletResponse.flushBuffer()
   override def getBufferSize: Int = httpServletResponse.getBufferSize
-  override def getCharacterEncoding: String = httpServletResponse.getCharacterEncoding
-  override def getContentType: String = httpServletResponse.getContentType
-  override def getLocale: ju.Locale = httpServletResponse.getLocale
-  override def getOutputStream: ServletOutputStream = ServletOutputStream(httpServletResponse.getOutputStream)
-  override def getWriter: PrintWriter = httpServletResponse.getWriter
-  override def isCommitted: Boolean = httpServletResponse.isCommitted
-  override def reset(): Unit = httpServletResponse.reset()
-  override def resetBuffer(): Unit = httpServletResponse.resetBuffer()
   override def sendError(sc: Int): Unit = httpServletResponse.sendError(sc)
   override def sendError(sc: Int, msg: String): Unit = httpServletResponse.sendError(sc, msg)
-  override def setCharacterEncoding(charset: String): Unit = httpServletResponse.setCharacterEncoding(charset)
   override def sendRedirect(location: String): Unit = httpServletResponse.sendRedirect(location)
   override def setBufferSize(size: Int): Unit = httpServletResponse.setBufferSize(size)
-  override def setContentLength(len: Int): Unit = httpServletResponse.setContentLength(len)
-  override def setContentType(`type`: String): Unit = httpServletResponse.setContentType(`type`)
   override def setDateHeader(name: String, date: Long): Unit = httpServletResponse.setDateHeader(name, date)
   override def setHeader(name: String, value: String): Unit = httpServletResponse.setHeader(name, value)
   override def setIntHeader(name: String, value: Int): Unit = httpServletResponse.setIntHeader(name, value)

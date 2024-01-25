156f6ef496f52b011404a7bcb8227b0dc5eef65b
Erik Bruchez <ebruchez@orbeon.com>
2024-01-25 13:23:51 -0800

    For #6157: update doc

--- common/jvm/src/main/scala/org/orbeon/connection/ConnectionContextProvider.scala
+++ common/jvm/src/main/scala/org/orbeon/connection/ConnectionContextProvider.scala
@@ -6,9 +6,9 @@ import java.net.URI
 
 /**
  * Interface for a provider that can provide a connection context. Orbeon Forms can perform internal
- * service connections that run on different threads than the incoming service thread. In case information related
- * to the incoming service thread needs to be passed downstream, an implementation of this interface can be used.
- * For example, information can be retrieved and set from and to `ThreadLocal`s.
+ * service connections that run on different threads than the incoming service thread. In case information
+ * related to the incoming service thread needs to be passed downstream, an implementation of this interface
+ * can be used. For example, information can be retrieved and set from and to `ThreadLocal`s.
  *
  * @tparam Context The type of the context object, defined by the implementation
  */
@@ -17,8 +17,17 @@ trait ConnectionContextProvider[Context <: AnyRef] {
   /**
    * Get context information. This method is called on the incoming service thread.
    *
-   * The returned object must not keep reference to `Thread` or other mutable data, as the calling thread can be
-   * returned to a thread pool before `pushContext()` is called.
+   * The returned object must not keep references to `Thread` or other mutable data, as the calling thread
+   * can be returned to a thread pool before `pushContext()` is called.
+   *
+   * This can be called multiple times for the same request, for example if multiple outgoing connections
+   * are made.
+   *
+   * In addition, this can be called once and followed by multiple calls to `pushContext()`/`popContext()`,
+   * for example when saving a document with multiple attachments.
+   *
+   * Conversely, this might be called but not followed by any calls to `pushContext()`/`popContext()`, for
+   * example if an error happens before the outgoing connection is made.
    *
    * @param extension immutable extension map, reserved for the future, currently empty and not `null`
    * @return instance of context information or `null`
@@ -45,8 +54,8 @@ trait ConnectionContextProvider[Context <: AnyRef] {
   ): Unit
 
   /**
-   * Pop context information. This method is called on the outgoing connection thread. The purpose is to allow
-   * cleanup of resources allocated by `pushContext()`. For example, a `ThreadLocal` can be removed.
+   * Pop context information. This method is called on the outgoing connection thread. The purpose is to
+   * allow cleanup of resources allocated by `pushContext()`. For example, a `ThreadLocal` can be removed.
    *
    * If `getContext()` returned `null`, this method is not called.
    *

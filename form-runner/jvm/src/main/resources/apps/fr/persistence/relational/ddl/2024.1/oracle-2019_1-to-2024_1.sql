b5443d3a986935400a3b1b33799b5c990457ced6
avernet <avernet@orbeon.com>
2024-12-13 14:02:24 -0800

    For #5689: Changes to Form Definition Indexes
    - Combine indexes `orbeon_form_definition_i1` and
      `orbeon_form_definition_i2` if `orbeon_form_definition_i2` exists
    - Add missing columns to index `orbeon_form_definition_i1`

---
+++ form-runner/jvm/src/main/resources/apps/fr/persistence/relational/ddl/2024.1/oracle-2019_1-to-2024_1.sql
@@ -0,0 +1,3 @@
+DROP INDEX orbeon_form_definition_i1;
+DROP INDEX orbeon_form_definition_i2;
+CREATE INDEX orbeon_form_definition_i1 ON orbeon_form_definition (app, form, form_version, last_modified_time);
---
+++ form-runner/jvm/src/main/resources/apps/fr/persistence/relational/ddl/2024.1/sqlserver-2023_1-to-2024_1.sql
@@ -0,0 +1,3 @@
+DROP INDEX orbeon_form_definition_i1 ON orbeon_form_definition;
+DROP INDEX orbeon_form_definition_i2 ON orbeon_form_definition;
+CREATE INDEX orbeon_form_definition_i1 ON orbeon_form_definition (app, form, form_version, last_modified_time);

ea28f534a8500c3d626d49f2092e7ba9c9bc3c12
avernet <avernet@orbeon.com>
2024-12-17 09:07:36 -0800

    Add missing `NOT NULL` in upgrade script

--- form-runner/jvm/src/main/resources/apps/fr/persistence/relational/ddl/2023.1/sqlserver-2019_1-to-2023_1.sql
+++ form-runner/jvm/src/main/resources/apps/fr/persistence/relational/ddl/2023.1/sqlserver-2019_1-to-2023_1.sql
@@ -23,13 +23,13 @@ ALTER TABLE  orbeon_form_data_attach
 ALTER COLUMN last_modified_time DATETIME2(3);
 
 ALTER TABLE  orbeon_form_data_lease
-ALTER COLUMN expiration         DATETIME2(3);
+ALTER COLUMN expiration         DATETIME2(3) NOT NULL;
 
 ALTER TABLE  orbeon_i_current
-ALTER COLUMN created            DATETIME2(3);
+ALTER COLUMN created            DATETIME2(3) NOT NULL;
 
 ALTER TABLE  orbeon_i_current
-ALTER COLUMN last_modified_time DATETIME2(3);
+ALTER COLUMN last_modified_time DATETIME2(3) NOT NULL;
 
 ALTER TABLE  orbeon_i_control_text
-ALTER COLUMN val                NVARCHAR(MAX);
+ALTER COLUMN val                NVARCHAR(MAX) NOT NULL;

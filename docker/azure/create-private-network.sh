3cf7a59b15b0c547551aaf69462b7e97cbf24e6d
Olivier Bruchez <olivier@bruchez.org>
2025-02-07 16:45:46 +0100

    For #6613: cleanup

--- docker/azure/create-private-network.sh
+++ docker/azure/create-private-network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Create private DNS zone
+# Create a private DNS zone
 az network private-dns zone create \
   --resource-group "$RESOURCE_GROUP" \
   --name "$NETWORK_POSTGRES_DNS_ZONE"
@@ -10,25 +10,25 @@ az network private-dns zone wait \
   --name "$NETWORK_POSTGRES_DNS_ZONE" \
   --created
 
-# Get VNET name
-VNET_NAME=$(az network vnet list \
-            --resource-group "$K8S_NODE_RESOURCE_GROUP" \
-            --query '[0].name' \
-            --output tsv)
+# Retrieve the virtual network name
+NETWORK_VNET_NAME=$(az network vnet list \
+                    --resource-group "$K8S_NODE_RESOURCE_GROUP" \
+                    --query '[0].name' \
+                    --output tsv)
 
-# Get VNET ID
-VNET_ID=$(az network vnet show \
-          --resource-group "$K8S_NODE_RESOURCE_GROUP" \
-          --name "$VNET_NAME" \
-          --query 'id' \
-          --output tsv)
+# Retrieve the virtual network ID
+NETWORK_VNET_ID=$(az network vnet show \
+                  --resource-group "$K8S_NODE_RESOURCE_GROUP" \
+                  --name "$NETWORK_VNET_NAME" \
+                  --query 'id' \
+                  --output tsv)
 
-# Link private DNS zone to AKS VNET
+# Link the private DNS zone to the Kubernetes cluster virtual network
 az network private-dns link vnet create \
   --resource-group "$RESOURCE_GROUP" \
   --zone-name "$NETWORK_POSTGRES_DNS_ZONE" \
   --name 'MyDNSLink' \
-  --virtual-network "$VNET_ID" \
+  --virtual-network "$NETWORK_VNET_ID" \
   --registration-enabled false
 
 az network private-dns link vnet wait \
@@ -37,34 +37,34 @@ az network private-dns link vnet wait \
   --name "MyDNSLink" \
   --created
 
-# Get subnet name
-SUBNET_NAME=$(az network vnet subnet list \
-  --resource-group "$K8S_NODE_RESOURCE_GROUP" \
-  --vnet-name "$VNET_NAME" \
-  --query '[0].name' \
-  --output tsv)
+# Retrieve the subnet name
+NETWORK_SUBNET_NAME=$(az network vnet subnet list \
+                      --resource-group "$K8S_NODE_RESOURCE_GROUP" \
+                      --vnet-name "$NETWORK_VNET_NAME" \
+                      --query '[0].name' \
+                      --output tsv)
 
-# Get subnet ID
-SUBNET_ID=$(az network vnet subnet show \
-  --resource-group "$K8S_NODE_RESOURCE_GROUP" \
-  --vnet-name "$VNET_NAME" \
-  --name "$SUBNET_NAME" \
-  --query 'id' \
-  --output tsv)
+# Retrieve the subnet ID
+NETWORK_SUBNET_ID=$(az network vnet subnet show \
+                    --resource-group "$K8S_NODE_RESOURCE_GROUP" \
+                    --vnet-name "$NETWORK_VNET_NAME" \
+                    --name "$NETWORK_SUBNET_NAME" \
+                    --query 'id' \
+                    --output tsv)
 
-# Get PostgreSQL server ID
-POSTGRES_ID=$(az postgres flexible-server show \
-  --resource-group "$RESOURCE_GROUP" \
-  --name "$DATABASE_SERVER" \
-  --query 'id' \
-  --output tsv)
+# Retrieve the database server ID
+DATABASE_SERVER_ID=$(az postgres flexible-server show \
+                     --resource-group "$RESOURCE_GROUP" \
+                     --name "$DATABASE_SERVER" \
+                     --query 'id' \
+                     --output tsv)
 
-# Create private endpoint
+# Create a private endpoint
 az network private-endpoint create \
   --resource-group "$RESOURCE_GROUP" \
   --name "$NETWORK_PRIVATE_ENDPOINT" \
-  --subnet "$SUBNET_ID" \
-  --private-connection-resource-id "$POSTGRES_ID" \
+  --subnet "$NETWORK_SUBNET_ID" \
+  --private-connection-resource-id "$DATABASE_SERVER_ID" \
   --connection-name "$NETWORK_PRIVATE_ENDPOINT_CONNECTION" \
   --group-id 'postgresqlServer'
 
@@ -73,16 +73,16 @@ az network private-endpoint wait \
   --resource-group "$RESOURCE_GROUP" \
   --created
 
-# Get private IP
-PRIVATE_IP=$(az network private-endpoint show \
-             --resource-group "$RESOURCE_GROUP" \
-             --name "$NETWORK_PRIVATE_ENDPOINT" \
-             --query 'customDnsConfigs[0].ipAddresses[0]' \
-             --output tsv)
+# Retrieve the private IP
+NETWORK_PRIVATE_IP=$(az network private-endpoint show \
+                     --resource-group "$RESOURCE_GROUP" \
+                     --name "$NETWORK_PRIVATE_ENDPOINT" \
+                     --query 'customDnsConfigs[0].ipAddresses[0]' \
+                     --output tsv)
 
-# Create private DNS record (keep DATABASE_SERVER unique across all of Azure for consistency, although it's not mandatory)
+# Create a private DNS record (keep DATABASE_SERVER unique across all of Azure for consistency, although it's not mandatory)
 az network private-dns record-set a add-record \
   --resource-group "$RESOURCE_GROUP" \
   --zone-name "$NETWORK_POSTGRES_DNS_ZONE" \
   --record-set-name "$DATABASE_SERVER" \
-  --ipv4-address "$PRIVATE_IP"
+  --ipv4-address "$NETWORK_PRIVATE_IP"

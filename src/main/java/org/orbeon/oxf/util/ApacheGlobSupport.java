feda4c9f6c446519319db354e5d401b253aaf507
Erik Bruchez <erik@bruchez.org>
2025-02-07 17:34:42 -0800

    Minor: fix copyright

--- src/main/java/org/orbeon/oxf/util/ApacheGlobSupport.java
+++ src/main/java/org/orbeon/oxf/util/ApacheGlobSupport.java
@@ -1,15 +1,17 @@
-/**
- * Copyright (C) 2010 Orbeon, Inc.
+/*
+ * Copyright 2000-2005 The Apache Software Foundation
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the
- * GNU Lesser General Public License as published by the Free Software Foundation; either version
- * 2.1 of the License, or (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * The full text of the license is available at http://www.gnu.org/copyleft/lesser.html
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.orbeon.oxf.util;
 
@@ -17,22 +19,7 @@ package org.orbeon.oxf.util;
  * Utility class to rewrite URLs.
  */
 public class ApacheGlobSupport {
-
-    /*
-     * Copyright 2000-2005 The Apache Software Foundation
-     *
-     * Licensed under the Apache License, Version 2.0 (the "License");
-     * you may not use this file except in compliance with the License.
-     * You may obtain a copy of the License at
-     *
-     *     http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing, software
-     * distributed under the License is distributed on an "AS IS" BASIS,
-     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     * See the License for the specific language governing permissions and
-     * limitations under the License.
-     */
+    
     public static String globToRegexp(char[] pattern) {
         int ch;
         StringBuffer buffer;

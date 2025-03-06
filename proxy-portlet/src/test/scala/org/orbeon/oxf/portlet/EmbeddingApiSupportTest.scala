4fb5f756f502c80b174458ad593a119e3a285f0b
Erik Bruchez <erik@bruchez.org>
2025-03-05 17:46:27 -0800

    For #6833: update regex

--- proxy-portlet/src/test/scala/org/orbeon/oxf/portlet/EmbeddingApiSupportTest.scala
+++ proxy-portlet/src/test/scala/org/orbeon/oxf/portlet/EmbeddingApiSupportTest.scala
@@ -21,37 +21,41 @@ class EmbeddingApiSupportTest extends AnyFunSpec {
 
   import APISupport.*
 
-  val Versioned = "0f55c3fc5685f7ed8e45e4e18f1ab8912ecb227c/"
+  val Versioned1 = "0f55c3fc5685f7ed8e45e4e18f1ab8912ecb227c/"
+  val Versioned2 = "2024.1-SNAPSHOT/"
 
   def allowedPaths(versioned: Boolean) = {
 
-    val optionalPath = if (versioned) Versioned else ""
+    val optionalPath1 = if (versioned) Versioned1 else ""
+    val optionalPath2 = if (versioned) Versioned2 else ""
 
     List(
-      s"/xforms-server/${optionalPath}orbeon-6894eff3d69256b3d8e076b0b689b84d810606af.css",
-      s"/xforms-server/${optionalPath}orbeon-b1e8ca0abe4480447f361045b4620f13e92a0953.js",
+      s"/xforms-server/${optionalPath1}orbeon-6894eff3d69256b3d8e076b0b689b84d810606af.css",
+      s"/xforms-server/${optionalPath1}orbeon-b1e8ca0abe4480447f361045b4620f13e92a0953.js",
 
       s"/xforms-server",
       s"/xforms-server/upload",
       s"/xforms-server/dynamic/d921062a9e04c30f098cce5659d3fcd769d161d7",
 
-      s"/${optionalPath}apps/fr/style/bootstrap/img/glyphicons-halflings.png",
-      s"/${optionalPath}apps/fr/style/images/pixelmixer/bubble_64.png",
-      s"/${optionalPath}apps/fr/style/orbeon-navbar-logo.png",
-      s"/${optionalPath}ops/images/xforms/calendar.png",
-      s"/${optionalPath}ops/yui/assets/skins/sam/sprite.png",
-      s"/${optionalPath}xbl/orbeon/california-plate/images/platetahoe_small.jpg",
+      s"/${optionalPath1}apps/fr/style/bootstrap/img/glyphicons-halflings.png",
+      s"/${optionalPath1}apps/fr/style/images/pixelmixer/bubble_64.png",
+      s"/${optionalPath1}apps/fr/style/orbeon-navbar-logo.png",
+      s"/${optionalPath1}ops/images/xforms/calendar.png",
+      s"/${optionalPath1}ops/yui/assets/skins/sam/sprite.png",
+      s"/${optionalPath1}xbl/orbeon/california-plate/images/platetahoe_small.jpg",
 
-      s"/$Versioned../xbl/orbeon/california-plate/images/platetahoe_small.jpg",
-      s"/$optionalPath./xbl/orbeon/california-plate/images/platetahoe_small.jpg",
-      s"/$Versioned../apps/fr/style/bootstrap/img/glyphicons-halflings.png",
-      s"/$Versioned../${optionalPath}apps/fr/style/bootstrap/img/glyphicons-halflings.png",
+      s"/$Versioned1../xbl/orbeon/california-plate/images/platetahoe_small.jpg",
+      s"/$optionalPath1./xbl/orbeon/california-plate/images/platetahoe_small.jpg",
+      s"/$Versioned1../apps/fr/style/bootstrap/img/glyphicons-halflings.png",
+      s"/$Versioned1../${optionalPath1}apps/fr/style/bootstrap/img/glyphicons-halflings.png",
 
       s"/fr/service/acme/sales/pdf/0f8f0ed8d1b3e24392bffc440feb882cfab53a4c/4a9573727be04cf59662f61f7c1574055c094dbc/abc.pdf",
       s"/fr/service/acme/sales/tiff/0f8f0ed8d1b3e24392bffc440feb882cfab53a4c/4a9573727be04cf59662f61f7c1574055c094dbc/abc.tiff",
 
       s"/fr/orbeon/bookshelf/pdf/fd5dfce7a236758e72910d333bf832f8?fr-language=fr&",
-      s"/fr/orbeon/bookshelf/tiff/fd5dfce7a236758e72910d333bf832f8?fr-language=fr&"
+      s"/fr/orbeon/bookshelf/tiff/fd5dfce7a236758e72910d333bf832f8?fr-language=fr&",
+
+      s"/${optionalPath2}ops/images/xforms/section-closed.png",
     )
   }
 
@@ -60,7 +64,7 @@ class EmbeddingApiSupportTest extends AnyFunSpec {
 
   def rejectedPaths(versioned: Boolean): List[String] = {
 
-    val optionalPath = if (versioned) Versioned else ""
+    val optionalPath = if (versioned) Versioned1 else ""
 
     List(
       s"xforms-server",
@@ -81,7 +85,7 @@ class EmbeddingApiSupportTest extends AnyFunSpec {
       s"/${optionalPath}apps/fr/orbeon/bookshelf/new",
       s"/apps/fr/orbeon/bookshelf/new",
 
-      s"/$Versioned../../xbl/orbeon/california-plate/images/platetahoe_small.jpg",
+      s"/$Versioned1../../xbl/orbeon/california-plate/images/platetahoe_small.jpg",
 
       s"/${optionalPath}xbl/orbeon/california-plate/images/platetahoe_small.xml",
       s"/${optionalPath}xbl/orbeon/california-plate/images/platetahoe_small",

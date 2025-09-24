a10d8348bf5cb3320ec916d5812ab3c9cceca240
Alex Vernet <avernet@orbeon.com>
2025-09-23 23:06:37 -0700

    Use image with Java 11 since we use `--release 11`
    Also switch from Tomcat 8.5 to Tomcat 9, as it is now the oldest version
    we support.

--- orbeon-war/js/src/test/resources/tomcat/setenv.sh
+++ orbeon-war/js/src/test/resources/tomcat/setenv.sh
@@ -1,4 +1,4 @@
-ORBEON_MEMORY_OPTS="-Xms300m -Xmx2g -verbosegc -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCDetails"
+ORBEON_MEMORY_OPTS="-Xms300m -Xmx2g -Xlog:gc*,safepoint"
 ORBEON_DEBUG_OPTS=""
 
 JAVA_OPTS="-ea $ORBEON_MEMORY_OPTS $ORBEON_DEBUG_OPTS -Djava.net.preferIPv4Stack=true"

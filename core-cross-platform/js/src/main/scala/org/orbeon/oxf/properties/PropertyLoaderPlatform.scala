880c5b38e1b72ea0766db81a3f868ecaa3ace8ad
Erik Bruchez <erik@bruchez.org>
2025-12-11 11:43:48 -0800

    Fix #7390 "Test offline: error when initializing form"

--- core-cross-platform/js/src/main/scala/org/orbeon/oxf/properties/PropertyLoaderPlatform.scala
+++ core-cross-platform/js/src/main/scala/org/orbeon/oxf/properties/PropertyLoaderPlatform.scala
@@ -7,17 +7,31 @@ import org.orbeon.oxf.externalcontext.ExternalContext.Request
 // This is unused on the JS platform for now
 trait PropertyLoaderPlatform extends PropertyLoaderTrait {
   def initialize(): Unit = ()
-  def getPropertyStoreImpl(requestOpt: Option[Request]): PropertyStore = _combinedPropertyStore
+  def getPropertyStoreImpl(requestOpt: Option[Request]): PropertyStore =
+    _combinedPropertyStore
 
   private var _serverPropertyStore  : PropertyStore = _
+  private var _clientPropertyStore  : PropertyStore = _
   private var _combinedPropertyStore: PropertyStore = _
 
-  def setServerPropertyStore(propertyStore: PropertyStore): Unit =
+  def setServerPropertyStore(propertyStore: PropertyStore): Unit = {
     _serverPropertyStore = propertyStore
+    updateCombinedPropertyStore()
+  }
 
-  def setClientPropertyStore(propertyStore: PropertyStore): Unit =
-    _combinedPropertyStore =
-      CombinedPropertyStore.combine(NonEmptyList(_serverPropertyStore, List(propertyStore)).map(Option.apply))
-        .getOrElse(_serverPropertyStore)
+  def setClientPropertyStore(propertyStore: PropertyStore): Unit = {
+    _clientPropertyStore = propertyStore
+    updateCombinedPropertyStore()
+  }
 
+  private def updateCombinedPropertyStore(): Unit =
+    (_clientPropertyStore, _serverPropertyStore) match {
+      case (null, null) => _combinedPropertyStore = PropertyStore.empty
+      case (null, ss)   => _combinedPropertyStore = ss
+      case (cs, null)   => _combinedPropertyStore = cs
+      case (cs,  ss)    =>
+        _combinedPropertyStore =
+          CombinedPropertyStore.combine(NonEmptyList(ss, List(cs)).map(Option.apply))
+            .getOrElse(throw new IllegalStateException)
+    }
 }

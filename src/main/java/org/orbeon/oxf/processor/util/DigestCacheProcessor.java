e323c66a8a582a3540d80367a9a604ad7d56c15c
Erik Bruchez <erik@bruchez.org>
2026-01-16 08:14:35 -0800

    Remove unused class

--- src/main/java/org/orbeon/oxf/processor/util/DigestCacheProcessor.java
+++
@@ -1,9 +0,0 @@
-package org.orbeon.oxf.processor.util;
-
-/**
- * The DigestCache processor produces on its data output the document received on the data input,
- * while allowing caching of its output based on a digest of the input document.
- */
-public class DigestCacheProcessor {
-    // TODO!
-}

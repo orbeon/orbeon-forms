fbca49f383b69d7b7bd1e92a18c53680f9cbb66b
Alex Vernet <avernet@orbeon.com>
2026-01-28 13:20:25 -0800

    Improve logging for container used by JSDOM tests
    - Don't log GC safe points (which pollute the logs).
    - Add logging for XForms server.

--- orbeon-war/js/src/test/resources/tomcat/setenv.sh
+++ orbeon-war/js/src/test/resources/tomcat/setenv.sh
@@ -1,4 +1,4 @@
-ORBEON_MEMORY_OPTS="-Xms300m -Xmx2g -Xlog:gc*,safepoint"
+ORBEON_MEMORY_OPTS="-Xms300m -Xmx2g -Xlog:gc*"
 ORBEON_DEBUG_OPTS=""
 
 JAVA_OPTS="-ea $ORBEON_MEMORY_OPTS $ORBEON_DEBUG_OPTS -Djava.net.preferIPv4Stack=true"

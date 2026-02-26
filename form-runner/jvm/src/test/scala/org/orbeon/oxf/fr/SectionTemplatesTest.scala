799a592d21e8152e69692d256a2a9151ff07935f
Erik Bruchez <erik@bruchez.org>
2026-02-25 20:15:25 -0800

    For #7427: tests, add `FormRunnerActionApi`

--- form-runner/jvm/src/test/scala/org/orbeon/oxf/fr/SectionTemplatesTest.scala
+++ form-runner/jvm/src/test/scala/org/orbeon/oxf/fr/SectionTemplatesTest.scala
@@ -1,9 +1,12 @@
 package org.orbeon.oxf.fr
 
 import cats.syntax.option.*
+import org.orbeon.oxf.fr.process.FormRunnerActionApi
 import org.orbeon.oxf.test.{DocumentTestBase, ResourceManagerSupport, XFormsSupport}
-import org.orbeon.oxf.xforms.control.{XFormsComponentControl, XFormsValueControl}
+import org.orbeon.oxf.util.{IndentedLogger, LoggerFactory}
+import org.orbeon.oxf.xforms.control.XFormsValueControl
 import org.orbeon.oxf.xforms.event.EventCollector
+import org.orbeon.oxf.xforms.function.XFormsFunction
 import org.scalatest.funspec.AnyFunSpecLike
 
 
@@ -13,6 +16,8 @@ class SectionTemplatesTest
      with AnyFunSpecLike
      with FormRunnerSupport
      with XFormsSupport {
+
+  private val Logger = LoggerFactory.createLogger(classOf[SectionTemplatesTest])
 
   describe("#7492: form with repeated section templates with dependent values") {
 
@@ -30,16 +35,89 @@ class SectionTemplatesTest
             assert(
               resolveObject[XFormsValueControl](
                 staticOrAbsoluteId = "top-level-current-workflow-stage-control"
-              ).get.getValue(EventCollector.Throw) == workflowStage)
+              ).get.getValue(EventCollector.Throw) == workflowStage
+            )
 
             for (index <- 1 to 2)
               assert(
                 resolveObjectInsideComponent[XFormsValueControl](
                   componentStaticOrAbsoluteId = "my-repeated-workflow-stage-section-content-control",
                   targetStaticOrAbsoluteId    = "current-workflow-stage-control",
-                  targetIndexes      = List(index)
+                  targetIndexes               = List(index)
                 ).get.getValue(EventCollector.Throw) == workflowStage
               )
+          }
+        }
+      }
+    }
+  }
+
+  describe("#7427: Section templates: value dependencies") {
+
+    it("must update control values in all top-level and section template dependencies") {
+
+      val (processorService, Some(xfcd), _) =
+        runFormRunner("issue", "7427", "new", initialize = true)
+
+      withTestExternalContext { _ =>
+        withFormRunnerDocument(processorService, xfcd) {
+
+          implicit val fnCtx: XFormsFunction.Context = XFormsFunction.Context(xfcd)
+          implicit val logger: IndentedLogger        = new IndentedLogger(Logger)
+
+          locally {
+
+            val Sections =
+              List(
+                None,
+                s"issue-7427-s2".some,
+                s"issue-7427-s3".some,
+                None,
+              )
+
+            for ((sectionNameOpt, sectionIndex0) <- Sections.zipWithIndex) {
+
+              val sectionIndex1 = sectionIndex0 + 1
+              val controlName   = s"s$sectionIndex1-in"
+              val valueToSet    = s"v$sectionIndex1"
+
+              document.withOutermostActionHandler {
+                FormRunnerActionApi.controlSetvalue(
+                  controlName    = controlName,
+                  valueExpr      = s"'$valueToSet'",
+                  sectionNameOpt = sectionNameOpt,
+                  atOpt          = None,
+                )
+              }
+
+              val TopLevelControls =
+                List(
+                  s"s1-s$sectionIndex1-out-control",
+                  s"s4-s$sectionIndex1-out-control",
+                )
+
+              for (topLevelControlStaticId <- TopLevelControls)
+                assert(
+                  resolveObject[XFormsValueControl]( // TODO: would be good to have Form Runner API for that
+                    staticOrAbsoluteId = topLevelControlStaticId
+                  ).get.getValue(EventCollector.Throw) == valueToSet
+                )
+
+              val NestedControls =
+                List(
+                  "issue-7427-s2-content-control" -> s"s2-s$sectionIndex1-out-control",
+                  "issue-7427-s3-content-control" -> s"s3-s$sectionIndex1-out-control"
+                )
+
+              for ((sectionTemplateComponentStaticId, controlStaticId) <- NestedControls)
+                assert(
+                  resolveObjectInsideComponent[XFormsValueControl]( // TODO: would be good to have Form Runner API for that
+                    componentStaticOrAbsoluteId = sectionTemplateComponentStaticId,
+                    targetStaticOrAbsoluteId    = controlStaticId,
+                    targetIndexes               = Nil
+                  ).get.getValue(EventCollector.Throw) == valueToSet
+                )
+            }
           }
         }
       }
---
+++ form-runner/shared/src/main/scala/org/orbeon/oxf/fr/process/FormRunnerActionApi.scala
@@ -0,0 +1,85 @@
+package org.orbeon.oxf.fr.process
+
+import org.orbeon.oxf.fr.*
+import org.orbeon.oxf.fr.FormRunnerPersistence.*
+import org.orbeon.oxf.util.*
+import org.orbeon.oxf.util.StringUtils.OrbeonStringOps
+import org.orbeon.oxf.xforms.action.XFormsAPI
+import org.orbeon.oxf.xforms.function.XFormsFunction
+import org.orbeon.saxon.om
+import org.orbeon.scaxon.SimplePath.*
+import org.orbeon.xforms.XFormsId
+import org.orbeon.xml.NamespaceMapping
+
+
+object FormRunnerActionApi {
+
+  def controlSetvalue(
+    controlName     : String,
+    valueExpr       : String,
+    sectionNameOpt  : Option[String],
+    atOpt           : Option[String],
+    namespaceMapping: NamespaceMapping = ProcessInterpreter.StandardNamespaceMapping,
+  )(implicit
+    xfc             : XFormsFunction.Context,
+    logger          : IndentedLogger
+  ): Unit = {
+
+    val updatedValueExpr =
+      FormRunnerRename.replaceVarReferencesWithFunctionCallsForAction(
+      xpathString      = valueExpr,
+      namespaceMapping = namespaceMapping,
+      library          = process.SimpleProcess.xpathFunctionLibrary,
+      avt              = false,
+      libraryNameOpt   = None,     // TODO
+      norewrite        = Set.empty // TODO: `$fr-form-model-vars`
+    )
+
+    val value =
+      process.SimpleProcess.evaluateString(
+        expr            = updatedValueExpr,
+        item            = process.SimpleProcess.xpathContext,
+        mapping         = namespaceMapping,
+        functionContext = xfc
+      )
+
+//    val modelAbsoluteId: String = ???
+    val modelAbsoluteId: String = XFormsId.effectiveIdToAbsoluteId(Names.FormModel)
+
+    val selectedItems =
+      atOpt match {
+        case Some(at) =>
+
+          val allItems =
+            FormRunner.resolveTargetRelativeToActionSourceFromControlsFromBindOpt(
+              xfc.container,
+              xfc.modelOpt,
+              xfc.sourceEffectiveId,
+              modelAbsoluteId,
+              controlName
+            ).toList.flatMap(_.toList) // https://github.com/orbeon/orbeon-forms/issues/6016
+
+          at match {
+            case "start" => allItems.headOption.toList
+            case "end"   => allItems.lastOption.toList
+            case "all"   => allItems
+            case i       => allItems.lift(i.toInt - 1).toList
+          }
+
+        case None =>
+          FormRunner.resolveTargetRelativeToActionSourceOpt(
+            actionSourceAbsoluteId  = modelAbsoluteId,
+            targetControlName       = controlName,
+            followIndexes           = true, // TODO
+            libraryOrSectionNameOpt = sectionNameOpt.map(_.trimAllToEmpty).map(Right.apply),
+          ).toList.flatMap(_.toList)
+      }
+
+    selectedItems
+      .collect { case n: om.NodeInfo => List(n) }
+      .foreach { ref =>
+        XFormsAPI.setvalue(ref, value)
+        XFormsAPI.delete(ref /@ TmpFileAttributeName) // https://github.com/orbeon/orbeon-forms/issues/5768
+      }
+  }
+}

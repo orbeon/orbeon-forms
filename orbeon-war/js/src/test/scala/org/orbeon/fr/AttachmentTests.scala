4bfdf62c88315a94302e1690ee9947be32e823cb
Alex Vernet <avernet@orbeon.com>
2026-03-06 15:10:14 -0800

    For #4294: test error dialog shows when uploading empty file

--- orbeon-war/js/src/test/scala/org/orbeon/fr/AttachmentTests.scala
+++ orbeon-war/js/src/test/scala/org/orbeon/fr/AttachmentTests.scala
@@ -16,6 +16,8 @@ import scala.scalajs.js
 trait AttachmentTests {
   this: FixtureAsyncFunSpecLike & ClientTestSupport =>
 
+  import Private.*
+
   describe("Attachment control tests") {
     it("must show 'Drag file here or select file.' message") { _ =>
       withFormReady(app = "tests", form = "attachment") { case FormRunnerWindow(xformsWindow, _) =>
@@ -31,29 +33,9 @@ trait AttachmentTests {
     it("must upload file successfully") { _ =>
       withFormReady(app = "tests", form = "attachment") { case FormRunnerWindow(xformsWindow, _) =>
         async {
-          val window            = xformsWindow.window
-          val document          = window.document
-          val attachmentControl = document.querySelectorT(".fr-attachment")
-          val fileInputElem     = attachmentControl.querySelectorT("input[type='file']").asInstanceOf[html.Input]
-          val fileContent       = "Hello World"
-          val file              = new dom.File(js.Array(fileContent), "test.txt")
-          val fileList          = js.Array(file).asInstanceOf[dom.FileList]
+          val document = xformsWindow.window.document
+          simulateFileUpload(document, fileContent = "Hello World")
 
-          js.Object.defineProperty(
-            fileInputElem,
-            "files",
-            new js.PropertyDescriptor {
-              value    = js.defined(fileList)
-              writable = false
-            }
-          )
-          assert(fileInputElem.files.length == 1)
-
-          val changeEvent = new dom.Event("change", new dom.EventInit { bubbles = true })
-          fileInputElem.dispatchEvent(changeEvent)
-
-          // Poll until the attachment filename link appears in the DOM
-          // (upload HTTP request + Ajax response processing must complete)
           await(eventually(500.millis, 30.seconds) {
             Future {
               val filenameLink = document.querySelectorOpt(".fr-attachment .fr-attachment-filename a")
@@ -63,6 +45,54 @@ trait AttachmentTests {
           })
         }
       }
+    }
+
+    it("must fail uploading an empty file") { _ =>
+      withFormReady(app = "tests", form = "attachment") { case FormRunnerWindow(xformsWindow, _) =>
+        async {
+          val document = xformsWindow.window.document
+          simulateFileUpload(document, fileContent = "")
+
+          await(eventually(500.millis, 30.seconds) {
+            Future {
+              // The dialog tells us that we can't upload empty files.
+              val dialog = document.querySelectorOpt("dialog[open]")
+              assert(dialog.isDefined, "Error dialog not yet visible")
+              val dialogHead = dialog.get.querySelectorT(".xxforms-dialog-head")
+              assert(dialogHead.textContent == "Unable to complete action")
+              val dialogBody = dialog.get.querySelectorT("div.xforms-output-output")
+              assert(dialogBody.textContent == "The file uploaded is empty.")
+            }
+          })
+
+          // The attachment control is still empty.
+          val attachmentControl = document.querySelectorT(".fr-attachment")
+          val messageText = attachmentControl.textContent
+          assert(messageText.contains("Drag file here or select file."))
+        }
+      }
+    }
+  }
+
+  private object Private {
+    def simulateFileUpload(document: html.Document, fileContent: String): Unit = {
+      val attachmentControl = document.querySelectorT(".fr-attachment")
+      val fileInputElem     = attachmentControl.querySelectorT("input[type='file']").asInstanceOf[html.Input]
+      val file              = new dom.File(js.Array(fileContent), "test.txt")
+      val fileList          = js.Array(file).asInstanceOf[dom.FileList]
+
+      js.Object.defineProperty(
+        fileInputElem,
+        "files",
+        new js.PropertyDescriptor {
+          value    = js.defined(fileList)
+          writable = false
+        }
+      )
+      assert(fileInputElem.files.length == 1)
+
+      val changeEvent = new dom.Event("change", new dom.EventInit { bubbles = true })
+      fileInputElem.dispatchEvent(changeEvent)
     }
   }
 }

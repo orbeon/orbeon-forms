21367873193fb29156aa7837a084dce1256abd3a
Alex Vernet <avernet@orbeon.com>
2026-05-22 15:01:39 -0700

    For #7699: update bound node on `DOMActivate DOMFocusOut`

--- orbeon-war/js/src/test/scala/org/orbeon/fr/DateTimeTests.scala
+++ orbeon-war/js/src/test/scala/org/orbeon/fr/DateTimeTests.scala
@@ -1,6 +1,7 @@
 package org.orbeon.fr
 
 import org.orbeon.web.DomSupport.*
+import org.scalajs.dom
 import org.scalajs.dom.html
 import org.scalatest.funspec.FixtureAsyncFunSpecLike
 
@@ -12,28 +13,64 @@ trait DateTimeTests {
   this: FixtureAsyncFunSpecLike & ClientTestSupport =>
 
   describe("DateTime control") {
+
     it("must not show validation error when tabbing from date input to calendar icon") { _ =>
       withFormReady(app = "orbeon-features", form = "controls", queryStringOpt = Some("fr-wizard-page=date-time-controls")) {
         case FormRunnerWindow(xformsWindow, formRunnerApi) =>
           async {
-
             val form            = formRunnerApi.getForm(js.undefined)
             val datetimeControl = form.findControlsByName("datetime").head
+            val dateUsControl   = form.findControlsByName("date-us").head
             val dateSubControl  = datetimeControl.querySelectorT(".xbl-fr-date")
+            val timeSubControl  = datetimeControl.querySelectorT(".xbl-fr-time")
             val dateInput       = dateSubControl.querySelectorT("input[type='text']").asInstanceOf[html.Input]
+            val timeInput       = timeSubControl.querySelectorT("input[type='text']").asInstanceOf[html.Input]
             val calendarIcon    = dateSubControl.querySelectorT(".add-on")
+            val dateUsInput     = dateUsControl.querySelectorT("input[type='text']").asInstanceOf[html.Input]
 
-            // Clear the date input, then move focus to the calendar icon (simulating Tab).
-            // We don't call dateInput.blur() explicitly; focusing another element automatically
-            // blurs the previous one with the correct relatedTarget on the focusout event.
             dateInput.focus()
             dateInput.value = ""
+            dateInput.dispatchEvent(new dom.Event("change", new dom.EventInit { bubbles = true }))
             calendarIcon.focus()
             await(xformsWindow.ajaxServer.allEventsProcessedP().toFuture)
+            assert(
+              datetimeControl.querySelectorOpt(".xforms-alert.xforms-active").isEmpty,
+              "Clear the date, focus on the calendar icon; no alert shows since we're still within the datetime control"
+            )
 
-            // No validation alert should be visible since focus is still within the datetime control
-            val alert = datetimeControl.querySelectorOpt(".xforms-alert.xforms-active")
-            assert(alert.isEmpty, "Validation error should not show when focus is still within the datetime control")
+            dateUsInput.focus()
+            await(xformsWindow.ajaxServer.allEventsProcessedP().toFuture)
+            assert(
+              datetimeControl.querySelectorOpt(".xforms-alert.xforms-active").isDefined,
+              "Exit the datetime control; the alert shows"
+            )
+
+            dateInput.focus()
+            dateInput.value = "2026-05-22"
+            dateInput.dispatchEvent(new dom.Event("change", new dom.EventInit { bubbles = true }))
+            timeInput.focus()
+            await(xformsWindow.ajaxServer.allEventsProcessedP().toFuture)
+            assert(
+              datetimeControl.querySelectorOpt(".xforms-alert.xforms-active").isDefined,
+              "Enter a valid date; alert still shows as we have exited the datetime"
+            )
+
+            dateUsInput.focus()
+            await(xformsWindow.ajaxServer.allEventsProcessedP().toFuture)
+            assert(
+              datetimeControl.querySelectorOpt(".xforms-alert.xforms-active").isEmpty,
+              "Exit the datetime control; the alert is hidden"
+            )
+
+            dateInput.focus()
+            dateInput.value = ""
+            dateInput.dispatchEvent(new dom.Event("change", new dom.EventInit { bubbles = true }))
+            timeInput.focus()
+            await(xformsWindow.ajaxServer.allEventsProcessedP().toFuture)
+            assert(
+              datetimeControl.querySelectorOpt(".xforms-alert.xforms-active").isEmpty,
+              "Clear the date, focus on the time (same as earlier, but this time the datetime is visited); no alert"
+            )
           }
       }
     }

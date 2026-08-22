473ee6f3b35feeba8dad568c1d547d869910cc9d
Alex Vernet <avernet@orbeon.com>
2026-08-21 17:32:15 -0700

    For #7833: test checking we exclude the correct date

--- orbeon-war/js/src/test/scala/org/orbeon/fr/DateTimeTests.scala
+++ orbeon-war/js/src/test/scala/org/orbeon/fr/DateTimeTests.scala
@@ -74,5 +74,40 @@ trait DateTimeTests {
           }
       }
     }
+
+    it("must disable the correct excluded dates in the datepicker in positive timezones") { _ =>
+      withTimezone("Europe/Helsinki") {
+        withFormReady(app = "orbeon-features", form = "controls", queryStringOpt = Some("fr-wizard-page=date-time-controls")) {
+          case FormRunnerWindow(xformsWindow, formRunnerApi) =>
+            async {
+              val form          = formRunnerApi.getForm(js.undefined)
+              val dateEuControl = form.findControlsByName("date-eu").head
+              val calendarIcon  = dateEuControl.querySelectorT(".add-on")
+
+              calendarIcon.dispatchEvent(new dom.MouseEvent("click", new dom.MouseEventInit {
+                bubbles    = true
+                cancelable = true
+              }))
+
+              val day19Opt = xformsWindow.window.document.documentElement.querySelectorAllT(".datepicker .day").find(el => el.textContent.trim == "19" && !el.hasClass("old"))
+              val day20Opt = xformsWindow.window.document.documentElement.querySelectorAllT(".datepicker .day").find(el => el.textContent.trim == "20" && !el.hasClass("old"))
+
+              assert(day19Opt.isDefined && !day19Opt.get.hasClass("disabled"), "Day 19 must not be disabled")
+              assert(day20Opt.isDefined && day20Opt.get.hasClass("disabled"), "Day 20 must be disabled")
+            }
+        }
+      }
+    }
+  }
+
+  private def withTimezone[T](tz: String)(f: => scala.concurrent.Future[T]): scala.concurrent.Future[T] = {
+    val originalTz = js.Dynamic.global.process.env.TZ
+    js.Dynamic.global.process.env.TZ = tz
+    f.andThen { case _ =>
+      if (js.isUndefined(originalTz))
+        js.Dynamic.global.process.env.TZ = js.undefined
+      else
+        js.Dynamic.global.process.env.TZ = originalTz
+    }
   }
 }
